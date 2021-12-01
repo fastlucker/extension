7d64318368121370475478c5d4652b5ecc22f9cd
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2021-12-01 17:08:06 +0200

    Configured: automatic imports sorter on save

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
+
 import Placeholder from '@components/Placeholder'
 
 const styles = StyleSheet.create({
