2eba62be0de590bcac7107c415fc26a0c5707114
Kaloyan Kosev <superkalo@devlabs.bg>
2021-12-07 19:01:17 +0200

    Change: android related package name

--- android/app/src/main/java/com/ambiremobilewallet/MainApplication.java
+++ android/app/src/main/java/com/ambiremobilewallet/MainApplication.java
@@ -1,4 +1,4 @@
-package com.ambiremobilewallet;
+package com.ambire.wallet;
 
 import android.app.Application;
 import android.content.Context;
@@ -85,7 +85,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.ambiremobilewallet.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.ambire.wallet.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
