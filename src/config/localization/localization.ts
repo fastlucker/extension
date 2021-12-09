1c5cbe57bf07bd1c50c21952205aedf86d4363fc
Kaloyan Kosev <superkalo@devlabs.bg>
2021-12-09 16:59:56 +0200

    Add: localizations scanner, that suppports typescript + localization JSONs

--- src/config/localization/localization.ts
+++ src/config/localization/localization.ts
@@ -1,10 +1,13 @@
 import * as Localization from 'expo-localization'
 import i18n from 'i18n-js'
 
+import en from '@config/localization/languages/en.json'
+import bg from '@config/localization/languages/bg.json'
+
 // Set the key-value pairs for the different languages you want to support.
 i18n.translations = {
-  en: { welcome: 'Hello' },
-  bg: { welcome: 'Добре дошли' },
+  en,
+  bg,
 }
 
 export const setLocale = () => (i18n.locale = Localization.locale)
