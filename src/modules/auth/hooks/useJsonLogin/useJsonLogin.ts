78058ac2b17c6129b128d92fc3ac512dea0f73a7
Kaloyan Kosev <superkalo@devlabs.bg>
2021-12-13 13:13:23 +0200

    Add: basic error handling

--- src/modules/auth/hooks/useJsonLogin/useJsonLogin.ts
+++ src/modules/auth/hooks/useJsonLogin/useJsonLogin.ts
@@ -1,26 +1,32 @@
 import * as DocumentPicker from 'expo-document-picker'
 import * as FileSystem from 'expo-file-system'
+import { useState } from 'react'
 
+import { useTranslation } from '@config/localization'
 import useAccounts from '@modules/common/hooks/useAccounts'
 
 export default function useJsonLogin() {
+  const { t } = useTranslation()
+  const [error, setError] = useState<null | string>(null)
   const { onAddAccount } = useAccounts()
 
   const handleLogin = async () => {
-    // TODO: add iCloud entitlement
-    const documentResult = await DocumentPicker.getDocumentAsync()
+    setError('')
 
-    if (documentResult.type !== 'success') {
-      // TODO: Notify user JSON file not selected.
+    // TODO: add iCloud entitlement
+    const document = await DocumentPicker.getDocumentAsync()
+
+    if (document.type !== 'success') {
+      setError(t('JSON file was not selected or something went wrong selecting it.'))
       return
     }
 
     let fileContent
     try {
-      fileContent = await FileSystem.readAsStringAsync(documentResult.uri)
+      fileContent = await FileSystem.readAsStringAsync(document.uri)
       fileContent = JSON.parse(fileContent)
     } catch (exception) {
-      // TODO: Notify user for the exception
+      setError('Something went wrong with pulling the information from the JSON file selected.')
       return
     }
 
@@ -31,5 +37,5 @@ export default function useJsonLogin() {
     onAddAccount(fileContent, { select: true })
   }
 
-  return { handleLogin }
+  return { handleLogin, error }
 }
--- src/modules/auth/screens/JsonLoginScreen/JsonLoginScreen.tsx
+++ src/modules/auth/screens/JsonLoginScreen/JsonLoginScreen.tsx
@@ -4,15 +4,17 @@ import { View } from 'react-native'
 import { useTranslation } from '@config/localization'
 import useJsonLogin from '@modules/auth/hooks/useJsonLogin'
 import Button from '@modules/common/components/Button'
+import P from '@modules/common/components/P'
 
 import styles from './styles'
 
 const JsonLoginScreen = () => {
   const { t } = useTranslation()
-  const { handleLogin } = useJsonLogin()
+  const { handleLogin, error } = useJsonLogin()
 
   return (
     <View style={styles.container}>
+      {<P>{error}</P>}
       <Button text={t('Select file')} onPress={handleLogin} />
     </View>
   )
