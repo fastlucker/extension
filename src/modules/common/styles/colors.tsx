a81b6fb0887630cd07fc250449976c93ff9d4434
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-05 12:41:03 +0200

    Add: text utils and colors + balances text style

---
+++ src/modules/common/styles/colors.tsx
@@ -0,0 +1,19 @@
+export const colors = {
+  backgroundColor: '#282b33',
+  panelBackgroundColor: '#202229',
+  inputBackgroundColor: '#1c1e24',
+  inputBackgroundColorDarker: '#191b20',
+
+  primaryAccentColor: '#80ffdb',
+  secondaryAccentColor: '#AA6AFF',
+  tertiaryAccentColor: '#C3D4F6',
+  dangerColor: '#da5151',
+
+  ambireDarkBlue: '#6000FF',
+  ambirePurple: '#AF50FF',
+
+  textColor: '#FFF'
+  // TODO:
+  // $textContainerColor: mix($text-color, $panel-background-color, 60%);
+  // $box-shadow: 0px 5px 10px 0 rgba(0, 0, 0, 0.25);
+}
--- src/modules/dashboard/components/Balances/Balances.tsx
+++ src/modules/dashboard/components/Balances/Balances.tsx
@@ -6,6 +6,7 @@ import Text from '@modules/common/components/Text'
 import useAccounts from '@modules/common/hooks/useAccounts'
 import useNetwork from '@modules/common/hooks/useNetwork'
 import usePortfolio from '@modules/common/hooks/usePortfolio'
+import textStyles from '@modules/common/styles/utils/text'
 
 const Balances = () => {
   const { t } = useTranslation()
@@ -18,8 +19,10 @@ const Balances = () => {
   return (
     <View>
       <Text>{t('Balance')}</Text>
-      <Text>$ {balance.total?.truncated}</Text>
-      <Text>.{balance.total?.decimals}</Text>
+      <Text>
+        <Text style={textStyles.highlightPrimary}>$</Text> {balance.total?.truncated}{' '}
+        <Text style={textStyles.highlightPrimary}>.{balance.total?.decimals}</Text>
+      </Text>
     </View>
   )
 }
