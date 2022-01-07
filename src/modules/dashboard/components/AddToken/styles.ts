970d31cb9d1118e98b92f268ed10fc4aa6e31ba6
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-07 18:08:47 +0200

    Add: on entering address find token, display it, display balanace, icon and show error msg

--- src/modules/dashboard/components/AddToken/styles.ts
+++ src/modules/dashboard/components/AddToken/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet, TextStyle, ViewStyle } from 'react-native'
+import { ImageStyle, StyleSheet, TextStyle, ViewStyle } from 'react-native'
 
 import colors from '@modules/common/styles/colors'
 import spacings from '@modules/common/styles/spacings'
@@ -7,6 +7,7 @@ import textStyles from '@modules/common/styles/utils/text'
 interface Style {
   btnContainer: ViewStyle
   btn: TextStyle
+  img: ImageStyle
 }
 
 const styles = StyleSheet.create<Style>({
@@ -19,6 +20,10 @@ const styles = StyleSheet.create<Style>({
     fontSize: 14,
     textTransform: 'uppercase',
     ...textStyles.bold
+  },
+  img: {
+    width: 22,
+    height: 22
   }
 })
 
