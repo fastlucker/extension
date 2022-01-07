80f235bb888ea7be99d03cdb9944fcf00ce81c8a
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-07 13:01:46 +0200

    Add: make main wrapper scrollable

--- src/modules/common/components/Wrapper/Wrapper.tsx
+++ src/modules/common/components/Wrapper/Wrapper.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { TextProps, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
 import styles from './styles'
 
 const Wrapper: React.FC<TextProps> = ({ style = {}, children }) => (
-  <View style={[styles.wrapper, style]}>{children}</View>
+  <ScrollView style={[styles.wrapper, style]} contentContainerStyle={styles.contentContainerStyle}>
+    {children}
+  </ScrollView>
 )
 
 export default Wrapper
--- src/modules/common/components/Wrapper/styles.ts
+++ src/modules/common/components/Wrapper/styles.ts
@@ -4,13 +4,16 @@ import colors from '@modules/common/styles/colors'
 
 interface Style {
   wrapper: ViewStyle
+  contentContainerStyle: ViewStyle
 }
 
 const styles = StyleSheet.create<Style>({
   wrapper: {
     flex: 1,
     backgroundColor: colors.backgroundColor,
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
+  },
+  contentContainerStyle: {
     paddingVertical: 15
   }
 })
