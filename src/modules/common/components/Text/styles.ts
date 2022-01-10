7be9c9c3f08c933b5754a250c5d2c83d7b6c1f58
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-10 15:57:05 +0200

    Add: text types

--- src/modules/common/components/Text/styles.ts
+++ src/modules/common/components/Text/styles.ts
@@ -4,13 +4,20 @@ import colors from '@modules/common/styles/colors'
 
 interface Style {
   text: TextStyle
+  textRegular: TextStyle
+  textDanger: TextStyle
   underline: TextStyle
 }
 
 const styles = StyleSheet.create<Style>({
   text: {
-    color: colors.textColor,
     fontSize: 16
+  },
+  textRegular: {
+    color: colors.textColor
+  },
+  textDanger: {
+    color: colors.dangerColor
   },
   underline: {
     textDecorationLine: 'underline'
