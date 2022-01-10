c48729a05d734530d3ec0aabbc1317eddef683f1
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-10 17:06:39 +0200

    Change: JSON login layout

--- src/modules/auth/screens/JsonLoginScreen/JsonLoginScreen.tsx
+++ src/modules/auth/screens/JsonLoginScreen/JsonLoginScreen.tsx
@@ -1,28 +1,26 @@
 import React from 'react'
-import { View } from 'react-native'
 
 import { useTranslation } from '@config/localization'
 import useJsonLogin from '@modules/auth/hooks/useJsonLogin'
 import Button from '@modules/common/components/Button'
-import Heading from '@modules/common/components/Heading'
 import P from '@modules/common/components/P'
-
-import styles from './styles'
+import Title from '@modules/common/components/Title'
+import Wrapper from '@modules/common/components/Wrapper'
 
 const JsonLoginScreen = () => {
   const { t } = useTranslation()
   const { handleLogin, error, inProgress } = useJsonLogin()
 
   return (
-    <View style={styles.container}>
-      <Heading>{t('Import from JSON')}</Heading>
+    <Wrapper>
+      <Title>{t('Import from JSON')}</Title>
       <Button
         disabled={inProgress}
         text={inProgress ? t('Importing...') : t('Select file')}
         onPress={handleLogin}
       />
       {!!error && <P>{error}</P>}
-    </View>
+    </Wrapper>
   )
 }
 
