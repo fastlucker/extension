d634846a62d71a8609f1750c77a7a093f4adc78f
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-19 18:49:42 +0200

    Add: display table with staking rewards

--- src/modules/dashboard/components/Rewards/Rewards.tsx
+++ src/modules/dashboard/components/Rewards/Rewards.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { ActivityIndicator, Linking } from 'react-native'
+import { ActivityIndicator, Linking, View } from 'react-native'
 
 import BottomSheet from '@modules/common/components/BottomSheet'
 import useBottomSheet from '@modules/common/components/BottomSheet/hooks/useBottomSheet'
 import Button, { BUTTON_SIZES, BUTTON_TYPES } from '@modules/common/components/Button'
 import P from '@modules/common/components/P'
+import { Row } from '@modules/common/components/Table'
+import Text from '@modules/common/components/Text'
 import Title from '@modules/common/components/Title'
 import useAccounts from '@modules/common/hooks/useAccounts'
 import useRelayerData from '@modules/common/hooks/useRelayerData'
 import colors from '@modules/common/styles/colors'
+import spacings from '@modules/common/styles/spacings'
+import textStyles from '@modules/common/styles/utils/text'
 
 const BLOG_POST_URL = 'https://blog.ambire.com/announcing-the-wallet-token-a137aeda9747'
 
@@ -56,6 +60,24 @@ const Rewards = () => {
       />
       <BottomSheet sheetRef={sheetRef} cancelText={t('Close')}>
         <Title>{t('Wallet')}</Title>
+
+        <Row index={0}>
+          <View style={spacings.prTy}>
+            <Text>{t('Early users Incentive')}</Text>
+          </View>
+          <View style={spacings.plTy}>
+            <Text>{rewards['balance-rewards']}</Text>
+          </View>
+        </Row>
+        <Row index={1}>
+          <View style={spacings.prTy}>
+            <Text>{t('ADX Staking Bonus')}</Text>
+          </View>
+          <View style={spacings.plTy}>
+            <Text>{rewards['adx-rewards']}</Text>
+          </View>
+        </Row>
+
         {!!rewards?.multipliers &&
           rewards?.multipliers.map(({ mul, name }) => (
             <Button
