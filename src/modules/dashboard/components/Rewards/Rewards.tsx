e9a4b1151b722b50a73a2478a53881895c26571e
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-19 18:07:15 +0200

    Add: display rewards + post url

--- src/modules/dashboard/components/Rewards/Rewards.tsx
+++ src/modules/dashboard/components/Rewards/Rewards.tsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { ActivityIndicator } from 'react-native'
+import { ActivityIndicator, Linking } from 'react-native'
 
 import BottomSheet from '@modules/common/components/BottomSheet'
 import useBottomSheet from '@modules/common/components/BottomSheet/hooks/useBottomSheet'
 import Button, { BUTTON_SIZES, BUTTON_TYPES } from '@modules/common/components/Button'
+import P from '@modules/common/components/P'
 import Title from '@modules/common/components/Title'
 import useAccounts from '@modules/common/hooks/useAccounts'
 import useRelayerData from '@modules/common/hooks/useRelayerData'
 import colors from '@modules/common/styles/colors'
 
+const BLOG_POST_URL = 'https://blog.ambire.com/announcing-the-wallet-token-a137aeda9747'
+
 const Rewards = () => {
   const { t } = useTranslation()
-  const { sheetRef, openBottomSheet } = useBottomSheet()
+  const { sheetRef, openBottomSheet, closeBottomSheet } = useBottomSheet()
   const { account } = useAccounts()
   const { isLoading, data, errMsg } = useRelayerData()
+  const [rewards, setRewards] = useState({})
   const [rewardsTotal, setRewardsTotal] = useState<number>(0)
 
   useEffect(() => {
@@ -30,9 +34,12 @@ const Rewards = () => {
     rewardsDetails.multipliers = multipliers
 
     setRewardsTotal(total)
+    setRewards(rewardsDetails)
   }, [data, errMsg, account])
 
   const walletTokensAmount = rewardsTotal.toFixed(3)
+
+  const handleReadMore = () => Linking.openURL(BLOG_POST_URL).finally(closeBottomSheet)
 
   if (isLoading) {
     return <ActivityIndicator />
@@ -48,8 +55,24 @@ const Rewards = () => {
         style={{ width: 'auto' }}
         size={BUTTON_SIZES.SMALL}
       />
-      <BottomSheet sheetRef={sheetRef}>
+      <BottomSheet sheetRef={sheetRef} cancelText={t('Close')}>
         <Title>{t('Wallet')}</Title>
+        {!!rewards?.multipliers &&
+          rewards?.multipliers.map(({ mul, name }) => (
+            <Button
+              accentColor={colors.primaryAccentColor}
+              type={BUTTON_TYPES.SECONDARY}
+              text={t('{{mul}}x {{name}} multiplier', { mul, name })}
+              disabled
+              key={name}
+            />
+          ))}
+        <P>
+          {t(
+            'You are receiving $WALLETS for holding funds on your Ambire wallet as an early user. Have in mind that $WALLET has not launched yet.'
+          )}
+        </P>
+        <Button onPress={handleReadMore} text={t('Read more')} />
       </BottomSheet>
     </>
   )
