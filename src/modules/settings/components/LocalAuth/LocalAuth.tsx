e340c2cf082567b7248e7ff92a82af9a818e02f0
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-20 18:40:05 +0200

    Add: local auth basic set-up

--- src/modules/settings/components/LocalAuth/LocalAuth.tsx
+++ src/modules/settings/components/LocalAuth/LocalAuth.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import * as LocalAuthentication from 'expo-local-authentication'
+import React, { useState } from 'react'
 
 import { useTranslation } from '@config/localization'
 import Button from '@modules/common/components/Button'
+import P from '@modules/common/components/P'
+import Text from '@modules/common/components/Text'
 import Title from '@modules/common/components/Title'
 
 const LocalAuth = () => {
   const { t } = useTranslation()
+  const [isEnabled, setIsEnabled] = useState(false)
+
+  const handleOnActivate = async () => {
+    const { success } = await LocalAuthentication.authenticateAsync()
+
+    setIsEnabled(success)
+  }
 
   return (
     <>
       <Title>{t('Local authentication')}</Title>
-      <Button text={t('Enable')} />
+      <P>{t('Use your fingerprint for quick access.')}</P>
+      <Button onPress={handleOnActivate} text={t('Enable')} />
+      <Text>{isEnabled ? 'Enabled!' : 'Not enabled yet!'}</Text>
     </>
   )
 }
