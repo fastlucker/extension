96640c1fb417a4628ee4043de942c51a1c171ad6
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-20 11:51:02 +0200

    Add: extend Row to accept all ViewProps supported

--- src/modules/common/components/Table/Row.tsx
+++ src/modules/common/components/Table/Row.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ViewProps } from 'react-native'
 
 import colors from '@modules/common/styles/colors'
 import spacings from '@modules/common/styles/spacings'
 import flexboxStyles from '@modules/common/styles/utils/flexbox'
 
-interface Props {
+interface Props extends ViewProps {
   index?: number
 }
 
-export const Row: React.FC<Props> = ({ index = 0, children }) => {
+export const Row: React.FC<Props> = ({ index = 0, style, children, ...rest }) => {
   return (
     <View
       style={[
         flexboxStyles.directionRow,
         spacings.pvSm,
         spacings.ph,
-        { backgroundColor: index % 2 ? colors.rowEvenColor : colors.rowOddColor }
+        { backgroundColor: index % 2 ? colors.rowEvenColor : colors.rowOddColor },
+        !!style && style
       ]}
+      {...rest}
     >
       {children}
     </View>
