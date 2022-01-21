cbfe00de34a4ffa7a6cc4c1e3900419356a65858
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-21 16:40:20 +0200

    Add: Code input reusable component

---
+++ src/modules/common/components/CodeInput/CodeInput.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import RnCodeInput from 'react-native-confirmation-code-input'
+
+import colors from '@modules/common/styles/colors'
+
+const CodeInput: React.FC<RnCodeInput['props']> = (props) => {
+  return (
+    <RnCodeInput
+      secureTextEntry
+      className="border-b"
+      space={10}
+      codeLength={6}
+      activeColor={colors.primaryAccentColor}
+      size={30}
+      inputPosition="left"
+      {...props}
+    />
+  )
+}
+
+export default CodeInput
