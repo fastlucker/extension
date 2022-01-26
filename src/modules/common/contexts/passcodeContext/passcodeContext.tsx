135d6677c703dd212f7b44dff8dbefe1830095ea
Kaloyan Kosev <superkalo@devlabs.bg>
2022-01-26 15:21:37 +0200

    Add: by removing passcode, remove the local auth and saved pass too

--- src/modules/common/contexts/passcodeContext/passcodeContext.tsx
+++ src/modules/common/contexts/passcodeContext/passcodeContext.tsx
@@ -4,6 +4,7 @@ import React, { createContext, useEffect, useMemo, useState } from 'react'
 import { Platform, Vibration } from 'react-native'
 
 import i18n from '@config/localization/localization'
+import useAccountsPasswords from '@modules/common/hooks/useAccountsPasswords'
 import { SECURE_STORE_KEY_PASSCODE } from '@modules/settings/constants'
 
 export enum PASSCODE_STATES {
@@ -122,6 +123,7 @@ const PasscodeContext = createContext<PasscodeContextData>({
 })
 
 const PasscodeProvider: React.FC = ({ children }) => {
+  const { selectedAccHasPassword, removeSelectedAccPassword } = useAccountsPasswords()
   const [state, setState] = useState<PASSCODE_STATES>(PASSCODE_STATES.NO_PASSCODE)
   const [deviceSecurityLevel, setDeviceSecurityLevel] = useState<DEVICE_SECURITY_LEVEL>(
     DEVICE_SECURITY_LEVEL.NONE
@@ -174,6 +176,24 @@ const PasscodeProvider: React.FC = ({ children }) => {
     })()
   }, [])
 
+  const addLocalAuth = async () => {
+    const { success } = await LocalAuthentication.authenticateAsync()
+
+    if (success) {
+      setState(PASSCODE_STATES.PASSCODE_AND_LOCAL_AUTH)
+    }
+  }
+  const removeLocalAuth = async () => {
+    await LocalAuthentication.cancelAuthenticate()
+
+    setState(PASSCODE_STATES.PASSCODE_ONLY)
+  }
+  const isValidLocalAuth = async () => {
+    const { success } = await LocalAuthentication.authenticateAsync()
+
+    return success
+  }
+
   const addPasscode = async (code: string) => {
     await SecureStore.setItemAsync(SECURE_STORE_KEY_PASSCODE, code)
     setPasscode(code)
@@ -186,9 +206,21 @@ const PasscodeProvider: React.FC = ({ children }) => {
       )
   }
   const removePasscode = async () => {
+    // First, remove the local auth (if set), because without passcode
+    // using local auth is not allowed.
+    if (state === PASSCODE_STATES.PASSCODE_AND_LOCAL_AUTH) {
+      await removeLocalAuth()
+    }
+    // And remove the account password stored too, because without passcode,
+    // this is not allowed neither.
+    if (selectedAccHasPassword) {
+      await removeSelectedAccPassword()
+    }
+
     await SecureStore.deleteItemAsync(SECURE_STORE_KEY_PASSCODE)
     setPasscode(null)
-    setState(PASSCODE_STATES.NO_PASSCODE)
+
+    return setState(PASSCODE_STATES.NO_PASSCODE)
   }
   const isValidPasscode = (code: string) => {
     const isValid = code === passcode
@@ -196,22 +228,6 @@ const PasscodeProvider: React.FC = ({ children }) => {
     if (!isValid) Vibration.vibrate()
 
     return isValid
-  }
-
-  const addLocalAuth = async () => {
-    const { success } = await LocalAuthentication.authenticateAsync()
-
-    if (success) {
-      setState(PASSCODE_STATES.PASSCODE_AND_LOCAL_AUTH)
-    }
-  }
-  const removeLocalAuth = async () => {
-    setState(PASSCODE_STATES.PASSCODE_ONLY)
-  }
-  const isValidLocalAuth = async () => {
-    const { success } = await LocalAuthentication.authenticateAsync()
-
-    return success
   }
 
   return (
