c835db686a79973fce623e9b9a30dbcc0e8e6839
superKalo <superkalo@devlabs.bg>
2022-01-31 14:15:21 +0200

    Refactor: move device specific checks

--- src/modules/common/contexts/passcodeContext/passcodeContext.tsx
+++ src/modules/common/contexts/passcodeContext/passcodeContext.tsx
@@ -8,77 +8,13 @@ import i18n from '@config/localization/localization'
 import BottomSheet from '@modules/common/components/BottomSheet'
 import useBottomSheet from '@modules/common/components/BottomSheet/hooks/useBottomSheet'
 import PasscodeAuth from '@modules/common/components/PasscodeAuth'
-import Text from '@modules/common/components/Text'
 import useAccountsPasswords from '@modules/common/hooks/useAccountsPasswords'
 import useToast from '@modules/common/hooks/useToast'
+import { getDeviceSupportedAuthTypesLabel } from '@modules/common/services/device'
 import { SECURE_STORE_KEY_PASSCODE } from '@modules/settings/constants'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { DEVICE_SECURITY_LEVEL, DEVICE_SUPPORTED_AUTH_TYPES, PASSCODE_STATES } from './constants'
-
-const getDeviceSupportedAuthTypesLabel = (types: DEVICE_SUPPORTED_AUTH_TYPES[]) => {
-  if (Platform.OS === 'ios') {
-    if (
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FACIAL_RECOGNITION) &&
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FINGERPRINT)
-    ) {
-      return i18n.t('Face ID or Touch ID')
-    }
-
-    if (types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FACIAL_RECOGNITION)) {
-      return i18n.t('Face ID')
-    }
-
-    if (types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FINGERPRINT)) {
-      return i18n.t('Touch ID')
-    }
-  }
-
-  if (Platform.OS === 'android') {
-    if (
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FACIAL_RECOGNITION) &&
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.IRIS) &&
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FINGERPRINT)
-    ) {
-      return i18n.t('facial recognition, iris recognition or fingerprint')
-    }
-
-    if (
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.IRIS) &&
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FINGERPRINT)
-    ) {
-      return i18n.t('iris recognition or fingerprint')
-    }
-
-    if (
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FACIAL_RECOGNITION) &&
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FINGERPRINT)
-    ) {
-      return i18n.t('facial recognition or fingerprint')
-    }
-
-    if (
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FACIAL_RECOGNITION) &&
-      types.includes(DEVICE_SUPPORTED_AUTH_TYPES.IRIS)
-    ) {
-      return i18n.t('facial recognition or iris recognition')
-    }
-
-    if (types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FACIAL_RECOGNITION)) {
-      return i18n.t('facial recognition')
-    }
-
-    if (types.includes(DEVICE_SUPPORTED_AUTH_TYPES.IRIS)) {
-      return i18n.t('iris recognition')
-    }
-
-    if (types.includes(DEVICE_SUPPORTED_AUTH_TYPES.FINGERPRINT)) {
-      return i18n.t('fingerprint')
-    }
-  }
-
-  return ''
-}
 
 type PasscodeContextData = {
   state: PASSCODE_STATES
