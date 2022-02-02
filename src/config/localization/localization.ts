481893451f646ca739dac9ea75d756f3644a806e
superKalo <superkalo@devlabs.bg>
2022-02-02 11:53:30 +0200

    Fix: fallback to compatibilityJSON v3 due to Android ongoing issue

--- src/config/localization/localization.ts
+++ src/config/localization/localization.ts
@@ -1,13 +1,21 @@
-import * as Localization from 'expo-localization'
+// import * as Localization from 'expo-localization'
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-import en from '@config/localization/translations/en.json'
-import bg from '@config/localization/translations/bg.json'
 import { Locale } from '@config/localization/constants'
+import bg from '@config/localization/translations/bg.json'
+import en from '@config/localization/translations/en.json'
 
 // Set the key-value pairs for the different languages you want to support.
 i18n.use(initReactI18next).init({
+  // The v4 is causing an issue on Android, because of a change in the lib
+  // regarding suffixing plurals. Figure out when we roll out translations.
+  // This change streamlines the suffix with the one used in the Intl API.
+  // We may need to polyfill the Intl.PluralRules API, in case it is not
+  // available it will fallback to the i18next JSON format v3 plural handling.
+  // {@link https://stackoverflow.com/a/70521614/1333836}
+  // {@link https://www.i18next.com/misc/migration-guide#json-format-v4-pluralization}
+  compatibilityJSON: 'v3',
   // In future, when a second language gets activated, uncomment this
   // in order to set the initial language to device language.
   // lng: Localization.locale
