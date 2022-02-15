24a82caab7d7299119b50654f0ecdbd60309e6a0
superKalo <superkalo@devlabs.bg>
2022-02-15 14:47:55 +0200

    Add: wrap methods in callback, so they do not rerender

--- src/modules/common/components/BottomSheet/hooks/useBottomSheet.tsx
+++ src/modules/common/components/BottomSheet/hooks/useBottomSheet.tsx
@@ -1,14 +1,20 @@
-import { useRef } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { Keyboard } from 'react-native'
 
 export default function useBottomSheet() {
   const sheetRef = useRef<any>(0)
-  const openBottomSheet = () => sheetRef.current?.snapTo(1)
-  const closeBottomSheet = (ref: any = sheetRef) => {
-    ref.current?.snapTo(0)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const openBottomSheet = useCallback(() => {
+    setIsOpen(true)
+    setTimeout(() => sheetRef.current?.snapTo(1), 100)
+  }, [])
+  const closeBottomSheet = useCallback(() => {
+    setIsOpen(false)
+    sheetRef.current?.snapTo(0)
 
     Keyboard.dismiss()
-  }
+  }, [])
 
-  return { sheetRef, openBottomSheet, closeBottomSheet }
+  return { sheetRef, openBottomSheet, closeBottomSheet, isOpen }
 }
