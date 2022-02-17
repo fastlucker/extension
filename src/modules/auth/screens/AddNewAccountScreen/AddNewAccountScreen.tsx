f293aad207bc0e61e1a8d97e7fc86aaf17eebb6c
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-02-17 23:31:58 +0200

    Fix: create account white screen; Add no backup checkbox

--- src/modules/auth/screens/AddNewAccountScreen/AddNewAccountScreen.tsx
+++ src/modules/auth/screens/AddNewAccountScreen/AddNewAccountScreen.tsx
@@ -13,7 +13,10 @@ import P from '@modules/common/components/P'
 import Text, { TEXT_TYPES } from '@modules/common/components/Text'
 import Title from '@modules/common/components/Title'
 import Wrapper from '@modules/common/components/Wrapper'
+import accountPresets from '@modules/common/constants/accountPresets'
 import { isEmail } from '@modules/common/services/validate'
+
+const days = Math.ceil(accountPresets.quickAccTimelock / 86400)
 
 const AddNewAccountScreen = () => {
   const { t } = useTranslation()
@@ -22,6 +25,7 @@ const AddNewAccountScreen = () => {
     control,
     handleSubmit,
     getValues,
+    watch,
     formState: { errors, isSubmitting }
   } = useForm({
     defaultValues: {
@@ -29,7 +33,8 @@ const AddNewAccountScreen = () => {
       password: '',
       confirmPassword: '',
       terms: false,
-      backup: true
+      backup: true,
+      noBackup: false
     }
   })
 
@@ -39,7 +44,7 @@ const AddNewAccountScreen = () => {
         Keyboard.dismiss()
       }}
     >
-      <Wrapper>
+      <Wrapper keyboardDismissMode="on-drag">
         <Title>{t('Create a new account')}</Title>
         <Controller
           control={control}
@@ -96,6 +101,9 @@ const AddNewAccountScreen = () => {
         {!!errors.confirmPassword && <P type={TEXT_TYPES.DANGER}>{t("Passwords don't match.")}</P>}
         <Controller
           control={control}
+          rules={{
+            required: true
+          }}
           render={({ field: { onChange, value } }) => (
             <Checkbox value={value} onValueChange={() => onChange(!value)}>
               <Trans t={t}>
@@ -110,6 +118,9 @@ const AddNewAccountScreen = () => {
           )}
           name="terms"
         />
+
+        {!!errors.terms && <P>{t('Please agree to our Terms of Service and Privacy policy')}</P>}
+
         <Controller
           control={control}
           render={({ field: { onChange, value } }) => (
@@ -126,7 +137,31 @@ const AddNewAccountScreen = () => {
           )}
           name="backup"
         />
-        {!!errors.terms && <P>{t('Please agree to our Terms of Service and Privacy policy')}</P>}
+        <Controller
+          control={control}
+          rules={{
+            required: watch('backup', true) === false
+          }}
+          render={({ field: { onChange, value } }) =>
+            watch('backup', true) === false ? (
+              <Checkbox value={value} onValueChange={() => onChange(!value)}>
+                <Text onPress={() => onChange(!value)}>
+                  {t(
+                    'In case you forget your password or lose your backup, you will have to wait {{days}} days and pay the recovery fee to restore access to your account.',
+                    { days }
+                  )}
+                </Text>
+              </Checkbox>
+            ) : (
+              // eslint-disable-next-line react/jsx-no-useless-fragment
+              <></>
+            )
+          }
+          name="noBackup"
+        />
+        {!!errors.noBackup && watch('backup', true) === false && (
+          <P>{t('Please tick this box if you want to proceed.')}</P>
+        )}
 
         <Button
           disabled={isSubmitting}
