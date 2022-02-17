13c21adb5a44c96a5c793437d99e69c6004387ad
superKalo <superkalo@devlabs.bg>
2022-02-17 15:03:33 +0200

    Add: global flag when permission asking is in progress

---
+++ global.js
@@ -0,0 +1,1 @@
+global.isAskingForPermission = false
--- src/config/customTypes/customTypes.d.ts
+++ src/config/customTypes/customTypes.d.ts
@@ -1,2 +1,7 @@
+// Global flag that keeps track if permission is currently in process of being requested
+// Syntax requires the use of `var`, technically, it's a `let`.
+/* eslint-disable no-var */
+declare var isAskingForPermission: boolean
+
 // TODO: add types
 declare module 'adex-protocol-eth/js/IdentityProxyDeploy'
