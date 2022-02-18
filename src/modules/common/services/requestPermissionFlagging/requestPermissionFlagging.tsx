34c3bc70648c2820670c5106c317d83083075f49
superKalo <superkalo@devlabs.bg>
2022-02-18 18:39:09 +0200

    Add: a note

--- src/modules/common/services/requestPermissionFlagging/requestPermissionFlagging.tsx
+++ src/modules/common/services/requestPermissionFlagging/requestPermissionFlagging.tsx
@@ -13,6 +13,12 @@ export default function requestPermissionFlagging<R>(p: () => Promise<R>): Promi
   })
 }
 
+// Wraps a request permission promise and sets / un-sets the appropriate flag.
+// This is needed for iOS because the local auth requests, because
+// they all trigger AppState change (to `background` and then `active`).
+// Which triggers the AppState listeners, like the one on the PasscodeContext,
+// that locks the app (in case lock when inactive option is active).
+// Apply this for both iOS and Android, for consistency.
 export function requestLocalAuthFlagging<R>(p: () => Promise<R>): Promise<R> {
   global.isAskingForLocalAuth = true
 
