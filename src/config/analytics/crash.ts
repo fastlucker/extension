954592c419fc0b506b50a37545e1ea6d79e6edc2
superKalo <superkalo@devlabs.bg>
2022-02-25 11:59:09 +0200

    Add: helpers for crash analytics tracking

--- src/config/analytics/crash.ts
+++ src/config/analytics/crash.ts
@@ -1,9 +1,12 @@
 import Constants from 'expo-constants'
-import * as Sentry from 'sentry-expo'
+import * as SentryLib from 'sentry-expo'
 
 import CONFIG from '@config/env'
 
-Sentry.init({
+// Ref to access all @sentry/react-native exports:
+const Sentry = SentryLib.Native
+
+SentryLib.init({
   dsn: CONFIG.SENTRY_DSN,
   // In order to use the published release source maps with Issues in Sentry,
   // set your Expo revisionId as the Sentry release identifier:
@@ -22,11 +25,13 @@ Sentry.init({
   // enableInExpoDevelopment: true,
   // If `true`, Sentry will try to print out useful debugging information
   // if something goes wrong with sending the event. Set it to `false` in production
-  // debug: true,
+  // debug: true
 })
 
-// Access any @sentry/react-native exports via:
-// Sentry.Native.*
+export const setUserContext = (u: SentryLib.Native.User) => Sentry.setUser(u)
 
-// // Access any @sentry/browser exports via:
-// Sentry.Browser.*
+export const setTags = (tags: { [key: string]: string }) => Sentry.setTags(tags)
+
+export const captureException = (e: any) => Sentry.captureException(e)
+
+export const captureMessage = (message: string) => Sentry.captureMessage(message)
