aebccc7be7f258cdc7855d08b17e01b1631359d5
superKalo <superkalo@devlabs.bg>
2022-02-25 12:21:45 +0200

    Add: a note

--- src/config/analytics/CrashAnalytics.ts
+++ src/config/analytics/CrashAnalytics.ts
@@ -12,7 +12,7 @@ SentryLib.init({
   // set your Expo revisionId as the Sentry release identifier:
   release: Constants?.manifest?.revisionId || 'N/A',
   // Match an error to a specific environment
-  environment: CONFIG.APP_ENV,
+  environment: CONFIG.APP_ENV
   // TODO:
   // We get a lot of "Network request failed" or similar errors from users.
   // They are connected to network problems of the users.
@@ -22,9 +22,9 @@ SentryLib.init({
   // {@link https://github.com/getsentry/sentry/issues/12676#issuecomment-533538114}
   // ignoreErrors: ['Network request failed'],
   // Use these two for debugging purposed only
-  enableInExpoDevelopment: true
-  // If `true`, Sentry will try to print out useful debugging information
-  // if something goes wrong with sending the event. Set it to `false` in production
+  // enableInExpoDevelopment: true
+  // If `true`, Sentry will try to print out useful debugging information,
+  // uncomment when debugging only
   // debug: true
 })
 
