9e606fd1104193a59d9ec6f1711d40b8f4af809c
superKalo <superkalo@devlabs.bg>
2022-02-25 14:36:01 +0200

    Change: remove this until we need it later on

--- src/config/analytics/CrashAnalytics.ts
+++ src/config/analytics/CrashAnalytics.ts
@@ -13,14 +13,6 @@ SentryLib.init({
   release: Constants?.manifest?.revisionId || 'unspecified',
   // Match an error to a specific environment
   environment: CONFIG.APP_ENV
-  // TODO:
-  // We get a lot of "Network request failed" or similar errors from users.
-  // They are connected to network problems of the users.
-  // Having these issues in Sentry does not really help us.
-  // These errors are not really valuable to use
-  // and crowd the view on real issues. So ignore them all.
-  // {@link https://github.com/getsentry/sentry/issues/12676#issuecomment-533538114}
-  // ignoreErrors: ['Network request failed'],
   // Use these two for debugging purposed only
   // enableInExpoDevelopment: true
   // If `true`, Sentry will try to print out useful debugging information,
