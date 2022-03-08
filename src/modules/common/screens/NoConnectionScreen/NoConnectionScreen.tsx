0b74064f3943e9912fda551cfab8ea618c67b60a
superKalo <superkalo@devlabs.bg>
2022-03-08 15:08:20 +0200

    Change: no need for try agan btn

--- src/modules/common/screens/NoConnectionScreen/NoConnectionScreen.tsx
+++ src/modules/common/screens/NoConnectionScreen/NoConnectionScreen.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
 import { useTranslation } from '@config/localization'
-import Button from '@modules/common/components/Button'
 import Text from '@modules/common/components/Text'
 import Title from '@modules/common/components/Title'
 import Wrapper from '@modules/common/components/Wrapper'
-import useNetInfo from '@modules/common/hooks/useNetInfo'
 import spacings from '@modules/common/styles/spacings'
 import textStyles from '@modules/common/styles/utils/text'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
@@ -14,7 +12,6 @@ interface Props extends NativeStackScreenProps<any, 'no-connection'> {}
 
 const NoConnectionScreen: React.FC<Props> = () => {
   const { t } = useTranslation()
-  const { checkIsConnected } = useNetInfo()
 
   return (
     <Wrapper>
@@ -24,7 +21,6 @@ const NoConnectionScreen: React.FC<Props> = () => {
           'You are not connected to the Internet. Make sure Wi-Fi is on, Airplane Mode is off and try again.'
         )}
       </Text>
-      <Button text={t('Try again')} onPress={checkIsConnected} />
     </Wrapper>
   )
 }
