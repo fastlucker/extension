ba14e7ed3a78258fdd642e4a9411be426b87f229
superKalo <superkalo@devlabs.bg>
2022-03-08 12:36:03 +0200

    Change: use hooks instead of subscriptions; Add: get if `isInternetReachable` too

--- src/modules/common/hooks/useNetInfo/useNetInfo.tsx
+++ src/modules/common/hooks/useNetInfo/useNetInfo.tsx
@@ -1,19 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useNetInfo as RnUseNetInfo } from '@react-native-community/netinfo'
 
-import NetInfo from '@react-native-community/netinfo'
+interface Props {
+  isConnected: null | boolean
+}
 
-export default function useNetInfo() {
-  const [isConnected, setIsConnected] = useState<null | boolean>(null)
-  const [type, setType] = useState(null)
+export default function useNetInfo(): Props {
+  const { isConnected, isInternetReachable } = RnUseNetInfo()
 
-  useEffect(() => {
-    const unsubscribe = NetInfo.addEventListener((state) => {
-      setIsConnected(!!state.isConnected)
-      setType(state.type)
-    })
+  const getIsConnected =
+    // In case any of these is `null`, return `null`, it acts as a loading state
+    isConnected === null || isInternetReachable === null ? null : isConnected && isInternetReachable
 
-    return () => unsubscribe()
-  }, [])
-
-  return { isConnected, type }
+  return { isConnected: getIsConnected }
 }
