323d607fff6eb06ec0184018b897f2f557317898
Kaloyan Kosev <superkalo@devlabs.bg>
2022-03-18 16:44:18 +0200

    Add: prompt to turn on bluetooth

--- src/modules/common/components/RequireBluetooth/RequireBluetooth.tsx
+++ src/modules/common/components/RequireBluetooth/RequireBluetooth.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { BleManager } from 'react-native-ble-plx'
+import BluetoothStateManager from 'react-native-bluetooth-state-manager'
 
+import Button from '@modules/common/components/Button'
 import P from '@modules/common/components/P'
-
-import { TEXT_TYPES } from '../Text'
+import { TEXT_TYPES } from '@modules/common/components/Text'
 
 const RequireBluetooth: React.FC = ({ children }) => {
   const { t } = useTranslation()
@@ -18,10 +19,17 @@ const RequireBluetooth: React.FC = ({ children }) => {
     return () => subscription.remove()
   }, [])
 
+  const turnOnBluetooth = () => BluetoothStateManager.enable()
+
   return isBluetoothPoweredOn ? (
     children
   ) : (
-    <P type={TEXT_TYPES.DANGER}>{t('Please enable the Bluetooth first!')}</P>
+    <>
+      <P type={TEXT_TYPES.DANGER}>
+        {t('Please turn on the Bluetooth first, in order to connect to hardware wallet devices.')}
+      </P>
+      <Button text={t('Turn on Bluetooth')} onPress={turnOnBluetooth} />
+    </>
   )
 }
 
