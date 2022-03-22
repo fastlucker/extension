b720c47aceed843a3624f844ef55f3ea0706bc4f
superKalo <superkalo@devlabs.bg>
2022-03-22 11:31:55 +0200

    Change: wrap component thhat requires Bluetooth

--- src/modules/auth/screens/HardwareWalletScreen/HardwareWalletScreen.tsx
+++ src/modules/auth/screens/HardwareWalletScreen/HardwareWalletScreen.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import useHardwareWalletActions from '@modules/auth/hooks/useHardwareWalletActions'
 import HardwareWalletScanDevices from '@modules/common/components/HardwareWalletScanDevices'
+import RequireBluetooth from '@modules/common/components/RequireBluetooth'
 
 const HardwareWalletScreen = () => {
   const { addAccount } = useHardwareWalletActions()
 
-  return <HardwareWalletScanDevices onSelectDevice={addAccount} />
+  return (
+    <RequireBluetooth>
+      <HardwareWalletScanDevices onSelectDevice={addAccount} />
+    </RequireBluetooth>
+  )
 }
 
 export default HardwareWalletScreen
