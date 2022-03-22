e07fd5f21b3df42f3c2f2ff478c26fd973b5d046
superKalo <superkalo@devlabs.bg>
2022-03-22 15:43:51 +0200

    Change: remove subscriptions

--- src/modules/common/components/RequireLocation/RequireLocation.tsx
+++ src/modules/common/components/RequireLocation/RequireLocation.tsx
@@ -12,15 +12,16 @@ import Button from '../Button'
 
 const RequireLocation: React.FC = ({ children }) => {
   const [permissionGranted, setPermissionGranted] = useState<boolean | null>(null)
+  // Assumes they are enabled by default and sets the flag
+  // in case the `new Observable(TransportBLE.listen).subscribe` fails.
   const [locationServicesEnabled, setLocationServicesEnabled] = useState<boolean | null>(true)
 
   useEffect(() => {
     const sub = new Observable(TransportBLE.listen).subscribe({
       complete: () => {
-        console.log('complete')
         setLocationServicesEnabled(true)
       },
-      next: (e: any) => {
+      next: () => {
         setLocationServicesEnabled(true)
       },
       error: (e) => {
@@ -29,25 +30,7 @@ const RequireLocation: React.FC = ({ children }) => {
       }
     })
 
-    // TODO:
-    // DeviceEventEmitter.addListener('locationProviderStatusChange', (status) => {
-    //   console.log(status) //  status => {enabled: false, status: "disabled"} or {enabled: true, status: "enabled"}
-    // })
-    // TODO:
-    // LocationServicesDialogBox.checkLocationServicesIsEnabled({
-    //   enableHighAccuracy: true, // true => GPS AND NETWORK PROVIDER, false => GPS OR NETWORK PROVIDER
-    //   showDialog: false, // false => Opens the Location access page directly
-    //   openLocationServices: false, // false => Directly catch method is called if location services are turned off
-    //   providerListener: true // true ==> Trigger "locationProviderStatusChange" listener when the location state changes
-    // }).then((s) => console.log(s))
-
-    return () => {
-      sub.unsubscribe()
-
-      // TODO:
-      // Stops the "locationProviderStatusChange" listener.
-      // LocationServicesDialogBox.stopListener()
-    }
+    return () => sub.unsubscribe()
   }, [])
 
   useEffect(() => {
