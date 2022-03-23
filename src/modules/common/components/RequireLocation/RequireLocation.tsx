7499c355ad080d4afea3dd228a742c1d0f3764fd
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-03-23 18:04:18 +0200

    Listens for location state change

--- src/modules/common/components/RequireLocation/RequireLocation.tsx
+++ src/modules/common/components/RequireLocation/RequireLocation.tsx
@@ -2,7 +2,7 @@ import { t } from 'i18next'
 import Button from 'modules/common/components/Button'
 import { TEXT_TYPES } from 'modules/common/components/Text'
 import React, { useEffect, useState } from 'react'
-import { PermissionsAndroid } from 'react-native'
+import { AppState, PermissionsAndroid } from 'react-native'
 import LocationServicesDialogBox from 'react-native-android-location-services-dialog-box'
 import { BleErrorCode } from 'react-native-ble-plx'
 import { Observable } from 'rxjs'
@@ -10,29 +10,38 @@ import { Observable } from 'rxjs'
 import TransportBLE from '@ledgerhq/react-native-hw-transport-ble'
 import P from '@modules/common/components/P'
 
-const RequireLocation: React.FC = ({ children }) => {
+const RequireLocation: React.FC<any> = ({ children }) => {
   const [permissionGranted, setPermissionGranted] = useState<boolean | null>(null)
   // Assumes they are enabled by default and sets the flag
   // in case the `new Observable(TransportBLE.listen).subscribe` fails.
   const [locationServicesEnabled, setLocationServicesEnabled] = useState<boolean | null>(true)
 
-  useEffect(() => {
-    // TODO: Figure out a way to flip the `locationServicesEnabled` flag,
-    // in case the location services are enabled (but they were disabled before).
+  const setLocationState = () => {
     const sub = new Observable(TransportBLE.listen).subscribe({
-      complete: () => {
-        setLocationServicesEnabled(true)
-      },
       next: () => {
         setLocationServicesEnabled(true)
+        sub.unsubscribe()
       },
       error: (e) => {
         const disabled = e?.errorCode === BleErrorCode.LocationServicesDisabled
         setLocationServicesEnabled(!disabled)
+        sub.unsubscribe()
       }
     })
+  }
 
-    return () => sub.unsubscribe()
+  useEffect(() => {
+    const subscription: any = AppState.addEventListener('focus', () => {
+      // Called when the app state is moves from blur to focus
+      setLocationState()
+    })
+
+    // Initial call
+    setLocationState()
+
+    return () => {
+      if (subscription) subscription?.remove()
+    }
   }, [])
 
   useEffect(() => {
