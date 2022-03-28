e2c8cb927cf5dcf16aecd86a1a3012531c7e7dc1
superKalo <superkalo@devlabs.bg>
2022-03-28 15:59:23 +0300

    Change: import only fonts that are used

--- src/modules/common/hooks/useFonts/useFonts.tsx
+++ src/modules/common/hooks/useFonts/useFonts.tsx
@@ -1,45 +1,32 @@
 import {
-  Poppins_100Thin,
-  Poppins_100Thin_Italic,
-  Poppins_200ExtraLight,
-  Poppins_200ExtraLight_Italic,
   Poppins_300Light,
-  Poppins_300Light_Italic,
   Poppins_400Regular,
-  Poppins_400Regular_Italic,
   Poppins_500Medium,
-  Poppins_500Medium_Italic,
-  Poppins_600SemiBold,
-  Poppins_600SemiBold_Italic,
-  Poppins_700Bold,
-  Poppins_700Bold_Italic,
-  Poppins_800ExtraBold,
-  Poppins_800ExtraBold_Italic,
-  Poppins_900Black,
-  Poppins_900Black_Italic,
   useFonts as useFontsRn
 } from '@expo-google-fonts/poppins'
 
 export default function useFonts() {
+  // Import only the ones that are used, because having all could lead to a performance
+  // penalty, since loading of the fonts take some time during initial app open.
   const [fontsLoaded] = useFontsRn({
-    Poppins_100Thin,
-    Poppins_100Thin_Italic,
-    Poppins_200ExtraLight,
-    Poppins_200ExtraLight_Italic,
+    // Poppins_100Thin,
+    // Poppins_100Thin_Italic,
+    // Poppins_200ExtraLight,
+    // Poppins_200ExtraLight_Italic,
     Poppins_300Light,
-    Poppins_300Light_Italic,
+    // Poppins_300Light_Italic,
     Poppins_400Regular,
-    Poppins_400Regular_Italic,
-    Poppins_500Medium,
-    Poppins_500Medium_Italic,
-    Poppins_600SemiBold,
-    Poppins_600SemiBold_Italic,
-    Poppins_700Bold,
-    Poppins_700Bold_Italic,
-    Poppins_800ExtraBold,
-    Poppins_800ExtraBold_Italic,
-    Poppins_900Black,
-    Poppins_900Black_Italic
+    // Poppins_400Regular_Italic,
+    Poppins_500Medium
+    // Poppins_500Medium_Italic,
+    // Poppins_600SemiBold,
+    // Poppins_600SemiBold_Italic,
+    // Poppins_700Bold,
+    // Poppins_700Bold_Italic,
+    // Poppins_800ExtraBold,
+    // Poppins_800ExtraBold_Italic,
+    // Poppins_900Black,
+    // Poppins_900Black_Italic
   })
 
   return { fontsLoaded }
