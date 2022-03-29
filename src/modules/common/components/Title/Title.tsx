efedc6bf77a49b01e094848e27bb55962de5a57e
superKalo <superkalo@devlabs.bg>
2022-03-29 13:19:31 +0300

    Change: apply the typography config to the title component

--- src/modules/common/components/Title/Title.tsx
+++ src/modules/common/components/Title/Title.tsx
@@ -4,19 +4,37 @@ import { TextProps } from 'react-native'
 import Text from '../Text'
 import styles from './styles'
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export enum TITLE_TYPES {
+  REGULAR = 'regular',
+  SMALL = 'small'
+}
+
 interface Props extends TextProps {
+  type: TITLE_TYPES
   hasBottomSpacing?: boolean
   color?: string
 }
 
+const titleStyles = {
+  [TITLE_TYPES.REGULAR]: styles.titleRegular,
+  [TITLE_TYPES.SMALL]: styles.titleSmall
+}
+
 const Title: React.FC<Props> = ({
+  type = TITLE_TYPES.REGULAR,
   style = {},
   hasBottomSpacing = true,
   children,
   color
 }: Props) => (
   <Text
-    style={[styles.text, !!hasBottomSpacing && styles.bottomSpacing, !!color && { color }, style]}
+    style={[
+      titleStyles[type],
+      !!hasBottomSpacing && styles.bottomSpacing,
+      !!color && { color },
+      style
+    ]}
   >
     {children}
   </Text>
--- src/modules/common/components/Title/styles.ts
+++ src/modules/common/components/Title/styles.ts
@@ -1,14 +1,23 @@
 import { StyleSheet, TextStyle } from 'react-native'
 
 interface Style {
-  text: TextStyle
+  titleRegular: TextStyle
+  titleSmall: TextStyle
   bottomSpacing: TextStyle
 }
 
 const styles = StyleSheet.create<Style>({
-  text: {
-    fontSize: 24,
-    fontWeight: '600'
+  titleRegular: {
+    fontFamily: 'Poppins_400Regular',
+    fontSize: 20,
+    letterSpacing: 0,
+    lineHeight: 30
+  },
+  titleSmall: {
+    fontFamily: 'Poppins_500Medium',
+    fontSize: 16,
+    letterSpacing: 5,
+    lineHeight: 25
   },
   bottomSpacing: {
     paddingBottom: 20
