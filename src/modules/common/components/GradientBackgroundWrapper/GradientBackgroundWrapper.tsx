d9c081784db13a8867efd731db994c6aa5eb82c6
superKalo <superkalo@devlabs.bg>
2022-04-01 14:17:01 +0300

    Add: TODO

--- src/modules/common/components/GradientBackgroundWrapper/GradientBackgroundWrapper.tsx
+++ src/modules/common/components/GradientBackgroundWrapper/GradientBackgroundWrapper.tsx
@@ -19,7 +19,8 @@ const GradientBackgroundWrapper = ({ children, gradient }: Props) => (
             colors={gradient || theme?.backgroundGradient}
             start={{ x: 0.5, y: 0 }}
             end={{ x: 0.5, y: 1 }}
-            style={[flexboxStyles.flex1 /* FIXME: { paddingTop: headerHeight } */]}
+            // TODO: Static paddingTop might work better now. Check out.
+            style={[flexboxStyles.flex1, { paddingTop: 100 }]}
           >
             {children}
           </LinearGradient>
