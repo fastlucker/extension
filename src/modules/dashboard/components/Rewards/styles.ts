2ae7087c512826030e5fb79a6a4f2364e7bacfe8
Kaloyan Kosev <superkalo@devlabs.bg>
2022-04-07 17:52:41 +0300

    Change: not needed anymore

--- src/modules/dashboard/components/Rewards/styles.ts
+++ src/modules/dashboard/components/Rewards/styles.ts
@@ -2,16 +2,12 @@ import { StyleSheet, ViewStyle } from 'react-native'
 
 interface Style {
   loading: ViewStyle
-  button: ViewStyle
   buttonClaim: ViewStyle
 }
 
 const styles = StyleSheet.create<Style>({
   loading: {
     height: 30
-  },
-  button: {
-    width: 'auto'
   },
   buttonClaim: {
     width: 'auto',
