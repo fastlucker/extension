a44bc331ce64eca691ee0ea5571ba9af012a63ca
superKalo <superkalo@devlabs.bg>
2022-04-20 17:34:15 +0300

    Add: extend the token icon to handle fallback cases

--- src/modules/common/components/TokenIcon/TokenIcon.tsx
+++ src/modules/common/components/TokenIcon/TokenIcon.tsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image, ImageProps, View } from 'react-native'
+
+import MissingTokenIcon from '@assets/svg/MissingTokenIcon'
+import { getTokenIcon } from '@modules/common/services/icons'
 
 import styles from './styles'
 
-interface Props extends ImageProps {}
+interface Props extends Partial<ImageProps> {
+  uri?: string
+  networkId?: string
+  address?: string
+  withContainer?: boolean
+}
 
-const TokenIcon: React.FC<Props> = (props) => (
-  <View style={styles.container}>
-    <Image style={styles.img} {...props} />
-  </View>
-)
+const TokenIcon: React.FC<Props> = ({
+  uri,
+  networkId = '',
+  address = '',
+  withContainer = false,
+  ...props
+}) => {
+  const [failedImg, setFailedImg] = useState(!uri)
+  const [failedImgFallback, setFailedImgFallback] = useState(false)
+
+  return failedImg && failedImgFallback ? (
+    <MissingTokenIcon />
+  ) : (
+    <View style={withContainer && styles.container}>
+      {failedImg ? (
+        <Image
+          source={{ uri: getTokenIcon(networkId, address) }}
+          onError={() => setFailedImgFallback(true)}
+          style={styles.img}
+          {...props}
+        />
+      ) : (
+        <Image source={{ uri }} onError={() => setFailedImg(true)} style={styles.img} {...props} />
+      )}
+    </View>
+  )
+}
 
 export default TokenIcon
