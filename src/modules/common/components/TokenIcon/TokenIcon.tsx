54e663259ad0db6ab44320f0c52be5040b620771
superKalo <superkalo@devlabs.bg>
2022-04-20 17:48:24 +0300

    Change: allow configurations and diff sizing with and without container

--- src/modules/common/components/TokenIcon/TokenIcon.tsx
+++ src/modules/common/components/TokenIcon/TokenIcon.tsx
@@ -11,6 +11,8 @@ interface Props extends Partial<ImageProps> {
   networkId?: string
   address?: string
   withContainer?: boolean
+  containerWidth?: number
+  containerHeight?: number
 }
 
 const TokenIcon: React.FC<Props> = ({
@@ -18,24 +20,37 @@ const TokenIcon: React.FC<Props> = ({
   networkId = '',
   address = '',
   withContainer = false,
+  width = 22,
+  height = 22,
   ...props
 }) => {
   const [failedImg, setFailedImg] = useState(!uri)
   const [failedImgFallback, setFailedImgFallback] = useState(false)
 
   return failedImg && failedImgFallback ? (
-    <MissingTokenIcon />
+    <MissingTokenIcon
+      withRect={withContainer}
+      // A bit larger when they don't have a container,
+      // because the SVG sizings are made with rectangle in mind
+      width={withContainer ? width : width * 1.3}
+      height={withContainer ? height : height * 1.3}
+    />
   ) : (
     <View style={withContainer && styles.container}>
       {failedImg ? (
         <Image
           source={{ uri: getTokenIcon(networkId, address) }}
           onError={() => setFailedImgFallback(true)}
-          style={styles.img}
+          style={{ width, height, borderRadius: width / 2 }}
           {...props}
         />
       ) : (
-        <Image source={{ uri }} onError={() => setFailedImg(true)} style={styles.img} {...props} />
+        <Image
+          source={{ uri }}
+          onError={() => setFailedImg(true)}
+          style={{ width, height, borderRadius: width / 2 }}
+          {...props}
+        />
       )}
     </View>
   )
--- src/modules/common/components/TokenIcon/styles.ts
+++ src/modules/common/components/TokenIcon/styles.ts
@@ -1,4 +1,4 @@
-import { ImageStyle, StyleSheet, ViewStyle } from 'react-native'
+import { StyleSheet, ViewStyle } from 'react-native'
 
 import { colorPalette as colors } from '@modules/common/styles/colors'
 import commonStyles from '@modules/common/styles/utils/common'
@@ -6,7 +6,6 @@ import flexboxStyles from '@modules/common/styles/utils/flexbox'
 
 interface Style {
   container: ViewStyle
-  img: ImageStyle
 }
 
 const styles = StyleSheet.create<Style>({
@@ -17,11 +16,6 @@ const styles = StyleSheet.create<Style>({
     overflow: 'hidden',
     ...commonStyles.borderRadiusPrimary,
     ...flexboxStyles.center
-  },
-  img: {
-    width: 22,
-    height: 22,
-    borderRadius: 22
   }
 })
 
