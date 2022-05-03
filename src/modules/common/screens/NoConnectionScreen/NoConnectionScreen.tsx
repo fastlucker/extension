51451af324784cf5fbf60a479dfa90a57ef59d2e
Kaloyan Kosev <superkalo@devlabs.bg>
2022-05-03 16:52:24 +0300

    Add: connection screen UI

--- src/modules/common/screens/NoConnectionScreen/NoConnectionScreen.tsx
+++ src/modules/common/screens/NoConnectionScreen/NoConnectionScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Satellite from '@assets/svg/Satellite'
 import { useTranslation } from '@config/localization'
 import GradientBackgroundWrapper from '@modules/common/components/GradientBackgroundWrapper'
 import Text from '@modules/common/components/Text'
 import Title from '@modules/common/components/Title'
 import Wrapper from '@modules/common/components/Wrapper'
 import spacings from '@modules/common/styles/spacings'
+import flexboxStyles from '@modules/common/styles/utils/flexbox'
 import textStyles from '@modules/common/styles/utils/text'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
@@ -16,8 +18,10 @@ const NoConnectionScreen: React.FC<Props> = () => {
 
   return (
     <GradientBackgroundWrapper>
-      <Wrapper>
-        <Text style={spacings.mb}>
+      <Wrapper contentContainerStyle={flexboxStyles.center}>
+        <Satellite style={spacings.mbLg} />
+        <Title style={textStyles.center}>{t('No Internet Connection')}</Title>
+        <Text style={[spacings.mb, textStyles.center]}>
           {t(
             'You are not connected to the Internet. Make sure you are connected over Wi-Fi or your phone mobile data is turned on.'
           )}
