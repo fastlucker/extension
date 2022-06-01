9df64d6af05257b84b67f23f68106dcd2bda4e3a
superKalo <superkalo@devlabs.bg>
2022-06-01 15:11:12 +0300

    Add: add and hide token bottom sheet segments

--- src/modules/dashboard/components/AddToken/AddToken.tsx
+++ src/modules/dashboard/components/AddToken/AddToken.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { LayoutAnimation, TouchableOpacity, View } from 'react-native'
 
 import { useTranslation } from '@config/localization'
 import BottomSheet from '@modules/common/components/BottomSheet'
 import useBottomSheet from '@modules/common/components/BottomSheet/hooks/useBottomSheet'
+import Segments from '@modules/common/components/Segments'
 import Text from '@modules/common/components/Text'
 import Title from '@modules/common/components/Title'
 import usePortfolio from '@modules/common/hooks/usePortfolio'
+import { triggerLayoutAnimation } from '@modules/common/services/layoutAnimation'
 import spacings from '@modules/common/styles/spacings'
+import flexboxStyles from '@modules/common/styles/utils/flexbox'
 import textStyles from '@modules/common/styles/utils/text'
 
 import AddTokenForm from './AddTokenForm'
 import styles from './styles'
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
+enum FORM_TYPE {
+  ADD_TOKEN = 'Add Token',
+  HIDE_TOKEN = 'Hide Token'
+}
+
+const segments = [{ value: FORM_TYPE.ADD_TOKEN }, { value: FORM_TYPE.HIDE_TOKEN }]
+
 const AddToken = () => {
   const { t } = useTranslation()
   const { onAddExtraToken } = usePortfolio()
   const { sheetRef, isOpen, openBottomSheet, closeBottomSheet } = useBottomSheet()
+  const [formType, setFormType] = useState<FORM_TYPE>(FORM_TYPE.ADD_TOKEN)
 
   const handleOnSubmit = (token) => {
     onAddExtraToken(token)
@@ -37,9 +49,38 @@ const AddToken = () => {
         closeBottomSheet={closeBottomSheet}
         dynamicInitialHeight={false}
       >
-        <Title style={textStyles.center}>{t('Add Token')}</Title>
+        <Segments
+          defaultValue={formType}
+          segments={segments}
+          onChange={(value: FORM_TYPE) => {
+            setFormType(value)
+            triggerLayoutAnimation({
+              forceAnimate: true,
+              config: LayoutAnimation.create(300, 'linear', 'opacity')
+            })
+          }}
+          fontSize={14}
+        />
+        <View style={[flexboxStyles.flex1, flexboxStyles.justifyEnd, spacings.mtMd]}>
+          {formType === FORM_TYPE.ADD_TOKEN && (
+            <>
+              <Title type="small" style={textStyles.center}>
+                {t('Add Token')}
+              </Title>
 
-        <AddTokenForm onSubmit={handleOnSubmit} />
+              <AddTokenForm onSubmit={handleOnSubmit} />
+            </>
+          )}
+          {formType === FORM_TYPE.HIDE_TOKEN && (
+            <>
+              <Title type="small" style={textStyles.center}>
+                {t('Hide Token')}
+              </Title>
+
+              {/* TODO: Form. */}
+            </>
+          )}
+        </View>
       </BottomSheet>
     </>
   )
