bfe236455bd835932b035035664b844fa22830b3
superKalo <superkalo@devlabs.bg>
2022-06-10 18:16:10 +0300

    Change: align app name and routes; migrate from ambiremobilewallet to ambire

--- ios/ambire/AppDelegate.mm
+++ ios/ambire/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"ambiremobilewallet", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"ambire", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
