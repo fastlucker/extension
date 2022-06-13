54df8f2f024162d612d5284616627764bec48dde
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-06-13 18:46:59 +0300

    add: support for multiple dapps

--- src/modules/common/contexts/unsupportedDAppsBottomSheetContext/unsupportedDAppsBottomSheetContext.tsx
+++ src/modules/common/contexts/unsupportedDAppsBottomSheetContext/unsupportedDAppsBottomSheetContext.tsx
@@ -3,6 +3,7 @@ import React, { createContext, useCallback, useMemo, useState } from 'react'
 import { Linking, View } from 'react-native'
 
 import { Trans, useTranslation } from '@config/localization'
+import { TouchableOpacity } from '@gorhom/bottom-sheet'
 import BottomSheet from '@modules/common/components/BottomSheet'
 import useBottomSheet, { UseBottomSheetReturnType } from '@modules/common/components/BottomSheet/hooks/useBottomSheet'
 import Button from '@modules/common/components/Button'
@@ -12,7 +13,7 @@ import Title from '@modules/common/components/Title'
 import useStorage from '@modules/common/hooks/useStorage'
 import useWalletConnect from '@modules/common/hooks/useWalletConnect'
 import { colorPalette as colors } from '@modules/common/styles/colors'
-import spacings from '@modules/common/styles/spacings'
+import spacings, { IS_SCREEN_SIZE_S } from '@modules/common/styles/spacings'
 import flexboxStyles from '@modules/common/styles/utils/flexbox'
 
 import UnsupportedDAppItem from './UnsupportedDAppItem'
@@ -26,14 +27,13 @@ const UnsupportedDAppsBottomSheetContext =
     closeBottomSheet: () => {}
   })
 
+const numOfItemsWhenNotExpanded = IS_SCREEN_SIZE_S ? 1 : 2
+
 const UnsupportedDAppsBottomSheetProvider: React.FC = ({ children }) => {
   const { sheetRef, closeBottomSheet } = useBottomSheet()
   const { connections, disconnect } = useWalletConnect()
   const { t } = useTranslation()
   const [advancedMode, setAdvancedMode] = useState<boolean>(false)
-  // TODO: implement multi tokens support in an expandable list if there is some actual use case for that
-  // Currently, it is not possible to interact with the app if there is an unconfirmed unsupported connection (accepted or canceled) in order to connect a second dapp
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isExpanded, setIsExpanded] = useState(false)
 
   const [advancedModeList, setAdvancedModeList] = useStorage({
@@ -41,7 +41,7 @@ const UnsupportedDAppsBottomSheetProvider: React.FC = ({ children }) => {
     defaultValue: []
   })
 
-  const unsupported = useMemo(
+  const unsupportedRaw = useMemo(
     () =>
       connections.filter(
         ({ session }) =>
@@ -53,32 +53,39 @@ const UnsupportedDAppsBottomSheetProvider: React.FC = ({ children }) => {
     [connections, advancedModeList]
   )
 
-  const handleCancel = useCallback(() => {
-    closeBottomSheet()
-    if (!advancedMode) {
-      setTimeout(() => {
-        unsupported.map(({ uri }) => disconnect(uri))
-      }, 150)
+  const unsupported = useMemo(() => {
+    if (unsupportedRaw.length > numOfItemsWhenNotExpanded) {
+      return isExpanded ? unsupportedRaw : unsupportedRaw.slice(0, numOfItemsWhenNotExpanded)
     }
-  }, [closeBottomSheet, unsupported, disconnect, advancedMode])
+
+    return unsupportedRaw
+  }, [unsupportedRaw, isExpanded])
+
+  const handleCancel = useCallback(() => {
+    if (!advancedMode) {
+      unsupportedRaw.map(({ uri }) => disconnect(uri))
+    }
+    closeBottomSheet()
+  }, [closeBottomSheet, unsupportedRaw, disconnect, advancedMode])
 
   const handleContinue = useCallback(() => {
     setAdvancedModeList([
       ...advancedModeList,
-      ...unsupported.map(({ session }) => session.peerMeta.url)
+      ...unsupportedRaw.map(({ session }) => session.peerMeta.url)
     ])
     closeBottomSheet()
-  }, [advancedModeList, setAdvancedModeList, unsupported, closeBottomSheet])
+  }, [advancedModeList, setAdvancedModeList, unsupportedRaw, closeBottomSheet])
+
+  const toggleItemsList = () => {
+    setIsExpanded((prev) => !prev)
+  }
 
   return (
     <UnsupportedDAppsBottomSheetContext.Provider
-      value={useMemo(
-        () => ({ closeBottomSheet, isOpen: !!unsupported.length }),
-        [closeBottomSheet, unsupported]
-      )}
+      value={useMemo(() => ({ closeBottomSheet }), [closeBottomSheet])}
     >
       {children}
-      {!!unsupported.length && (
+      {!!unsupportedRaw.length && (
         <BottomSheet
           id="unsupportedDAppsBottomSheet"
           sheetRef={sheetRef}
@@ -87,26 +94,37 @@ const UnsupportedDAppsBottomSheetProvider: React.FC = ({ children }) => {
           initialIndex={0}
         >
           <View style={[spacings.mbTy, flexboxStyles.alignCenter]}>
-            <Title>{t('Unsupported dApp')}</Title>
+            <Title>{t('Unsupported dApps')}</Title>
           </View>
           <Text fontSize={12} style={spacings.mbTy}>
-            {t('This dApp does not fully support smart wallets and/or WalletConnect:')}
+            {t('These dApps do not fully support smart wallets and/or WalletConnect:')}
           </Text>
-          {unsupported.map(({ session }, i) => (
-            <UnsupportedDAppItem
-              // eslint-disable-next-line react/no-array-index-key
-              key={`dapp-${i}`}
-              name={session.peerMeta?.name}
-              icon={session.peerMeta?.icons?.[0]}
-              url={session.peerMeta?.url}
-            />
-          ))}
+          <View>
+            {unsupported.map(({ session }, i) => (
+              <UnsupportedDAppItem
+                // eslint-disable-next-line react/no-array-index-key
+                key={`dapp-${i}`}
+                name={session.peerMeta?.name}
+                icon={session.peerMeta?.icons?.[0]}
+                url={session.peerMeta?.url}
+              />
+            ))}
+            {unsupportedRaw.length > numOfItemsWhenNotExpanded && (
+              <TouchableOpacity
+                style={flexboxStyles.alignCenter}
+                hitSlop={{ top: 5, bottom: 5 }}
+                onPress={toggleItemsList}
+              >
+                <Text fontSize={12}>{isExpanded ? t('See less') : t('See more...')}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
           <View style={[spacings.pbMd, spacings.ptSm]}>
             <Trans>
               <Text>
                 <Text fontSize={12}>
                   {
-                    'For more information on why this dApp does not support Ambire, please read this '
+                    'For more information on why these dApps do not support Ambire, please read this '
                   }
                 </Text>
                 <Text
