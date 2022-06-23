ad581d3f520dbf1eb427d20673705274a7f5f8a5
superKalo <superkalo@devlabs.bg>
2022-06-23 13:20:18 +0300

    Add: a note

--- src/modules/common/hooks/useDisableHardwareBackPress/useDisableHardwareBackPress.tsx
+++ src/modules/common/hooks/useDisableHardwareBackPress/useDisableHardwareBackPress.tsx
@@ -3,7 +3,7 @@ import { BackHandler } from 'react-native'
 
 // Only disables the hardware back button action, which some devices have.
 // Don't forget to add `headerLeft: () => null` and `gestureEnabled: false`
-// on the screen settings, so that the back button in the header
+// on the screen settings, so that the back button in the header is also hidden
 const useDisableHardwareBackPress = () => {
   useEffect(() => {
     const backHandler = BackHandler.addEventListener('hardwareBackPress', () => true)
