c0a86797240b4522d84d22489bf9e6a501cb282a
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-07-07 12:32:53 +0300

    Add: modal info component to the gas tank screen

---
+++ src/modules/common/components/Modal/Backdrop.tsx
@@ -0,0 +1,50 @@
+import { BlurView } from 'expo-blur'
+import React from 'react'
+import { Animated, StyleSheet, TouchableOpacity } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import CloseIcon from '@assets/svg/CloseIcon'
+import { isiOS } from '@config/env'
+import NavIconWrapper from '@modules/common/components/NavIconWrapper'
+import { colorPalette as colors } from '@modules/common/styles/colors'
+
+import styles from './styles'
+
+const Backdrop = ({ onPress }: { onPress: () => void }) => {
+  const insets = useSafeAreaInsets()
+  // The header should start a little bit below the end of the notch,
+  // and right in the vertical middle of the nav.
+  const notchInset = insets.top + 10
+  return (
+    <TouchableOpacity style={styles.backDrop} activeOpacity={1} onPress={onPress}>
+      <NavIconWrapper onPress={onPress} style={[styles.closeBtn, { top: notchInset }]}>
+        <CloseIcon />
+      </NavIconWrapper>
+      {isiOS ? (
+        // The blurred view works on iOS only
+        <BlurView
+          intensity={55}
+          tint="dark"
+          style={[
+            StyleSheet.absoluteFillObject,
+            {
+              opacity: 0.98
+            }
+          ]}
+        />
+      ) : (
+        <Animated.View
+          style={[
+            StyleSheet.absoluteFillObject,
+            {
+              backgroundColor: colors.valhalla,
+              opacity: 0.8
+            }
+          ]}
+        />
+      )}
+    </TouchableOpacity>
+  )
+}
+
+export default React.memo(Backdrop)
---
+++ src/modules/common/components/Modal/styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet, ViewProps } from 'react-native'
+
+import { colorPalette as colors } from '@modules/common/styles/colors'
+import spacings, { DEVICE_HEIGHT, DEVICE_WIDTH } from '@modules/common/styles/spacings'
+import commonStyles from '@modules/common/styles/utils/common'
+
+interface Style {
+  container: ViewProps
+  closeBtn: ViewProps
+  backDrop: ViewProps
+}
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    ...commonStyles.borderRadiusPrimary,
+    backgroundColor: colors.clay,
+    ...spacings.pv,
+    ...spacings.ph
+  },
+  closeBtn: {
+    position: 'absolute',
+    right: 20,
+    zIndex: 2
+  },
+  backDrop: {
+    width: DEVICE_WIDTH,
+    height: DEVICE_HEIGHT,
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    zIndex: 1,
+    ...commonStyles.shadowPrimary
+  }
+})
+
+export default styles
