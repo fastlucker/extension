82707c15989092a87a7546ef7040db9cf4ac82f5
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-07-08 17:48:31 +0300

    Add: comment + types

--- src/config/Router/hooks/useGetSelectedRoute/useGetSelectedRoute.tsx
+++ src/config/Router/hooks/useGetSelectedRoute/useGetSelectedRoute.tsx
@@ -1,6 +1,12 @@
 import { useNavigationState } from '@react-navigation/native'
 
-const defaultRoute = 'dashboard'
+const defaultRoute: string = 'dashboard'
+
+// non-existent routes are navigator stacks containing the routes that represent an actual screen
+// these routes should be replaced with the default route on initial render
+// because initially the router doesn't render the whole tree of nested routes
+// thus looping through the routes tree ends up in some of those non-existent ones
+const nonExistentRoutes: string[] = ['drawer', 'tabs']
 
 const useGetSelectedRoute = () => {
   const state = useNavigationState((s) => s)
@@ -10,8 +16,10 @@ const useGetSelectedRoute = () => {
     route = route.state?.routes?.[route?.state?.index]
   }
 
-  const routeName =
-    !route.name || route.name === 'drawer' || route.name === 'tabs' ? defaultRoute : route.name
+  const routeName: string =
+    !route.name || nonExistentRoutes.some((r: string) => r === route.name)
+      ? defaultRoute
+      : route.name
 
   return {
     key: route.key,
