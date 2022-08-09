9541ee540542967f481b71e3dc2ca37258d2eb3e
superKalo <superkalo@devlabs.bg>
2022-08-09 17:59:53 +0300

    Change: refactor useRewards and simplify it

--- src/modules/dashboard/hooks/useRewards/useRewards.tsx
+++ src/modules/dashboard/hooks/useRewards/useRewards.tsx
@@ -3,23 +3,12 @@ import useRewardsCommon from 'ambire-common/src/hooks/useRewards'
 import CONFIG from '@config/env'
 import useAccounts from '@modules/common/hooks/useAccounts'
 import useRelayerData from '@modules/common/hooks/useRelayerData'
-import useClaimableWalletToken from '@modules/dashboard/hooks/useClaimableWalletToken'
 
-export default function useRewards() {
-  const { isLoading, errMsg, data, rewards, pendingTokensTotal, claimableWalletToken } =
-    useRewardsCommon({
-      relayerURL: CONFIG.RELAYER_URL,
-      useAccounts,
-      useClaimableWalletToken,
-      useRelayerData
-    })
+const useRewards = () =>
+  useRewardsCommon({
+    relayerURL: CONFIG.RELAYER_URL,
+    useAccounts,
+    useRelayerData
+  })
 
-  return {
-    isLoading,
-    errMsg,
-    data,
-    rewards,
-    pendingTokensTotal,
-    claimableWalletToken
-  }
-}
+export default useRewards
