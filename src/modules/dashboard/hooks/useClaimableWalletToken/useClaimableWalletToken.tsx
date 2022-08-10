6f02b015c4128ed11ddeaaf62744adda7884fbc8
superKalo <superkalo@devlabs.bg>
2022-08-10 16:09:07 +0300

    CHange: pass additional params to hook

--- src/modules/dashboard/hooks/useClaimableWalletToken/useClaimableWalletToken.tsx
+++ src/modules/dashboard/hooks/useClaimableWalletToken/useClaimableWalletToken.tsx
@@ -4,11 +4,12 @@ import useAccounts from '@modules/common/hooks/useAccounts'
 import useNetwork from '@modules/common/hooks/useNetwork'
 import useRequests from '@modules/common/hooks/useRequests'
 
-const useClaimableWalletToken = () =>
+const useClaimableWalletToken = (params) =>
   useClaimableWalletTokenCommon({
     useAccounts,
     useNetwork,
-    useRequests
+    useRequests,
+    ...params
   })
 
 export default useClaimableWalletToken
