76be3eabbd5791a3923a581167d54ba368294cbf
Kaloyan Kosev <superkalo@devlabs.bg>
2022-09-13 16:49:26 +0300

    Add: error view

--- src/modules/common/contexts/constantsContext/constantsContext.tsx
+++ src/modules/common/contexts/constantsContext/constantsContext.tsx
@@ -1,9 +1,18 @@
 import useFetchConstants, { ConstantsType } from 'ambire-common/src/hooks/useFetchConstants'
 import React, { createContext, useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 
+import Satellite from '@assets/svg/Satellite'
+import Button from '@modules/common/components/Button'
+import GradientBackgroundWrapper from '@modules/common/components/GradientBackgroundWrapper'
 import SafeAreaView from '@modules/common/components/SafeAreaView'
 import Spinner from '@modules/common/components/Spinner'
+import Text from '@modules/common/components/Text'
+import Title from '@modules/common/components/Title'
+import Wrapper from '@modules/common/components/Wrapper'
+import spacings from '@modules/common/styles/spacings'
 import flexboxStyles from '@modules/common/styles/utils/flexbox'
+import textStyles from '@modules/common/styles/utils/text'
 
 const ConstantsContext = createContext<{
   constants: ConstantsType | null
@@ -14,7 +23,22 @@ const ConstantsContext = createContext<{
 })
 
 const ConstantsProvider: React.FC = ({ children }) => {
+  const { t } = useTranslation()
   const { constants, isLoading } = useFetchConstants({ fetch })
+  // TODO: wire up
+  const hasError = false
+
+  const ErrorView = (
+    <GradientBackgroundWrapper>
+      <Wrapper contentContainerStyle={flexboxStyles.center}>
+        <Satellite style={spacings.mbLg} />
+        <Title style={textStyles.center}>{t("Can't connect to our server")}</Title>
+        <Text style={[spacings.mb, spacings.mhTy, textStyles.center]}>
+          {t('Something went wrong, but your funds are safe! Please try again later.')}
+        </Text>
+      </Wrapper>
+    </GradientBackgroundWrapper>
+  )
 
   return (
     <ConstantsContext.Provider
@@ -24,6 +48,8 @@ const ConstantsProvider: React.FC = ({ children }) => {
         <SafeAreaView style={[flexboxStyles.center, flexboxStyles.flex1]}>
           <Spinner />
         </SafeAreaView>
+      ) : hasError ? (
+        ErrorView
       ) : (
         children
       )}
