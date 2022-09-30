285ce736fa06e95ca87dec3d2b1b6e2ffe457127
Kaloyan Kosev <superkalo@devlabs.bg>
2022-09-30 17:10:56 +0300

    Fix: oops, provider name

--- src/modules/common/hooks/useConstants/useConstants.tsx
+++ src/modules/common/hooks/useConstants/useConstants.tsx
@@ -6,7 +6,7 @@ export default function useConstants() {
   const context = useContext(ConstantsContext)
 
   if (!context) {
-    throw new Error('useConstants must be used within an AccountsProvider')
+    throw new Error('useConstants must be used within an ConstantsProvider')
   }
 
   return context
