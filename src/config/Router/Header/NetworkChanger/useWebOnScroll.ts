ee5526021dbaf98855f2c86df4196482d01f8dc0
superKalo <superkalo@devlabs.bg>
2022-10-05 12:10:23 +0300

    Refactor: add missing types (and props)

--- src/config/Router/Header/NetworkChanger/useWebOnScroll.ts
+++ src/config/Router/Header/NetworkChanger/useWebOnScroll.ts
@@ -1,11 +1,19 @@
 import { useEffect, useRef } from 'react'
+import { NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
 
-const useWebOnScroll = ({ onScroll, onScrollEnd }) => {
-  const lastScrollEvent = useRef(null)
-  const scrollEndTimeout = useRef(null)
+interface Props {
+  onScroll: (event: NativeSyntheticEvent<NativeScrollEvent>) => any
+  onScrollEnd: (event: NativeSyntheticEvent<NativeScrollEvent>) => any
+}
 
-  const handleWebScroll = (event) => {
-    !!onScroll && onScroll(event)
+type ReturnType = (event: NativeSyntheticEvent<NativeScrollEvent>) => void
+
+const useWebOnScroll = ({ onScroll, onScrollEnd }: Props): ReturnType => {
+  const lastScrollEvent = useRef<null | number>(null)
+  const scrollEndTimeout = useRef<any>(null)
+
+  const handleWebScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    onScroll(event)
 
     const timestamp = Date.now()
 
@@ -18,7 +26,7 @@ const useWebOnScroll = ({ onScroll, onScrollEnd }) => {
       scrollEndTimeout.current = setTimeout(() => {
         if (lastScrollEvent.current === timestamp) {
           lastScrollEvent.current = null
-          !!onScrollEnd && onScrollEnd(event)
+          onScrollEnd(event)
         }
       }, 450)
     }
