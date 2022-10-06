374e039330e876df4027b9be1680003355271aba
superKalo <superkalo@devlabs.bg>
2022-10-06 12:57:06 +0300

    Add: a note

--- src/modules/extension/screens/PermissionRequestScreen/PermissionRequestScreen.tsx
+++ src/modules/extension/screens/PermissionRequestScreen/PermissionRequestScreen.tsx
@@ -1,3 +1,4 @@
+// For the mobile app, this screen doesn't need to exist. It's web only. See `PermissionRequestScreen.web.tsx`
 const PermissionRequestScreen = () => null
 
 export default PermissionRequestScreen
