2f7125d8dae8eb678a839a9fab0088aa92cc1d9d
superKalo <superkalo@devlabs.bg>
2022-10-14 15:38:52 +0300

    Change: no need to comment this out, since there is a flag that enables this for mobile only anyways

--- shim.js
+++ shim.js
@@ -38,6 +38,6 @@ if (typeof localStorage !== 'undefined') {
   localStorage.debug = isDev ? '*' : ''
 }
 
-// // If using the crypto shim, uncomment the following line to ensure
-// // crypto is loaded first, so it can populate global.crypto
-// require('crypto')
+// If using the crypto shim, uncomment the following line to ensure
+// crypto is loaded first, so it can populate global.crypto
+require('crypto')
