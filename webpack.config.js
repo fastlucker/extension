f4f00bb7f845f9eb4a130a2a5aa504887bb5c9a4
superKalo <superkalo@devlabs.bg>
2022-10-14 15:14:28 +0300

    Add: clarification for the alias used

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = async function (env, argv) {
     argv
   )
 
+  // Alias the 'react-native-webview' package, in order to add support
+  // (web implementation) of React Native's WebView. See:
+  // {@link https://github.com/react-native-web-community/react-native-web-webview}
   config.resolve.alias['react-native-webview'] = 'react-native-web-webview'
 
   return config
