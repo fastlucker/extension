eac0748b3e69283d3766adca79209e0c5f64acb7
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-10-17 16:46:52 +0300

    exclude the ambire wallet url from the web3 script injection

--- web/constants/browserAPI.js
+++ web/constants/browserAPI.js
@@ -1,6 +1,18 @@
-export const browserAPI = process.env.WEB_ENGINE === 'gecko' ? browser : chrome
+// modified to browser if the web engine is GECKO in the webpack config
+let browserAPI = null
+
+if (typeof browser !== 'undefined') {
+  browserAPI = browser
+}
+
+if (typeof chrome !== 'undefined') {
+  browserAPI = chrome
+}
 
 // FF compatibility
 if (process.env.WEB_ENGINE === 'gecko') {
   browserAPI.action = browserAPI.browserAction
 }
+
+
+export { browserAPI }
