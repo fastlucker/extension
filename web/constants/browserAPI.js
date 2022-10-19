d1f9ed9adf6feede507013d534b2d1a5ab3a892d
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-10-19 12:29:01 +0300

    Fix: dev server crashes

--- web/constants/browserAPI.js
+++ web/constants/browserAPI.js
@@ -1,6 +1,7 @@
 // modified to browser if the web engine is GECKO in the webpack config
 let browserAPI = null
 let engine = null
+let isExtension = false
 
 if (process.env.WEB_ENGINE === 'webkit') {
   browserAPI = chrome
@@ -13,5 +14,10 @@ if (process.env.WEB_ENGINE === 'gecko') {
   engine = 'gecko'
 }
 
+if (!!browserAPI && browserAPI.runtime && browserAPI.runtime.id) {
+  console.log(browserAPI.runtime.id)
+  isExtension = true
+}
 
-export { browserAPI, engine }
+
+export { browserAPI, engine, isExtension }
