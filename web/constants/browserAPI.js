97eeacb92b2cf49b99f228b6cefbd44780b2d12a
superKalo <superkalo@devlabs.bg>
2022-10-25 18:44:15 +0300

    Add: a note

--- web/constants/browserAPI.js
+++ web/constants/browserAPI.js
@@ -14,6 +14,8 @@ if (process.env.WEB_ENGINE === 'gecko') {
   engine = 'gecko'
 }
 
+// Code running in a Chrome extension (content script, background page, etc.)
+// {@link https://stackoverflow.com/a/22563123/1333836}
 if (!!browserAPI && browserAPI.runtime && browserAPI.runtime.id) {
   isExtension = true
 }
