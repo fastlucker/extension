a07b186c58eae074c98de30a1675774caa0c8f1c
superKalo <superkalo@devlabs.bg>
2022-10-26 16:10:24 +0300

    Add: Image component, compatible with both web and mobile

---
+++ src/modules/common/components/Image/Image.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import FastImage, { FastImageProps } from 'react-native-fast-image'
+
+const Image: React.ComponentType<FastImageProps> = (props) => <FastImage {...props} />
+
+export default Image
---
+++ src/modules/common/components/Image/Image.web.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Image as RnImage, ImageProps } from 'react-native'
+
+const Image: React.ComponentType<ImageProps> = (props) => <RnImage {...props} />
+
+export default Image
