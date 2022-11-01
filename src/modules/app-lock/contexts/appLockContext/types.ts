7e971da4e7d5133593a124c69c7f96ff76a1534f
superKalo <superkalo@devlabs.bg>
2022-11-01 17:14:19 +0200

    Change: re-order types + remove the obsolete ones

--- src/modules/app-lock/contexts/appLockContext/types.ts
+++ src/modules/app-lock/contexts/appLockContext/types.ts
@@ -1,49 +1,42 @@
-import { DEVICE_SECURITY_LEVEL, DEVICE_SUPPORTED_AUTH_TYPES, PASSCODE_STATES } from './constants'
+import { PASSCODE_STATES } from './constants'
 
 export interface AppLockContextReturnType {
   state: PASSCODE_STATES
-  deviceSecurityLevel: DEVICE_SECURITY_LEVEL
-  deviceSupportedAuthTypes: DEVICE_SUPPORTED_AUTH_TYPES[]
-  deviceSupportedAuthTypesLabel: string
-  fallbackSupportedAuthTypesLabel: string
-  setAppLockPin: (code: string) => Promise<boolean>
-  removeAppLock: (accountId?: string) => Promise<void>
   isLoading: boolean
+
   isValidPasscode: (code: string) => boolean
-  isLocalAuthSupported: null | boolean
+  triggerEnteringPasscode: () => void
+  resetValidPasscodeEntered: () => void
+  hasEnteredValidPasscode: boolean | null
+
+  setAppLockPin: (code: string) => Promise<boolean>
   // Be aware that the Promise should always return something for `setAppLockBiometrics`
   // and `isValidLocalAuth`, because Promise<void> makes the local auth to hang
   // on Android and always return `false`, without rejecting the promise,
   // which leads to strange results.
   setAppLockBiometrics: () => Promise<boolean>
+  removeAppLock: (accountId?: string) => Promise<void>
   removeAppLockBiometrics: () => void
-  triggerEnteringPasscode: () => void
-  resetValidPasscodeEntered: () => void
-  hasEnteredValidPasscode: boolean | null
+
+  lockOnStartup: boolean
+  lockWhenInactive: boolean
   enableLockOnStartup: () => void
   disableLockOnStartup: () => void
   enableLockWhenInactive: () => void
   disableLockWhenInactive: () => void
-  lockOnStartup: boolean
-  lockWhenInactive: boolean
 }
 
 export const appLockContextDefaults: AppLockContextReturnType = {
   state: PASSCODE_STATES.NO_PASSCODE,
-  deviceSecurityLevel: DEVICE_SECURITY_LEVEL.NONE,
-  deviceSupportedAuthTypes: [],
-  deviceSupportedAuthTypesLabel: '',
-  fallbackSupportedAuthTypesLabel: '',
-  setAppLockPin: () => Promise.resolve(false),
-  removeAppLock: () => Promise.resolve(),
   isLoading: true,
   isValidPasscode: () => false,
-  isLocalAuthSupported: null,
-  setAppLockBiometrics: () => Promise.resolve(false),
-  removeAppLockBiometrics: () => {},
   triggerEnteringPasscode: () => {},
   resetValidPasscodeEntered: () => {},
   hasEnteredValidPasscode: null,
+  setAppLockPin: () => Promise.resolve(false),
+  setAppLockBiometrics: () => Promise.resolve(false),
+  removeAppLock: () => Promise.resolve(),
+  removeAppLockBiometrics: () => {},
   enableLockOnStartup: () => {},
   disableLockOnStartup: () => {},
   enableLockWhenInactive: () => {},
