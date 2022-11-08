a3b283d89ebbdf82e6a218d6e8e04b97e9f7173b
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-11-08 11:32:34 +0200

    created useEOA hook

--- src/modules/external-signers/hooks/useExternalSigners/useExternalSigners.ts
+++ src/modules/external-signers/hooks/useExternalSigners/useExternalSigners.ts
@@ -1,16 +1,11 @@
-import accountPresets from 'ambire-common/src/constants/accountPresets'
-import { generateAddress2 } from 'ethereumjs-util'
 import { Wallet } from 'ethers'
-import { getAddress, hexZeroPad } from 'ethers/lib/utils'
 import { useCallback } from 'react'
 import { Keyboard } from 'react-native'
 
-import CONFIG, { isWeb } from '@config/env'
-import { getProxyDeployBytecode } from '@modules/auth/services/IdentityProxyDeploy'
-import useAccounts from '@modules/common/hooks/useAccounts'
+import { isWeb } from '@config/env'
+import useEOA from '@modules/common/hooks/useEOA'
 import useStorage from '@modules/common/hooks/useStorage'
 import useToast from '@modules/common/hooks/useToast'
-import { fetchPost } from '@modules/common/services/fetch'
 import { decrypt, encrypt } from '@modules/common/services/passworder'
 
 type AddSignerFormValues = {
@@ -21,119 +16,15 @@ type AddSignerFormValues = {
 
 const SIGNERS_KEY = 'externalSigners'
 
-const relayerURL = CONFIG.RELAYER_URL
-
 const useExternalSigners = () => {
-  const { onAddAccount } = useAccounts()
   const { addToast } = useToast()
+  const { onEOASelected } = useEOA()
   const [externalSigners, setExternalSigners] = useStorage<any>({
     key: SIGNERS_KEY,
     defaultValue: {}
   })
 
   const hasRegisteredPassword = !!Object.keys(externalSigners).length
-
-  const getAccountByAddr = useCallback(async (idAddr, signerAddr) => {
-    // In principle, we need these values to be able to operate in relayerless mode,
-    // so we just store them in all cases
-    // Plus, in the future this call may be used to retrieve other things
-    const { salt, identityFactoryAddr, baseIdentityAddr, bytecode } = await fetch(
-      `${relayerURL}/identity/${idAddr}`
-    ).then((r) => r.json())
-    if (!(salt && identityFactoryAddr && baseIdentityAddr && bytecode))
-      throw new Error(`Incomplete data from relayer for ${idAddr}`)
-    return {
-      id: idAddr,
-      salt,
-      identityFactoryAddr,
-      baseIdentityAddr,
-      bytecode,
-      signer: { address: signerAddr }
-    }
-  }, [])
-
-  const getOwnedByEOAs = useCallback(
-    async (eoas) => {
-      const allUniqueOwned: any = {}
-
-      await Promise.all(
-        eoas.map(async (signerAddr: any) => {
-          const resp = await fetch(
-            `${relayerURL}/identity/any/by-owner/${signerAddr}?includeFormerlyOwned=true`
-          )
-          const privEntries = Object.entries(await resp.json())
-          // discard the privileges value, we do not need it as we wanna add all accounts EVER owned by this eoa
-          // eslint-disable-next-line no-return-assign
-          privEntries.forEach(([id]) => (allUniqueOwned[id] = getAddress(signerAddr)))
-        })
-      )
-
-      return Promise.all(
-        Object.entries(allUniqueOwned).map(([id, signer]) => getAccountByAddr(id, signer))
-      )
-    },
-    [getAccountByAddr]
-  )
-
-  // EOA implementations = Externally Owned Account
-  // Add or create accounts from Trezor/Ledger/Metamask/etc.
-  const createFromEOA = useCallback(async (addr, signerType) => {
-    const privileges = [[getAddress(addr), hexZeroPad('0x01', 32)]]
-    const { salt, baseIdentityAddr, identityFactoryAddr } = accountPresets
-    const bytecode = getProxyDeployBytecode(baseIdentityAddr, privileges, { privSlot: 0 })
-    const identityAddr = getAddress(
-      `0x${generateAddress2(
-        // Converting to buffer is required in ethereumjs-util version: 7.1.3
-        Buffer.from(identityFactoryAddr.slice(2), 'hex'),
-        Buffer.from(salt.slice(2), 'hex'),
-        Buffer.from(bytecode.slice(2), 'hex')
-      ).toString('hex')}`
-    )
-
-    if (relayerURL) {
-      const createResp = await fetchPost(`${relayerURL}/identity/${identityAddr}`, {
-        salt,
-        identityFactoryAddr,
-        baseIdentityAddr,
-        privileges,
-        signerType
-      })
-      if (
-        !createResp.success &&
-        !(createResp.message && createResp.message.includes('already exists'))
-      )
-        throw createResp
-    }
-
-    return {
-      id: identityAddr,
-      salt,
-      identityFactoryAddr,
-      baseIdentityAddr,
-      bytecode,
-      signer: { address: getAddress(addr) }
-    }
-  }, [])
-
-  const onEOASelected = useCallback(
-    async (addr, signerExtra) => {
-      const addAccount = (acc, opts) => onAddAccount({ ...acc, signerExtra }, opts)
-      // when there is no relayer, we can only add the 'default' account created from that EOA
-      // @TODO in the future, it would be nice to do getLogs from the provider here to find out which other addrs we control
-      //   ... maybe we can isolate the code for that in lib/relayerless or something like that to not clutter this code
-      if (!relayerURL)
-        return addAccount(await createFromEOA(addr, signerExtra.type), { select: true })
-      // otherwise check which accs we already own and add them
-      const owned = await getOwnedByEOAs([addr])
-      if (!owned.length) {
-        addAccount(await createFromEOA(addr, signerExtra.type), { select: true, isNew: true })
-      } else {
-        addToast(`Found ${owned.length} existing accounts with signer ${addr}`, { timeout: 15000 })
-        owned.forEach((acc, i) => addAccount(acc, { select: i === 0 }))
-      }
-    },
-    [addToast, createFromEOA, getOwnedByEOAs, onAddAccount]
-  )
 
   // Add new Ambire account or login with an existing one
   // default signer of the account will be the passed external private key
