075b86b5c709a0ddfb3b288f8f1e06fec38fa1a6
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-11-08 18:20:29 +0200

    Remove browser-passworder package and replaced with crypto-js for both mobile and web

--- src/modules/common/services/passworder/passworder.web.ts
+++
@@ -1,7 +0,0 @@
-import passworder from 'browser-passworder'
-
-const encrypt = (password: string, data: any) => passworder.encrypt(password, data)
-
-const decrypt = (password: string, hash: string) => passworder.decrypt(password, hash)
-
-export { encrypt, decrypt }
