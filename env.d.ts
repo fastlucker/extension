7a0bf996bd8243e75def7a9bb67af4856b55416d
superKalo <superkalo@devlabs.bg>
2022-11-17 17:34:08 +0200

    Add: types for the env file

---
+++ env.d.ts
@@ -0,0 +1,18 @@
+declare module '@env' {
+  export const RPC_URL_ETHEREUM: string
+  export const RPC_URL_POLYGON: string
+  export const RPC_URL_AVALANCHE: string
+  export const RPC_URL_BNB_CHAIN: string
+  export const RPC_URL_FANTOM: string
+  export const RPC_URL_MOONBEAM: string
+  export const RPC_URL_MOONRIVER: string
+  export const RPC_URL_ARBITRUM: string
+  export const RPC_URL_GNOSIS: string
+  export const RPC_URL_KUCOIN: string
+  export const RPC_URL_OPTIMISM: string
+  export const RPC_URL_ANDROMEDA: string
+  export const RPC_URL_RINKEBY: string
+  export const RPC_URL_CRONOS: string
+  export const RPC_URL_AURORA: string
+  export const RPC_URL_ETHEREUM_POW: string
+}
