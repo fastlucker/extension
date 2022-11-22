9b85ce2c1e8b80b0fa371f6e358fee03c1c94bd6
superKalo <superkalo@devlabs.bg>
2022-11-22 18:45:08 +0200

    Change: use only for webkit, since gecko does not support this yet

--- webpack.config.js
+++ webpack.config.js
@@ -34,12 +34,14 @@ module.exports = async function (env, argv) {
     // Maintain the same versioning between the web extension and the mobile app
     manifest.version = appJSON.expo.version
 
-    // This value can be used to control the unique ID of an extension,
-    // when it is loaded during development. In prod, the ID is generated
-    // in Chrome Web Store and can't be changed.
-    // {@link https://developer.chrome.com/extensions/manifest/key}
-    // TODO: Figure out if this works for gecko
-    manifest.key = process.env.BROWSER_EXTENSION_KEY_DEV
+    if (process.env.WEB_ENGINE === 'webkit') {
+      // This value can be used to control the unique ID of an extension,
+      // when it is loaded during development. In prod, the ID is generated
+      // in Chrome Web Store and can't be changed.
+      // {@link https://developer.chrome.com/extensions/manifest/key}
+      // TODO: Figure out if this works for gecko
+      manifest.key = process.env.BROWSER_EXTENSION_KEY_DEV
+    }
 
     // Tweak manifest file, so it's compatible with gecko extensions specifics
     if (process.env.WEB_ENGINE === 'gecko') {
