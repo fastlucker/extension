b2ffe4f185423789ea434ca2430a641201d1f64d
superKalo <superkalo@devlabs.bg>
2022-11-30 19:27:00 +0200

    Change: wire-up the storage context vs the sync storage

--- src/modules/common/hooks/useStorage/useStorage.tsx
+++ src/modules/common/hooks/useStorage/useStorage.tsx
@@ -1,6 +1,9 @@
 import useStorage, { UseStorageProps } from 'ambire-common/src/hooks/useStorage'
+import { useContext } from 'react'
 
-import { SyncStorage } from '@config/storage'
+import { StorageContext } from '@modules/common/contexts/storageContext'
+
+// import { SyncStorage } from '@config/storage'
 
 export default function useSyncStorage<ValueType>({
   key,
@@ -8,8 +11,12 @@ export default function useSyncStorage<ValueType>({
   isStringStorage,
   setInit
 }: Omit<UseStorageProps<ValueType>, 'storage'>) {
+  // TODO: Move to another hook maybe?
+  const storageContext = useContext(StorageContext)
+
   return useStorage<ValueType>({
-    storage: SyncStorage,
+    // storage: SyncStorage,
+    storage: storageContext,
     key,
     defaultValue,
     isStringStorage,
