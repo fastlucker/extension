aab13bb06e33b0d49452f0a8a178c0d996791de5
superKalo <superkalo@devlabs.bg>
2022-12-02 12:51:35 +0200

    Change: no longer needed

--- src/modules/common/contexts/storageContext/storageContext.tsx
+++ src/modules/common/contexts/storageContext/storageContext.tsx
@@ -28,26 +28,6 @@ const StorageProvider: React.FC = ({ children }) => {
     }
   }, [storageControllerInstance])
 
-  // TODO: Figure out if this is still needed
-  // Syncs browser async storage with the extension's local storage
-  // Browser local storage used mainly in the extension's background service
-  // useMMKVListener(async (key) => {
-  //   const store: any = await getStore()
-  //   const value = SyncStorage.getItem(key)
-  //   let parsedValue
-  //   try {
-  //     parsedValue = JSON.parse(value as string)
-  //   } catch (e) {
-  //     parsedValue = value
-  //   }
-  //   store[key] = parsedValue
-  //   // local storage keeps only the networkId
-  //   // but the background service needs the whole network object
-  //   store.network = network || {}
-  //   store.selectedAcc = selectedAccount || ''
-  //   browserAPI?.storage?.local?.set(store)
-  // })
-
   const getItem = useCallback(
     (key: string) => storageControllerInstance.getItem(key),
     [storageControllerInstance]
