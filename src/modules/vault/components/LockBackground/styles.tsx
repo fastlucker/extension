5f85ab248eb467e2d2065cb81b5c18fb37e7c2d4
superKalo <superkalo@devlabs.bg>
2022-12-12 13:13:07 +0200

    Change: adjust mobile vs web

--- src/modules/vault/components/LockBackground/styles.tsx
+++ src/modules/vault/components/LockBackground/styles.tsx
@@ -1,10 +1,15 @@
 import { StyleSheet, ViewStyle } from 'react-native'
 
+import { isWeb } from '@config/env'
 import { HEADER_HEIGHT } from '@config/Router/Header/style'
 
 interface Style {
   backgroundImgWrapper: ViewStyle
 }
+
+// Bump up the negative top part more than the header height,
+// so that it bleeds out of the top part of the screen more.
+const TOP_OFFSET = isWeb ? 30 : 85
 
 const styles = StyleSheet.create<Style>({
   backgroundImgWrapper: {
@@ -13,9 +18,7 @@ const styles = StyleSheet.create<Style>({
     alignItems: 'center',
     justifyContent: 'center',
     zIndex: 0,
-    // Bump up the negative top part more than the header height,
-    // so that it bleeds out of the top part of the screen more.
-    top: -(HEADER_HEIGHT + 85),
+    top: -(HEADER_HEIGHT + TOP_OFFSET),
     paddingLeft: 2,
     alignSelf: 'center'
   }
