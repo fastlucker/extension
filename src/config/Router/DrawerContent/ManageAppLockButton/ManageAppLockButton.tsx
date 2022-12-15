283fb3312f0b3c83c27e56b532192b75444c01f5
superKalo <superkalo@devlabs.bg>
2022-12-15 11:48:10 +0200

    Change: wire-up manage app lock and remove oboslete logic

--- src/config/Router/DrawerContent/ManageAppLockButton/ManageAppLockButton.tsx
+++ src/config/Router/DrawerContent/ManageAppLockButton/ManageAppLockButton.tsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from 'react'
-import { TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
 
 import { useTranslation } from '@config/localization'
-import { APP_LOCK_STATES } from '@modules/app-lock/contexts/appLockContext/constants'
-import useAppLock from '@modules/app-lock/hooks/useAppLock'
-import Spinner from '@modules/common/components/Spinner'
 import Text from '@modules/common/components/Text'
 import colors from '@modules/common/styles/colors'
 import spacings from '@modules/common/styles/spacings'
-import { useIsFocused } from '@react-navigation/native'
 
 interface Props {
   handleNavigate: (route: string) => void
@@ -16,35 +12,9 @@ interface Props {
 
 const ManageAppLockButton: React.FC<Props> = ({ handleNavigate }) => {
   const { t } = useTranslation()
-  const isFocused = useIsFocused()
-  const {
-    isLoading,
-    lockState,
-    triggerEnteringPasscode,
-    hasEnteredValidPasscode,
-    resetValidPasscodeEntered
-  } = useAppLock()
-
-  useEffect(() => {
-    if (hasEnteredValidPasscode && isFocused) {
-      handleNavigate('manage-app-locking')
-      resetValidPasscodeEntered()
-    }
-  }, [handleNavigate, resetValidPasscodeEntered, hasEnteredValidPasscode, isFocused])
-
-  if (isLoading)
-    return (
-      <View style={spacings.mv}>
-        <Spinner />
-      </View>
-    )
-
-  if (lockState === APP_LOCK_STATES.UNLOCKED) {
-    return null
-  }
 
   return (
-    <TouchableOpacity onPress={triggerEnteringPasscode}>
+    <TouchableOpacity onPress={() => handleNavigate('manage-app-locking')}>
       <Text style={spacings.mbSm} color={colors.titan_50}>
         {t('Manage app lock')}
       </Text>
--- src/modules/app-lock/screens/ManageAppLockScreen/ManageAppLockScreen.tsx
+++ src/modules/app-lock/screens/ManageAppLockScreen/ManageAppLockScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { useTranslation } from '@config/localization'
-import { APP_LOCK_STATES } from '@modules/app-lock/contexts/appLockContext/constants'
 import useAppLock from '@modules/app-lock/hooks/useAppLock'
 import Button from '@modules/common/components/Button'
 import GradientBackgroundWrapper from '@modules/common/components/GradientBackgroundWrapper'
@@ -15,97 +14,29 @@ import useToast from '@modules/common/hooks/useToast'
 import alert from '@modules/common/services/alert'
 import spacings from '@modules/common/styles/spacings'
 import flexboxStyles from '@modules/common/styles/utils/flexbox'
-import { useNavigation } from '@react-navigation/native'
 
 import styles from './styles'
 
 const ManageAppLockScreen = () => {
   const { t } = useTranslation()
-  const navigation = useNavigation()
-  const { addToast } = useToast()
-  const {
-    lockState,
-    removeAppLock,
-    lockOnStartup,
-    lockWhenInactive,
-    enableLockOnStartup,
-    disableLockOnStartup,
-    enableLockWhenInactive,
-    disableLockWhenInactive
-  } = useAppLock()
-  const { hasBiometricsHardware } = useBiometrics()
-
-  const handleOnRemoveAppLock = () =>
-    alert(
-      t('Remove app lock?'),
-      t(
-        'By removing the app lock you can open the Ambire app without PIN or biometrics unlock needed'
-      ),
-      [
-        {
-          text: t('Yes, remove'),
-          onPress: async () => {
-            await removeAppLock()
-
-            addToast(t('App lock removed!') as string, { timeout: 5000 })
-            navigation.navigate('dashboard')
-          },
-          style: 'destructive'
-        },
-        {
-          text: t('Cancel'),
-          style: 'cancel'
-        }
-      ]
-    )
+  const { lockWhenInactive, enableLockWhenInactive, disableLockWhenInactive } = useAppLock()
 
   const renderContent = () => {
-    if (lockState === APP_LOCK_STATES.UNLOCKED) {
-      return (
-        <>
-          <TextWarning>{t('In order to manage it, first you need to set app lock.')}</TextWarning>
-          <Button text={t('Set app lock')} onPress={() => navigation.navigate('set-app-lock')} />
-        </>
-      )
-    }
-
     return (
-      <>
-        <Panel
-          type="filled"
-          contentContainerStyle={styles.appLockingItemContainer}
-          style={spacings.mbTy}
-        >
-          <Text fontSize={16} weight="regular" numberOfLines={1} style={flexboxStyles.flex1}>
-            {t('Lock on startup')}
-          </Text>
-          <Toggle
-            isOn={lockOnStartup}
-            label={lockOnStartup ? t('Enabled') : t('Disabled')}
-            onToggle={lockOnStartup ? disableLockOnStartup : enableLockOnStartup}
-          />
-        </Panel>
-        <Panel
-          type="filled"
-          contentContainerStyle={styles.appLockingItemContainer}
-          style={spacings.mb}
-        >
-          <Text fontSize={16} weight="regular" numberOfLines={1} style={flexboxStyles.flex1}>
-            {t('Lock when inactive')}
-          </Text>
-          <Toggle
-            isOn={lockWhenInactive}
-            label={lockWhenInactive ? t('Enabled') : t('Disabled')}
-            onToggle={lockWhenInactive ? disableLockWhenInactive : enableLockWhenInactive}
-          />
-        </Panel>
-        <Button
-          type="secondary"
-          text={hasBiometricsHardware ? t('Change PIN or biometrics') : t('Change PIN')}
-          onPress={() => navigation.navigate('set-app-lock')}
+      <Panel
+        type="filled"
+        contentContainerStyle={styles.appLockingItemContainer}
+        style={spacings.mb}
+      >
+        <Text fontSize={16} weight="regular" numberOfLines={1} style={flexboxStyles.flex1}>
+          {t('Lock when inactive')}
+        </Text>
+        <Toggle
+          isOn={lockWhenInactive}
+          label={lockWhenInactive ? t('Enabled') : t('Disabled')}
+          onToggle={lockWhenInactive ? disableLockWhenInactive : enableLockWhenInactive}
         />
-        <Button type="secondary" text={t('Remove app lock')} onPress={handleOnRemoveAppLock} />
-      </>
+      </Panel>
     )
   }
 
