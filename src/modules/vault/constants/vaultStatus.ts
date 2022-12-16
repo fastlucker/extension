849a482f22b579964ffaca84cb1f989514efcafe
superKalo <superkalo@devlabs.bg>
2022-12-16 17:24:18 +0200

    Add: note

--- src/modules/vault/constants/vaultStatus.ts
+++ src/modules/vault/constants/vaultStatus.ts
@@ -2,7 +2,11 @@
 export enum VAULT_STATUS {
   LOADING = 'loading',
   NOT_INITIALIZED = 'not-initialized',
+  // Initially locked, so it pups the <VaultStack /> and doesn't render
+  // anything else until vault gets unlocked
   LOCKED = 'locked',
+  // Locked temporarily, when the app goes to background (inactive),
+  // but it does not unmount the main app stack.
   LOCKED_TEMPORARILY = 'locked-temporarily',
   UNLOCKED = 'unlocked'
 }
