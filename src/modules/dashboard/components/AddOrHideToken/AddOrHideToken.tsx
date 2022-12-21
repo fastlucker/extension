566bdfb3b1b77c0827d2fac784488cad7088416e
superKalo <superkalo@devlabs.bg>
2022-12-21 18:18:17 +0200

    Fix: disable animation for Android, it breaks the bottom sheet backdrop

--- src/modules/dashboard/components/AddOrHideToken/AddOrHideToken.tsx
+++ src/modules/dashboard/components/AddOrHideToken/AddOrHideToken.tsx
@@ -84,7 +84,9 @@ const AddOrHideToken = ({
           onChange={(value: MODES) => {
             setFormType(value)
             triggerLayoutAnimation({
-              forceAnimate: true,
+              // Animate only for iOS, because on Android it conflicts with
+              // the bottom sheet backdrop animation.
+              forceAnimate: false,
               config: LayoutAnimation.create(300, 'linear', 'opacity')
             })
           }}
