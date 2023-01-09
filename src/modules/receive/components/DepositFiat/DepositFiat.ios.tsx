b1d097ce63b036ea35042b081147761b73d8c2ba
superKalo <superkalo@devlabs.bg>
2023-01-09 18:56:09 +0200

    Change: exclude from the bundle by including an empty DepositFiat.ios.tsx

---
+++ src/modules/receive/components/DepositFiat/DepositFiat.ios.tsx
@@ -0,0 +1,5 @@
+// TODO: Temporary disabled for iOS since v1.6.0 as part of the Apple app review feedback
+// Also excluded from the bundle by including an empty DepositFiat.ios.tsx */}
+const DepositFiat = () => null
+
+export default DepositFiat
