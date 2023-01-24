11412d65b2b885269dcd34c17a47f8b339ab14cc
superKalo <superkalo@devlabs.bg>
2023-01-24 14:29:07 +0200

    Change: oboslete logic

--- web/background/wallet.ts
+++ web/background/wallet.ts
@@ -1,18 +1,5 @@
 import permissionService from '@web/background/services/permission'
 
-export type WalletControllerType = Object.Merge<
-  {
-    [key in keyof WalletControllerClass]: WalletControllerClass[key] extends (
-      ...args: infer ARGS
-    ) => infer RET
-      ? <T extends IExtractFromPromise<RET> = IExtractFromPromise<RET>>(
-          ...args: ARGS
-        ) => Promise<IExtractFromPromise<T>>
-      : WalletControllerClass[key]
-  },
-  Record<string, <T = any>(...params: any) => Promise<T>>
->
-
 export class WalletController {
   isUnlocked = () => true
 
