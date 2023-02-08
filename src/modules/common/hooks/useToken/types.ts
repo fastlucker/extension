d48d163c409d7674b7a524994307dbacbf503efc
superKalo <superkalo@devlabs.bg>
2023-02-08 12:29:24 +0200

    Fix: not needed anymore

--- src/modules/common/hooks/useToken/types.ts
+++ src/modules/common/hooks/useToken/types.ts
@@ -1,10 +1,5 @@
 import { Token } from 'ambire-common/src/hooks/usePortfolio'
 
-import { MODES } from '@modules/dashboard/components/AddOrHideToken/constants'
-
 export type UseTokenReturnType = {
-  getTokenDetails: (
-    tokenAddress: string,
-    checkIfAlreadyHandledAgainst: MODES
-  ) => Promise<Token | null>
+  getTokenDetails: (tokenAddress: string) => Promise<Token | null>
 }
