0b7ec1437f214ee072b2f4bf11762458a8171171
superKalo <superkalo@devlabs.bg>
2023-02-16 11:31:43 +0200

    Add: handle url click + spacings

--- src/modules/settings/screens/DataDeletionPolicyScreen/DataDeletionPolicyScreen.tsx
+++ src/modules/settings/screens/DataDeletionPolicyScreen/DataDeletionPolicyScreen.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Linking } from 'react-native'
 
 import Button from '@modules/common/components/Button'
 import GradientBackgroundWrapper from '@modules/common/components/GradientBackgroundWrapper'
@@ -7,14 +8,19 @@ import Text from '@modules/common/components/Text'
 import Wrapper from '@modules/common/components/Wrapper'
 import spacings from '@modules/common/styles/spacings'
 
+const MANAGE_IDENTIFIABLE_DATA_LINK = 'https://wallet.ambire.com/#/wallet/security'
+
 const DataDeletionPolicy = () => {
   const { t } = useTranslation()
+
+  const handleLinkPress = useCallback(() => Linking.openURL(MANAGE_IDENTIFIABLE_DATA_LINK), [])
 
   return (
     <GradientBackgroundWrapper>
       <Wrapper style={spacings.mt}>
         <Text style={spacings.mbSm}>
-          {t('Ambire accounts are blockchain accounts and they contain no identifiable data.')}
+          <Text>{t('Ambire accounts are blockchain accounts and they contain ')}</Text>
+          <Text weight="regular">{t('no identifiable data.')}</Text>
         </Text>
         <Text style={spacings.mbSm}>
           {t(
@@ -28,10 +34,14 @@ const DataDeletionPolicy = () => {
         </Text>
         <Text style={spacings.mbSm}>
           {t(
-            'There is a procedure to delete the email and it can be securely done via accessing the web dashboard at http://wallet.ambire.com , from the security page: 1) add a secondary signer key and 2) Remove the email signer key (the only identifiable data there is).'
+            'There is a procedure to delete the email and it can be securely done via accessing the Ambire web app. From the security page:'
           )}
         </Text>
-        <Button text={t('Manage account data')} />
+        <Text style={spacings.mbSm}>{t('1) Add a secondary signer key and')}</Text>
+        <Text style={spacings.mbLg}>
+          {t('2) Remove the email signer key\n(the only identifiable data there is).')}
+        </Text>
+        <Button text={t('Manage identifiable data')} onPress={handleLinkPress} />
       </Wrapper>
     </GradientBackgroundWrapper>
   )
