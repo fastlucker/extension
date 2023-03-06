f9765461fc054b8245c6c7e120d05ca47466ebfc
superKalo <superkalo@devlabs.bg>
2023-03-06 16:45:24 +0200

    Add: aave rpc urls

--- web/inpage/inpage.ts
+++ web/inpage/inpage.ts
@@ -18,6 +18,16 @@ const DAPP_PROVIDER_URLS = {
     [NETWORKS.polygon]: 'https://polygon-mainnet.infura.io/v3/099fc58e0de9451d80b18d7c74caa7c1',
     [NETWORKS.optimism]: 'https://optimism-mainnet.infura.io/v3/099fc58e0de9451d80b18d7c74caa7c1',
     [NETWORKS.arbitrum]: 'https://arbitrum-mainnet.infura.io/v3/099fc58e0de9451d80b18d7c74caa7c1'
+  },
+  'app.aave.com': {
+    [NETWORKS.ethereum]: 'https://rpc.ankr.com/eth',
+    [NETWORKS.arbitrum]: ' https://arb1.arbitrum.io/rpc',
+    [NETWORKS.avalanche]:
+      'https://avax-mainnet.gateway.pokt.network/v1/lb/62b3314e123e6f00397f19ca/ext/bc/C/rpc',
+    [NETWORKS.fantom]: 'https://fantom-mainnet.gateway.pokt.network/v1/lb/62b3314e123e6f00397f19ca',
+    [NETWORKS.optimism]:
+      'https://optimism-mainnet.gateway.pokt.network/v1/lb/62b3314e123e6f00397f19ca',
+    [NETWORKS.polygon]: 'https://poly-mainnet.gateway.pokt.network/v1/lb/62b3314e123e6f00397f19ca'
   }
 }
 
