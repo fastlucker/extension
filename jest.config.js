8e3b8b63a5f6f3e9dd0be909a471c438e055c85d
superKalo <superkalo@devlabs.bg>
2023-03-08 15:53:44 +0200

    Add: jest, puppeteer and jest-puppeteer configuratio (with compatible versions)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'jest-puppeteer',
+  // Longer timeout than the default one needed for heavier dapps, like https://myetherwallet.com,
+  // otherwise, tests fail because the default timeout gets reached.
+  testTimeout: 30000,
+  transformIgnorePatterns: [
+    // Include the "ambire-common" package files in the ignore, since these
+    // files are not compiled and written in TypeScript. So they must be
+    // included in the compile process.
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|ambire-common)'
+  ]
 }
