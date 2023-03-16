e1aca371e2c1ad0f26d9c8cb3407329ca0d604b6
superKalo <superkalo@devlabs.bg>
2023-03-16 12:47:55 +0200

    Change: revert to mobile sprcific get started screen

--- src/common/modules/vault/screens/VaultSetupGetStartedScreen/VaultSetupGetStartedScreen.tsx
+++ src/common/modules/vault/screens/VaultSetupGetStartedScreen/VaultSetupGetStartedScreen.tsx
@@ -13,60 +13,52 @@ import { ROUTES } from '@common/modules/router/config/routesConfig'
 import colors from '@common/styles/colors'
 import spacings from '@common/styles/spacings'
 import flexboxStyles from '@common/styles/utils/flexbox'
-import AuthLayoutWrapper from '@web/components/AuthLayoutWrapper'
 
 const VaultSetupGetStartedScreen = () => {
   const { t } = useTranslation()
   const { navigate } = useNavigation()
 
   return (
-    <AuthLayoutWrapper>
-      <AuthLayoutWrapper.MainContent>
-        <GradientBackgroundWrapper>
-          <Wrapper
-            contentContainerStyle={spacings.pbLg}
-            type={WRAPPER_TYPES.KEYBOARD_AWARE_SCROLL_VIEW}
-            extraHeight={220}
-          >
-            <AmbireLogo />
-            <View style={[isWeb && spacings.ph, flexboxStyles.flex1, flexboxStyles.justifyEnd]}>
-              <View style={[spacings.mbLg, spacings.phTy]}>
-                <Text weight="light" style={spacings.mbTy} color={colors.titan} fontSize={13}>
-                  {t('Welcome to the {{name}}. Let’s set up your Key Store passphrase.', {
-                    name: isWeb ? t('Ambire Wallet extension') : t('Ambire Wallet')
-                  })}
-                </Text>
-                <Text weight="light" style={spacings.mbTy} color={colors.titan} fontSize={13}>
-                  {t(
-                    '1.  Ambire Key Store will protect your Ambire wallet with email password or external signer on this device.'
-                  )}
-                </Text>
-                <Text weight="light" style={spacings.mbTy} color={colors.titan} fontSize={13}>
-                  {t(
-                    '2.  First, pick your Ambire Key Store passphrase. It is unique for this device and it should be different from your account password.'
-                  )}
-                </Text>
-                <Text weight="light" color={colors.titan} fontSize={13}>
-                  {t(
-                    '3.  You will use your passphrase to unlock the {{name}} and sign transactions on this device.',
-                    { name: isWeb ? t('Ambire extension') : t('Ambire Wallet') }
-                  )}
-                </Text>
-              </View>
+    <GradientBackgroundWrapper>
+      <Wrapper
+        contentContainerStyle={spacings.pbLg}
+        type={WRAPPER_TYPES.KEYBOARD_AWARE_SCROLL_VIEW}
+        extraHeight={220}
+      >
+        <AmbireLogo />
+        <View style={[isWeb && spacings.ph, flexboxStyles.flex1, flexboxStyles.justifyEnd]}>
+          <View style={[spacings.mbLg, spacings.phTy]}>
+            <Text weight="light" style={spacings.mbTy} color={colors.titan} fontSize={13}>
+              {t('Welcome to the {{name}}. Let’s set up your Key Store passphrase.', {
+                name: isWeb ? t('Ambire Wallet extension') : t('Ambire Wallet')
+              })}
+            </Text>
+            <Text weight="light" style={spacings.mbTy} color={colors.titan} fontSize={13}>
+              {t(
+                '1.  Ambire Key Store will protect your Ambire wallet with email password or external signer on this device.'
+              )}
+            </Text>
+            <Text weight="light" style={spacings.mbTy} color={colors.titan} fontSize={13}>
+              {t(
+                '2.  First, pick your Ambire Key Store passphrase. It is unique for this device and it should be different from your account password.'
+              )}
+            </Text>
+            <Text weight="light" color={colors.titan} fontSize={13}>
+              {t(
+                '3.  You will use your passphrase to unlock the {{name}} and sign transactions on this device.',
+                { name: isWeb ? t('Ambire extension') : t('Ambire Wallet') }
+              )}
+            </Text>
+          </View>
 
-              <Button
-                style={spacings.mt}
-                text={t('Get Started')}
-                onPress={() => navigate(ROUTES.createVault, { replace: true })}
-              />
-            </View>
-          </Wrapper>
-        </GradientBackgroundWrapper>
-      </AuthLayoutWrapper.MainContent>
-      <AuthLayoutWrapper.SideContent>
-        <Text>Hello</Text>
-      </AuthLayoutWrapper.SideContent>
-    </AuthLayoutWrapper>
+          <Button
+            style={spacings.mt}
+            text={t('Get Started')}
+            onPress={() => navigate(ROUTES.createVault, { replace: true })}
+          />
+        </View>
+      </Wrapper>
+    </GradientBackgroundWrapper>
   )
 }
 
