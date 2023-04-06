b0a1f67fbed1c07087836d276056942de8ff857c
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2023-04-06 12:33:32 +0300

    remove log

--- src/common/hooks/useRoute/useRoute.tsx
+++ src/common/hooks/useRoute/useRoute.tsx
@@ -4,11 +4,7 @@ import { UseRouteReturnType } from './types'
 
 const useRoute = (): UseRouteReturnType => {
   const route = navigationRef.current.getCurrentRoute()
-console.log({
-  ...route,
-  params: route?.params || {},
-  path: route?.name
-})
+
   return {
     ...route,
     params: route?.params || {},
