7f614485196c89727d958a912c3a7ad2a1ae4137
superKalo <superkalo@devlabs.bg>
2023-04-13 12:45:31 +0300

    Add: wire-up dummy provider (for testing purposes)

--- src/mobile/modules/web3/screens/Web3BrowserScreen/Web3BrowserScreen.tsx
+++ src/mobile/modules/web3/screens/Web3BrowserScreen/Web3BrowserScreen.tsx
@@ -13,6 +13,54 @@ import injectionScript from '@mobile/modules/web3/services/webview-inpage/inject
 
 import styles from './styles'
 
+const dummyProvider = `
+  alert('Dummy provider injected!')
+  window.ReactNativeWebView = window.ReactNativeWebView || {};
+
+  window.ReactNativeWebView.postMessage = function (message) {
+    window.ReactNativeWebView.postMessage(JSON.stringify(message));
+  };
+
+  function handleRequest(args) {
+    alert('Request received: ' + JSON.stringify(args));
+    switch (args.method) {
+      case 'eth_accounts':
+        return ['0x742d35Cc6634C0532925a3b844Bc454e4438f44e'];
+      default:
+        throw new Error('Method not supported: ' + args.method);
+    }
+  }
+
+  const customProvider = {
+    isMetaMask: false,
+    isConnected: function () {
+      return true
+    },
+    request: async function (args) {
+      switch (args.method) {
+        case 'eth_accounts':
+          return ['0xdd2a7Dc3d038b5EA4164D41B3617aDa5eb4179bf']
+        case 'eth_chainId':
+          return '0x1'
+        case 'net_version':
+          return '1'
+        case 'eth_requestAccounts':
+          return ['0xdd2a7Dc3d038b5EA4164D41B3617aDa5eb4179bf']
+        case 'personal_sign':
+          return '0xYourSignedMessage'
+        case 'eth_sendTransaction':
+          return '0xYourTransactionHash'
+        // Add more methods as needed
+        default:
+          throw new Error('Method ' + args.method + ' not supported')
+      }
+    }
+  };
+
+  window.ethereum = customProvider;
+  true;
+`
+
 const Web3BrowserScreen = () => {
   const [scriptToInject, setScriptToInject] = useState<any>(null)
 
@@ -37,30 +85,28 @@ const Web3BrowserScreen = () => {
   const selectedDappUrl = route?.params?.selectedDappUrl
 
   return (
-    <>
-      <GradientBackgroundWrapper>
-        <Wrapper style={spacings.ph0} hasBottomTabNav>
-          {!!selectedDappUrl && (
-            <WebView
-              ref={webViewRef}
-              source={{ uri: selectedDappUrl }}
-              onMessage={handleEthereumProviderMessage}
-              injectedJavaScript={injectionScript}
-              // TODO:
-              // injectedJavaScript={scriptToInject}
-              startInLoadingState
-              renderLoading={() => (
-                <View style={styles.loadingWrapper}>
-                  <Spinner />
-                </View>
-              )}
-              containerStyle={styles.container}
-              style={styles.webview}
-            />
-          )}
-        </Wrapper>
-      </GradientBackgroundWrapper>
-    </>
+    <GradientBackgroundWrapper>
+      <Wrapper style={spacings.ph0} hasBottomTabNav>
+        {!!selectedDappUrl && (
+          <WebView
+            ref={webViewRef}
+            source={{ uri: selectedDappUrl }}
+            onMessage={handleEthereumProviderMessage}
+            injectedJavaScriptBeforeContentLoaded={dummyProvider}
+            // TODO:
+            // injectedJavaScript={scriptToInject}
+            startInLoadingState
+            renderLoading={() => (
+              <View style={styles.loadingWrapper}>
+                <Spinner />
+              </View>
+            )}
+            containerStyle={styles.container}
+            style={styles.webview}
+          />
+        )}
+      </Wrapper>
+    </GradientBackgroundWrapper>
   )
 }
 
