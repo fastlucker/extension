9b87b1e1c90b25e511311819937047b5acc38d55
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2023-05-04 14:19:40 +0300

    Add: set dapp as favorite

--- src/mobile/modules/web3/screens/DappsCatalogScreen/DappsCatalogScreen.tsx
+++ src/mobile/modules/web3/screens/DappsCatalogScreen/DappsCatalogScreen.tsx
@@ -1,6 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Keyboard, StyleSheet, TouchableOpacity, View } from 'react-native'
+import {
+  Keyboard,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native'
 import { useModalize } from 'react-native-modalize'
 import { SvgUri } from 'react-native-svg'
 
@@ -8,6 +14,7 @@ import CheckIcon from '@common/assets/svg/CheckIcon'
 import ManifestFallbackIcon from '@common/assets/svg/ManifestFallbackIcon'
 import SearchIcon from '@common/assets/svg/SearchIcon'
 import SortIcon from '@common/assets/svg/SortIcon'
+import StarIcon from '@common/assets/svg/StarIcon'
 import BottomSheet from '@common/components/BottomSheet'
 import FastImage from '@common/components/FastImage'
 import GradientBackgroundWrapper from '@common/components/GradientBackgroundWrapper'
@@ -17,6 +24,7 @@ import Spinner from '@common/components/Spinner'
 import Text from '@common/components/Text'
 import Title from '@common/components/Title'
 import Wrapper, { WRAPPER_TYPES } from '@common/components/Wrapper'
+import { isWeb } from '@common/config/env'
 import useNavigation from '@common/hooks/useNavigation'
 import useNetwork from '@common/hooks/useNetwork'
 import { ROUTES } from '@common/modules/router/constants/common'
@@ -32,8 +40,16 @@ import styles from './styles'
 const DappsCatalogScreen = () => {
   const { navigate } = useNavigation()
   const { network } = useNetwork()
-  const { filteredCatalog, search, categories, categoryFilter, onSearchChange, onCategorySelect } =
-    useDapps()
+  const {
+    filteredCatalog,
+    search,
+    categories,
+    categoryFilter,
+    favorites,
+    toggleFavorite,
+    onSearchChange,
+    onCategorySelect
+  } = useDapps()
   const { t } = useTranslation()
   const [loaded, setLoaded] = useState<boolean>(false)
 
@@ -81,7 +97,7 @@ const DappsCatalogScreen = () => {
         activeOpacity={0.8}
         onPress={() => {
           handleOpenDapp(item)
-          Keyboard.dismiss()
+          !isWeb && Keyboard.dismiss()
         }}
       >
         <View style={[flexbox.directionRow, spacings.mb]}>
@@ -99,9 +115,25 @@ const DappsCatalogScreen = () => {
             )}
           </View>
           <View style={flexbox.flex1}>
-            <Text fontSize={16} weight="medium" numberOfLines={1} style={spacings.mbMi}>
-              {item.name}
-            </Text>
+            <View style={[flexbox.directionRow, flexbox.justifySpaceBetween]}>
+              <Text
+                fontSize={16}
+                weight="medium"
+                numberOfLines={1}
+                style={[spacings.mbMi, flexbox.flex1, spacings.prSm]}
+              >
+                {item.name}
+              </Text>
+              <TouchableOpacity
+                hitSlop={{ top: 10, bottom: 10, right: 10, left: 5 }}
+                onPress={() => {
+                  toggleFavorite(item)
+                  !isWeb && Keyboard.dismiss()
+                }}
+              >
+                <StarIcon isFilled={favorites[item.url]} />
+              </TouchableOpacity>
+            </View>
             <Text color={colors.baileyBells} fontSize={12} numberOfLines={8}>
               {item.description}
             </Text>
@@ -137,68 +169,84 @@ const DappsCatalogScreen = () => {
 
   return (
     <GradientBackgroundWrapper>
-      <View style={[spacings.ph, spacings.mb]}>
-        <Title style={[text.center, spacings.pbSm]} numberOfLines={1}>
-          {t('Web3 dApp Catalog')}
-        </Title>
-        <View style={[flexbox.directionRow, flexbox.alignCenter]}>
-          <View style={[flexbox.flex1, spacings.prTy]}>
-            <Input
-              containerStyle={spacings.mb0}
-              placeholder={t('Search filter')}
-              onChangeText={onSearchChange}
-              leftIcon={() => <SearchIcon />}
-              value={search}
-            />
-          </View>
-          <TouchableOpacity onPress={openBottomSheet as any}>
-            <SortIcon />
-          </TouchableOpacity>
-        </View>
-      </View>
-      {!loaded && (
-        <View style={[StyleSheet.absoluteFill, flexbox.alignCenter, flexbox.justifyCenter]}>
-          <Spinner />
-        </View>
-      )}
-      {!!loaded && (
-        <Wrapper
-          hasBottomTabNav
-          type={WRAPPER_TYPES.FLAT_LIST}
-          data={sortFiltered(filteredCatalog)}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          keyboardDismissMode="on-drag"
-        />
-      )}
-      <BottomSheet
-        id="dapps-filter-bottom-sheet"
-        sheetRef={sheetRef}
-        closeBottomSheet={closeBottomSheet}
+      <TouchableWithoutFeedback
+        onPress={() => {
+          !isWeb && Keyboard.dismiss()
+        }}
       >
-        <Title style={text.center}>{t('Filter dApps by')}</Title>
-        <View style={spacings.pt}>
-          {categories
-            // will temporarily support only these 2 categories
-            .filter((e) => e.name === 'all' || e.name === 'favorites')
-            .map((category: any) => {
-              return (
-                <TouchableOpacity
-                  style={[
-                    styles.filterItem,
-                    categoryFilter?.name === category.name && { backgroundColor: colors.howl }
-                  ]}
-                  onPress={() => onCategorySelect(category)}
-                >
-                  <Text fontSize={16} style={text.capitalize}>
-                    {category.name}
-                  </Text>
-                  {categoryFilter?.name === category.name && <CheckIcon />}
-                </TouchableOpacity>
-              )
-            })}
+        <View style={flexbox.flex1}>
+          <View style={[spacings.ph, spacings.mb]}>
+            <Title style={[text.center, spacings.pbSm]} numberOfLines={1}>
+              {t('Web3 dApp Catalog')}
+            </Title>
+            <View style={[flexbox.directionRow, flexbox.alignCenter]}>
+              <View style={[flexbox.flex1, spacings.prTy]}>
+                <Input
+                  containerStyle={spacings.mb0}
+                  placeholder={t('Search filter')}
+                  onChangeText={onSearchChange}
+                  leftIcon={() => <SearchIcon />}
+                  value={search}
+                />
+              </View>
+              <TouchableOpacity
+                hitSlop={{ top: 10, bottom: 10, right: 10, left: 5 }}
+                onPress={() => {
+                  openBottomSheet()
+                  !isWeb && Keyboard.dismiss()
+                }}
+              >
+                <SortIcon />
+              </TouchableOpacity>
+            </View>
+          </View>
+          {!loaded && (
+            <View style={[StyleSheet.absoluteFill, flexbox.alignCenter, flexbox.justifyCenter]}>
+              <Spinner />
+            </View>
+          )}
+          {!!loaded && (
+            <Wrapper
+              hasBottomTabNav
+              type={WRAPPER_TYPES.FLAT_LIST}
+              style={spacings.mbTy}
+              data={sortFiltered(filteredCatalog)}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id}
+              keyboardDismissMode="on-drag"
+            />
+          )}
+          <BottomSheet
+            id="dapps-filter-bottom-sheet"
+            sheetRef={sheetRef}
+            closeBottomSheet={closeBottomSheet}
+          >
+            <Title style={text.center}>{t('Filter dApps by')}</Title>
+            <View style={[spacings.pt, spacings.pbMd]}>
+              {categories
+                // will temporarily support only these 2 categories
+                .filter((e) => e.name === 'all' || e.name === 'favorites')
+                .map((category: any) => {
+                  return (
+                    <TouchableOpacity
+                      key={category.name}
+                      onPress={() => onCategorySelect(category)}
+                      style={[
+                        styles.filterItem,
+                        categoryFilter?.name === category.name && { backgroundColor: colors.howl }
+                      ]}
+                    >
+                      <Text fontSize={16} style={text.capitalize}>
+                        {category.name}
+                      </Text>
+                      {categoryFilter?.name === category.name && <CheckIcon />}
+                    </TouchableOpacity>
+                  )
+                })}
+            </View>
+          </BottomSheet>
         </View>
-      </BottomSheet>
+      </TouchableWithoutFeedback>
     </GradientBackgroundWrapper>
   )
 }
