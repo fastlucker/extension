ea8b01d464ff1d6b381106543cd91ece8e610c01
superKalo <superkalo@devlabs.bg>
2023-05-10 18:26:43 +0300

    Change: AAVE ethereum provider credentials

--- src/web/extension-services/inpage/config/dapp-providers.ts
+++ src/web/extension-services/inpage/config/dapp-providers.ts
@@ -8,7 +8,7 @@ export const DAPP_PROVIDER_URLS: { [key: string]: { [key in NETWORKS]?: string }
     [NETWORKS.arbitrum]: 'https://arbitrum-mainnet.infura.io/v3/099fc58e0de9451d80b18d7c74caa7c1'
   },
   'app.aave.com': {
-    [NETWORKS.ethereum]: 'https://rpc.ankr.com/eth',
+    [NETWORKS.ethereum]: 'https://eth-mainnet.gateway.pokt.network/v1/lb/62b3314e123e6f00397f19ca',
     [NETWORKS.arbitrum]: ' https://arb1.arbitrum.io/rpc',
     [NETWORKS.avalanche]:
       'https://avax-mainnet.gateway.pokt.network/v1/lb/62b3314e123e6f00397f19ca/ext/bc/C/rpc',
