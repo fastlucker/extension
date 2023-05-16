6a1edc09ea9aa1d2ef09edc99d11c41bc2f8e877
superKalo <superkalo@devlabs.bg>
2023-05-16 16:43:26 +0300

    Add: back, forward, reload and (temporarily disabled) address bar for dapps

--- src/mobile/modules/web3/screens/Web3BrowserScreen/Web3BrowserScreen.tsx
+++ src/mobile/modules/web3/screens/Web3BrowserScreen/Web3BrowserScreen.tsx
@@ -1,20 +1,30 @@
-import React, { useEffect, useRef } from 'react'
-import { View } from 'react-native'
-import { WebView } from 'react-native-webview'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { TouchableHighlight, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { WebView, WebViewNavigation } from 'react-native-webview'
 
+import Button from '@common/components/Button'
 import GradientBackgroundWrapper from '@common/components/GradientBackgroundWrapper'
+import Input from '@common/components/Input'
 import Spinner from '@common/components/Spinner'
 import Wrapper from '@common/components/Wrapper'
 import useRoute from '@common/hooks/useRoute'
-import spacings from '@common/styles/spacings'
+import colors from '@common/styles/colors'
+import spacings, { SPACING_MI, SPACING_TY } from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
+import { AntDesign } from '@expo/vector-icons'
 import useWeb3 from '@mobile/modules/web3/hooks/useWeb3'
 import useGetProviderInjection from '@mobile/modules/web3/services/webview-inpage/injection-script'
 
 import styles from './styles'
 
+const HIT_SLOP = { bottom: 15, left: 5, right: 5, top: 15 }
+
 const Web3BrowserScreen = () => {
-  const webViewRef = useRef(null)
+  const webViewRef = useRef<WebView | null>(null)
   const route = useRoute()
+  const [canGoBack, setCanGoBack] = useState(false)
+  const [canGoForward, setCanGoForward] = useState(false)
 
   const { setWeb3ViewRef, handleWeb3Request, setSelectedDappUrl } = useWeb3()
 
@@ -41,13 +51,73 @@ const Web3BrowserScreen = () => {
     }
   }
 
+  const onNavigationStateChange = useCallback((navState: WebViewNavigation) => {
+    setCanGoBack(navState.canGoBack)
+    setCanGoForward(navState.canGoForward)
+  }, [])
+
+  const uri = providerToInject ? selectedDappUrl : null
+
   return (
     <GradientBackgroundWrapper>
       <Wrapper style={spacings.ph0} hasBottomTabNav>
+        <View
+          style={[
+            flexbox.directionRow,
+            flexbox.alignCenter,
+            spacings.pbTy,
+            spacings.prSm,
+            spacings.plSm
+          ]}
+        >
+          <TouchableHighlight
+            hitSlop={HIT_SLOP}
+            onPress={webViewRef.current?.goBack}
+            style={[styles.webviewButtonCommon, styles.left]}
+            disabled={!canGoBack}
+            underlayColor={colors.heliotrope}
+          >
+            <AntDesign
+              color={canGoBack ? colors.white : colors.titan_50}
+              name="left"
+              size={24}
+              spot
+            />
+          </TouchableHighlight>
+          <TouchableHighlight
+            hitSlop={HIT_SLOP}
+            onPress={webViewRef.current?.goForward}
+            style={[styles.webviewButtonCommon, styles.right]}
+            disabled={!canGoForward}
+            underlayColor={colors.heliotrope}
+          >
+            <AntDesign
+              color={canGoForward ? colors.white : colors.titan_50}
+              name="right"
+              size={24}
+            />
+          </TouchableHighlight>
+          <Input
+            containerStyle={[flexbox.flex1, spacings.mb0]}
+            disabled
+            inputStyle={styles.addressInputStyle}
+            inputWrapperStyle={styles.addressInputWrapperStyle}
+            value={uri || ''}
+          />
+          <TouchableHighlight
+            hitSlop={HIT_SLOP}
+            onPress={webViewRef.current?.reload}
+            style={[styles.webviewButtonCommon, styles.reload]}
+            underlayColor={colors.heliotrope}
+          >
+            <AntDesign name="reload1" size={22} color={colors.white} />
+          </TouchableHighlight>
+        </View>
         <WebView
           ref={webViewRef}
-          source={{ uri: providerToInject ? selectedDappUrl : null }}
+          source={{ uri }}
           onMessage={handleEthereumProviderMessage}
+          onNavigationStateChange={onNavigationStateChange}
           injectedJavaScriptBeforeContentLoaded={providerToInject}
           javaScriptEnabled
           startInLoadingState
--- src/mobile/modules/web3/screens/Web3BrowserScreen/styles.ts
+++ src/mobile/modules/web3/screens/Web3BrowserScreen/styles.ts
@@ -1,9 +1,17 @@
 import { StyleSheet, ViewStyle } from 'react-native'
 
+import { SPACING_MI } from '@common/styles/spacings'
+
 interface Style {
   container: ViewStyle
   webview: ViewStyle
   loadingWrapper: ViewStyle
+  webviewButtonCommon: ViewStyle
+  left: ViewStyle
+  right: ViewStyle
+  reload: ViewStyle
+  addressInputStyle: ViewStyle
+  addressInputWrapperStyle: ViewStyle
 }
 
 const styles = StyleSheet.create<Style>({
@@ -21,6 +29,32 @@ const styles = StyleSheet.create<Style>({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  webviewButtonCommon: {
+    borderRadius: 22,
+    padding: SPACING_MI
+  },
+  left: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  right: {
+    marginRight: SPACING_MI,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  reload: {
+    marginLeft: SPACING_MI,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  addressInputStyle: {
+    height: 'auto',
+    fontSize: 14
+  },
+  addressInputWrapperStyle: {
+    height: 'auto',
+    paddingVertical: SPACING_MI
   }
 })
 
