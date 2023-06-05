c9651b532c1f9da4cd8af129d5fa6a5b12a17731
Gergana Mihaylova <gergana@devlabs.bg>
2023-06-05 23:19:07 +0300

    Add: connect trezor changes

---
+++ src/web/modules/hardware-wallet/screens/ConnectLedgerScreen/Ambire.tsx
@@ -0,0 +1,214 @@
+const AmbireScreen: React.FC<any> = (props) => (
+  <svg width="144" height="94" viewBox="0 0 144 94" {...props}>
+    <defs>
+      <linearGradient
+        id="linear-gradient"
+        x1="0.554"
+        y1="0.58"
+        x2="0.052"
+        y2="0.409"
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset="0" stopColor="#6000ff" />
+        <stop offset="0.651" stopColor="#4900c3" />
+        <stop offset="1" stopColor="#320086" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-2"
+        x1="0.06"
+        y1="-0.087"
+        x2="0.486"
+        y2="0.653"
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset="0" stopColor="#6a0aff" />
+        <stop offset="0.047" stopColor="#8c2dff" />
+        <stop offset="0.102" stopColor="#6a0aff" />
+        <stop offset="0.902" stopColor="#af50ff" />
+        <stop offset="1" stopColor="#af50ff" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-3"
+        x1="1.071"
+        y1="0.061"
+        x2="0.095"
+        y2="1.049"
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset="0" stopColor="#6a0aff" />
+        <stop offset="0.51" stopColor="#8c2dff" />
+        <stop offset="0.969" stopColor="#af50ff" />
+        <stop offset="1" stopColor="#af50ff" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-4"
+        x1="0.448"
+        y1="0.297"
+        x2="0.538"
+        y2="0.8"
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset="0" stopColor="#6000ff" />
+        <stop offset="1" stopColor="#3e00a5" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-5"
+        x1="-0.529"
+        y1="1.069"
+        x2="1.092"
+        y2="0.86"
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset="0" stopColor="#ae60ff" />
+        <stop offset="0.322" stopColor="#af50ff" />
+        <stop offset="1" stopColor="#6000ff" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-6"
+        x1="-0.111"
+        y1="0.274"
+        x2="0.872"
+        y2="1.224"
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset="0" stopColor="#6f0fff" />
+        <stop offset="0.702" stopColor="#af50ff" />
+        <stop offset="1" stopColor="#af50ff" />
+      </linearGradient>
+      <linearGradient
+        id="linear-gradient-7"
+        x1="0.015"
+        y1="0.007"
+        x2="0.986"
+        y2="0.95"
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset="0" stopColor="#ae60ff" />
+        <stop offset="0.031" stopColor="#b670fa" />
+        <stop offset="1" stopColor="#be80f5" />
+      </linearGradient>
+    </defs>
+    <g id="Group_1126" data-name="Group 1126" transform="translate(-499 15308)">
+      <rect
+        id="Rectangle_1231"
+        data-name="Rectangle 1231"
+        width="130"
+        height="83"
+        rx="5"
+        transform="translate(506 -15308)"
+        fill="rgba(103,112,179,0.2)"
+      />
+      <rect
+        id="Rectangle_1230"
+        data-name="Rectangle 1230"
+        width="144"
+        height="8"
+        rx="4"
+        transform="translate(499 -15222)"
+        fill="rgba(103,112,179,0.2)"
+      />
+      <rect
+        id="Rectangle_1232"
+        data-name="Rectangle 1232"
+        width="120"
+        height="72"
+        rx="2"
+        transform="translate(511 -15303)"
+        fill="#ebecff"
+      />
+      <g id="logo" transform="translate(525.344 -15281.437)" opacity="0.3">
+        <g id="_1967955583232" transform="translate(36.039 -0.01)">
+          <g id="Group_219" data-name="Group 219">
+            <path
+              id="Path_658"
+              data-name="Path 658"
+              d="M727.2,420.5l3.791,8.071a.229.229,0,0,1-.047.252l-9.974,9.637a.11.11,0,0,1-.187-.084v-8.854l6.285-6.067a.2.2,0,0,0,.065-.158l.019-2.8c0-.019.037-.019.047,0Z"
+              transform="translate(-711.771 -409.601)"
+              fill="#6000ff"
+            />
+            <path
+              id="Path_659"
+              data-name="Path 659"
+              d="M720.76,540.047V548.9a.111.111,0,0,0,.187.084h0l9.974-9.637a.229.229,0,0,0,.047-.252h0l-3.894-5.147-6.314,6.1Z"
+              transform="translate(-711.752 -520.122)"
+              fillRule="evenodd"
+              fill="url(#linear-gradient)"
+            />
+            <path
+              id="Path_660"
+              data-name="Path 660"
+              d="M964.646,420.11h0a.021.021,0,0,0-.022.022h0l-.016,2.8a.23.23,0,0,1-.036.13l3.909,5.276a.245.245,0,0,0-.019-.14h0l-3.792-8.071a.026.026,0,0,0-.023-.013Z"
+              transform="translate(-949.248 -409.23)"
+              fillRule="evenodd"
+              fill="url(#linear-gradient-2)"
+            />
+            <path
+              id="Path_661"
+              data-name="Path 661"
+              d="M974.684,547.594a.245.245,0,0,0-.019-.14h0l-.2-.419-3.529-4.494Z"
+              transform="translate(-955.453 -528.49)"
+              fill="#be80f5"
+              fillRule="evenodd"
+            />
+            <path
+              id="Path_662"
+              data-name="Path 662"
+              d="M401.887,158.056l-1.784,5.051a.234.234,0,0,0,.009.177l1.662,3.271-4.576,2.591a.12.12,0,0,1-.159-.047l-.99-2.06a.2.2,0,0,1,.019-.214l5.771-8.8a.028.028,0,0,1,.047.028Z"
+              transform="translate(-395.429 -153.928)"
+              fill="#6000ff"
+            />
+            <path
+              id="Path_663"
+              data-name="Path 663"
+              d="M412.49,158.03h0a.026.026,0,0,0-.018.008h0l-5.525,8.422-.006.011,3.794-3.354.012-.034h0v0l1.771-5.015a.028.028,0,0,0-.027-.035Z"
+              transform="translate(-406.059 -153.937)"
+              fillRule="evenodd"
+              fill="url(#linear-gradient-3)"
+            />
+            <path
+              id="Path_664"
+              data-name="Path 664"
+              d="M396.083,358.169a.2.2,0,0,0-.019.214h0l.99,2.06a.12.12,0,0,0,.159.046h0l4.576-2.591-1.662-3.271a.234.234,0,0,1-.009-.177Z"
+              transform="translate(-395.442 -345.271)"
+              fillRule="evenodd"
+              fill="url(#linear-gradient-4)"
+            />
+            <path
+              id="Path_665"
+              data-name="Path 665"
+              d="M381.92.046V7.9a.223.223,0,0,1-.047.131h0l-8.919,12.088a.216.216,0,0,0,.028.289h0l6.257,6.039a.107.107,0,0,0,.177-.037h0l6.8-15.909a.258.258,0,0,0,0-.168h0L382,.027A.04.04,0,0,0,381.966,0h0a.046.046,0,0,0-.046.046Z"
+              transform="translate(-372.912 0)"
+              fillRule="evenodd"
+              fill="url(#linear-gradient-5)"
+            />
+            <path
+              id="Path_666"
+              data-name="Path 666"
+              d="M720.375-.01h0a.046.046,0,0,0-.046.046l0,7.857a.221.221,0,0,1-.012.065l4.252,2.238L720.413.017a.04.04,0,0,0-.037-.027Zm-.055,7.968.812.466Z"
+              transform="translate(-711.323 0.01)"
+              fillRule="evenodd"
+              fill="url(#linear-gradient-6)"
+            />
+            <path
+              id="Path_667"
+              data-name="Path 667"
+              d="M724.642,310.124h0a.257.257,0,0,0-.013-.082h0l-.054-.133-4.255-2.238.815.466,3.507,1.989Z"
+              transform="translate(-711.323 -299.702)"
+              fillRule="evenodd"
+              fill="url(#linear-gradient-7)"
+            />
+            <path
+              id="Path_668"
+              data-name="Path 668"
+              d="M398,357.816l-.235.358,4.031-3.714Z"
+              transform="translate(-397.126 -345.28)"
+              fill="#be80f5"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+)
+
+export default AmbireScreen
--- src/web/modules/hardware-wallet/screens/ConnectLedgerScreen/ConnectLedgerScreen.tsx
+++ src/web/modules/hardware-wallet/screens/ConnectLedgerScreen/ConnectLedgerScreen.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { View } from 'react-native'
 
 import Button from '@common/components/Button'
-import GradientBackgroundWrapper from '@common/components/GradientBackgroundWrapper'
+import { AuthLayoutWrapperMainContent } from '@web/components/AuthLayoutWrapper/AuthLayoutWrapper'
 import Text from '@common/components/Text'
-import Title from '@common/components/Title'
 import Wrapper from '@common/components/Wrapper'
 import useNavigation from '@common/hooks/useNavigation'
+import { useTranslation } from '@common/config/localization'
 import { WEB_ROUTES } from '@common/modules/router/constants/common'
 import TransportWebHID from '@ledgerhq/hw-transport-webhid'
 import { HARDWARE_WALLETS } from '@web/modules/hardware-wallet/constants/common'
 import { hasConnectedLedgerDevice } from '@web/modules/hardware-wallet/utils/ledger'
+import AmbireScreen from '@web/modules/hardware-wallet/screens/ConnectLedgerScreen/Ambire'
+import Drive from '@web/modules/hardware-wallet/screens/ConnectLedgerScreen/Drive'
+import ArrowIcon from '@web/modules/hardware-wallet/screens/ConnectLedgerScreen/ArrowIcon'
+
+import flexbox from '@common/styles/utils/flexbox'
+import colors from '@common/styles/colors'
+import spacings from '@common/styles/spacings'
 
 const ConnectLedgerScreen = () => {
   const { navigate } = useNavigation()
+  const { t } = useTranslation()
 
   const onSubmit = async () => {
     const supportWebHID = await TransportWebHID.isSupported()
@@ -38,14 +47,32 @@ const ConnectLedgerScreen = () => {
   }
 
   return (
-    <GradientBackgroundWrapper>
+    <AuthLayoutWrapperMainContent fullWidth>
       <Wrapper>
-        <Title>Connect Ledger</Title>
-        <Text>1. Plug your Ledger wallet into your computer</Text>
-        <Text>2. Unlock Ledger and open the Ethereum app</Text>
-        <Button text="Next" onPress={onSubmit} />
+        <View
+          style={{
+            backgroundColor: colors.melrose_15,
+            ...flexbox.alignCenter,
+            paddingTop: 50,
+            paddingBottom: 50
+          }}
+        >
+          <Text weight="medium" style={{ marginBottom: 16 }} fontSize={20}>
+            {t('Connect your HW wallet')}
+          </Text>
+          <Text fontSize={16}>{t('1. Plug your Ledger wallet into your computer')}</Text>
+          <Text fontSize={16} style={{ marginBottom: 40 }}>
+            {t('2. Unlock Ledger and open the Ethereum app')}
+          </Text>
+          <View style={[flexbox.directionRow, flexbox.alignCenter, spacings.mbLg]}>
+            <Drive style={{ ...spacings.mrLg }} />
+            <ArrowIcon style={{ ...spacings.mrLg }} />
+            <AmbireScreen />
+          </View>
+          <Button text="Next" style={{ width: 264 }} onPress={onSubmit} />
+        </View>
       </Wrapper>
-    </GradientBackgroundWrapper>
+    </AuthLayoutWrapperMainContent>
   )
 }
 
