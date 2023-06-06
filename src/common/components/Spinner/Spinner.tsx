9bf7e47cd8fc5e3681a2550e4fa9d731df92b8a8
Gergana Mihaylova <gergana@devlabs.bg>
2023-06-06 17:01:02 +0300

    Add: spinner animation same for web and mobile

--- src/common/components/Spinner/Spinner.tsx
+++ src/common/components/Spinner/Spinner.tsx
@@ -1,11 +1,12 @@
 import LottieView from 'lottie-react-native'
 import React from 'react'
+import { ViewStyle } from 'react-native'
 
 import SpinnerAnimation from './spinner-animation.json'
 import styles from './styles'
 
-const Spinner = () => {
-  return <LottieView source={SpinnerAnimation} style={styles.spinner} autoPlay loop />
+const Spinner = ({ style }: { style: ViewStyle }) => {
+  return <LottieView source={SpinnerAnimation} style={(styles.spinner, style)} autoPlay loop />
 }
 
 export default Spinner
--- src/common/components/Spinner/Spinner.web.tsx
+++ src/common/components/Spinner/Spinner.web.tsx
@@ -1,10 +1,17 @@
 import LottieView from 'lottie-react'
 import React from 'react'
 
-import SpinnerAnimation from './spinner-animation-web.json'
+import SpinnerAnimation from './spinner-animation.json'
 
-const Spinner = ({ width = 40, height = 40 }: { width: number; height: number }) => {
-  return <LottieView animationData={SpinnerAnimation} style={{ width, height }} autoPlay loop />
+const Spinner = ({ style }: { style: any }) => {
+  return (
+    <LottieView
+      animationData={SpinnerAnimation}
+      style={{ width: 40, height: 40, ...style }}
+      autoPlay
+      loop
+    />
+  )
 }
 
 export default Spinner
