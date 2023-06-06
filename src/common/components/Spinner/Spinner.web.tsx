ec9d63fb8bebecf09e26800a224d5c6d15d00179
Gergana Mihaylova <gergana@devlabs.bg>
2023-06-06 13:03:27 +0300

    Add: web spinner changes

--- src/common/components/Spinner/Spinner.web.tsx
+++ src/common/components/Spinner/Spinner.web.tsx
@@ -1,12 +1,10 @@
 import LottieView from 'lottie-react'
 import React from 'react'
 
-import SpinnerAnimation from './spinner-animation.json'
+import SpinnerAnimation from './spinner-animation-web.json'
 
-const Spinner = () => {
-  return (
-    <LottieView animationData={SpinnerAnimation} style={{ width: 40, height: 40 }} autoPlay loop />
-  )
+const Spinner = ({ width = 40, height = 40 }: { width: number; height: number }) => {
+  return <LottieView animationData={SpinnerAnimation} style={{ width, height }} autoPlay loop />
 }
 
 export default Spinner
