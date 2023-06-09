791eb43795dc72d7c1cf0189cc024eb07f3662f6
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2023-06-09 14:56:19 +0300

    added keyboard aware view to the 2fa screen

--- src/common/modules/settings/screens/EnableOtp2FaScreen/EnableOtp2FaScreen.tsx
+++ src/common/modules/settings/screens/EnableOtp2FaScreen/EnableOtp2FaScreen.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 import GradientBackgroundWrapper from '@common/components/GradientBackgroundWrapper'
-import Wrapper from '@common/components/Wrapper'
+import Wrapper, { WRAPPER_TYPES } from '@common/components/Wrapper'
 import EnableOTP2FaForm from '@common/modules/settings/components/EnableOTP2FaForm'
 import spacings from '@common/styles/spacings'
 
 const EnableOtp2FaScreen = () => {
   return (
     <GradientBackgroundWrapper>
-      <Wrapper style={spacings.mt}>
+      <Wrapper
+        style={spacings.mt}
+        type={WRAPPER_TYPES.KEYBOARD_AWARE_SCROLL_VIEW}
+        extraHeight={220}
+      >
         <EnableOTP2FaForm />
       </Wrapper>
     </GradientBackgroundWrapper>
