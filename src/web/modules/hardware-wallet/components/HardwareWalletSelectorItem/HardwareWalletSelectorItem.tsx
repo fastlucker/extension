55f785a220dfc03d5cbb5e5c63f0498fad856eac
Gergana Mihaylova <gergana@devlabs.bg>
2023-06-14 12:52:16 +0300

    Add: hoverable selector item

--- src/web/modules/hardware-wallet/components/HardwareWalletSelectorItem/HardwareWalletSelectorItem.tsx
+++ src/web/modules/hardware-wallet/components/HardwareWalletSelectorItem/HardwareWalletSelectorItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Image, ViewStyle } from 'react-native'
+import { Pressable } from 'react-native-web-hover'
 
 import Card from '@web/modules/auth/components/Card'
 import Button from '@common/components/Button'
 
+import colors from '@common/styles/colors'
 import styles from './styles'
 
 type Props = {
@@ -16,24 +18,36 @@ type Props = {
 
 const HardwareWalletSelectorItem = ({ name, text, icon, style, onSelect }: Props) => {
   return (
-    <Card
-      text={text}
-      style={[styles.itemContainer, style]}
-      icon={
-        <Image
-          source={icon}
-          style={{
-            height: 136,
-            width: 120,
-            marginBottom: 27,
-            alignSelf: 'center'
-          }}
-          resizeMode="contain"
-        />
-      }
+    <Pressable
+      onPress={() => {
+        onSelect()
+      }}
     >
-      <Button text={name} onPress={onSelect} />
-    </Card>
+      {({ hovered }) => (
+        <Card
+          text={text}
+          style={[
+            styles.itemContainer,
+            style,
+            { borderWidth: 1, borderColor: hovered ? colors.violet : colors.melrose_15 }
+          ]}
+          icon={
+            <Image
+              source={icon}
+              style={{
+                height: 136,
+                width: 120,
+                marginBottom: 27,
+                alignSelf: 'center'
+              }}
+              resizeMode="contain"
+            />
+          }
+        >
+          <Button text={name} onPress={onSelect} />
+        </Card>
+      )}
+    </Pressable>
   )
 }
 
