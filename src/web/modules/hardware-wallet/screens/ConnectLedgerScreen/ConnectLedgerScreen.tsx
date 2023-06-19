476b101a14bb6f77b9867600e64606b699b8e68f
Gergana Mihaylova <gergana@devlabs.bg>
2023-06-19 20:07:09 +0300

    Change: connect ledger alignment

--- src/web/modules/hardware-wallet/screens/ConnectLedgerScreen/ConnectLedgerScreen.tsx
+++ src/web/modules/hardware-wallet/screens/ConnectLedgerScreen/ConnectLedgerScreen.tsx
@@ -4,13 +4,13 @@ import { View } from 'react-native'
 import Button from '@common/components/Button'
 import { AuthLayoutWrapperMainContent } from '@web/components/AuthLayoutWrapper/AuthLayoutWrapper'
 import Text from '@common/components/Text'
-import Wrapper from '@common/components/Wrapper'
 import useNavigation from '@common/hooks/useNavigation'
 import { useTranslation } from '@common/config/localization'
 import { WEB_ROUTES } from '@common/modules/router/constants/common'
 import TransportWebHID from '@ledgerhq/hw-transport-webhid'
 import { HARDWARE_WALLETS } from '@web/modules/hardware-wallet/constants/common'
 import { hasConnectedLedgerDevice } from '@web/modules/hardware-wallet/utils/ledger'
+import useHardwareWallets from '@web/modules/hardware-wallet/hooks/useHardwareWallets'
 import AmbireScreen from '@web/modules/hardware-wallet/screens/ConnectLedgerScreen/Ambire'
 import Drive from '@web/modules/hardware-wallet/screens/ConnectLedgerScreen/Drive'
 import ArrowIcon from '@web/modules/hardware-wallet/screens/ConnectLedgerScreen/ArrowIcon'
@@ -23,55 +23,83 @@ const ConnectLedgerScreen = () => {
   const { navigate } = useNavigation()
   const { t } = useTranslation()
 
-  const onSubmit = async () => {
-    const supportWebHID = await TransportWebHID.isSupported()
-    const hasConnectedLedger = await hasConnectedLedgerDevice()
+  // const onSubmit = async () => {
+  //   const supportWebHID = await TransportWebHID.isSupported()
+  //   const hasConnectedLedger = await hasConnectedLedgerDevice()
 
-    if (!supportWebHID) {
-      navigate(WEB_ROUTES.hardwareWalletImportAccount, {
-        state: {
-          walletType: HARDWARE_WALLETS.LEDGER,
-          isWebHID: false
-        }
-      })
-    } else if (hasConnectedLedger) {
-      navigate(WEB_ROUTES.hardwareWalletImportAccount, {
+  //   if (!supportWebHID) {
+  //     navigate(WEB_ROUTES.accountsImporter, {
+  //       state: {
+  //         walletType: HARDWARE_WALLETS.LEDGER,
+  //         isWebHID: false
+  //       }
+  //     })
+  //   } else if (hasConnectedLedger) {
+  //     navigate(WEB_ROUTES.accountsImporter, {
+  //       state: {
+  //         walletType: HARDWARE_WALLETS.LEDGER,
+  //         isWebHID: true
+  //       }
+  //     })
+  //   } else {
+  //     navigate(WEB_ROUTES.hardwareWalletLedgerPermission)
+  //   }
+  // }
+
+  const { hardwareWallets } = useHardwareWallets()
+
+  const onSubmit = async () => {
+    try {
+      const transport = await TransportWebHID.create()
+      await transport.close()
+      await hardwareWallets[HARDWARE_WALLETS.LEDGER].authorizeHIDPermission()
+
+      navigate(WEB_ROUTES.accountsImporter, {
         state: {
           walletType: HARDWARE_WALLETS.LEDGER,
           isWebHID: true
         }
       })
-    } else {
-      navigate(WEB_ROUTES.hardwareWalletImportAccount)
-    }
+    } catch (e) {}
   }
 
   return (
     <AuthLayoutWrapperMainContent fullWidth>
-      <Wrapper>
+      <View style={[flexbox.flex1, flexbox.justifyCenter]}>
         <View
           style={{
             backgroundColor: colors.melrose_15,
-            ...flexbox.alignCenter,
             paddingTop: 50,
             paddingBottom: 50
           }}
         >
-          <Text weight="medium" style={{ marginBottom: 14 }} fontSize={20}>
+          <Text
+            weight="medium"
+            style={{ marginBottom: 14, ...flexbox.alignSelfCenter }}
+            fontSize={20}
+          >
             {t('Connect your HW wallet')}
           </Text>
-          <Text fontSize={14}>{t('1. Plug your Ledger wallet into your computer')}</Text>
-          <Text fontSize={14} style={{ marginBottom: 40 }}>
-            {t('2. Unlock Ledger and open the Ethereum app')}
-          </Text>
-          <View style={[flexbox.directionRow, flexbox.alignCenter, spacings.mbLg]}>
+          <View style={{ minWidth: 420, ...flexbox.alignSelfCenter }}>
+            <Text weight="regular" style={[spacings.mbTy]} fontSize={14}>
+              {t('1. Plug your Trezor / Ledger wallet into your computer')}
+            </Text>
+            <Text weight="regular" fontSize={14} style={{ marginBottom: 40 }}>
+              {t('2. Unlock Trezor / Ledger and open the Ambire extension')}
+            </Text>
+          </View>
+          <View style={[flexbox.directionRow, flexbox.alignSelfCenter, spacings.mbLg]}>
             <Drive style={{ ...spacings.mrLg }} />
             <ArrowIcon style={{ ...spacings.mrLg }} />
             <AmbireScreen />
           </View>
-          <Button text="Next" style={{ width: 264 }} onPress={onSubmit} />
+          <Button
+            text="Next"
+            style={{ width: 264, ...flexbox.alignSelfCenter }}
+            onPress={onSubmit}
+          />
         </View>
-      </Wrapper>
+      </View>
     </AuthLayoutWrapperMainContent>
   )
 }
