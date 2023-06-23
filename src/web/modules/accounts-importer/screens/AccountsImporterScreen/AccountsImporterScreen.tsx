bc0ea944f7a7f09292a7d11ef8e89c0ec0126e1c
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2023-06-23 10:52:15 +0300

    Accounts Importer Screen cleanup + added comments

--- src/web/modules/accounts-importer/screens/AccountsImporterScreen/AccountsImporterScreen.tsx
+++ src/web/modules/accounts-importer/screens/AccountsImporterScreen/AccountsImporterScreen.tsx
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { View } from 'react-native'
 
-import GradientBackgroundWrapper from '@common/components/GradientBackgroundWrapper'
 import Title from '@common/components/Title'
-import Wrapper from '@common/components/Wrapper'
 import useNavigation from '@common/hooks/useNavigation'
 import useRoute from '@common/hooks/useRoute'
-import useToast from '@common/hooks/useToast'
 import spacings from '@common/styles/spacings'
-import useWalletControllerRequest from '@web/hooks/useWalletControllerRequest'
-import AccountsList from '@web/modules/accounts-importer/components/AccountsList'
 import LatticeManager from '@web/modules/accounts-importer/components/LatticeManager'
 import LedgerManager from '@web/modules/accounts-importer/components/LedgerManager'
 import TrezorManager from '@web/modules/accounts-importer/components/TrezorManager'
@@ -36,12 +31,12 @@ const WALLET_MAP = {
 
 const AccountsImporterScreen = () => {
   const { params } = useRoute()
-  const { addToast } = useToast()
+
   const { goBack } = useNavigation()
   const { hardwareWallets } = useHardwareWallets()
   const { t } = useTranslation()
 
-  const { walletType, isMnemonics, isWebHID, ledgerLive, path, brand } = params
+  const { walletType }: any = params
 
   const isGrid = walletType === HARDWARE_WALLETS.GRIDPLUS
   const isLedger = walletType === HARDWARE_WALLETS.LEDGER
@@ -85,99 +80,7 @@ const AccountsImporterScreen = () => {
     )
   }
 
-  return null
-
-  const [isLoading, setIsLoading] = useState(true)
-  const [accounts, setAccounts] = useState<any[]>([])
-  const [importedAccounts, setImportedAccounts] = useState<any[]>([])
-
-  const [end, setEnd] = useState(1)
-  const [currentPage, setCurrentPage] = useState(1)
-
-  const [getAccounts] = useWalletControllerRequest(
-    async (firstFlag, start?, end?, cb?): Promise<Account[]> => {
-      setIsLoading(true)
-
-      return firstFlag
-        ? // eslint-disable-next-line @typescript-eslint/return-await
-          await hardwareWallets[walletType].getFirstPage()
-        : end && !isGrid && !ledgerLive
-        ? // eslint-disable-next-line @typescript-eslint/return-await
-          await hardwareWallets[walletType].getAddresses(start, end)
-        : // eslint-disable-next-line @typescript-eslint/return-await
-          await hardwareWallets[walletType].getNextPage()
-    },
-    {
-      onSuccess(_accounts, { args: [, , , cb] }) {
-        if (_accounts.length <= 0) {
-          setIsLoading(false)
-          addToast(t('No accounts found'))
-
-          return
-        }
-        if (_accounts.length < 5) {
-          throw new Error(t('You need to make use your last account before you can add a new one'))
-        }
-        setIsLoading(false)
-        setAccounts(_accounts)
-        cb?.()
-      },
-      onError(err) {
-        if (isLedger) {
-          // setHasError(true)
-          try {
-            hardwareWallets[walletType]?.cleanUp()
-          } catch (e) {
-            console.log(e)
-          }
-        } else {
-          addToast(t('Please check the connection with your wallet.'))
-        }
-
-        setIsLoading(false)
-      }
-    }
-  )
-
-  const init = async () => {
-    // setHasError(false)
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    const _importedAccounts = await hardwareWallets[walletType].getAccounts()
-
-    setImportedAccounts(_importedAccounts)
-    getAccounts(true)
-  }
-
-  useEffect(() => {
-    if (walletType) {
-      init()
-    }
-  }, [walletType])
-
-  useEffect(() => {
-    if (!walletType) return () => {}
-
-    return () => {
-      try {
-        hardwareWallets[walletType].cleanUp()
-      } catch (e) {
-        console.log(e)
-      }
-    }
-  }, [hardwareWallets, walletType])
-
-  return (
-    <AccountsPaginationProvider walletType={walletType}>
-      <GradientBackgroundWrapper>
-        <Wrapper>
-          {isGrid && (
-            <Title>{t('Connected to a {{walletType}} hardware device', { walletType })}</Title>
-          )}
-          <AccountsList accounts={accounts} loading={isLoading} />
-        </Wrapper>
-      </GradientBackgroundWrapper>
-    </AccountsPaginationProvider>
-  )
+  // TODO: implement a logic for displaying a list of Ambire Smart Accounts and EOA
 }
 
 export default AccountsImporterScreen
