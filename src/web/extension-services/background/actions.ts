f1c8faa66e8b0e9f152539d45381a63f819f3a12
Kaloyan Kosev <superkalo@devlabs.bg>
2023-08-01 16:46:01 +0300

    Add: all wallet controller action types + params

--- src/web/extension-services/background/actions.ts
+++ src/web/extension-services/background/actions.ts
@@ -1,5 +1,7 @@
 import { Account } from 'ambire-common/src/interfaces/account'
 
+import { NetworkType } from '@common/constants/networks'
+
 type MainControllerAccountAdderInitLedgerAction = {
   type: 'MAIN_CONTROLLER_ACCOUNT_ADDER_INIT_LEDGER'
   params: {
@@ -50,6 +52,58 @@ type LedgerControllerGetPathForIndexAction = {
 type LedgerControllerAppAction = {
   type: 'LEDGER_CONTROLLER_APP'
 }
+type WalletControllerIsUnlockedAction = {
+  type: 'WALLET_CONTROLLER_IS_UNLOCKED'
+}
+type WalletControllerGetConnectedSiteAction = {
+  type: 'WALLET_CONTROLLER_GET_CONNECTED_SITE'
+  params: { origin: string }
+}
+type WalletControllerGetConnectedSitesAction = {
+  type: 'WALLET_CONTROLLER_GET_CONNECTED_SITES'
+}
+type WalletControllerRequestVaultControllerMethodAction = {
+  type: 'WALLET_CONTROLLER_REQUEST_VAULT_CONTROLLER_METHOD'
+  params: { method: string; props: any }
+}
+type WalletControllerSetStorageAction = {
+  type: 'WALLET_CONTROLLER_SET_STORAGE'
+  params: { key: string; value: any }
+}
+type WalletControllerGetCurrentSiteAction = {
+  type: 'WALLET_CONTROLLER_GET_CURRENT_SITE'
+  params: { tabId: number; domain: string }
+}
+type WalletControllerRemoveConnectedSiteAction = {
+  type: 'WALLET_CONTROLLER_REMOVE_CONNECTED_SITE'
+  params: { origin: string }
+}
+type WalletControllerActiveFirstApprovalAction = {
+  type: 'WALLET_CONTROLLER_ACTIVE_FIRST_APPROVAL'
+}
+type WalletControllerGetApprovalAction = {
+  type: 'WALLET_CONTROLLER_GET_APPROVAL'
+}
+type WalletControllerResolveApprovalAction = {
+  type: 'WALLET_CONTROLLER_RESOLVE_APPROVAL'
+  params: any
+}
+type WalletControllerRejectApprovalAction = {
+  type: 'WALLET_CONTROLLER_REJECT_APPROVAL'
+  params: { err?: string; stay?: boolean; isInternal?: boolean }
+}
+type WalletControllerNetworkChangeAction = {
+  type: 'WALLET_CONTROLLER_NETWORK_CHANGE'
+  params: { network: NetworkType }
+}
+type WalletControllerAccountChangeAction = {
+  type: 'WALLET_CONTROLLER_ACCOUNT_CHANGE'
+  params: { selectedAcc: Account['addr'] }
+}
+type WalletControllerSendRequestAction = {
+  type: 'WALLET_CONTROLLER_SEND_REQUEST'
+  params: { data: any }
+}
 
 export type Action =
   | MainControllerAccountAdderInitLatticeAction
@@ -60,3 +114,17 @@ export type Action =
   | LedgerControllerUnlockAction
   | LedgerControllerGetPathForIndexAction
   | LedgerControllerAppAction
+  | WalletControllerIsUnlockedAction
+  | WalletControllerGetConnectedSiteAction
+  | WalletControllerRequestVaultControllerMethodAction
+  | WalletControllerSetStorageAction
+  | WalletControllerGetCurrentSiteAction
+  | WalletControllerRemoveConnectedSiteAction
+  | WalletControllerGetConnectedSitesAction
+  | WalletControllerActiveFirstApprovalAction
+  | WalletControllerGetApprovalAction
+  | WalletControllerResolveApprovalAction
+  | WalletControllerRejectApprovalAction
+  | WalletControllerNetworkChangeAction
+  | WalletControllerAccountChangeAction
+  | WalletControllerSendRequestAction
