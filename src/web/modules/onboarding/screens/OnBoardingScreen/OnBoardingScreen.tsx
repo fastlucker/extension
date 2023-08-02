af980ae11ae3f8d8afb1beeb4c7db9783b2dc414
Gergana Mihaylova <gergana@devlabs.bg>
2023-08-02 14:37:12 +0300

    Add: onboarding changes

--- src/web/modules/onboarding/screens/OnBoardingScreen/OnBoardingScreen.tsx
+++ src/web/modules/onboarding/screens/OnBoardingScreen/OnBoardingScreen.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import { View } from 'react-native'
 
+import RightArrowIcon from '@common/assets/svg/RightArrowIcon'
 import Text from '@common/components/Text'
-import { THEME_TYPES } from '@common/styles/themeConfig'
-import {
-  AuthLayoutWrapperMainContent,
-  AuthLayoutWrapperSideContent
-} from '@web/components/AuthLayoutWrapper/AuthLayoutWrapper'
-import ConfettiLogo from '@web/modules/onboarding/components/ConfettiLogo'
+import colors from '@common/styles/colors'
+import { AuthLayoutWrapperMainContent } from '@web/components/AuthLayoutWrapper/AuthLayoutWrapper'
 import PinExtension from '@web/modules/onboarding/components/PinExtension/PinExtension'
 import { ONBOARDING_VALUES } from '@web/modules/onboarding/contexts/onboardingContext/types'
 import useOnboarding from '@web/modules/onboarding/hooks/useOnboarding'
@@ -23,16 +21,45 @@ const OnBoardingScreen = () => {
   }, [setOnboardingStatus])
 
   return (
-    <>
-      <AuthLayoutWrapperMainContent>
-        <ConfettiLogo />
+    <AuthLayoutWrapperMainContent fullWidth hideHeader>
+      <View style={styles.wrapper}>
         <Text shouldScale={false} fontSize={20} weight="medium" style={styles.title}>
-          {t('You are ready!')}
+          {t('How To Use Ambire Wallet')}
         </Text>
-        <PinExtension style={styles.pinExtension} />
-      </AuthLayoutWrapperMainContent>
-      <AuthLayoutWrapperSideContent backgroundType="beta" style={styles.sideContent} />
-    </>
+        <View style={styles.videoBackground} />
+        <Text style={styles.link} underline>
+          <RightArrowIcon
+            withRect={false}
+            color={colors.martinique}
+            width={20}
+            height={20}
+            style={{ marginBottom: -4 }}
+          />
+          {t('How to pin Ambire Wallet extension?')}
+        </Text>
+        <Text style={styles.link} underline>
+          <RightArrowIcon
+            withRect={false}
+            color={colors.martinique}
+            width={20}
+            height={20}
+            style={{ marginBottom: -4 }}
+          />
+          {t('How to fund your wallet?')}
+        </Text>
+        <Text style={styles.link} underline>
+          <RightArrowIcon
+            withRect={false}
+            color={colors.martinique}
+            width={20}
+            height={20}
+            style={{ marginBottom: -4 }}
+          />
+          {t('What are the Wallet Reward?')}
+        </Text>
+      </View>
+      <PinExtension style={styles.pinExtension} />
+    </AuthLayoutWrapperMainContent>
   )
 }
 
