f5f43ea082e11ce2b46ee4d6d561a9335e87a305
Kaloyan Kosev <superkalo@devlabs.bg>
2023-08-07 14:30:30 +0300

    WIP: Create accounts logic

--- src/web/modules/account-adder/components/LegacyImportManager/LegacyImportManager.tsx
+++ src/web/modules/account-adder/components/LegacyImportManager/LegacyImportManager.tsx
@@ -4,7 +4,6 @@ import React, { useEffect } from 'react'
 
 import useNavigation from '@common/hooks/useNavigation'
 import useStepper from '@common/modules/auth/hooks/useStepper'
-import { WEB_ROUTES } from '@common/modules/router/constants/common'
 import useBackgroundService from '@web/hooks/useBackgroundService'
 import AccountsOnPageList from '@web/modules/account-adder/components/AccountsOnPageList'
 import useTaskQueue from '@web/modules/hardware-wallet/hooks/useTaskQueue'
@@ -19,17 +18,34 @@ const LegacyImportManager = (props: Props) => {
   const { updateStepperState } = useStepper()
   const { createTask } = useTaskQueue()
   const { state, dispatch } = useBackgroundService('accountAdder')
+  const { state: mainControllerState } = useBackgroundService('main')
+
+  useEffect(() => {
+    if (state.selectedAccounts) {
+      console.log('main controller state changed')
+    }
+  }, [mainControllerState])
 
   const onImportReady = () => {
-    updateStepperState(1, 'legacyAuth')
-    shouldCreateEmailVault
-      ? navigate(WEB_ROUTES.createEmailVault, {
-          state: {
-            hideStepper: true,
-            hideFormTitle: true
-          }
-        })
-      : navigate(WEB_ROUTES.createKeyStore)
+    dispatch({
+      type: 'MAIN_CONTROLLER_CREATE_ACCOUNTS',
+      params: {
+        accounts: state.selectedAccounts
+      }
+    })
+
+    // TODO: reset selected accounts
+
+    // TODO:
+    // updateStepperState(1, 'legacyAuth')
+    // shouldCreateEmailVault
+    //   ? navigate(WEB_ROUTES.createEmailVault, {
+    //       state: {
+    //         hideStepper: true,
+    //         hideFormTitle: true
+    //       }
+    //     })
+    //   : navigate(WEB_ROUTES.createKeyStore)
   }
 
   const setPage: any = React.useCallback(
