4dccf111e1722db126287f87b2e48a6810113986
Gergana Mihaylova <gergana@devlabs.bg>
2023-08-07 18:48:24 +0300

    Add: search component

---
+++ src/common/components/Search/styles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet, ViewStyle } from 'react-native'
+
+import colors from '@common/styles/colors'
+import spacings from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
+
+interface Style {
+  textarea: ViewStyle
+  searchSection: ViewStyle
+}
+
+const styles = StyleSheet.create<Style>({
+  textarea: {
+    flex: 1,
+    color: colors.martinique,
+    ...spacings.plMi
+  },
+  searchSection: {
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 12,
+    backgroundColor: colors.melrose_15,
+    borderColor: colors.scampi_20,
+    color: colors.martinique,
+    width: 200,
+    alignSelf: 'flex-start',
+    ...flexbox.directionRow,
+    ...flexbox.justifyCenter,
+    ...flexbox.alignCenter
+  }
+})
+
+export default styles
--- src/common/modules/dashboard/screens/styles.ts
+++ src/common/modules/dashboard/screens/styles.ts
@@ -1,35 +1,16 @@
 import { StyleSheet, ViewStyle } from 'react-native'
 
 import colors from '@common/styles/colors'
-import spacings from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
 
 interface Style {
-  textarea: ViewStyle
-  searchSection: ViewStyle
-  searchIcon: ViewStyle
+  container: ViewStyle
 }
 
 const styles = StyleSheet.create<Style>({
-  textarea: {
-    flex: 1,
-    color: colors.martinique,
-    paddingLeft: 10
-  },
-  searchSection: {
-    borderWidth: 1,
-    padding: 8,
-    borderRadius: 12,
-    backgroundColor: colors.melrose_15,
-    borderColor: colors.scampi_20,
-    color: colors.martinique,
-    width: 200,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'flex-start'
-  },
-  searchIcon: {
-    padding: 10
+  container: {
+    backgroundColor: colors.white,
+    ...flexbox.flex1
   }
 })
 
