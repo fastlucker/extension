4c9de0ca374a53e497891852be3bc492bd5a8a57
Kaloyan Kosev <superkalo@devlabs.bg>
2023-08-11 13:27:08 +0300

    Change: comment out redirect

--- src/web/modules/router/Router/Router.tsx
+++ src/web/modules/router/Router/Router.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useContext, useEffect, useState } from 'react'
+import React, { lazy, Suspense, useContext } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Route, Routes } from 'react-router-dom'
 
@@ -6,7 +6,6 @@ import Spinner from '@common/components/Spinner'
 import useNavigation from '@common/hooks/useNavigation'
 import usePrevious from '@common/hooks/usePrevious'
 import useRoute from '@common/hooks/useRoute'
-import { AUTH_STATUS } from '@common/modules/auth/constants/authStatus'
 import useAuth from '@common/modules/auth/hooks/useAuth'
 import flexbox from '@common/styles/utils/flexbox'
 import { ControllersStateLoadedContext } from '@web/contexts/controllersStateLoadedContext'
@@ -23,16 +22,19 @@ const Router = () => {
   const prevAuthStatus = usePrevious(authStatus)
   const isControllersStateLoaded = useContext(ControllersStateLoadedContext)
 
-  useEffect(() => {
-    if (
-      path !== '/' &&
-      authStatus !== prevAuthStatus &&
-      authStatus !== AUTH_STATUS.LOADING &&
-      prevAuthStatus !== AUTH_STATUS.LOADING
-    ) {
-      navigate('/', { replace: true })
-    }
-  }, [authStatus, navigate, path, prevAuthStatus])
+  // TODO: Figure out if this is still relevant. It breaks the flow of adding an
+  // account (when there is no selected account yet), as soon as an account
+  // gets selected - this clicks and redirects, which is not wanted.
+  // useEffect(() => {
+  //   if (
+  //     path !== '/' &&
+  //     authStatus !== prevAuthStatus &&
+  //     authStatus !== AUTH_STATUS.LOADING &&
+  //     prevAuthStatus !== AUTH_STATUS.LOADING
+  //   ) {
+  //     navigate('/', { replace: true })
+  //   }
+  // }, [authStatus, navigate, path, prevAuthStatus])
 
   if (!hasCheckedForApprovalInitially || !isControllersStateLoaded) {
     return (
