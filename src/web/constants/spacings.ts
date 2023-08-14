27abb4523595eb2758e4fe820bca85314d6e4d11
Gergana Mihaylova <gergana@devlabs.bg>
2023-08-14 12:19:23 +0300

    Add: height for firefox changes

--- src/web/constants/spacings.ts
+++ src/web/constants/spacings.ts
@@ -8,9 +8,7 @@ export const POPUP_WIDTH = 600
 // So this is about the size of this tab bar (magic number), that hopefully 🙏
 // is enough to extend the height so that the content doesn't get cut.
 const BROWSER_WINDOW_TOP_BAR_HEIGHT = engine === 'gecko' ? 20 : 30
-export const POPUP_HEIGHT = Math.round(
-  BROWSER_WINDOW_TOP_BAR_HEIGHT + (engine === 'gecko' ? 600 : 730)
-)
+export const POPUP_HEIGHT = Math.round(BROWSER_WINDOW_TOP_BAR_HEIGHT + 600)
 
 // This are the dimensions for a collapsed notification window
 // by default the notification window will be full screen and won't be with these dimensions
