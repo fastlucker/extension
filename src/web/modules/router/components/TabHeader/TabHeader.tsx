797ca6ba8ac64bb18ee259a44e57ba148c71cb35
Petromir <petromir@devlabs.bg>
2023-08-18 16:13:54 +0300

    change: separate HeaderLeft from TabHeader

--- src/web/modules/router/components/TabHeader/TabHeader.tsx
+++ src/web/modules/router/components/TabHeader/TabHeader.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
-import { View } from 'react-native'
+import { Pressable, View } from 'react-native'
 
 import LeftArrowIcon from '@common/assets/svg/LeftArrowIcon'
 import NavIconWrapper from '@common/components/NavIconWrapper'
@@ -8,6 +8,7 @@ import Text from '@common/components/Text'
 import useNavigation from '@common/hooks/useNavigation'
 import useRoute from '@common/hooks/useRoute'
 import routesConfig from '@common/modules/router/config/routesConfig'
+import { ROUTES } from '@common/modules/router/constants/common'
 import colors from '@common/styles/colors'
 import spacings from '@common/styles/spacings'
 import flexboxStyles from '@common/styles/utils/flexbox'
@@ -15,13 +16,30 @@ import Stepper from '@web/modules/router/components/Stepper'
 
 import styles from './styles'
 
+export const HeaderLeft = ({ handleGoBack }: { handleGoBack: () => void }) => {
+  const { t } = useTranslation()
+
+  return (
+    <Pressable
+      onPress={handleGoBack}
+      style={{ ...flexboxStyles.directionRow, ...flexboxStyles.alignCenter }}
+    >
+      <NavIconWrapper width={40} height={40} onPress={handleGoBack}>
+        <LeftArrowIcon width={40} height={40} color={colors.violet} />
+      </NavIconWrapper>
+      <Text fontSize={14} weight="regular" color={colors.martinique} style={spacings.mlTy}>
+        {t('Back')}
+      </Text>
+    </Pressable>
+  )
+}
+
 const TabHeader: React.FC<any> = ({
   hideStepper = false,
   pageTitle = '',
   forceCanGoBack,
   onBack
 }) => {
-  const { t } = useTranslation()
   const { path, params } = useRoute()
   const { navigate } = useNavigation()
 
@@ -31,33 +49,14 @@ const TabHeader: React.FC<any> = ({
   // However, there are instances when we lack a previous route (e.g., transitioning from a Popup context to a Tab).
   // To accommodate such cases and ensure button visibility, we introduce the `forceCanGoBack` flag.
   const canGoBack = forceCanGoBack || !!params?.prevRoute
-
-  const renderHeaderLeft = () => {
-    if (canGoBack) {
-      return (
-        <NavIconWrapper
-          onPress={handleGoBack}
-          style={{ ...flexboxStyles.directionRow, ...flexboxStyles.alignCenter }}
-        >
-          <LeftArrowIcon width={36} height={36} color={colors.violet} />
-          <Text fontSize={14} weight="regular" color={colors.martinique} style={spacings.ml}>
-            {t('Back')}
-          </Text>
-        </NavIconWrapper>
-      )
-    }
-
-    return null
-  }
-
-  const nextRoute = path?.substring(1) as ROUTES
+  const nextRoute = path?.substring(1) as keyof typeof ROUTES
   const { title, flow, flowStep } = routesConfig[nextRoute]
 
   const shouldDisplayStepper = flow && !hideStepper
 
   return (
     <View style={[styles.container, spacings.pv, spacings.ph]}>
-      <View>{renderHeaderLeft()}</View>
+      {canGoBack ? <HeaderLeft handleGoBack={handleGoBack} /> : null}
       {!!shouldDisplayStepper && <Stepper step={flowStep} />}
       {!shouldDisplayStepper && (!!title || !!pageTitle) && (
         <Text
