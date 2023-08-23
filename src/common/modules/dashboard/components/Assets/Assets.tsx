bbc128b0fa9664ad2c3df73d424aa4d4fec932bd
Gergana Mihaylova <gergana@devlabs.bg>
2023-08-23 13:18:41 +0300

    Add: assets toggle fixes

--- src/common/modules/dashboard/components/Assets/Assets.tsx
+++ src/common/modules/dashboard/components/Assets/Assets.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View } from 'react-native'
 
 import AfterInteractions from '@common/components/AfterInteractions'
+import { AssetsToggleContext } from '@common/modules/dashboard/contexts/assetsToggleContext'
 import colors from '@common/styles/colors'
 import spacings from '@common/styles/spacings'
 import flexbox from '@common/styles/utils/flexbox'
@@ -10,36 +11,39 @@ import Tokens from '../Tokens'
 
 // TODO props
 interface Props {
-  type: 'tokens' | 'collectibles'
   tokens: []
 }
 
-const Assets = ({ type, tokens }: Props) => (
-  <View
-    style={{
-      ...flexbox.flex1,
-      ...spacings.ph,
-      ...spacings.pv,
-      backgroundColor: colors.zircon,
-      borderTopRightRadius: 12,
-      borderTopLeftRadius: 12
-    }}
-  >
-    {type === 'tokens' && (
-      <AfterInteractions
-      /**
-       * TODO: Implementation on AfterInteractions
-       * when TokensListLoader is created
-       */
-      //   placeholder={<TokensListLoader />}
-      >
-        <Tokens tokens={tokens} />
-      </AfterInteractions>
-    )}
-    {/* {type === 'collectibles' && (
+const Assets = ({ tokens }: Props) => {
+  const { type } = useContext(AssetsToggleContext)
+
+  return (
+    <View
+      style={{
+        ...flexbox.flex1,
+        ...spacings.ph,
+        ...spacings.pv,
+        backgroundColor: colors.zircon,
+        borderTopRightRadius: 12,
+        borderTopLeftRadius: 12
+      }}
+    >
+      {type === 'tokens' && (
+        <AfterInteractions
+        /**
+         * TODO: Implementation on AfterInteractions
+         * when TokensListLoader is created
+         */
+        //   placeholder={<TokensListLoader />}
+        >
+          <Tokens tokens={tokens} />
+        </AfterInteractions>
+      )}
+      {/* {type === 'collectibles' && (
        TODO: Collectibles
       )} */}
-  </View>
-)
+    </View>
+  )
+}
 
 export default React.memo(Assets)
--- src/common/modules/dashboard/contexts/assetsToggleContext/assetsToggleContext.tsx
+++ src/common/modules/dashboard/contexts/assetsToggleContext/assetsToggleContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useMemo, useState } from 'react'
 import { Pressable, View } from 'react-native'
 
+import Search from '@common/components/Search'
 import Text from '@common/components/Text'
 import { useTranslation } from '@common/config/localization'
 import colors from '@common/styles/colors'
@@ -17,7 +18,7 @@ const AssetsToggleContext = createContext<AssetsToggleContextReturnType>({
   type: 'tokens'
 })
 
-const AssetsToggleProvider: React.FC = () => {
+const AssetsToggleProvider: React.FC = ({ children }) => {
   const [type, setType] = useState<'tokens' | 'collectibles'>('tokens')
 
   const { t } = useTranslation()
@@ -31,45 +32,50 @@ const AssetsToggleProvider: React.FC = () => {
         [type]
       )}
     >
-      <View style={[flexbox.directionRow]}>
-        <Pressable onPress={() => setType('tokens')}>
-          {/* todo: add the border radius here */}
-          <View
-            style={{
-              borderBottomColor: type === 'tokens' ? colors.violet : 'transparent',
-              ...styles.toggleItem
-            }}
-          >
-            <Text
-              shouldScale={false}
-              weight="regular"
-              color={type === 'tokens' ? colors.violet : colors.martinique_65}
-              fontSize={16}
-              style={[spacings.mbTy]}
+      <View style={[flexbox.directionRow, flexbox.justifySpaceBetween, spacings.ph]}>
+        <View style={[flexbox.directionRow]}>
+          <Pressable onPress={() => setType('tokens')}>
+            {/* todo: add the border radius here */}
+            <View
+              style={{
+                borderBottomColor: type === 'tokens' ? colors.violet : 'transparent',
+                ...styles.toggleItem
+              }}
             >
-              {t('Tokens')}
-            </Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => setType('collectibles')}>
-          <View
-            style={{
-              borderBottomColor: type === 'collectibles' ? colors.violet : 'transparent',
-              ...styles.toggleItem
-            }}
-          >
-            <Text
-              shouldScale={false}
-              weight="regular"
-              color={type === 'collectibles' ? colors.violet : colors.martinique_65}
-              fontSize={16}
-              style={[spacings.mbTy]}
+              <Text
+                shouldScale={false}
+                weight="regular"
+                color={type === 'tokens' ? colors.violet : colors.martinique_65}
+                fontSize={16}
+                style={[spacings.mbTy]}
+              >
+                {t('Tokens')}
+              </Text>
+            </View>
+          </Pressable>
+          <Pressable onPress={() => setType('collectibles')}>
+            <View
+              style={{
+                borderBottomColor: type === 'collectibles' ? colors.violet : 'transparent',
+                ...styles.toggleItem
+              }}
             >
-              {t('Collectibles')}
-            </Text>
-          </View>
-        </Pressable>
+              <Text
+                shouldScale={false}
+                weight="regular"
+                color={type === 'collectibles' ? colors.violet : colors.martinique_65}
+                fontSize={16}
+                style={[spacings.mbTy]}
+              >
+                {t('Collectibles')}
+              </Text>
+            </View>
+          </Pressable>
+        </View>
+
+        <Search />
       </View>
+      {children}
     </AssetsToggleContext.Provider>
   )
 }
