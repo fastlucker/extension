587d5032ce4a449dee1cb6ec1ae81801a086c54c
Gergana Mihaylova <gergana@devlabs.bg>
2023-08-23 13:19:58 +0300

    Add: display tokens and gas tokens

--- src/common/modules/dashboard/components/Tokens/Tokens.tsx
+++ src/common/modules/dashboard/components/Tokens/Tokens.tsx
@@ -18,7 +18,7 @@ interface Props {
 const Tokens = ({ tokens }: Props) => {
   const { t } = useTranslation()
   // TODO: we will have different sorting here on v2. We will have pinned tokens with 0 balance, gas tokens and etc so this will be decided over time once all of them are wired up
-  const sortedTokens = tokens.sort((a, b) => b.balanceUSD - a.balanceUSD)
+  const sortedTokens = tokens
 
   // TODO: Calculate each token height and apply scroll
   return (
@@ -29,16 +29,20 @@ const Tokens = ({ tokens }: Props) => {
       {/* {!isCurrNetworkBalanceLoading && <Rewards />} */}
 
       {!!sortedTokens.length &&
-        sortedTokens.map(({ address, symbol, network, balance, balanceUSD }: any, i: number) => (
-          <TokenItem
-            key={`token-${address}-${i}`}
-            symbol={symbol}
-            balance={balance}
-            balanceUSD={balanceUSD}
-            address={address}
-            network={network}
-          />
-        ))}
+        sortedTokens.map(
+          ({ address, amount, decimals, networkId, priceIn, symbol, gasToken }: any, i: number) => (
+            <TokenItem
+              key={`token-${address}-${i}`}
+              address={address}
+              amount={amount}
+              decimals={decimals}
+              networkId={networkId}
+              priceIn={priceIn}
+              symbol={symbol}
+              gasToken={gasToken}
+            />
+          )
+        )}
       {/* TODO: implementation of add custom token will be in sprint 4 */}
       <Pressable>
         {({ hovered }: any) => (
