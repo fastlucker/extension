530c6baf2a65033f11d0fd69eb6ac460ca159202
Petromir <petromir@devlabs.bg>
2023-09-20 10:42:04 +0300

    fix: account button in header

--- src/common/modules/header/components/Header/styles.ts
+++ src/common/modules/header/components/Header/styles.ts
@@ -1,8 +1,9 @@
-import { Platform, StyleSheet, TextStyle, ViewStyle } from 'react-native'
+import { ImageStyle, Platform, StyleSheet, TextStyle, ViewStyle } from 'react-native'
 
 import { isWeb } from '@common/config/env'
 import colors from '@common/styles/colors'
-import spacings, { SPACING } from '@common/styles/spacings'
+import spacings from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
 import commonWebStyles from '@web/styles/utils/common'
 
 export const HEADER_HEIGHT = Platform.select({
@@ -16,6 +17,14 @@ interface Styles {
   navIconContainerRegular: ViewStyle
   title: TextStyle
   sideContainer: ViewStyle
+  // Account
+  account: ViewStyle
+  accountButton: ViewStyle
+  accountButtonRightIcon: ViewStyle
+  accountButtonInfo: ViewStyle
+  accountButtonInfoIcon: ImageStyle
+  accountButtonInfoText: TextStyle
+  accountCopyIcon: ViewStyle
 }
 
 const styles = StyleSheet.create<Styles>({
@@ -48,7 +57,29 @@ const styles = StyleSheet.create<Styles>({
   sideContainer: {
     width: isWeb ? 180 : 120,
     minWidth: isWeb ? 180 : 120
-  }
+  },
+  // Account
+  account: {
+    ...flexbox.directionRow,
+    ...flexbox.alignCenter
+  },
+  accountButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 8,
+    ...spacings.phTy,
+    backgroundColor: '#B6B9FF26',
+    borderWidth: 1,
+    borderColor: '#6770B333',
+    borderRadius: 12,
+    minWidth: 235
+  },
+  accountButtonRightIcon: { borderColor: 'transparent', borderRadius: 10 },
+  accountButtonInfo: { ...flexbox.directionRow, ...flexbox.alignCenter },
+  accountButtonInfoIcon: { width: 30, height: 30, borderRadius: 10 },
+  accountButtonInfoText: { ...spacings.mlTy },
+  accountCopyIcon: { backgroundColor: 'transparent', borderColor: 'transparent' }
 })
 
 export default styles
