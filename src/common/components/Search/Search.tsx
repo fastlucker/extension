d576f45fb6c39f8dc7e8ff277d336d1800c0168d
Petromir <petromir@devlabs.bg>
2023-09-26 17:37:28 +0300

    add: Controller to Search component

--- src/common/components/Search/Search.tsx
+++ src/common/components/Search/Search.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Control, Controller } from 'react-hook-form'
 import { View, ViewProps } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 
@@ -10,19 +11,29 @@ import styles from './styles'
 type Props = {
   placeholder?: string
   style?: ViewProps
+  control: Control<{ search: string }, any>
 }
 
-const Search = ({ placeholder = 'Search', style }: Props) => {
+const Search = ({ placeholder = 'Search', style, control }: Props) => {
   return (
     <View style={[styles.searchSection, style]}>
       <SearchIcon color={colors.martinique_65} />
-      <TextInput
-        editable
-        numberOfLines={1}
-        maxLength={25}
-        placeholder={placeholder}
-        style={[styles.textarea]}
-        placeholderTextColor={colors.martinique_65}
+      <Controller
+        control={control}
+        name="search"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            editable
+            numberOfLines={1}
+            maxLength={25}
+            placeholder={placeholder}
+            style={[styles.textarea]}
+            placeholderTextColor={colors.martinique_65}
+            onBlur={onBlur}
+            onChange={onChange}
+            value={value}
+          />
+        )}
       />
     </View>
   )
