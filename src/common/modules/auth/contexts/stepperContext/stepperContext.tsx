f8863a7bcafc15a60e48ef1f76706125b29ca023
Petromir <petromir@devlabs.bg>
2023-09-26 13:14:03 +0300

    change: rework stepperContext

--- src/common/modules/auth/contexts/stepperContext/stepperContext.tsx
+++ src/common/modules/auth/contexts/stepperContext/stepperContext.tsx
@@ -2,82 +2,99 @@ import React, { createContext, useCallback, useEffect, useMemo, useState } from 
 
 import useRoute from '@common/hooks/useRoute'
 import useStorageController from '@common/hooks/useStorageController'
+import { WEB_ROUTES } from '@common/modules/router/constants/common'
 
-const StepperContext = createContext<any>({
-  updateStepperState: () => Promise.resolve(),
-  stepperState: { currentStep: 0, currentFlow: 'email' },
-  getCurrentFlowSteps: () => {}
-})
-
-const flows = {
-  email: [
-    'Setup Key\nStore',
-    'Create Email\nVault',
-    'Email\nConfirmation',
-    'Personalize\nAccounts'
-  ],
-  hw: ['Pick Hardware Device', 'Pick Accounts To Import', 'Personalize\nAccounts'],
-  legacy: ['Setup Key\nStore', 'Import Legacy Account', 'Personalize\nAccounts']
+const STEPPER_FLOWS = {
+  email: {
+    [WEB_ROUTES.keyStoreSetup]: 'Setup Key\nStore',
+    [WEB_ROUTES.createEmailVault]: 'Create Email\nVault',
+    'email-confirmation': 'Email\nConfirmation',
+    [WEB_ROUTES.accountPersonalize]: 'Personalize\nAccounts'
+  },
+  hw: {
+    [WEB_ROUTES.hardwareWalletSelect]: 'Pick Hardware Wallet',
+    'connect-hardware-wallet': 'Connect Hardware Wallet',
+    [WEB_ROUTES.accountAdder]: 'Pick Accounts To Import',
+    [WEB_ROUTES.accountPersonalize]: 'Personalize\nAccounts'
+  },
+  legacy: {
+    [WEB_ROUTES.keyStoreSetup]: 'Setup Key\nStore',
+    [WEB_ROUTES.externalSigner]: 'Import Legacy Account',
+    [WEB_ROUTES.accountAdder]: 'Pick Accounts To Import',
+    [WEB_ROUTES.accountPersonalize]: 'Personalize\nAccounts'
+  }
 }
 
+// Edge cases like connecting HW wallets, because one step can be many routes.
+const EXTRA_STEP_SCREENS = [WEB_ROUTES.hardwareWalletLedger]
+
+const ALL_STEPS_SCREENS = Object.keys(STEPPER_FLOWS)
+  .map((key) => {
+    // @ts-ignore
+    return Object.keys(STEPPER_FLOWS[key]).map((key1) => {
+      return key1
+    })
+  })
+  .concat(EXTRA_STEP_SCREENS)
+  .flat()
+
+const StepperContext = createContext<{
+  updateStepperState: (newStep: string, newFlow: keyof typeof STEPPER_FLOWS) => void
+  stepperState: { currentStep: number; currentFlow: keyof typeof STEPPER_FLOWS } | null
+  getCurrentFlowSteps: () => string[]
+}>({
+  updateStepperState: () => Promise.resolve(),
+  stepperState: null,
+  getCurrentFlowSteps: () => []
+})
+
 const StepperProvider = ({ children }: { children: React.ReactNode }) => {
-  const { getItem, setItem } = useStorageController()
   const { path } = useRoute()
-  const [paths, setPaths] = useState<string[]>(() => {
-    const storedState = getItem('navigationPaths')
-    if (!storedState) return []
+  const { getItem, setItem } = useStorageController()
 
-    const parsedState = JSON.parse(storedState)
-
-    // In case the user refreshes the page, we need to make sure that the path is in the array
-    if (!parsedState.includes(path)) return []
-
-    return parsedState
-  })
-
-  const [stepperState, setStepperState] = useState(() => {
+  const [stepperState, setStepperState] = useState<{
+    currentStep: number
+    currentFlow: keyof typeof STEPPER_FLOWS
+  } | null>(() => {
     const storedState = getItem('stepperState')
-    return storedState
-      ? JSON.parse(storedState)
-      : {
-          currentStep: 0,
-          currentFlow: 'email'
-        }
+
+    if (!storedState) return null
+
+    return JSON.parse(storedState)
   })
+
+  useEffect(() => {
+    if (!path) return
+
+    const pathWithoutSlash = path.slice(1)
+
+    if (ALL_STEPS_SCREENS.includes(pathWithoutSlash)) return
+
+    setStepperState(null)
+  }, [path])
 
   useEffect(() => {
     setItem('stepperState', JSON.stringify(stepperState))
-    setItem('navigationPaths', JSON.stringify(paths))
-  }, [stepperState, setItem, paths])
+  }, [stepperState, setItem])
 
-  const updateStepperState = (newStep: number, newFlow: string) => {
-    setStepperState((prevState: { newStep: number; newFlow: string }) => ({
-      ...prevState,
-      ...{ currentStep: newStep, currentFlow: newFlow }
-    }))
-  }
-
-  // This useEffect is used to update the stepperState when the user goes back.
-  // Using a paths list ensures that we can detect the user going back, even if
-  // they use browser back buttons, shortcuts or refresh the page.
-  useEffect(() => {
-    if (path && typeof path === 'string') {
-      setPaths((prevState: string[]) => {
-        if (!prevState.includes(path)) {
-          return [...prevState, path]
-        }
-        setStepperState((prevStepperState: { currentStep: number; currentFlow: string }) => ({
-          ...prevStepperState,
-          currentStep: prevStepperState.currentStep - 1
-        }))
-        return prevState.slice(0, -1)
-      })
+  const updateStepperState = useCallback((newStep: string, newFlow: keyof typeof STEPPER_FLOWS) => {
+    const newStepIndex = Object.keys(STEPPER_FLOWS[newFlow]).indexOf(newStep)
+    if (newStepIndex === -1) {
+      console.error(`Step ${newStep} does not exist in flow ${newFlow}`)
+      return
     }
-  }, [path])
+
+    setStepperState({ currentStep: newStepIndex, currentFlow: newFlow })
+  }, [])
 
   const getCurrentFlowSteps = useCallback(() => {
+    if (!stepperState) return []
+
     const currentFlow = stepperState.currentFlow
-    return flows[currentFlow] || []
+
+    return Object.keys(STEPPER_FLOWS[currentFlow]).map((key) => {
+      return STEPPER_FLOWS[currentFlow][key]
+    })
   }, [stepperState])
 
   const value = useMemo(
@@ -86,7 +103,7 @@ const StepperProvider = ({ children }: { children: React.ReactNode }) => {
       updateStepperState,
       getCurrentFlowSteps
     }),
-    [stepperState, getCurrentFlowSteps]
+    [stepperState, updateStepperState, getCurrentFlowSteps]
   )
 
   return <StepperContext.Provider value={value}>{children}</StepperContext.Provider>
