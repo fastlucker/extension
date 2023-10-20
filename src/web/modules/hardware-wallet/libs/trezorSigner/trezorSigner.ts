f0c9323252cacba232f992292605aaa952bd50f3
Kaloyan Kosev <superkalo@devlabs.bg>
2023-10-20 14:42:08 +0300

    Add: types

--- src/web/modules/hardware-wallet/libs/trezorSigner/trezorSigner.ts
+++ src/web/modules/hardware-wallet/libs/trezorSigner/trezorSigner.ts
@@ -1,15 +1,13 @@
-import { stripHexPrefix, toChecksumAddress } from 'ethereumjs-util'
+import { stripHexPrefix } from 'ethereumjs-util'
 
 import { TREZOR_HD_PATH } from '@ambire-common/consts/derivation'
-import { ExternalKey, Key, KeystoreSigner } from '@ambire-common/interfaces/keystore'
+import { ExternalKey, KeystoreSigner } from '@ambire-common/interfaces/keystore'
 import { TypedMessage } from '@ambire-common/interfaces/userRequest'
 import { delayPromise } from '@common/utils/promises'
 import { serialize } from '@ethersproject/transactions'
 import transformTypedData from '@trezor/connect-plugin-ethereum'
 import trezorConnect from '@trezor/connect-web'
 import TrezorController from '@web/modules/hardware-wallet/controllers/TrezorController'
-
-import type { TypedDataDomain, TypedDataField } from '@ethersproject/abstract-signer'
 
 const DELAY_BETWEEN_POPUPS = 1000
 const EIP_155_CONSTANT = 35
@@ -71,14 +69,12 @@ class TrezorSigner implements KeystoreSigner {
 
   async signTypedData({ domain, types, message, primaryType }: TypedMessage) {
     if (!this.controller) {
-      throw new Error('trezorSigner: trezorController not initialized')
+      throw new Error(
+        'Something went wrong with triggering the sign message mechanism. Please try again or contact support if the problem persists.'
+      )
     }
 
-    if (!types.EIP712Domain) {
-      throw new Error('trezorSigner: only EIP712 messages are supported')
-    }
-
-    const dataWithHashes: any = transformTypedData({ domain, types, message, primaryType }, true)
+    const dataWithHashes = transformTypedData({ domain, types, message, primaryType }, true)
 
     // eslint-disable-next-line @typescript-eslint/naming-convention
     const { domain_separator_hash, message_hash } = dataWithHashes
@@ -111,7 +107,9 @@ class TrezorSigner implements KeystoreSigner {
 
   async signMessage(hash: string | Uint8Array) {
     if (!this.controller) {
-      throw new Error('trezorSigner: trezorController not initialized')
+      throw new Error(
+        'Something went wrong with triggering the sign message mechanism. Please try again or contact support if the problem persists.'
+      )
     }
 
     const status = await this.controller.unlock()
