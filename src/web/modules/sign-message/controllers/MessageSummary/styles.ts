ccf33e365aa8000c70b57c3f5958c20dea02fb67
Petromir <petromir@devlabs.bg>
2023-10-30 10:47:29 +0200

    change: simplify MessageSummary

--- src/web/modules/sign-message/controllers/MessageSummary/styles.ts
+++ src/web/modules/sign-message/controllers/MessageSummary/styles.ts
@@ -1,56 +1,34 @@
-import { StyleSheet, TextStyle, ViewStyle } from 'react-native'
+import { StyleSheet, ViewStyle } from 'react-native'
 
-import colors from '@common/styles/colors'
 import spacings from '@common/styles/spacings'
+import { ThemeProps } from '@common/styles/themeConfig'
+import common from '@common/styles/utils/common'
 import flexbox from '@common/styles/utils/flexbox'
-import { getUiType } from '@web/utils/uiType'
 
 interface Style {
   container: ViewStyle
   header: ViewStyle
-  headerContent: ViewStyle
-  body: ViewStyle
-  bodyText: TextStyle
-  explorerIcon: ViewStyle
 }
 
-const { isTab } = getUiType()
+const getStyles = (theme: ThemeProps) =>
+  StyleSheet.create<Style>({
+    container: {
+      backgroundColor: theme.secondaryBackground,
+      borderWidth: 1,
+      ...common.borderRadiusPrimary,
+      borderColor: theme.secondaryBorder,
+      ...spacings.mb,
+      minHeight: 52,
+      justifyContent: 'center'
+    },
+    header: {
+      ...flexbox.directionRow,
+      ...flexbox.alignCenter,
+      ...flexbox.justifyCenter,
+      ...flexbox.justifySpaceBetween,
+      ...spacings.ph,
+      ...spacings.pvSm
+    }
+  })
 
-const styles = StyleSheet.create<Style>({
-  container: {
-    backgroundColor: colors.melrose_15,
-    borderWidth: 1,
-    borderRadius: 12,
-    borderColor: colors.chetwode_50,
-    ...spacings.mb,
-    minHeight: 53,
-    justifyContent: 'center'
-  },
-  header: {
-    ...flexbox.directionRow,
-    ...flexbox.alignCenter,
-    ...flexbox.justifyCenter,
-    ...flexbox.justifySpaceBetween,
-    paddingHorizontal: 10,
-    paddingVertical: 10
-  },
-  headerContent: {
-    ...flexbox.flex1,
-    ...flexbox.directionRow,
-    ...flexbox.alignCenter,
-    ...flexbox.wrap,
-    marginHorizontal: 10
-  },
-  body: {
-    padding: 10
-  },
-  bodyText: {
-    marginBottom: isTab ? 25 : 5,
-    color: colors.martinique
-  },
-  explorerIcon: {
-    marginLeft: -4
-  }
-})
-
-export default styles
+export default getStyles
