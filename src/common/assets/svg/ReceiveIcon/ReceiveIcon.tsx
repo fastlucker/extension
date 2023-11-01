c2198ea2440a10035f8b092665895f408642aa30
Petromir <petromir@devlabs.bg>
2023-11-01 15:09:34 +0200

    fix: receiveicon angle

--- src/common/assets/svg/ReceiveIcon/ReceiveIcon.tsx
+++ src/common/assets/svg/ReceiveIcon/ReceiveIcon.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
-import Svg, { Path, SvgProps } from 'react-native-svg'
+import { ColorValue } from 'react-native'
+import Svg, { G, Path, SvgProps } from 'react-native-svg'
 
 import colors from '@common/styles/colors'
 
 interface Props extends SvgProps {
   width?: number
   height?: number
+  color?: string | ColorValue
 }
 
-const ReceiveIcon: React.FC<Props> = ({ width = 24, height = 24, color, ...rest }) => (
-  <Svg width={width} height={height} viewBox="0 0 24 24" {...rest}>
-    <Path fill="none" d="M0 24V0h24v24z" />
-    <Path
-      d="m18.153 16.38-9.186.048 9.56-9.56a.992.992 0 0 1 .698-.293.978.978 0 0 1 .7.291.986.986 0 0 1-.005 1.395l-6.172 6.172 4.41-.026a.977.977 0 0 1 .983.982.991.991 0 0 1-.291.7.992.992 0 0 1-.697.29ZM4.777 17.838l.049-9.186a1 1 0 0 1 .99-.99.976.976 0 0 1 .981.981l-.022 4.414 6.172-6.172a.986.986 0 0 1 1.395-.005.978.978 0 0 1 .29.7.993.993 0 0 1-.292.698Z"
-      fill={colors.titan}
-    />
+const ReceiveIcon: React.FC<Props> = ({
+  width = 24,
+  height = 24,
+  color = colors.martinique,
+  ...rest
+}) => (
+  <Svg viewBox="0 0 23.988 18.609" width={width} height={height} {...rest}>
+    <G fill="none" stroke={color} strokeLinecap="round" strokeWidth="1.5">
+      <Path d="M13.668 4.829 5.58 12.917l.002-7.1" data-name="Path 2205" />
+      <Path d="m19.159 4.94-8.089 8.089 7.101-.003" data-name="Path 2206" />
+    </G>
   </Svg>
 )
 
