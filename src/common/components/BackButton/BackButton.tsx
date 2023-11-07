632e957961e6339f4dbe86d8e75c8a370d129f91
Petromir <petromir@devlabs.bg>
2023-11-07 17:40:34 +0200

    change: simplify backbutton

--- src/common/components/BackButton/BackButton.tsx
+++ src/common/components/BackButton/BackButton.tsx
@@ -3,21 +3,31 @@ import { useTranslation } from 'react-i18next'
 import { TouchableOpacity } from 'react-native'
 
 import LeftArrowIcon from '@common/assets/svg/LeftArrowIcon'
+import useNavigation from '@common/hooks/useNavigation'
 import spacings from '@common/styles/spacings'
 import flexbox from '@common/styles/utils/flexbox'
 
 import Text from '../Text'
 
 interface Props {
-  onPress: () => void
+  onPress?: () => void
 }
 
 const BackButton: FC<Props> = ({ onPress }) => {
   const { t } = useTranslation()
+  const { goBack } = useNavigation()
+
   return (
     <TouchableOpacity
       style={[flexbox.directionRow, flexbox.alignCenter, spacings.mr2Xl]}
-      onPress={onPress}
+      onPress={() => {
+        if (onPress) {
+          onPress()
+          return
+        }
+
+        goBack()
+      }}
     >
       <LeftArrowIcon />
       <Text style={spacings.plTy} fontSize={16} weight="medium" appearance="secondaryText">
