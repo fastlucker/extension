a79f9651b893276b7c1bd38945b913519ad5bb23
Petromir <petromir@devlabs.bg>
2023-11-07 13:59:56 +0200

    delete: unused style, GetStartedScreen

--- src/web/modules/auth/screens/GetStartedScreen/styles.ts
+++ src/web/modules/auth/screens/GetStartedScreen/styles.ts
@@ -1,13 +1,10 @@
 import { StyleSheet, ViewStyle } from 'react-native'
 
-import colors from '@common/styles/colors'
-import spacings, { SPACING_MD } from '@common/styles/spacings'
+import spacings from '@common/styles/spacings'
 import { ThemeProps } from '@common/styles/themeConfig'
 import flexbox from '@common/styles/utils/flexbox'
 
 interface Style {
-  hr: ViewStyle
-  logo: ViewStyle
   separatorHorizontal: ViewStyle
   showMoreOptionsButtonContainer: ViewStyle
   showMoreOptionsButton: ViewStyle
@@ -15,19 +12,6 @@ interface Style {
 
 const getStyles = (theme: ThemeProps) =>
   StyleSheet.create<Style>({
-    hr: {
-      marginVertical: SPACING_MD * 2,
-      height: 1,
-      width: 500,
-      backgroundColor: colors.scampi_20
-    },
-    logo: {
-      position: 'absolute',
-      bottom: 27,
-      right: '50%',
-      marginRight: -(SPACING_MD * 2),
-      zIndex: 10
-    },
     separatorHorizontal: {
       position: 'absolute',
       width: '100%',
