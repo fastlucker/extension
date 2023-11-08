5e7a470084870b9250c19b6b4dea0cf5ae23c6e2
Kaloyan Kosev <superkalo@devlabs.bg>
2023-11-08 17:48:50 +0200

    Add: settings controller state, hook and provider

---
+++ src/web/contexts/settingsControllerStateContext/settingsControllerStateContext.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+import React, { createContext, useEffect, useMemo, useState } from 'react'
+
+import { SettingsController } from '@ambire-common/controllers/settings/settings'
+import eventBus from '@web/extension-services/event/eventBus'
+import useBackgroundService from '@web/hooks/useBackgroundService'
+import useMainControllerState from '@web/hooks/useMainControllerState'
+
+const SettingsControllerStateContext = createContext<SettingsController>({} as SettingsController)
+
+const SettingsControllerStateProvider: React.FC<any> = ({ children }) => {
+  const [state, setState] = useState({} as SettingsController)
+  const { dispatch } = useBackgroundService()
+  const mainState = useMainControllerState()
+
+  useEffect(() => {
+    if (mainState.isReady && !Object.keys(state).length) {
+      dispatch({
+        type: 'INIT_CONTROLLER_STATE',
+        params: { controller: 'settings' }
+      })
+    }
+  }, [dispatch, mainState.isReady, state])
+
+  useEffect(() => {
+    const onUpdate = (newState: SettingsController) => {
+      setState(newState)
+    }
+
+    eventBus.addEventListener('settings', onUpdate)
+
+    return () => eventBus.removeEventListener('settings', onUpdate)
+  }, [])
+
+  return (
+    <SettingsControllerStateContext.Provider value={useMemo(() => state, [state])}>
+      {children}
+    </SettingsControllerStateContext.Provider>
+  )
+}
+
+export { SettingsControllerStateProvider, SettingsControllerStateContext }
