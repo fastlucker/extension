e4b34b9314978eabb912570dc9f59999b116e8d5
sonytooo <simeonpetkov11999977@gmail.com>
2023-11-09 13:14:18 +0200

    redundant code cleanup from styles

--- src/web/modules/notification-requests/screens/PermissionRequestScreen/styles.ts
+++ src/web/modules/notification-requests/screens/PermissionRequestScreen/styles.ts
@@ -1,15 +1,10 @@
 import { ImageStyle, StyleSheet, TextStyle, ViewStyle } from 'react-native'
 
-import colors from '@common/styles/colors'
-import spacings, { SPACING_MI, SPACING_SM } from '@common/styles/spacings'
+import spacings from '@common/styles/spacings'
 import common from '@common/styles/utils/common'
 import flexbox from '@common/styles/utils/flexbox'
 
 interface Style {
-  showQueueButton: ViewStyle
-  textarea: ViewStyle
-  buttonsContainer: ViewStyle
-  buttonWrapper: ViewStyle
   accountInfo: ViewStyle
   accountInfoIcon: ImageStyle
   accountInfoText: TextStyle
@@ -17,32 +12,6 @@ interface Style {
 }
 
 const styles = StyleSheet.create<Style>({
-  showQueueButton: {
-    position: 'absolute',
-    right: SPACING_SM,
-    top: SPACING_SM,
-    borderRadius: 50,
-    padding: 3,
-    backgroundColor: colors.wooed
-  },
-  textarea: {
-    minHeight: 120,
-    flex: 1,
-    backgroundColor: colors.mirage,
-    ...common.borderRadiusPrimary,
-    ...spacings.ph,
-    ...spacings.pv,
-    ...spacings.mbLg
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    marginHorizontal: -SPACING_MI,
-    ...spacings.mbTy
-  },
-  buttonWrapper: {
-    marginHorizontal: SPACING_MI,
-    flex: 1
-  },
   accountInfo: {
     ...flexbox.directionRow,
     ...flexbox.alignCenter
