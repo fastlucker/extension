4a76a7ca74c02490d15840e1947f2f97aac42ed8
Kaloyan Kosev <superkalo@devlabs.bg>
2023-11-10 18:32:38 +0200

    Add: acc pref in the pay options

--- src/web/modules/sign-account-op/components/Estimation/components/PayOption.tsx
+++ src/web/modules/sign-account-op/components/Estimation/components/PayOption.tsx
@@ -2,23 +2,30 @@ import React from 'react'
 import { Image, View } from 'react-native'
 
 import Text from '@common/components/Text'
+import { DEFAULT_ACCOUNT_LABEL } from '@common/constants/account'
 import TokenIcon from '@common/modules/dashboard/components/TokenIcon'
 import spacings from '@common/styles/spacings'
 import common from '@common/styles/utils/common'
 import flexbox from '@common/styles/utils/flexbox'
+import useSettingsControllerState from '@web/hooks/useSettingsControllerState'
+import { getAccountPfpSource } from '@web/modules/account-personalize/components/AccountPersonalizeCard/avatars'
 import shortenAddress from '@web/utils/shortenAddress'
 
 const PayOption = ({ account, token }: any) => {
+  const settingsCtrl = useSettingsControllerState()
+  const accountPref = settingsCtrl.accountPreferences[account.addr]
+  const pfpSource = getAccountPfpSource(accountPref?.pfp)
+  const label = accountPref.label || DEFAULT_ACCOUNT_LABEL
+
   return (
     <View style={[flexbox.directionRow, flexbox.alignCenter]}>
       <Image
         style={[{ width: 32, height: 32, ...spacings.mrTy, ...common.borderRadiusPrimary } as any]}
-        source={{
-          uri:
-            account.pfp || 'https://mars-images.imgix.net/nft/1629012092532.png?auto=compress&w=600'
-        }}
+        source={pfpSource}
       />
-      <Text weight="medium">{shortenAddress(account.addr, 11)}</Text>
+      <Text weight="medium">
+        {label} ({shortenAddress(account.addr, 11)})
+      </Text>
       <Text weight="medium"> - </Text>
       <TokenIcon
         containerHeight={32}
