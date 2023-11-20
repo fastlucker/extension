07d0fa33d63ed44f3e91a427fc58fda82f63c3aa
Petromir <petromir@devlabs.bg>
2023-11-20 11:13:50 +0200

    add: temp add account btn in account settings

--- src/web/modules/settings/screens/AccountsSettingsScreen/AccountsSettingsScreen.tsx
+++ src/web/modules/settings/screens/AccountsSettingsScreen/AccountsSettingsScreen.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { View } from 'react-native'
 
+import Button from '@common/components/Button'
 import Search from '@common/components/Search'
 import Text from '@common/components/Text'
 import useAccounts from '@common/hooks/useAccounts/useAccounts'
+import useNavigation from '@common/hooks/useNavigation'
+import { ROUTES } from '@common/modules/router/constants/common'
 import spacings from '@common/styles/spacings'
 import flexboxStyles from '@common/styles/utils/flexbox'
 import Account from '@web/modules/account-select/components/Account'
@@ -12,6 +15,7 @@ import SettingsPage from '@web/modules/settings/components/SettingsPage'
 
 const AccountsSettingsScreen = () => {
   const { t } = useTranslation()
+  const { navigate } = useNavigation()
   const { accounts, control } = useAccounts()
 
   return (
@@ -29,11 +33,12 @@ const AccountsSettingsScreen = () => {
         </Text>
         <Search placeholder="Search for account" control={control} />
       </View>
-      <View>
+      <View style={spacings.mb}>
         {accounts.map((account) => (
           <Account isCopyVisible={false} key={account.addr} account={account} />
         ))}
       </View>
+      <Button onPress={() => navigate(ROUTES.getStarted)} text="Add account" />
     </SettingsPage>
   )
 }
