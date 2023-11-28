271bc4a7848bd81d935566f2893a6e4d581f15a9
sonytooo <simeonpetkov11999977@gmail.com>
2023-11-28 16:54:24 +0200

    add: content script comments

--- src/web/extension-services/content-script/content-script.ts
+++ src/web/extension-services/content-script/content-script.ts
@@ -1,31 +1,25 @@
-// @ts-nocheck
-
 // Content Script is mainly a relayer between pageContext(injected script) and the background service_worker
 import { nanoid } from 'nanoid'
 import { v4 as uuid } from 'uuid'
 
 import BroadcastChannelMessage from '@web/extension-services/message/broadcastChannelMessage'
-// Middleware for handling messages between dapps and the extension's background process
-// import { browserapi, engine } from '@web/constants/browserapi'
 import PortMessage from '@web/extension-services/message/portMessage'
 
 const channelName = nanoid()
+// we need an additional check for Opera because
+// there are some differences in the injection of the provider in inpage.ts
 const isOpera = /Opera|OPR\//i.test(navigator.userAgent)
 
-const injectProviderScript = async (isDefaultWallet: boolean) => {
-  // the script element with src won't execute immediately
-  // use inline script element instead!
+const injectProviderScript = (isDefaultWallet: boolean) => {
+  // the script element with src won't execute immediately use inline script element instead!
   const container = document.head || document.documentElement
   const ele = document.createElement('script')
-  // in prevent of webpack optimized code do some magic(e.g. double/sigle quote wrap),
-  // separate content assignment to two line
-  // use AssetReplacePlugin to replace page provider content
-
   let content = ';(function () {'
   content += `const ambireChannelName = '${channelName}';`
   content += `const ambireIsDefaultWallet = ${isDefaultWallet};`
   content += `const ambireId = '${uuid()}';`
   content += `const ambireIsOpera = ${isOpera};`
+  // PAGEPROVIDER comes from webpack to inject the content of the inpage.ts file into the webpage
   content += '#PAGEPROVIDER#'
   content += '\n})();'
   ele.textContent = content
@@ -34,9 +28,11 @@ const injectProviderScript = async (isDefaultWallet: boolean) => {
 }
 
 const pm = new PortMessage().connect()
-const bcm = new BroadcastChannelMessage(channelName).listen((data) => pm.request(data))
 
-// background notification
+// messages coming from inpage.ts will be passed to the background service (handled by pm.listen(...))
+const bcm = new BroadcastChannelMessage(channelName).listen((data: any) => pm.request(data))
+
+// messages coming from the background service and will be passed to the injected script (handled in inpage.ts)
 pm.on('message', (data) => bcm.send('message', data))
 
 document.addEventListener('beforeunload', () => {
