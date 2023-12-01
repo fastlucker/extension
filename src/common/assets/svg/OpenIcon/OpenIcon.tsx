50995c5578a2895c7856eb5b0a9d78058c077eda
sonytooo <simeonpetkov11999977@gmail.com>
2023-12-01 11:40:10 +0200

    fix: color prop on the OpenIcon

--- src/common/assets/svg/OpenIcon/OpenIcon.tsx
+++ src/common/assets/svg/OpenIcon/OpenIcon.tsx
@@ -11,14 +11,19 @@ interface Props extends SvgProps {
   strokeWidth?: string
 }
 
-const OpenIcon: React.FC<Props> = ({ width = 19, height = 19, color, strokeWidth = '1.5' }) => (
+const OpenIcon: React.FC<Props> = ({
+  width = 19,
+  height = 19,
+  color = iconColors.primary,
+  strokeWidth = '1.5'
+}) => (
   <Svg width={width} height={height} viewBox="0 0 18.951 18.951">
     <G id="open" transform="translate(0.75 0.75)">
       <Path
         d="M13,9.955,20.156,2.8"
         transform="translate(-3.403 -2.102)"
         fill="none"
-        stroke={iconColors.primary || color}
+        stroke={color}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={strokeWidth}
@@ -27,7 +32,7 @@ const OpenIcon: React.FC<Props> = ({ width = 19, height = 19, color, strokeWidth
         d="M21.39,6.188V2H17.2"
         transform="translate(-3.939 -2)"
         fill="none"
-        stroke={iconColors.primary || color}
+        stroke={color}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={strokeWidth}
@@ -36,7 +41,7 @@ const OpenIcon: React.FC<Props> = ({ width = 19, height = 19, color, strokeWidth
         d="M9.855,2H8.109C3.747,2,2,3.745,2,8.108v5.235c0,4.363,1.745,6.108,6.108,6.108h5.235c4.363,0,6.108-1.745,6.108-6.108V11.6"
         transform="translate(-2.001 -2)"
         fill="none"
-        stroke={iconColors.primary || color}
+        stroke={color}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={strokeWidth}
