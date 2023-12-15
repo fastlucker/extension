3fd64cc9c1bc7dc087801c7968b896649bd06668
Petromir <petromir@devlabs.bg>
2023-12-15 11:27:20 +0200

    add: toast and safearea providers, benzininit

--- src/benzin/BenzinInit.tsx
+++ src/benzin/BenzinInit.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 import { areRpcProvidersInitialized, initRpcProviders } from '@ambire-common/services/provider'
 import { ThemeProvider } from '@common/contexts/themeContext'
+import { ToastProvider } from '@common/contexts/toastContext'
 import useFonts from '@common/hooks/useFonts'
 import { rpcProviders } from '@common/services/providers'
 import { PortalHost, PortalProvider } from '@gorhom/portal'
@@ -28,8 +30,12 @@ const BenzinInit = () => {
     <Router>
       <PortalProvider>
         <ThemeProvider>
-          <TransactionProgressScreen />
-          <PortalHost name="global" />
+          <SafeAreaProvider>
+            <ToastProvider>
+              <TransactionProgressScreen />
+              <PortalHost name="global" />
+            </ToastProvider>
+          </SafeAreaProvider>
         </ThemeProvider>
       </PortalProvider>
     </Router>
