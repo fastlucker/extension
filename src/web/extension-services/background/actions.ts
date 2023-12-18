53111965a24395386f12f2624ff65ddf478e35e5
Petromir <petromir@devlabs.bg>
2023-12-18 12:39:42 +0200

    delete: MAIN_CONTROLLER_UPDATE_SELECTED_ACCOUNT action

--- src/web/extension-services/background/actions.ts
+++ src/web/extension-services/background/actions.ts
@@ -113,9 +113,6 @@ type MainControllerRemoveUserRequestAction = {
   type: 'MAIN_CONTROLLER_REMOVE_USER_REQUEST'
   params: { id: UserRequest['id'] }
 }
-type MainControllerRefetchPortfolio = {
-  type: 'MAIN_CONTROLLER_REFETCH_PORTFOLIO'
-}
 type MainControllerSignMessageInitAction = {
   type: 'MAIN_CONTROLLER_SIGN_MESSAGE_INIT'
   params: {
