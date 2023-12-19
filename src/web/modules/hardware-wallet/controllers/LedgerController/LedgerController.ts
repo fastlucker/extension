94c0147906fc9782aa797bf90882fd8331324963
Kaloyan Kosev <superkalo@devlabs.bg>
2023-12-19 15:52:49 +0200

    Refactor: remove oboelete duplicated method

--- src/web/modules/hardware-wallet/controllers/LedgerController/LedgerController.ts
+++ src/web/modules/hardware-wallet/controllers/LedgerController/LedgerController.ts
@@ -8,15 +8,6 @@ import Eth, { ledgerService } from '@ledgerhq/hw-app-eth'
 import Transport from '@ledgerhq/hw-transport'
 import TransportWebHID from '@ledgerhq/hw-transport-webhid'
 
-export const wait = (fn: () => void, ms = 1000) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      fn()
-      resolve(true)
-    }, ms)
-  })
-}
-
 class LedgerController implements ExternalSignerController {
   hdPathTemplate: HD_PATH_TEMPLATE_TYPE
 
