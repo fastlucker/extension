4f7ecb6205cf6c07b40ecad3b2d725dd6dd7c7c7
Petromir Petrov <petromir@devlabs.bg>
2024-01-02 12:22:52 +0200

    delete: benzinscreen stylesheet unused styles

--- src/benzin/screens/BenzinScreen/styles.ts
+++ src/benzin/screens/BenzinScreen/styles.ts
@@ -1,4 +1,4 @@
-import { Dimensions, StyleSheet, TextStyle, ViewStyle } from 'react-native'
+import { Dimensions, StyleSheet, ViewStyle } from 'react-native'
 
 import spacings from '@common/styles/spacings'
 import flexbox from '@common/styles/utils/flexbox'
@@ -9,12 +9,6 @@ interface Style {
   backgroundImage: ViewStyle
   container: ViewStyle
   content: ViewStyle
-  logoWrapper: ViewStyle
-  estimate: ViewStyle
-  steps: ViewStyle
-  buttons: ViewStyle
-  openExplorer: ViewStyle
-  openExplorerText: TextStyle
 }
 
 const getStyles = () =>
@@ -33,36 +27,6 @@ const getStyles = () =>
     content: {
       maxWidth: 620,
       width: '100%'
-    },
-    logoWrapper: {
-      ...flexbox.alignCenter,
-      ...(IS_MOBILE_UP_BENZIN_BREAKPOINT ? spacings.mbXl : {})
-    },
-    estimate: {
-      ...flexbox.directionRow,
-      ...flexbox.alignCenter,
-      ...flexbox.justifyCenter,
-      ...spacings.mbXl
-    },
-    steps: {
-      ...(IS_MOBILE_UP_BENZIN_BREAKPOINT ? spacings.mb3Xl : spacings.mbXl)
-    },
-    buttons: {
-      ...(IS_MOBILE_UP_BENZIN_BREAKPOINT
-        ? flexbox.directionRow
-        : { flexDirection: 'column-reverse' }),
-      ...flexbox.alignCenter,
-      ...flexbox.justifyCenter,
-      ...(IS_MOBILE_UP_BENZIN_BREAKPOINT ? spacings.mbXl : spacings.mbMd)
-    },
-    openExplorer: {
-      ...flexbox.directionRow,
-      ...flexbox.alignCenter,
-      ...(!IS_MOBILE_UP_BENZIN_BREAKPOINT ? spacings.mbXl : {})
-    },
-    openExplorerText: {
-      ...spacings.mlSm,
-      textDecorationLine: 'underline'
     }
   })
 
