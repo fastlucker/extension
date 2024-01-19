2544dd2ba843ef14fc937a7a311999630c701dbc
Jordan Enev <jordantoen@gmail.com>
2024-01-19 19:21:52 +0200

    Add: Style a bit Keystore recovery page.

--- src/web/modules/emailVault/screens/KeystoreRecover/KeystoreRecover.tsx
+++ src/web/modules/emailVault/screens/KeystoreRecover/KeystoreRecover.tsx
@@ -7,16 +7,25 @@ import Button from '@common/components/Button'
 import Input from '@common/components/Input'
 import { isWeb } from '@common/config/env'
 import { useTranslation } from '@common/config/localization'
-import { TabLayoutWrapperMainContent } from '@web/components/TabLayoutWrapper/TabLayoutWrapper'
+import {
+  TabLayoutContainer,
+  TabLayoutWrapperMainContent
+} from '@web/components/TabLayoutWrapper/TabLayoutWrapper'
 import useBackgroundService from '@web/hooks/useBackgroundService'
 import useEmailVaultControllerState from '@web/hooks/useEmailVaultControllerState'
 import useKeystoreControllerState from '@web/hooks/useKeystoreControllerState'
 import Text from '@common/components/Text'
+import spacings from '@common/styles/spacings'
+import { View } from 'react-native'
+import Header from '@common/modules/header/components/Header'
+import useTheme from '@common/hooks/useTheme'
+import getStyles from '@web/modules/onboarding/screens/OnBoardingScreen/styles'
 
 const KeystoreRecover = () => {
   const ev = useEmailVaultControllerState()
   const { t } = useTranslation()
   const keystoreState = useKeystoreControllerState()
+  const { theme } = useTheme(getStyles)
 
   const { dispatch } = useBackgroundService()
   const {
@@ -39,8 +48,6 @@ const KeystoreRecover = () => {
     }
   }, [keystoreState.isUnlocked])
 
-  console.log({ ev })
-
   const handleFormSubmit = useCallback(() => {
     handleSubmit(async () => {
       dispatch({ type: 'EMAIL_VAULT_CONTROLLER_RECOVER_KEYSTORE', params: { email } })
@@ -48,43 +55,56 @@ const KeystoreRecover = () => {
   }, [ev?.emailVaultStates?.email, handleSubmit, dispatch, email])
 
   return (
-    <TabLayoutWrapperMainContent>
-      {ev.currentState === EmailVaultState.WaitingEmailConfirmation && (
-        <Text>Waiting email confirmation!</Text>
-      )}
-      <Controller
-        control={control}
-        rules={{
-          validate: isEmail
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <Input
-            label={t('Please insert your email')}
-            onBlur={onBlur}
-            placeholder={t('Email Address')}
-            onChangeText={onChange}
-            onSubmitEditing={handleFormSubmit}
-            value={value}
-            autoFocus={isWeb}
-            isValid={isEmail(value)}
-            error={!!errors.email && (t('Please fill in a valid email.') as string)}
-          />
+    <TabLayoutContainer
+      backgroundColor={theme.secondaryBackground}
+      width="lg"
+      header={<Header withAmbireLogo />}
+    >
+      <TabLayoutWrapperMainContent contentContainerStyle={spacings.mbLg}>
+        <View style={spacings.mbLg}>
+          <Text fontSize={20} weight="medium">
+            {t('Restore Device Password')}
+          </Text>
+        </View>
+        {ev.currentState === EmailVaultState.WaitingEmailConfirmation && (
+          <View style={spacings.mbXl}>
+            <Text>Waiting email confirmation ...</Text>
+          </View>
         )}
-        name="email"
-      />
-      <Button
-        textStyle={{ fontSize: 14 }}
-        disabled={ev.currentState === EmailVaultState.Loading || isSubmitting || !email}
-        type="primary"
-        text={
-          // eslint-disable-next-line no-nested-ternary
-          isSubmitting || ev.currentState === EmailVaultState.Loading
-            ? t('Loading...')
-            : t('Recover')
-        }
-        onPress={handleFormSubmit}
-      />
-    </TabLayoutWrapperMainContent>
+        <Controller
+          control={control}
+          rules={{
+            validate: isEmail
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              label={t('Please insert your email')}
+              onBlur={onBlur}
+              placeholder={t('Email Address')}
+              onChangeText={onChange}
+              onSubmitEditing={handleFormSubmit}
+              value={value}
+              autoFocus={isWeb}
+              isValid={isEmail(value)}
+              error={!!errors.email && (t('Please fill in a valid email.') as string)}
+            />
+          )}
+          name="email"
+        />
+        <Button
+          textStyle={{ fontSize: 14 }}
+          disabled={ev.currentState === EmailVaultState.Loading || isSubmitting || !email}
+          type="primary"
+          text={
+            // eslint-disable-next-line no-nested-ternary
+            isSubmitting || ev.currentState === EmailVaultState.Loading
+              ? t('Loading...')
+              : t('Recover')
+          }
+          onPress={handleFormSubmit}
+        />
+      </TabLayoutWrapperMainContent>
+    </TabLayoutContainer>
   )
 }
 
