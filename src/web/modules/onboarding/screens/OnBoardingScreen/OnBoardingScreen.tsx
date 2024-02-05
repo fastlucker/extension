c10659dfbd087588d4b43f65475c3147adf22693
sonytooo <simeonpetkov11999977@gmail.com>
2024-02-05 17:07:36 +0200

    remove the onboarding context

--- src/web/modules/onboarding/screens/OnBoardingScreen/OnBoardingScreen.tsx
+++ src/web/modules/onboarding/screens/OnBoardingScreen/OnBoardingScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Image, Linking, Pressable, View } from 'react-native'
 
@@ -16,11 +16,9 @@ import {
   TabLayoutContainer,
   TabLayoutWrapperMainContent
 } from '@web/components/TabLayoutWrapper/TabLayoutWrapper'
+import { ONBOARDING_VALUES } from '@web/extension-services/background/controllers/wallet-state'
 import { storage } from '@web/extension-services/background/webapi/storage'
-import useMainControllerState from '@web/hooks/useMainControllerState'
 import PinExtension from '@web/modules/onboarding/components/PinExtension/PinExtension'
-import { ONBOARDING_VALUES } from '@web/modules/onboarding/contexts/onboardingContext/types'
-import useOnboarding from '@web/modules/onboarding/hooks/useOnboarding'
 
 import ConfettiAnimation from '../../components/ConfettiAnimation'
 import getStyles from './styles'
@@ -29,21 +27,12 @@ const OnBoardingScreen = () => {
   const { theme, styles } = useTheme(getStyles)
   const { t } = useTranslation()
   const { navigate } = useNavigation()
-  const { setOnboardingStatus } = useOnboarding()
-  const mainState = useMainControllerState()
-  const [initialStatusCheckPassed, setInitialStatusCheckPassed] = useState(false)
-  useEffect(() => {
-    if (mainState.accounts.length && initialStatusCheckPassed) {
-      setOnboardingStatus(ONBOARDING_VALUES.ON_BOARDED)
-    }
-  }, [setOnboardingStatus, mainState.accounts.length, initialStatusCheckPassed])
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     ;(async () => {
       const status = await storage.get('onboardingStatus', ONBOARDING_VALUES.NOT_ON_BOARDED)
 
-      setInitialStatusCheckPassed(true)
       if (status === ONBOARDING_VALUES.ON_BOARDED) {
         navigate(WEB_ROUTES.dashboard)
       }
