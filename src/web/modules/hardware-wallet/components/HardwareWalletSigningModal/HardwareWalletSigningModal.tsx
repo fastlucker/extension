c5adac4e98ae0159ae0ce414157d720a06de7d7d
Kaloyan Kosev <superkalo@devlabs.bg>
2024-02-06 12:50:38 +0200

    Add: device logo

--- src/web/modules/hardware-wallet/components/HardwareWalletSigningModal/HardwareWalletSigningModal.tsx
+++ src/web/modules/hardware-wallet/components/HardwareWalletSigningModal/HardwareWalletSigningModal.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { View } from 'react-native'
 
 import { ExternalKey } from '@ambire-common/interfaces/keystore'
 import AmbireDevice from '@common/assets/svg/AmbireDevice'
 import CloseIcon from '@common/assets/svg/CloseIcon'
 import DriveIcon from '@common/assets/svg/DriveIcon'
+import LatticeMiniIcon from '@common/assets/svg/LatticeMiniIcon'
+import LedgerMiniIcon from '@common/assets/svg/LedgerMiniIcon'
 import LeftPointerArrowIcon from '@common/assets/svg/LeftPointerArrowIcon'
+import TrezorMiniIcon from '@common/assets/svg/TrezorMiniIcon/TrezorMiniIcon'
 import Button from '@common/components/Button'
 import Modal from '@common/components/Modal'
 import Text from '@common/components/Text'
@@ -23,14 +26,28 @@ type Props = {
   onReject: () => void
 }
 
+const iconByKeyType = {
+  trezor: TrezorMiniIcon,
+  ledger: LedgerMiniIcon,
+  lattice: LatticeMiniIcon
+}
+
 const HardwareWalletSigningModal = ({ isOpen, keyType, onReject }: Props) => {
   const { t } = useTranslation()
   const { theme } = useTheme()
+
+  const titleSuffix = useMemo(() => {
+    const Icon = keyType && iconByKeyType[keyType as keyof typeof iconByKeyType]
+    if (!Icon) return undefined
+
+    return <Icon />
+  }, [keyType])
 
   return (
     <Modal
       modalStyle={{ minWidth: 'auto', height: 'auto' }}
       title={t(`Sign with your ${HARDWARE_WALLET_DEVICE_NAMES[keyType]} device`)}
+      titleSuffix={titleSuffix}
       isOpen={isOpen}
     >
       <View
