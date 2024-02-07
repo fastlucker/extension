7c3601009456a673effa5d551db7448b80adcce3
Petromir Petrov <petromir@devlabs.bg>
2024-02-07 11:51:17 +0200

    fix: CheckedList and HiddenList icons opacity

--- src/common/assets/svg/CheckedListIcon/CheckedListIcon.tsx
+++ src/common/assets/svg/CheckedListIcon/CheckedListIcon.tsx
@@ -10,7 +10,7 @@ const CheckedListIcon: React.FC<SvgProps> = ({
   ...rest
 }) => (
   <Svg width={width} height={height} viewBox="0 0 48 48" {...rest}>
-    <G opacity=".5" fill="none" stroke={color} strokeWidth="1.5">
+    <G fill="none" stroke={color} strokeWidth="1.5">
       <G>
         <Path d="M8 0h32a8 8 0 0 1 8 8v32a8 8 0 0 1-8 8H0V8a8 8 0 0 1 8-8Z" stroke="none" />
         <Path d="M8 .75h32A7.25 7.25 0 0 1 47.25 8v32A7.25 7.25 0 0 1 40 47.25H1.5a.75.75 0 0 1-.75-.75V8A7.25 7.25 0 0 1 8 .75Z" />
