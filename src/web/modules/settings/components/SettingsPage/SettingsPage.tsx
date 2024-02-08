b8995b354c2090a0cf2e8fa7b8b768a0e1a1f106
Petromir Petrov <petromir@devlabs.bg>
2024-02-08 17:56:41 +0200

    fix: SettingsPage scroll location

--- src/web/modules/settings/components/SettingsPage/SettingsPage.tsx
+++ src/web/modules/settings/components/SettingsPage/SettingsPage.tsx
@@ -2,8 +2,10 @@ import React, { FC, Fragment } from 'react'
 import { ScrollView, View } from 'react-native'
 
 import Panel from '@common/components/Panel'
+import { getPanelPaddings } from '@common/components/Panel/Panel'
 import useTheme from '@common/hooks/useTheme'
 import useWindowSize from '@common/hooks/useWindowSize'
+import spacings from '@common/styles/spacings'
 
 import Sidebar from './Sidebar'
 import getStyles from './styles'
@@ -19,15 +21,22 @@ const SettingsPage: FC<Props> = ({ children, currentPage, withPanelScrollView = 
   const { maxWidthSize } = useWindowSize()
   const isScreenXxl = maxWidthSize('xxl')
   const isScreenXl = maxWidthSize('xl')
-
+  const panelPaddings = getPanelPaddings(isScreenXl, false)
+  const wrapperProps = withPanelScrollView ? { contentContainerStyle: panelPaddings } : {}
   const Wrapper = withPanelScrollView ? ScrollView : Fragment
 
   return (
     <View style={styles.background}>
       <View style={[styles.container, !isScreenXl ? styles.fullWidth : {}]}>
         <Sidebar activeLink={currentPage} />
-        <Panel style={[styles.panel, !isScreenXl ? styles.fullWidth : {}]}>
-          <Wrapper>{children}</Wrapper>
+        <Panel
+          style={[
+            styles.panel,
+            !isScreenXl ? styles.fullWidth : {},
+            withPanelScrollView ? { ...spacings.ph0, ...spacings.pv0 } : {}
+          ]}
+        >
+          <Wrapper {...wrapperProps}>{children}</Wrapper>
         </Panel>
         {isScreenXxl ? (
           <View style={styles.sideContainer}>
