1475ba7bc0764cc10bfa3f16c9cd80a07a5bf801
Yosif Hamed <josif.hameed@gmail.com>
2024-02-22 16:53:03 +0200

    Remove unneccesary use effect

--- src/web/contexts/transferControllerStateContext/transferControllerStateContext.tsx
+++ src/web/contexts/transferControllerStateContext/transferControllerStateContext.tsx
@@ -1,14 +1,11 @@
 import React, { createContext, useEffect, useMemo, useState } from 'react'
 
 import { TransferControllerState } from '@ambire-common/interfaces/transfer'
-import useConstants from '@common/hooks/useConstants'
 import useRoute from '@common/hooks/useRoute'
 import eventBus from '@web/extension-services/event/eventBus'
 import useBackgroundService from '@web/hooks/useBackgroundService'
 import useMainControllerState from '@web/hooks/useMainControllerState'
 import usePortfolioControllerState from '@web/hooks/usePortfolioControllerState/usePortfolioControllerState'
-import humanizerInfo from '@ambire-common/consts/humanizer/humanizerInfo.json'
-import { HumanizerMeta } from '@ambire-common/libs/humanizer/interfaces'
 
 type ContextReturn = {
   state: TransferControllerState
@@ -32,7 +29,6 @@ const TransferControllerStateProvider: React.FC<any> = ({ children }) => {
   const [state, setState] = useState({} as TransferControllerState)
   const { dispatch } = useBackgroundService()
   const mainState = useMainControllerState()
-  const { constants } = useConstants()
   const { accountPortfolio } = usePortfolioControllerState()
   const { search } = useRoute()
   const tokens = accountPortfolio?.tokens
@@ -45,16 +41,6 @@ const TransferControllerStateProvider: React.FC<any> = ({ children }) => {
       })
     }
   }, [dispatch])
-
-  useEffect(() => {
-    if (!constants) return
-    dispatch({
-      type: 'MAIN_CONTROLLER_TRANSFER_UPDATE',
-      params: {
-        humanizerInfo: humanizerInfo as HumanizerMeta
-      }
-    })
-  }, [constants, dispatch])
 
   useEffect(() => {
     if (tokens?.length && selectedTokenFromUrl && !state.selectedToken) {
