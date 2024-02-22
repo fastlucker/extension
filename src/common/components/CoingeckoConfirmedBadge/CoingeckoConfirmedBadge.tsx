70b43bee737e76b67db5082cec893b683e6886c0
Gergana Mihaylova <gergana@devlabs.bg>
2024-02-22 18:22:33 +0200

    Add: Coingecko badge changes

---
+++ src/common/components/CoingeckoConfirmedBadge/CoingeckoConfirmedBadge.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import CoingeckoIcon from '@common/assets/svg/CoingeckoIcon'
+import SuccessIcon from '@common/assets/svg/SuccessIcon'
+import Text from '@common/components/Text'
+import useTheme from '@common/hooks/useTheme'
+import spacings from '@common/styles/spacings'
+
+import getStyles from './styles'
+
+type Props = {
+  text: string
+}
+
+const CoingeckoConfirmedBadge = ({ text }: Props) => {
+  const { styles } = useTheme(getStyles)
+
+  return (
+    <View style={styles.container}>
+      <Text weight="medium" fontSize={10} color="#8DC63F" style={spacings.mrMi}>
+        {text}
+      </Text>
+      <SuccessIcon color="#8DC63F" width={20} height={20} withCirc={false} />
+      <CoingeckoIcon />
+    </View>
+  )
+}
+
+export default React.memo(CoingeckoConfirmedBadge)
