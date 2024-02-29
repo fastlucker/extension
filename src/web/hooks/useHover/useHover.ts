ce47f1abf20553bc5e9d5e73f8caa0aba0b2a500
Petromir Petrov <petromir@devlabs.bg>
2024-02-29 10:51:39 +0200

    add: opacityInverted preset

--- src/web/hooks/useHover/useHover.ts
+++ src/web/hooks/useHover/useHover.ts
@@ -1,25 +1,39 @@
-import { Animated, Pressable } from 'react-native'
+import { Animated, Pressable, ViewStyle } from 'react-native'
 
 import useCustomHover from './useCustomHover'
 
 export const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
 
+interface Presets {
+  [key: string]: {
+    property: keyof ViewStyle
+    from: number
+    to: number
+  }
+}
+
+const presets: Presets = {
+  opacity: {
+    property: 'opacity',
+    from: 0.7,
+    to: 1
+  },
+  opacityInverted: {
+    property: 'opacity',
+    from: 1,
+    to: 0.7
+  }
+}
+
 interface Props {
-  preset: 'opacity'
+  preset: keyof typeof presets
   duration?: number
   forceHoveredStyle?: boolean
 }
 
-const presets = {
-  opacity: {
-    from: 0.7,
-    to: 1
-  }
-}
-
 const useHover = ({ preset, duration, forceHoveredStyle }: Props) => {
   const value = useCustomHover({
-    property: preset,
+    property: presets[preset].property,
     values: presets[preset],
     duration,
     forceHoveredStyle
