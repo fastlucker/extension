57f10b50818aa3ea9ac00f22f8af7a01501abecb
Petromir Petrov <petromir@devlabs.bg>
2024-03-05 11:04:13 +0200

    add: PayOption responsive breakpoints

--- src/web/modules/sign-account-op/components/Estimation/components/PayOption.tsx
+++ src/web/modules/sign-account-op/components/Estimation/components/PayOption.tsx
@@ -6,6 +6,7 @@ import { TokenResult } from '@ambire-common/libs/portfolio'
 import { Avatar } from '@common/components/Avatar'
 import Text from '@common/components/Text'
 import { DEFAULT_ACCOUNT_LABEL } from '@common/constants/account'
+import useWindowSize from '@common/hooks/useWindowSize'
 import TokenIcon from '@common/modules/dashboard/components/TokenIcon'
 import spacings from '@common/styles/spacings'
 import flexbox from '@common/styles/utils/flexbox'
@@ -22,6 +23,8 @@ const PayOption = ({
   isGasTank: boolean
 }) => {
   const settingsCtrl = useSettingsControllerState()
+  const { maxWidthSize } = useWindowSize()
+  const isL = maxWidthSize('l')
   const accountPref = settingsCtrl.accountPreferences[account.addr]
 
   const label = accountPref?.label || DEFAULT_ACCOUNT_LABEL
@@ -49,11 +52,11 @@ const PayOption = ({
       >
         <Avatar pfp={accountPref?.pfp} size={32} />
         <View style={[flexbox.flex1, spacings.mrMi]}>
-          <Text weight="medium" fontSize={12} numberOfLines={1}>
+          <Text weight="medium" fontSize={isL ? 14 : 12} numberOfLines={1}>
             {label}
           </Text>
           <Text weight="medium" fontSize={10} numberOfLines={1} appearance="secondaryText">
-            ({!isGasTank ? shortenAddress(account.addr, 13) : 'Gas Tank'})
+            ({!isGasTank ? shortenAddress(account.addr, isL ? 23 : 13) : 'Gas Tank'})
           </Text>
         </View>
       </View>
@@ -71,16 +74,16 @@ const PayOption = ({
           ]}
         >
           <TokenIcon
-            containerHeight={24}
-            containerWidth={24}
-            width={16}
-            height={16}
+            containerHeight={isL ? 32 : 24}
+            containerWidth={isL ? 32 : 24}
+            width={isL ? 20 : 16}
+            height={isL ? 20 : 16}
             networkSize={10}
             address={token.address}
             networkId={token.networkId}
             onGasTank={token.flags.onGasTank}
           />
-          <Text weight="medium" numberOfLines={1} style={spacings.mlMi} fontSize={12}>
+          <Text weight="medium" numberOfLines={1} style={spacings.mlMi} fontSize={isL ? 14 : 12}>
             {token.symbol}
           </Text>
         </View>
