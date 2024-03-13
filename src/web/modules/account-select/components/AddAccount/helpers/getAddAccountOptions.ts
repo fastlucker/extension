d38052266910f7dee63e4c9937e4867cfa072b87
Petromir Petrov <petromir@devlabs.bg>
2024-03-13 12:31:53 +0200

    add: getAddAccountOptions helper to AddAccount

---
+++ src/web/modules/account-select/components/AddAccount/helpers/getAddAccountOptions.ts
@@ -0,0 +1,58 @@
+import CreateWalletIcon from '@common/assets/svg/CreateWalletIcon'
+import HWIcon from '@common/assets/svg/HWIcon'
+import ImportAccountIcon from '@common/assets/svg/ImportAccountIcon'
+import ViewModeIcon from '@common/assets/svg/ViewModeIcon'
+import useNavigation from '@common/hooks/useNavigation'
+import { ROUTES } from '@common/modules/router/constants/common'
+import { openInternalPageInTab } from '@web/extension-services/background/webapi/tab'
+import { getUiType } from '@web/utils/uiType'
+
+const { isNotification } = getUiType()
+
+const getAddAccountOptions = ({
+  navigate,
+  t,
+  hiddenOptions = []
+}: {
+  navigate: ReturnType<typeof useNavigation>['navigate']
+  t: (str: string) => string
+  hiddenOptions?: ('hw' | 'hot-wallet' | 'create-wallet' | 'view-only')[]
+}) => {
+  const navigateWrapped = (route: string) => {
+    if (isNotification) {
+      openInternalPageInTab(route)
+      return
+    }
+    navigate(route)
+  }
+
+  return [
+    {
+      key: 'hw',
+      text: t('Connect a Hardware Wallet'),
+      icon: HWIcon,
+      onPress: () => navigateWrapped(ROUTES.hardwareWalletSelect)
+    },
+    {
+      key: 'hot-wallet',
+      text: t('Import an existing hot wallet'),
+      icon: ImportAccountIcon,
+      onPress: () => navigateWrapped(ROUTES.importHotWallet)
+    },
+    {
+      key: 'create-wallet',
+      text: t('Create a new hot wallet'),
+      icon: CreateWalletIcon,
+      onPress: () => navigateWrapped(ROUTES.createHotWallet),
+      hasLargerBottomSpace: true
+    },
+    {
+      key: 'view-only',
+      text: t('Watch an address'),
+      icon: ViewModeIcon,
+      onPress: () => navigateWrapped(ROUTES.viewOnlyAccountAdder)
+    }
+  ].filter((option) => !hiddenOptions.includes(option.key as any))
+}
+
+export { getAddAccountOptions }
