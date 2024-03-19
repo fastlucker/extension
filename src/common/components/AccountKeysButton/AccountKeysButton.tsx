bce99001df1af25f73a646776d965d0697ddef13
Petromir Petrov <petromir@devlabs.bg>
2024-03-19 09:36:05 +0200

    add: AccountKeysButton hover animation

--- src/common/components/AccountKeysButton/AccountKeysButton.tsx
+++ src/common/components/AccountKeysButton/AccountKeysButton.tsx
@@ -1,40 +1,44 @@
 import React from 'react'
-import { Pressable } from 'react-native'
 import { useModalize } from 'react-native-modalize'
 
 import NoKeysIcon from '@common/assets/svg/NoKeysIcon'
 import SingleKeyIcon from '@common/assets/svg/SingleKeyIcon'
 import AccountKeysBottomSheet from '@common/components/AccountKeysBottomSheet'
-import { BORDER_RADIUS_PRIMARY } from '@common/styles/utils/common'
-import flexbox from '@common/styles/utils/flexbox'
+import useTheme from '@common/hooks/useTheme'
+import { AnimatedPressable, useCustomHover } from '@web/hooks/useHover'
 import useKeystoreControllerState from '@web/hooks/useKeystoreControllerState'
 import useMainControllerState from '@web/hooks/useMainControllerState'
 import useSettingsControllerState from '@web/hooks/useSettingsControllerState'
 
+import getStyles from './styles'
+
 const AccountKeysButton = () => {
+  const { theme, styles } = useTheme(getStyles)
   const { accounts, selectedAccount } = useMainControllerState()
   const { keys } = useKeystoreControllerState()
   const { keyPreferences } = useSettingsControllerState()
   const { ref: sheetRef, open: openBottomSheet, close: closeBottomSheet } = useModalize()
+  const [bindAnim, animStyle] = useCustomHover({
+    property: 'borderColor',
+    values: {
+      from: `${String(theme.secondaryBackground)}00`,
+      to: `${String(theme.secondaryBackground)}50`
+    }
+  })
   const { associatedKeys = [] } = accounts.find(({ addr }) => addr === selectedAccount) || {}
   const importedAccountKeys = keys.filter(({ addr }) => associatedKeys.includes(addr))
 
   return (
     <>
-      <Pressable
-        style={{
-          width: 32,
-          height: 32,
-          ...flexbox.center,
-          backgroundColor: '#14183326',
-          borderRadius: BORDER_RADIUS_PRIMARY
-        }}
+      <AnimatedPressable
+        style={[styles.container, animStyle]}
         onPress={() => openBottomSheet()}
+        {...bindAnim}
       >
         {importedAccountKeys.length === 0 && <NoKeysIcon />}
         {importedAccountKeys.length === 1 && <SingleKeyIcon />}
         {importedAccountKeys.length > 1 && <NoKeysIcon />}
-      </Pressable>
+      </AnimatedPressable>
       <AccountKeysBottomSheet
         sheetRef={sheetRef}
         associatedKeys={associatedKeys}
