56512ce8e7e4cc983d87c65afa60ecca1d2a1643
sonytooo <simeonpetkov11999977@gmail.com>
2024-03-21 10:25:27 +0200

    tooltip fixes

--- src/common/components/Tooltip/Tooltip.tsx
+++ src/common/components/Tooltip/Tooltip.tsx
@@ -4,26 +4,30 @@ import { ITooltip, Tooltip as ReactTooltip } from 'react-tooltip'
 import { FONT_FAMILIES } from '@common/hooks/useFonts'
 import useTheme from '@common/hooks/useTheme'
 import { BORDER_RADIUS_PRIMARY } from '@common/styles/utils/common'
+import { Portal } from '@gorhom/portal'
 
-const Tooltip = ({ ...rest }: ITooltip) => {
+const Tooltip = ({ style, ...rest }: ITooltip) => {
   const { theme } = useTheme()
   return (
-    <ReactTooltip
-      place="top"
-      delayShow={150}
-      variant="light"
-      style={{
-        maxWidth: 380,
-        fontSize: 14,
-        fontFamily: FONT_FAMILIES.REGULAR,
-        backgroundColor: theme.tertiaryBackground as any,
-        color: theme.secondaryText as any,
-        borderRadius: BORDER_RADIUS_PRIMARY
-      }}
-      opacity={1}
-      border={`1px solid ${theme.secondaryBorder as any}`}
-      {...rest}
-    />
+    <Portal hostName="global">
+      <ReactTooltip
+        place="top"
+        delayShow={150}
+        variant="light"
+        style={{
+          maxWidth: 380,
+          fontSize: 14,
+          fontFamily: FONT_FAMILIES.REGULAR,
+          backgroundColor: theme.tertiaryBackground as any,
+          color: theme.secondaryText as any,
+          borderRadius: BORDER_RADIUS_PRIMARY,
+          ...style
+        }}
+        opacity={1}
+        border={`1px solid ${theme.secondaryBorder as any}`}
+        {...rest}
+      />
+    </Portal>
   )
 }
 
