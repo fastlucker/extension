f75e5c2f233ac2aa9d6bda5d938e26cfe0d73959
Kaloyan Kosev <superkalo@devlabs.bg>
2024-03-29 18:14:48 +0200

    Add: info tooltip option + derivation info tooltip text

--- src/common/components/AccountKeyDetailsBottomSheet/AccountKeyDetailsBottomSheet.tsx
+++ src/common/components/AccountKeyDetailsBottomSheet/AccountKeyDetailsBottomSheet.tsx
@@ -1,17 +1,25 @@
 import React, { FC, ReactNode, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { View } from 'react-native'
+import { Tooltip } from 'react-tooltip'
 
+import { SMART_ACCOUNT_SIGNER_KEY_DERIVATION_OFFSET } from '@ambire-common/consts/derivation'
 import { Key } from '@ambire-common/interfaces/keystore'
 import { KeyPreferences } from '@ambire-common/interfaces/settings'
+import { isDerivedForSmartAccountKeyOnly } from '@ambire-common/libs/account/account'
 import { getHdPathFromTemplate } from '@ambire-common/utils/hdPath'
+import InformationIcon from '@common/assets/svg/InformationIcon'
 import BottomSheet from '@common/components/BottomSheet'
 import Text from '@common/components/Text'
+import useTheme from '@common/hooks/useTheme'
 import colors from '@common/styles/colors'
 import spacings from '@common/styles/spacings'
 import { BORDER_RADIUS_PRIMARY } from '@common/styles/utils/common'
 import flexbox from '@common/styles/utils/flexbox'
 import { HARDWARE_WALLET_DEVICE_NAMES } from '@web/modules/hardware-wallet/constants/names'
+
+import Alert from '../Alert'
+import Label from '../Label'
 
 interface Props {
   sheetRef: any
@@ -28,6 +36,7 @@ const AccountKeyDetailsBottomSheet: FC<Props> = ({
   closeBottomSheet,
   children
 }) => {
+  const { theme } = useTheme()
   const { t } = useTranslation()
 
   // TODO: Implement internal key details
@@ -50,7 +59,15 @@ const AccountKeyDetailsBottomSheet: FC<Props> = ({
       },
       {
         key: t('Derivation'),
-        value: meta?.hdPathTemplate ? getHdPathFromTemplate(meta?.hdPathTemplate, meta?.index) : '-'
+        value: meta?.hdPathTemplate
+          ? getHdPathFromTemplate(meta?.hdPathTemplate, meta?.index)
+          : '-',
+        tooltip:
+          typeof meta?.index === 'number' && isDerivedForSmartAccountKeyOnly(meta?.index)
+            ? t(
+                `Ambire smart account keys use a derived address by an offset of ${SMART_ACCOUNT_SIGNER_KEY_DERIVATION_OFFSET}.`
+              )
+            : undefined
       }
     ],
     [t, type, meta?.deviceId, meta?.deviceModel, meta?.hdPathTemplate, meta?.index]
@@ -79,14 +96,33 @@ const AccountKeyDetailsBottomSheet: FC<Props> = ({
       >
         {children}
         <View style={[spacings.phSm, spacings.pvSm, spacings.mtMi]}>
-          {metaDetails.map(({ key, value }) => (
+          {metaDetails.map(({ key, value, tooltip }) => (
             <View
               key={key}
               style={[flexbox.directionRow, flexbox.justifySpaceBetween, spacings.mbMi]}
             >
               <Text fontSize={14}>{key}: </Text>
-              <Text fontSize={14} weight="semiBold">
+              <Text
+                fontSize={14}
+                weight="semiBold"
+                style={{
+                  // @ts-ignore missing in the types, but React Native Web supports it
+                  wordBreak: 'break-all'
+                }}
+              >
                 {value}
+                {tooltip && (
+                  <>
+                    {' '}
+                    <InformationIcon
+                      color={theme.infoDecorative}
+                      width={14}
+                      height={14}
+                      dataSet={{ tooltipId: `tooltip-for-${key}`, tooltipContent: tooltip }}
+                    />
+                    <Tooltip id={`tooltip-for-${key}`} />
+                  </>
+                )}
               </Text>
             </View>
           ))}
