292a34c8be808600d47a265c23f2ff7b6eeaa2d1
Borislav Itskov <borislav@devlabs.bg>
2024-04-19 14:25:10 +0300

    add: reflect balance changes on the dashboard

--- src/common/modules/dashboard/helpers/getTokenDetails.ts
+++ src/common/modules/dashboard/helpers/getTokenDetails.ts
@@ -18,24 +18,36 @@ const getTokenDetails = (
   const isRewards = rewardsType === 'wallet-rewards'
   const isVesting = rewardsType === 'wallet-vesting'
   const networkData = networks.find(({ id }) => networkId === id)
+  const postAmount = amountPostSimulation !== undefined ? amountPostSimulation : 0n
+  const isPending = amountPostSimulation !== undefined && amountPostSimulation !== amount
 
-  const displayAmount = amountPostSimulation !== undefined ? amountPostSimulation : amount
-  const balance = parseFloat(formatUnits(displayAmount, decimals))
+  const balance = parseFloat(formatUnits(amount, decimals))
+  const pendingBalance = isPending
+    ? parseFloat(formatUnits(amountPostSimulation, decimals))
+    : parseFloat('0')
   const priceUSD = priceIn.find(
     ({ baseCurrency }: { baseCurrency: string }) => baseCurrency.toLowerCase() === 'usd'
   )?.price
   const balanceUSD = priceUSD ? balance * priceUSD : undefined
+  const pendingBalanceUSD = priceUSD && isPending ? pendingBalance * priceUSD : undefined
+  const isBalanceIncrease = postAmount > amount
+  const balanceChange = isBalanceIncrease ? postAmount - amount : amount - postAmount
 
   return {
     balance,
     balanceFormatted: formatDecimals(balance, 'amount'),
+    pendingBalance,
+    pendingBalanceFormatted: formatDecimals(pendingBalance, 'amount'),
     priceUSD,
     priceUSDFormatted: formatDecimals(priceUSD, 'price'),
     balanceUSD,
     balanceUSDFormatted: formatDecimals(balanceUSD, 'value'),
+    pendingBalanceUSDFormatted: formatDecimals(pendingBalanceUSD, 'value'),
     networkData,
     isRewards,
-    isVesting
+    isVesting,
+    isBalanceIncrease,
+    balanceChange: formatDecimals(parseFloat(formatUnits(balanceChange, decimals)), 'amount')
   }
 }
 
