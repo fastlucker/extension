f936d38a1c65af53d95252e79776bda91bf3ec74
Petromir Petrov <petromir@devlabs.bg>
2024-04-25 13:57:46 +0300

    update: useOnEnterKeyPress types and logic

--- src/web/hooks/useOnEnterKeyPress/useOnEnterKeyPress.ts
+++ src/web/hooks/useOnEnterKeyPress/useOnEnterKeyPress.ts
@@ -1,22 +1,29 @@
 import { useEffect } from 'react'
+import { PressableProps } from 'react-native'
 
 interface Props {
-  action: () => void
+  action: PressableProps['onPress']
   disabled?: boolean
 }
 
 const useOnEnterKeyPress = ({ action, disabled }: Props) => {
   useEffect(() => {
+    if (!action) return
+
     const handleKeyDown = (e: KeyboardEvent) => {
+      e.preventDefault()
+      e.stopPropagation()
+
       if (e.key === 'Enter') {
         if (disabled) return
+        // @ts-ignore
         action()
       }
     }
 
-    window.addEventListener('keydown', handleKeyDown)
+    window.addEventListener('keyup', handleKeyDown)
 
-    return () => window.removeEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keyup', handleKeyDown)
   }, [disabled, action])
 }
 
