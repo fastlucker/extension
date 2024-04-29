f34f9ac02586159a5354dbc4493c68c49800430a
Jordan Enev <jordantoen@gmail.com>
2024-04-29 17:32:23 +0300

    Fix: Activate the hook on a keydown, instead of keyup. Otherwise, the event was bubbled up and invoked twice. Exclude some of the input or submit tags, as they already have an onClick/onSubmit callback (action) that performs a form submission.

--- src/web/hooks/useOnEnterKeyPress/useOnEnterKeyPress.ts
+++ src/web/hooks/useOnEnterKeyPress/useOnEnterKeyPress.ts
@@ -11,19 +11,34 @@ const useOnEnterKeyPress = ({ action, disabled }: Props) => {
     if (!action) return
 
     const handleKeyDown = (e: KeyboardEvent) => {
-      e.preventDefault()
-      e.stopPropagation()
+      // It the buttons is not 'Enter', we don't care.
+      if (e.key !== 'Enter') return
 
-      if (e.key === 'Enter') {
-        if (disabled) return
+      // If the hook is disabled (i.e., if the form is in the process of submitting, or there's a validation error),
+      // we should not allow submission with 'Enter'.
+      if (disabled) return
+
+      // If the user holds the 'Enter' key down, we should prevent calling the `action` multiple times.
+      if (e.repeat) return
+
+      // We exclude some of the input or submit tags, as they already have an onClick/onSubmit callback (action) that performs a form submission.
+      // If we skip this step, the user hitting the 'Enter' button would result in the action callback function being submitted twice.
+      const tagName = (e.target as HTMLElement)?.tagName
+      const excludedTags = ['BUTTON', 'INPUT']
+      const isTagAllowed = !excludedTags.includes(tagName)
+
+      if (isTagAllowed) {
+        e.preventDefault()
+        e.stopPropagation()
+
         // @ts-ignore
         action()
       }
     }
 
-    window.addEventListener('keyup', handleKeyDown)
+    window.addEventListener('keydown', handleKeyDown)
 
-    return () => window.removeEventListener('keyup', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
   }, [disabled, action])
 }
 
