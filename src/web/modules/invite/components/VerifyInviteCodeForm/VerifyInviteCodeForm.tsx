29364f0f9e3ac0a936b9a54ef293527c08290b9d
Kaloyan Kosev <superkalo@devlabs.bg>
2024-05-07 17:16:47 +0300

    Add: redirect on success

--- src/web/modules/invite/components/VerifyInviteCodeForm/VerifyInviteCodeForm.tsx
+++ src/web/modules/invite/components/VerifyInviteCodeForm/VerifyInviteCodeForm.tsx
@@ -1,20 +1,23 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 
 import Button from '@common/components/Button'
 import Input from '@common/components/Input'
 import { isWeb } from '@common/config/env'
 import { useTranslation } from '@common/config/localization'
+import useNavigation from '@common/hooks/useNavigation'
+import { INVITE_STATUS } from '@web/extension-services/background/controllers/invite'
 import useBackgroundService from '@web/hooks/useBackgroundService'
+import useInviteControllerState from '@web/hooks/useInviteControllerState'
 
 const VerifyInviteCodeForm: React.FC<any> = () => {
+  const { navigate } = useNavigation()
   const { dispatch } = useBackgroundService()
   const { t } = useTranslation()
+  const { inviteStatus } = useInviteControllerState()
   const {
     control,
     handleSubmit,
-    watch,
-    setValue,
     formState: { errors, isSubmitting, isValid }
   } = useForm({
     mode: 'all',
@@ -22,6 +25,12 @@ const VerifyInviteCodeForm: React.FC<any> = () => {
       code: ''
     }
   })
+
+  // If the invite status verified, navigate out from this route to 1) navigate
+  // on success and 2) prevent someone from navigating to this route directly.
+  useEffect(() => {
+    if (inviteStatus === INVITE_STATUS.VERIFIED) navigate('/')
+  }, [inviteStatus, navigate])
 
   const handleFormSubmit = useCallback(
     () =>
