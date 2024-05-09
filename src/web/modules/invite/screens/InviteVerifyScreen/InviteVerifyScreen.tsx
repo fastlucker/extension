b3526a39f64784a4282797358539a400ec958efd
Kaloyan Kosev <superkalo@devlabs.bg>
2024-05-09 12:33:10 +0300

    Add: refine spacings

--- src/web/modules/invite/screens/InviteVerifyScreen/InviteVerifyScreen.tsx
+++ src/web/modules/invite/screens/InviteVerifyScreen/InviteVerifyScreen.tsx
@@ -1,18 +1,18 @@
-import { View } from 'react-native'
+import React, { useCallback } from 'react'
+import { Linking, View } from 'react-native'
 
-import AmbireLogo from '@common/assets/svg/AmbireLogo'
 import AmbireLogoWithTextMonochrome from '@common/assets/svg/AmbireLogoWithTextMonochrome'
 import UnlockScreenBackground from '@common/assets/svg/UnlockScreenBackground'
 import SeparatorWithText from '@common/components/SeparatorWithText'
 import Text from '@common/components/Text'
 import Title from '@common/components/Title'
-import { useTranslation } from '@common/config/localization'
+import { Trans, useTranslation } from '@common/config/localization'
 import useTheme from '@common/hooks/useTheme'
-import Header from '@common/modules/header/components/Header'
+import colors from '@common/styles/colors'
 import spacings from '@common/styles/spacings'
-import flexbox from '@common/styles/utils/flexbox'
 import { TabLayoutContainer, TabLayoutWrapperMainContent } from '@web/components/TabLayoutWrapper'
 import VerifyInviteCodeForm from '@web/modules/invite/components/VerifyInviteCodeForm'
+import { DISCORD_URL, TWITTER_URL } from '@web/modules/router/components/NavMenu/NavMenu'
 
 import getStyles from './style'
 
@@ -20,7 +20,9 @@ const InviteVerifyScreen = () => {
   const { t } = useTranslation()
   const { theme, styles } = useTheme(getStyles)
 
-  // TODO: Invite verification flow
+  const handleOpenDiscord = useCallback(() => Linking.openURL(DISCORD_URL), [])
+  const handleOpenTwitter = useCallback(() => Linking.openURL(TWITTER_URL), [])
+
   return (
     <TabLayoutContainer
       backgroundColor={theme.secondaryBackground}
@@ -42,10 +44,23 @@ const InviteVerifyScreen = () => {
         <View style={styles.container}>
           <VerifyInviteCodeForm />
           <SeparatorWithText text={t("Don't have one?")} />
+          <Trans>
+            <Text>
+              <Text>{'You can join our '}</Text>
+              <Text color={colors.violet} onPress={handleOpenDiscord}>
+                Discord
+              </Text>
+              <Text>{' or check '}</Text>
+              <Text color={colors.violet} onPress={handleOpenTwitter}>
+                Twitter
+              </Text>
+              <Text>{' for invites.'}</Text>
+            </Text>
+          </Trans>
         </View>
       </TabLayoutWrapperMainContent>
     </TabLayoutContainer>
   )
 }
 
-export default InviteVerifyScreen
+export default React.memo(InviteVerifyScreen)
