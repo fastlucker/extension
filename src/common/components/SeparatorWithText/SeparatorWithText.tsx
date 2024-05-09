259a6259ad81945cc15e7953241b89767671b457
Kaloyan Kosev <superkalo@devlabs.bg>
2024-05-09 18:56:46 +0300

    Change: themify separator

--- src/common/components/SeparatorWithText/SeparatorWithText.tsx
+++ src/common/components/SeparatorWithText/SeparatorWithText.tsx
@@ -4,14 +4,14 @@ import { View } from 'react-native'
 import Text from '@common/components/Text'
 import useTheme from '@common/hooks/useTheme'
 
-import styles from './styles'
+import getStyles from './styles'
 
 interface Props {
   text: string
 }
 
 const SeparatorWithText: React.FC<Props> = ({ text }) => {
-  const { theme } = useTheme()
+  const { theme, styles } = useTheme(getStyles)
 
   return (
     <View style={styles.container}>
--- src/common/components/SeparatorWithText/styles.ts
+++ src/common/components/SeparatorWithText/styles.ts
@@ -1,25 +1,26 @@
 import { StyleSheet, TextStyle } from 'react-native'
 
-import spacings from '@common/styles/spacings'
+import { ThemeProps } from '@common/styles/themeConfig'
 
 interface Style {
   container: TextStyle
   line: TextStyle
 }
 
-const styles = StyleSheet.create<Style>({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: 20
-  },
-  line: {
-    height: 1,
-    backgroundColor: '#ccc',
-    flex: 1,
-    marginHorizontal: 10
-  }
-})
+const getStyles = (theme: ThemeProps) =>
+  StyleSheet.create<Style>({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginVertical: 20
+    },
+    line: {
+      height: 1,
+      backgroundColor: theme.secondaryBorder,
+      flex: 1,
+      marginHorizontal: 10
+    }
+  })
 
-export default styles
+export default getStyles
