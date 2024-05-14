3d5f44efa97cf057db9c166f01e70b6dc22d4502
Kaloyan Kosev <superkalo@devlabs.bg>
2024-05-14 11:28:48 +0300

    Refactor: express with utils what is written with a custom class

--- src/common/components/SeparatorWithText/SeparatorWithText.tsx
+++ src/common/components/SeparatorWithText/SeparatorWithText.tsx
@@ -3,6 +3,8 @@ import { View } from 'react-native'
 
 import Text from '@common/components/Text'
 import useTheme from '@common/hooks/useTheme'
+import spacings from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
 
 import getStyles from './styles'
 
@@ -14,7 +16,7 @@ const SeparatorWithText: React.FC<Props> = ({ text }) => {
   const { theme, styles } = useTheme(getStyles)
 
   return (
-    <View style={styles.container}>
+    <View style={[flexbox.directionRow, flexbox.center, spacings.mvMd]}>
       <View style={styles.line} />
       <Text fontSize={20} weight="medium" color={theme.secondaryText}>
         {text}
--- src/common/components/SeparatorWithText/styles.ts
+++ src/common/components/SeparatorWithText/styles.ts
@@ -3,18 +3,11 @@ import { StyleSheet, TextStyle } from 'react-native'
 import { ThemeProps } from '@common/styles/themeConfig'
 
 interface Style {
-  container: TextStyle
   line: TextStyle
 }
 
 const getStyles = (theme: ThemeProps) =>
   StyleSheet.create<Style>({
-    container: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginVertical: 20
-    },
     line: {
       height: 1,
       backgroundColor: theme.secondaryBorder,
