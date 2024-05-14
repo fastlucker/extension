8491da74c9e483867dc2f41b5e1c8dd88242036d
Kaloyan Kosev <superkalo@devlabs.bg>
2024-05-14 11:21:40 +0300

    Refactor: controller name const

--- src/web/contexts/inviteControllerStateContext/inviteControllerStateContext.tsx
+++ src/web/contexts/inviteControllerStateContext/inviteControllerStateContext.tsx
@@ -7,15 +7,15 @@ import useControllerState from '@web/hooks/useControllerState'
 
 const InviteControllerStateContext = createContext<InviteController>({} as InviteController)
 
+const CONTROLLER_NAME = 'invite'
 const InviteControllerStateProvider: React.FC<any> = ({ children }) => {
-  const controller = 'invite'
-  const state = useControllerState(controller)
+  const state = useControllerState(CONTROLLER_NAME)
   const { dispatch } = useBackgroundService()
 
   useEffect(() => {
     dispatch({
       type: 'INIT_CONTROLLER_STATE',
-      params: { controller }
+      params: { controller: CONTROLLER_NAME }
     })
   }, [dispatch])
 
