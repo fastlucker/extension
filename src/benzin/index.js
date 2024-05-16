2eb535804916a9d9ab5d89c76d54effbc8590ee4
Kaloyan Kosev <superkalo@devlabs.bg>
2024-05-16 19:06:46 +0300

    Add: TODOs for the humanizer storage update

--- src/benzin/index.js
+++ src/benzin/index.js
@@ -1,3 +1,7 @@
+// TODO: Figure out how to access these imports
+// import { HUMANIZER_META_KEY } from '@ambire-common/libs/humanizer'
+// import humanizerInfo from '@ambire-common/consts/humanizer/humanizerInfo.json'
+
 import '../../shim'
 
 import '../../global'
@@ -6,6 +10,9 @@ import 'expo-asset'
 
 import { registerRootComponent } from 'expo'
 import BenzinInit from './BenzinInit'
+
+// TODO: Check if setting the storage is needed, similar to the way that's checked in the background.js
+// localStorage.setItem(HUMANIZER_META_KEY, stringify(humanizerInfo))
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
