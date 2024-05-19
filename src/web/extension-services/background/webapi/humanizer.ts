5a3b922d596b9d9052c0e41b4e181d25187fb60d
Yosif Hamed <josif.hameed@gmail.com>
2024-05-19 16:56:07 +0300

    Fix check for stored humanizer meta

--- src/web/extension-services/background/webapi/humanizer.ts
+++ src/web/extension-services/background/webapi/humanizer.ts
@@ -8,14 +8,17 @@ export async function updateHumanizerMetaInStorage(storage: Storage) {
   // these checks are sfficient for knowing whether there is new hardcoded humanizer info json
   // when a user updates the extension this will update the humanizer meta record in the storage if needed
   if (
-    (humanizerMetaInStorage && Object.keys(humanizerMetaInStorage).length === 0) ||
+    !humanizerMetaInStorage ||
+    typeof humanizerMetaInStorage !== 'object' ||
+    Object.keys(humanizerMetaInStorage).length === 0 ||
     // check for new added address
-    Object.keys(humanizerMetaInStorage.knownAddresses).length <
+    Object.keys(humanizerMetaInStorage.knownAddresses || {}).length <
       Object.keys(humanizerJSON.knownAddresses).length ||
     // check for new added contract abi
-    Object.keys(humanizerMetaInStorage.abis).length < Object.keys(humanizerJSON.abis).length ||
+    Object.keys(humanizerMetaInStorage.abis || {}).length <
+      Object.keys(humanizerJSON.abis).length ||
     // check for new added function selector
-    Object.keys(humanizerMetaInStorage.abis.NO_ABI).length <
+    Object.keys(humanizerMetaInStorage.abis.NO_ABI || {}).length <
       Object.keys(humanizerJSON.abis.NO_ABI).length
   )
     await storage.set(HUMANIZER_META_KEY, humanizerJSON)
