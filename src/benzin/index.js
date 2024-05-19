6e8c629dfff2a33e58998a74f80b616ee2d94b47
Yosif Hamed <josif.hameed@gmail.com>
2024-05-19 16:04:05 +0300

    Revert "Add: TODOs for the humanizer storage update"
    This reverts commit 2eb535804916a9d9ab5d89c76d54effbc8590ee4.

--- src/benzin/index.js
+++ src/benzin/index.js
@@ -1,7 +1,3 @@
-// TODO: Figure out how to access these imports
-// import { HUMANIZER_META_KEY } from '@ambire-common/libs/humanizer'
-// import humanizerInfo from '@ambire-common/consts/humanizer/humanizerInfo.json'
-
 import '../../shim'
 
 import '../../global'
@@ -10,9 +6,6 @@ import 'expo-asset'
 
 import { registerRootComponent } from 'expo'
 import BenzinInit from './BenzinInit'
-
-// TODO: Check if setting the storage is needed, similar to the way that's checked in the background.js
-// localStorage.setItem(HUMANIZER_META_KEY, stringify(humanizerInfo))
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
