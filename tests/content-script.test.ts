943387592c17ab9b1fd38274ca0a102e24a4fa0a
Kaloyan Kosev <superkalo@devlabs.bg>
2024-05-20 11:46:55 +0300

    Refactor: make quota a bit more dynamic

--- tests/content-script.test.ts
+++ tests/content-script.test.ts
@@ -1,14 +1,14 @@
 import fs from 'fs'
 import path from 'path'
 
+const quotaInBytes = 1048576 // 1mb
 describe("Extension's content script", () => {
-  it('Should fit in the file size quota (1mb)', async () => {
+  it(`Should fit in the file size quota of ${quotaInBytes / (1024 * 1024)}mb`, async () => {
     const rootDir = process.cwd()
     const filePath = path.resolve(rootDir, 'webkit-prod/content-script.js')
     const stats = fs.statSync(filePath)
     const fileSizeInBytes = stats.size
 
-    const oneMBInBytes = 1048576
-    expect(fileSizeInBytes).toBeLessThan(oneMBInBytes)
+    expect(fileSizeInBytes).toBeLessThan(quotaInBytes)
   })
 })
