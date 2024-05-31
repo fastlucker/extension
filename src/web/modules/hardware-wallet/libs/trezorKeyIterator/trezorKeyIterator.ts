0000d50dd2030241bb62113aec0ca1980f490d00
Kaloyan Kosev <superkalo@devlabs.bg>
2024-05-31 19:07:03 +0300

    Add: mechanism to cache already retrieved addresses from Trezor

--- src/web/modules/hardware-wallet/libs/trezorKeyIterator/trezorKeyIterator.ts
+++ src/web/modules/hardware-wallet/libs/trezorKeyIterator/trezorKeyIterator.ts
@@ -15,6 +15,11 @@ class TrezorKeyIterator implements KeyIteratorInterface {
 
   walletSDK: KeyIteratorProps['walletSDK']
 
+  // Cache the already retrieved addresses, to avoid unnecessary requests to the
+  // Trezor device, because each request opens a popup window and requires the
+  // user to explicitly approve exporting the addresses, which leads to a bad UX.
+  cache: { [path: string]: string } = {}
+
   constructor({ walletSDK }: KeyIteratorProps) {
     if (!walletSDK) throw new Error('trezorKeyIterator: missing walletSDK prop')
 
@@ -27,18 +32,30 @@ class TrezorKeyIterator implements KeyIteratorInterface {
   ) {
     if (!this.walletSDK) throw new Error('trezorKeyIterator: walletSDK not initialized')
 
-    const bundle = []
+    const addrBundleToBeRequested: { path: string; showOnTrezor: boolean }[] = []
     fromToArr.forEach(({ from, to }) => {
       if ((!from && from !== 0) || (!to && to !== 0) || !hdPathTemplate)
         throw new Error('trezorKeyIterator: invalid or missing arguments')
 
       for (let i = from; i <= to; i++) {
-        bundle.push({ path: getHdPathFromTemplate(hdPathTemplate, i), showOnTrezor: false })
+        const path = getHdPathFromTemplate(hdPathTemplate, i)
+        addrBundleToBeRequested.push({ path, showOnTrezor: false })
       }
     })
-    const res = await this.walletSDK.ethereumGetAddress({ bundle })
+
+    const areAllAddressesInCache = addrBundleToBeRequested.every(({ path }) => this.cache[path])
+    if (areAllAddressesInCache) {
+      return addrBundleToBeRequested.map(({ path }) => this.cache[path])
+    }
+
+    const res = await this.walletSDK.ethereumGetAddress({ bundle: addrBundleToBeRequested })
 
     if (!res.success) throw new Error('trezorKeyIterator: failed to retrieve keys')
+
+    // Store the already retrieved addresses in the cache
+    res.payload.forEach(({ address, serializedPath }) => {
+      this.cache[serializedPath] = address
+    })
 
     return res.payload.map(({ address }) => address)
   }
