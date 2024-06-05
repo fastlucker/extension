d7ad3ae8c40becb68af221956124d1c9a56168a8
Krum <135636914+KrumKostov84@users.noreply.github.com>
2024-06-05 13:08:35 +0300

    changed MATIC selector in selectMaticToken()

--- tests/functions.js
+++ tests/functions.js
@@ -321,7 +321,7 @@ export async function selectMaticToken(page) {
   await clickOnElement(page, '[data-testid="tokens-select"]')
   await clickOnElement(
     page,
-    '[data-testid="option-0x0000000000000000000000000000000000000000-polygon-matic"]'
+    '[data-testid="option-0x0000000000000000000000000000000000000000.polygon.matic.false."]'
   )
 }
 
