ae199f6b808e5dda7abb6f6a4afeed6d581bf0d2
Krum <135636914+KrumKostov84@users.noreply.github.com>
2024-06-14 16:51:46 +0300

    Removed duplicated code in login tests

--- tests/basic_account/ba_login.test.js
+++ tests/basic_account/ba_login.test.js
@@ -1,4 +1,5 @@
 import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder'
+
 import {
   bootstrap,
   setAmbKeyStore,
@@ -7,6 +8,12 @@ import {
   typeText,
   INVITE_STORAGE_ITEM
 } from '../functions.js'
+
+import {
+  createAccountWithPhrase,
+  createAccountWithInvalidPhrase,
+  addViewOnlyAccount
+} from '../common.js'
 
 describe('ba_login', () => {
   let browser
@@ -88,49 +95,7 @@ describe('ba_login', () => {
 
   //------------------------------------------------------------------------------------------------------
   it('create basic account with phrase', async () => {
-    await setAmbKeyStore(page, '[data-testid="button-proceed-seed-phrase"]')
-
-    const passphraseWords = process.env.BA_PASSPHRASE
-    const wordArray = passphraseWords.split(' ')
-
-    await page.waitForSelector('[placeholder="Word 1"]')
-    for (let i = 0; i < wordArray.length; i++) {
-      const wordToType = wordArray[i]
-
-      // Type the word into the input field using page.type
-      const inputSelector = `[placeholder="Word ${i + 1}"]`
-      await page.type(inputSelector, wordToType)
-    }
-
-    // This function will complete the onboarsding stories and will select and retrieve first basic and first smarts account
-    const { firstSelectedBasicAccount, firstSelectedSmartAccount } =
-      await finishStoriesAndSelectAccount(page, true)
-
-    // Click on "Save and Continue" button
-    await new Promise((r) => setTimeout(r, 1000))
-    await clickOnElement(page, '[data-testid="button-save-and-continue"]:not([disabled])')
-
-    await page.waitForFunction(
-      () => {
-        return window.location.href.includes('/onboarding-completed')
-      },
-      { timeout: 60000 }
-    )
-
-    await page.goto(`${extensionRootUrl}/tab.html#/account-select`, { waitUntil: 'load' })
-
-    // Verify that selected accounts exist on the page
-    const selectedBasicAccount = await page.$$eval(
-      '[data-testid="account"]',
-      (el) => el[0].innerText
-    )
-    expect(selectedBasicAccount).toContain(firstSelectedBasicAccount)
-
-    const selectedSmartAccount = await page.$$eval(
-      '[data-testid="account"]',
-      (el) => el[1].innerText
-    )
-    expect(selectedSmartAccount).toContain(firstSelectedSmartAccount)
+    await createAccountWithPhrase(page, extensionRootUrl, process.env.BA_PASSPHRASE)
   })
 
   //------------------------------------------------------------------------------------------------------
@@ -175,68 +140,7 @@ describe('ba_login', () => {
 
   //--------------------------------------------------------------------------------------------------------------
   it('(-) Login into basic account with invalid phrase', async () => {
-    await setAmbKeyStore(page, '[data-testid="button-proceed-seed-phrase"]')
-
-    await page.waitForSelector('[placeholder="Word 1"]')
-
-    // This function types words in the passphrase fields and checks if the button is disabled.
-    async function typeWordsAndCheckButton(passphraseWords) {
-      const wordArray = passphraseWords.split(' ')
-
-      for (let i = 0; i < wordArray.length; i++) {
-        const wordToType = wordArray[i]
-
-        // Type the word into the input field using page.type
-        const inputSelector = `[placeholder="Word ${i + 1}"]`
-        await page.type(inputSelector, wordToType)
-      }
-
-      // Check whether button is disabled
-      const isButtonDisabled = await page.$eval('[data-testid="import-button"]', (button) => {
-        return button.getAttribute('aria-disabled')
-      })
-
-      expect(isButtonDisabled).toBe('true')
-    }
-    // This function waits until an error message appears on the page.
-    async function waitUntilError(validateMessage) {
-      await page.waitForFunction(
-        (text) => {
-          const element = document.querySelector('body')
-          return element && element.textContent.includes(text)
-        },
-        { timeout: 8000 },
-        validateMessage
-      )
-    }
-
-    // Try to login with empty phrase fields
-    let passphraseWords = ''
-    await typeWordsAndCheckButton(passphraseWords)
-
-    // Test cases with different phrases keys
-    passphraseWords =
-      '00000 000000 00000 000000 00000 000000 00000 000000 00000 000000 00000 000000'
-    await typeWordsAndCheckButton(passphraseWords)
-
-    const errorMessage = 'Invalid Seed Phrase. Please review every field carefully.'
-    // Wait until the error message appears on the page
-    await waitUntilError(errorMessage)
-
-    // Clear the passphrase fields before write the new phrase
-    const wordArray = passphraseWords.split(' ')
-    for (let i = 0; i < wordArray.length; i++) {
-      const wordToType = wordArray[i]
-      const inputSelector = `[placeholder="Word ${i + 1}"]`
-      await page.click(inputSelector, { clickCount: 3 }) // Select all content
-      await page.keyboard.press('Backspace') // Delete the selected content
-    }
-
-    passphraseWords =
-      'allow survey play weasel exhibit helmet industry bunker fish step garlic ababa'
-    await typeWordsAndCheckButton(passphraseWords)
-    // Wait until the error message appears on the page
-    await waitUntilError(errorMessage)
+    await createAccountWithInvalidPhrase(page)
   })
 
   //--------------------------------------------------------------------------------------------------------------
@@ -298,67 +202,6 @@ describe('ba_login', () => {
 
   //--------------------------------------------------------------------------------------------------------------
   it('add view-only basic account', async () => {
-    await new Promise((r) => setTimeout(r, 1000))
-    const buttonNext = '[data-testid="stories-button-next"]'
-
-    await page.waitForSelector(buttonNext)
-
-    // Click on "Next" button several times to finish the onboarding
-    await page.$eval(buttonNext, (button) => button.click())
-
-    await page.waitForSelector('[data-testid="stories-button-previous"]')
-
-    await page.$eval(buttonNext, (button) => button.click())
-    await page.$eval(buttonNext, (button) => button.click())
-    await page.$eval(buttonNext, (button) => button.click())
-    await page.$eval(buttonNext, (button) => button.click())
-
-    // check the checkbox "I agree ..."
-    await page.$eval('[data-testid="checkbox"]', (button) => button.click())
-    // Click on "Got it"
-    await page.$eval(buttonNext, (button) => button.click())
-
-    await page.waitForSelector('[data-testid="get-started-button-add"]')
-
-    // Select "Add"
-    await clickOnElement(page, '[data-testid="get-started-button-add"]')
-
-    const viewOnlyAddress = '0x048d8573402CE085A6c8f34d568eC2Ccc995196e'
-
-    await typeText(page, '[data-testid="address-ens-field"]', viewOnlyAddress)
-    await new Promise((r) => setTimeout(r, 500))
-
-    // Click on "Import View-Only Accounts" button
-    await clickOnElement(page, '[data-testid="view-only-button-import"]')
-
-    // Click on "Account"
-    await new Promise((r) => setTimeout(r, 1000))
-    await clickOnElement(page, '[data-testid="button-save-and-continue"]:not([disabled])')
-
-    await page.goto(`${extensionRootUrl}/tab.html#/account-select`, { waitUntil: 'load' })
-
-    // Find the element containing the specified address
-    const addressElement = await page.$x(`//*[contains(text(), '${viewOnlyAddress}')]`)
-
-    if (addressElement.length > 0) {
-      // Get the parent element of the element with the specified address
-      const parentElement = await addressElement[0].$x('..')
-
-      if (parentElement.length > 0) {
-        // Get the text content of the parent element and all elements within it
-        const parentTextContent = await page.evaluate((element) => {
-          const elements = element.querySelectorAll('*')
-          return Array.from(elements, (el) => el.textContent).join('\n')
-        }, parentElement[0])
-
-        // Verify that somewhere in the content there is the text 'View-only'
-        const containsViewOnly = parentTextContent.includes('View-only')
-
-        if (containsViewOnly) {
-        } else {
-          throw new Error('The content does not contain the text "View-only".')
-        }
-      }
-    }
+    await addViewOnlyAccount(page, extensionRootUrl, '0x048d8573402CE085A6c8f34d568eC2Ccc995196e')
   })
 })
--- tests/common.js
+++ tests/common.js
@@ -2,9 +2,8 @@ import {
   typeText,
   clickOnElement,
   clickElementWithRetry,
-  bootstrapWithStorage,
-  baParams,
-  saParams,
+  finishStoriesAndSelectAccount,
+  setAmbKeyStore,
   confirmTransaction,
   selectMaticToken
 } from './functions.js'
@@ -387,4 +386,174 @@ export async function signMessage(page, extensionRootUrl, browser, signerAddress
     {},
     validateMessage
   )
+}
+
+//--------------------------------------------------------------------------------------------------------------
+export async function createAccountWithPhrase(page, extensionRootUrl, phrase) {
+  await setAmbKeyStore(page, '[data-testid="button-proceed-seed-phrase"]')
+
+  const passphraseWords = phrase
+  const wordArray = passphraseWords.split(' ')
+
+  await page.waitForSelector('[placeholder="Word 1"]')
+  for (let i = 0; i < wordArray.length; i++) {
+    const wordToType = wordArray[i]
+
+    // Type the word into the input field using page.type
+    const inputSelector = `[placeholder="Word ${i + 1}"]`
+    await page.type(inputSelector, wordToType)
+  }
+
+  // This function will complete the onboarsding stories and will select and retrieve first basic and first smarts account
+  const { firstSelectedBasicAccount, firstSelectedSmartAccount } =
+    await finishStoriesAndSelectAccount(page, 'true')
+
+  // Click on "Save and Continue" button
+  await new Promise((r) => setTimeout(r, 1000))
+  await clickOnElement(page, '[data-testid="button-save-and-continue"]:not([disabled])')
+
+  await page.waitForFunction(
+    () => {
+      return window.location.href.includes('/onboarding-completed')
+    },
+    { timeout: 60000 }
+  )
+
+  await page.goto(`${extensionRootUrl}/tab.html#/account-select`, { waitUntil: 'load' })
+
+  // Verify that selected accounts exist on the page
+  const selectedBasicAccount = await page.$$eval('[data-testid="account"]', (el) => el[0].innerText)
+  expect(selectedBasicAccount).toContain(firstSelectedBasicAccount)
+
+  const selectedSmartAccount = await page.$$eval('[data-testid="account"]', (el) => el[1].innerText)
+  expect(selectedSmartAccount).toContain(firstSelectedSmartAccount)
+}
+
+//--------------------------------------------------------------------------------------------------------------
+export async function createAccountWithInvalidPhrase(page) {
+  await setAmbKeyStore(page, '[data-testid="button-proceed-seed-phrase"]')
+
+  await page.waitForSelector('[placeholder="Word 1"]')
+
+  // This function types words in the passphrase fields and checks if the button is disabled.
+  async function typeWordsAndCheckButton(passphraseWords) {
+    const wordArray = passphraseWords.split(' ')
+
+    for (let i = 0; i < wordArray.length; i++) {
+      const wordToType = wordArray[i]
+
+      // Type the word into the input field using page.type
+      const inputSelector = `[placeholder="Word ${i + 1}"]`
+      await page.type(inputSelector, wordToType)
+    }
+
+    // Check whether button is disabled
+    const isButtonDisabled = await page.$eval('[data-testid="import-button"]', (button) => {
+      return button.getAttribute('aria-disabled')
+    })
+
+    expect(isButtonDisabled).toBe('true')
+  }
+
+  // This function waits until an error message appears on the page.
+  async function waitUntilError(validateMessage) {
+    await page.waitForFunction(
+      (text) => {
+        const element = document.querySelector('body')
+        return element && element.textContent.includes(text)
+      },
+      { timeout: 8000 },
+      validateMessage
+    )
+  }
+
+  // Try to login with empty phrase fields
+  let passphraseWords = ''
+  await typeWordsAndCheckButton(passphraseWords)
+
+  // Test cases with different phrases keys
+  passphraseWords = '00000 000000 00000 000000 00000 000000 00000 000000 00000 000000 00000 000000'
+  await typeWordsAndCheckButton(passphraseWords)
+
+  const errorMessage = 'Invalid Seed Phrase. Please review every field carefully.'
+  // Wait until the error message appears on the page
+  await waitUntilError(errorMessage)
+
+  // Clear the passphrase fields before write the new phrase
+  const wordArray = passphraseWords.split(' ')
+  for (let i = 0; i < wordArray.length; i++) {
+    const wordToType = wordArray[i]
+    const inputSelector = `[placeholder="Word ${i + 1}"]`
+    await page.click(inputSelector, { clickCount: 3 }) // Select all content
+    await page.keyboard.press('Backspace') // Delete the selected content
+  }
+
+  passphraseWords = 'allow survey play weasel exhibit helmet industry bunker fish step garlic ababa'
+  await typeWordsAndCheckButton(passphraseWords)
+  // Wait until the error message appears on the page
+  await waitUntilError(errorMessage)
+}
+
+//--------------------------------------------------------------------------------------------------------------
+export async function addViewOnlyAccount(page, extensionRootUrl, viewOnlyAddress) {
+  await new Promise((r) => setTimeout(r, 1000))
+  const buttonNext = '[data-testid="stories-button-next"]'
+
+  await page.waitForSelector(buttonNext)
+
+  // Click on "Next" button several times to finish the onboarding
+  await page.$eval(buttonNext, (button) => button.click())
+
+  await page.waitForSelector('[data-testid="stories-button-previous"]')
+
+  await page.$eval(buttonNext, (button) => button.click())
+  await page.$eval(buttonNext, (button) => button.click())
+  await page.$eval(buttonNext, (button) => button.click())
+  await page.$eval(buttonNext, (button) => button.click())
+
+  // check the checkbox "I agree ..."
+  await page.$eval('[data-testid="checkbox"]', (button) => button.click())
+  // Click on "Got it"
+  await page.$eval(buttonNext, (button) => button.click())
+
+  await page.waitForSelector('[data-testid="get-started-button-add"]')
+
+  // Select "Add"
+  await clickOnElement(page, '[data-testid="get-started-button-add"]')
+
+  await typeText(page, '[data-testid="address-ens-field"]', viewOnlyAddress)
+  await new Promise((r) => setTimeout(r, 500))
+
+  // Click on "Import View-Only Accounts" button
+  await clickOnElement(page, '[data-testid="view-only-button-import"]')
+
+  // Click on "Account"
+  await new Promise((r) => setTimeout(r, 1000))
+  await clickOnElement(page, '[data-testid="button-save-and-continue"]:not([disabled])')
+
+  await page.goto(`${extensionRootUrl}/tab.html#/account-select`, { waitUntil: 'load' })
+
+  // Find the element containing the specified address
+  const addressElement = await page.$x(`//*[contains(text(), '${viewOnlyAddress}')]`)
+
+  if (addressElement.length > 0) {
+    // Get the parent element of the element with the specified address
+    const parentElement = await addressElement[0].$x('..')
+
+    if (parentElement.length > 0) {
+      // Get the text content of the parent element and all elements within it
+      const parentTextContent = await page.evaluate((element) => {
+        const elements = element.querySelectorAll('*')
+        return Array.from(elements, (el) => el.textContent).join('\n')
+      }, parentElement[0])
+
+      // Verify that somewhere in the content there is the text 'View-only'
+      const containsViewOnly = parentTextContent.includes('View-only')
+
+      if (containsViewOnly) {
+      } else {
+        throw new Error('The content does not contain the text "View-only".')
+      }
+    }
+  }
 }
