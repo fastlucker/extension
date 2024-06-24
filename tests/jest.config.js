1aa4f557e33c9f7c3a36a5fe4b039baaf6a5937c
Kaloyan Kosev <superkalo@devlabs.bg>
2024-06-24 13:54:23 +0300

    Change: limit the concurrently running test suits to 1 (otherwise, random test failings occur)

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   // Longer timeout than the default one needed for heavier dapps, like https://myetherwallet.com,
   // otherwise, tests fail because the default timeout gets reached.
   testTimeout: 120000,
+  maxWorkers: 1,
   testPathIgnorePatterns: [
     // FIXME: Temporary disable running the smart account tests, until they are running correctly
     // path.join('<rootDir>', 'tests/smart_account')
