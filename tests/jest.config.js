690d3448a214870cd044cdfc9a9ccb7c382ceec8
sonytooo <simeonpetkov11999977@gmail.com>
2024-07-01 14:46:56 +0300

    fix: comment

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
   // Limit the number of workers to prevent performance degradation.
   // Running multiple workers simultaneously can significantly slow down the tests,
   // causing them to fail randomly due to exceeding timeout limits.
+  // https://github.com/argos-ci/jest-puppeteer?tab=readme-ov-file#ci-timeout
   maxWorkers: 1,
   browserPerWorker: true,
   testPathIgnorePatterns: [
