fcac23519ecebad2e76760a59b32058b65e48b38
Gergana Mihaylova <gergana@devlabs.bg>
2024-07-04 16:27:10 +0300

    Fix: disable actions on rewards tokens and remove them from Transfer

--- src/web/contexts/transferControllerStateContext/transferControllerStateContext.tsx
+++ src/web/contexts/transferControllerStateContext/transferControllerStateContext.tsx
@@ -59,7 +59,7 @@ const TransferControllerStateProvider: React.FC<any> = ({ children }) => {
           return hasAmount && tokenNetwork?.hasRelayer && token.flags.canTopUpGasTank
         }
 
-        return hasAmount && !token.flags.onGasTank
+        return hasAmount && !token.flags.onGasTank && !token.flags.rewardsType
       }) || [],
     [accountPortfolio?.tokens, networks, selectedTokenFromUrl?.isTopUp]
   )
