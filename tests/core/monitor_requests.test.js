eee71f75b6cc2be725f6a39bd4a3bd1d64a92809
Petromir Petrov <petromirdev@gmail.com>
2024-07-24 12:34:23 +0300

    add a new test case and refactor monitor_requests

--- tests/core/monitor_requests.test.js
+++ tests/core/monitor_requests.test.js
@@ -1,17 +1,37 @@
 /* eslint-disable import/no-unresolved */
 import { networks } from '@ambire-common/consts/networks'
-import wait from '@ambire-common/utils/wait'
-import { baParams, bootstrapWithStorage, clickOnElement } from '../functions.js'
+import { makeValidTransaction } from '../common/transactions.js'
+import { baParams, bootstrapWithStorage, clickOnElement, monitorRequests } from '../functions.js'
+
+const getRequestsByType = (requests) => {
+  // -- Native token price requests
+  const nativeTokenPriceRequests = requests.filter((request) => request.includes('/simple/price'))
+  // -- ERC20 token price requests
+  const batchedErc20TokenPriceRequests = requests.filter((request) =>
+    request.includes('/simple/token_price')
+  )
+  // Hints requests
+  const hintsRequests = requests.filter((request) => request.includes('/multi-hints'))
+  // RPC requests
+  const rpcRequests = requests.filter((request) => request.includes('invictus'))
+
+  return {
+    nativeTokenPriceRequests,
+    batchedErc20TokenPriceRequests,
+    hintsRequests,
+    rpcRequests
+  }
+}
 
 describe('Monitor network requests and make sure only necessary requests are made', () => {
   let browser
   let page
   let recorder
   let serviceWorker
-  let httpRequests = []
+  let extensionURL
 
   beforeEach(async () => {
-    ;({ browser, page, recorder, serviceWorker } = await bootstrapWithStorage(
+    ;({ browser, page, recorder, extensionURL, serviceWorker } = await bootstrapWithStorage(
       'monitor_requests',
       baParams
     ))
@@ -20,54 +40,44 @@ describe('Monitor network requests and make sure only necessary requests are mad
   afterEach(async () => {
     await recorder.stop()
     await browser.close()
-    httpRequests = []
   })
 
   it('portfolio and account state reload', async () => {
-    const client = serviceWorker.client
-
-    await client.send('Fetch.enable', {
-      patterns: [
-        {
-          urlPattern: '*',
-          requestStage: 'Response'
-        }
-      ]
-    })
-    await client.on('Fetch.requestPaused', async ({ requestId, request }) => {
-      httpRequests.push(request.url)
-
-      await client.send('Fetch.continueRequest', {
-        requestId
-      })
+    const httpRequests = await monitorRequests(serviceWorker.client, async () => {
+      await clickOnElement(page, '[data-testid="refresh-button"]')
     })
 
-    await clickOnElement(page, '[data-testid="refresh-button"]')
-    await wait(6000)
-
-    // Price requests
-
-    // -- Native token price requests
-    const nativeTokenPriceRequests = httpRequests.filter((request) =>
-      request.includes('/simple/price')
-    )
+    const { nativeTokenPriceRequests, batchedErc20TokenPriceRequests, hintsRequests, rpcRequests } =
+      getRequestsByType(httpRequests)
 
     expect(nativeTokenPriceRequests.length).toBe(networks.length)
-    // -- ERC20 token price requests
-    const batchedErc20TokenPriceRequests = httpRequests.filter((request) =>
-      request.includes('/simple/token_price')
-    )
 
     expect(batchedErc20TokenPriceRequests.length).toBe(networks.length - 1)
 
-    // Hints requests
-    const hintsRequests = httpRequests.filter((request) => request.includes('/multi-hints'))
-
     expect(hintsRequests.length).toBe(networks.length)
-    // RPC requests
-
-    const rpcRequests = httpRequests.filter((request) => request.includes('invictus'))
 
     expect(rpcRequests.length).toBeLessThanOrEqual(20)
+  })
+
+  it('sign account op request created through transfer', async () => {
+    const httpRequests = await monitorRequests(serviceWorker.client, async () => {
+      await makeValidTransaction(page, extensionURL, browser, {
+        shouldStopBeforeSign: true
+      })
+    })
+
+    const { rpcRequests, nativeTokenPriceRequests, batchedErc20TokenPriceRequests } =
+      getRequestsByType(httpRequests)
+
+    const nonPolygonAndEthereumRpcRequests = rpcRequests.filter(
+      (request) => !request.includes('polygon') && !request.includes('ethereum')
+    )
+
+    // Expect more rpc requests because of ENS/UD resolution
+    // @TODO: check if we can reduce the number of requests
+    expect(rpcRequests.length).toBeLessThanOrEqual(10)
+    expect(nonPolygonAndEthereumRpcRequests.length).toBe(0)
+    expect(nativeTokenPriceRequests.length).toBeLessThanOrEqual(2)
+    expect(batchedErc20TokenPriceRequests.length).toBeLessThanOrEqual(2)
   })
 })
