12d3eb39bfd344aa6fde24e1b5e7dd701d0524c2
Jordan Enev <jordantoen@gmail.com>
2024-07-25 18:11:26 +0300

    Change: Create a reusable height constant for both the Balance line-height and the Balance skeleton. We want both components to have the same height; otherwise, clicking on the RefreshIcon causes a layout shift.

--- src/common/modules/dashboard/components/DashboardOverview/DashboardOverview.tsx
+++ src/common/modules/dashboard/components/DashboardOverview/DashboardOverview.tsx
@@ -40,6 +40,10 @@ interface Props {
   setDashboardOverviewSize: React.Dispatch<React.SetStateAction<{ width: number; height: number }>>
 }
 
+// We create a reusable height constant for both the Balance line-height and the Balance skeleton.
+// We want both components to have the same height; otherwise, clicking on the RefreshIcon causes a layout shift.
+const balanceHeight = 34
+
 const DashboardOverview: FC<Props> = ({
   openReceiveModal,
   animatedOverviewHeight,
@@ -215,14 +219,14 @@ const DashboardOverview: FC<Props> = ({
               <View>
                 <View style={[flexbox.directionRow, flexbox.alignCenter, spacings.mbTy]}>
                   {!accountPortfolio?.isAllReady ? (
-                    <SkeletonLoader lowOpacity width={200} height={42} borderRadius={8} />
+                    <SkeletonLoader lowOpacity width={200} height={balanceHeight} borderRadius={8} />
                   ) : (
                     <View testID="full-balance" style={[flexbox.directionRow, flexbox.alignCenter]}>
                       <Text selectable>
                         <Text
                           fontSize={32}
                           shouldScale={false}
-                          style={{ lineHeight: 34 }}
+                          style={{ lineHeight: balanceHeight }}
                           weight="number_bold"
                           color={theme.primaryBackground}
                           selectable
