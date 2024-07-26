0b90e29db697f05b3ce9e7f833ef0ade9f828988
Kaloyan Kosev <superkalo@devlabs.bg>
2024-07-26 12:15:33 +0300

    Change: clean up old file

--- src/web/modules/hardware-wallet/utils/ledger.ts
+++
@@ -1,6 +0,0 @@
-import { ledgerUSBVendorId } from '@ledgerhq/devices'
-
-export const hasConnectedLedgerDevice = async () => {
-  const devices = await navigator.hid.getDevices()
-  return devices.filter((device) => device.vendorId === ledgerUSBVendorId).length > 0
-}
