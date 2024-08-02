840bab2ae7b65ed5324bc5841b7ceb6abd3567c5
sonytooo <simeonpetkov11999977@gmail.com>
2024-08-02 15:43:43 +0300

    ts fix

--- src/web/extension-services/background/webapi/icon.ts
+++ src/web/extension-services/background/webapi/icon.ts
@@ -1,7 +1,7 @@
 import { browser } from '@web/constants/browserapi'
 
 export const setExtensionIcon = (type: 'default' | 'locked') => {
-  const icons = [16, 48, 96, 128].reduce((res: any, size) => {
+  const icons = [16, 48, 96, 128].reduce((res: { [key: number]: string }, size: number) => {
     if (type === 'locked') {
       res[size] = `assets/images/xicon_locked@${size}.png`
     } else {
