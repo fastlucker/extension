06f5aa4802a24c7c3b204f8aa9fcfedbd3fe2de9
Petromir Petrov <petromirdev@gmail.com>
2024-08-12 14:02:08 +0300

    delete: pin extension styles from dashboard styles

--- src/common/modules/dashboard/screens/styles.ts
+++ src/common/modules/dashboard/screens/styles.ts
@@ -1,40 +1,21 @@
 import { StyleSheet, ViewStyle } from 'react-native'
 
-import { BOTTOM_SHEET_Z_INDEX } from '@common/components/BottomSheet/styles'
-import { SPACING_4XL, SPACING_MI, SPACING_TY, SPACING_XL } from '@common/styles/spacings'
 import { ThemeProps } from '@common/styles/themeConfig'
 import flexbox from '@common/styles/utils/flexbox'
-import { getUiType } from '@web/utils/uiType'
 
 interface Style {
   container: ViewStyle
-  pinExtensionIcon: ViewStyle
-  closeIcon: ViewStyle
 }
 
 export const NEUTRAL_BACKGROUND = '#1418333D'
 export const NEUTRAL_BACKGROUND_HOVERED = '#14183352'
 export const DASHBOARD_OVERVIEW_BACKGROUND = '#353d6e'
 
-const { isPopup } = getUiType()
-
 const getStyles = (theme: ThemeProps) =>
   StyleSheet.create<Style>({
     container: {
       ...flexbox.flex1,
       backgroundColor: theme.primaryBackground
-    },
-    pinExtensionIcon: {
-      position: 'absolute',
-      right: isPopup ? -SPACING_XL - SPACING_MI : SPACING_4XL + SPACING_XL,
-      top: SPACING_TY,
-      zIndex: BOTTOM_SHEET_Z_INDEX
-    },
-    closeIcon: {
-      position: 'absolute',
-      zIndex: 2,
-      right: 68,
-      top: 24
     }
   })
 
