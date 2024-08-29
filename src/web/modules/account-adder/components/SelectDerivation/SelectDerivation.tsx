1a86326d9bc336243d9bbfe4f999317470b8cae5
Kaloyan Kosev <superkalo@devlabs.bg>
2024-08-29 17:47:10 +0300

    Add: HD Path label + tooltip + align the select

--- src/web/modules/account-adder/components/SelectDerivation/SelectDerivation.tsx
+++ src/web/modules/account-adder/components/SelectDerivation/SelectDerivation.tsx
@@ -1,14 +1,25 @@
 import React, { useCallback, useMemo } from 'react'
+import { View } from 'react-native'
 
 import { DERIVATION_OPTIONS, HD_PATH_TEMPLATE_TYPE } from '@ambire-common/consts/derivation'
+import InformationIcon from '@common/assets/svg/InformationIcon'
 import Select from '@common/components/Select'
 import { SelectValue } from '@common/components/Select/types'
+import Text from '@common/components/Text'
+import Tooltip from '@common/components/Tooltip'
+import { useTranslation } from '@common/config/localization'
+import colors from '@common/styles/colors'
+import spacings from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
 import useAccountAdderControllerState from '@web/hooks/useAccountAdderControllerState'
 import useBackgroundService from '@web/hooks/useBackgroundService'
+
+import styles from './styles'
 
 interface Props {}
 
 const SelectDerivation: React.FC<Props> = () => {
+  const { t } = useTranslation()
   const { dispatch } = useBackgroundService()
   const { hdPathTemplate } = useAccountAdderControllerState()
 
@@ -17,7 +28,7 @@ const SelectDerivation: React.FC<Props> = () => {
     [hdPathTemplate]
   )
 
-  const handleChangeDerivation = useCallback(
+  const handleChangeHdPath = useCallback(
     (s: SelectValue) => {
       dispatch({
         type: 'MAIN_CONTROLLER_ACCOUNT_ADDER_SET_HD_PATH_TEMPLATE',
@@ -30,13 +41,30 @@ const SelectDerivation: React.FC<Props> = () => {
   if (!value) return null
 
   return (
-    <Select
-      setValue={handleChangeDerivation}
-      containerStyle={{ width: 350 }}
-      selectStyle={{ height: 40 }}
-      options={DERIVATION_OPTIONS}
-      value={value}
-    />
+    <View style={[flexbox.directionRow, flexbox.center]}>
+      <Text fontSize={12} weight="medium" style={spacings.mrMi}>
+        {t('HD Path')}
+      </Text>
+      <InformationIcon
+        width={14}
+        height={14}
+        style={spacings.mrSm}
+        dataSet={{
+          tooltipId: 'hd-path-tooltip',
+          tooltipContent: t(
+            "If you don't see the expected accounts, try changing the HD path to access different sets of addresses within this wallet."
+          )
+        }}
+      />
+      <Tooltip id="hd-path-tooltip" />
+      <Select
+        setValue={handleChangeHdPath}
+        containerStyle={styles.selectContainer}
+        selectStyle={{ height: 40 }}
+        options={DERIVATION_OPTIONS}
+        value={value}
+      />
+    </View>
   )
 }
 
