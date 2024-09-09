05e603c5f17f4171dd5510ba1138403933684256
Yosif Hamed <josif.hameed@gmail.com>
2024-09-09 13:59:47 +0300

    Add contract logo to humanizer visualization

--- src/common/components/Address/Address.tsx
+++ src/common/components/Address/Address.tsx
@@ -1,10 +1,13 @@
 import { getAddress, ZeroAddress } from 'ethers'
 import React, { FC, useMemo } from 'react'
+import { Image, View } from 'react-native'
 
 import humanizerInfo from '@ambire-common/consts/humanizer/humanizerInfo.json'
 import { HumanizerMeta } from '@ambire-common/libs/humanizer/interfaces'
 import { Props as TextProps } from '@common/components/Text'
 import { useTranslation } from '@common/config/localization'
+import useTheme from '@common/hooks/useTheme'
+import flexbox from '@common/styles/utils/flexbox'
 import { isExtension } from '@web/constants/browserapi'
 import useAccountsControllerState from '@web/hooks/useAccountsControllerState'
 import useAddressBookControllerState from '@web/hooks/useAddressBookControllerState'
@@ -12,6 +15,7 @@ import usePortfolioControllerState from '@web/hooks/usePortfolioControllerState/
 
 import BaseAddress from './components/BaseAddress'
 import { BenzinDomainsAddress, DomainsAddress } from './components/DomainsAddress'
+import getStyles from './styles'
 
 interface Props extends TextProps {
   address: string
@@ -27,53 +31,70 @@ const Address: FC<Props> = ({ address, highestPriorityAlias, ...rest }) => {
   const { t } = useTranslation()
   const { contacts = [] } = useAddressBookControllerState()
   const checksummedAddress = useMemo(() => getAddress(address), [address])
+  const { styles } = useTheme(getStyles)
 
-  const account = useMemo(() => {
-    if (!accountsState?.accounts) return undefined
-    return accountsState.accounts.find((a) => a.addr === checksummedAddress)
-  }, [accountsState?.accounts, checksummedAddress])
-  const tokenInPortfolio = useMemo(() => {
-    if (!accountPortfolio?.tokens) return undefined
-    return accountPortfolio.tokens.find(
+  const contractLogoUrl = useMemo(
+    () => (humanizerInfo.knownAddresses as { [key: string]: any })[address.toLowerCase()]?.logo,
+    [address]
+  )
+
+  const addressComponentPart = useMemo(() => {
+    const account = accountsState.accounts.find((a) => a.addr === checksummedAddress)
+    const tokenInPortfolio = accountPortfolio.tokens.find(
       (token) => token.address.toLowerCase() === address.toLowerCase()
     )
-  }, [accountPortfolio?.tokens, address])
+    const hardcodedTokenSymbol = HUMANIZER_META.knownAddresses[address.toLowerCase()]?.token?.symbol
+    const hardcodedName = HUMANIZER_META.knownAddresses[address.toLowerCase()]?.name
+    let tokenLabel = ''
 
-  const hardcodedTokenSymbol = HUMANIZER_META.knownAddresses[address.toLowerCase()]?.token?.symbol
-  const hardcodedName = HUMANIZER_META.knownAddresses[address.toLowerCase()]?.name
-  let tokenLabel = ''
+    if (tokenInPortfolio) {
+      tokenLabel = `Token ${tokenInPortfolio?.symbol} Contract`
+    } else if (hardcodedTokenSymbol) {
+      tokenLabel = t(`Token ${hardcodedTokenSymbol} Contract`)
+    }
 
-  if (tokenInPortfolio) {
-    tokenLabel = `Token ${tokenInPortfolio?.symbol} Contract`
-  } else if (hardcodedTokenSymbol) {
-    tokenLabel = t(`Token ${hardcodedTokenSymbol} Contract`)
-  }
-
-  const contact = contacts.find((c) => c.address.toLowerCase() === address.toLowerCase())
-  const zeroAddressLabel = address === ZeroAddress && 'Zero Address'
-  // highestPriorityAlias and account labels are of higher priority than domains
-  if (
-    highestPriorityAlias ||
-    zeroAddressLabel ||
-    contact?.name ||
-    account?.preferences?.label ||
-    hardcodedName ||
-    tokenLabel
-  )
-    return (
-      <BaseAddress address={checksummedAddress} {...rest}>
-        {highestPriorityAlias ||
-          zeroAddressLabel ||
-          contact?.name ||
-          account?.preferences?.label ||
-          hardcodedName ||
-          tokenLabel}
-      </BaseAddress>
+    const contact = contacts.find((c) => c.address.toLowerCase() === address.toLowerCase())
+    const zeroAddressLabel = address === ZeroAddress && 'Zero Address'
+    // highestPriorityAlias and account labels are of higher priority than domains
+    if (
+      highestPriorityAlias ||
+      zeroAddressLabel ||
+      contact?.name ||
+      account?.preferences?.label ||
+      hardcodedName ||
+      tokenLabel
     )
+      return (
+        <BaseAddress address={checksummedAddress} {...rest}>
+          {highestPriorityAlias ||
+            zeroAddressLabel ||
+            contact?.name ||
+            account?.preferences?.label ||
+            hardcodedName ||
+            tokenLabel}
+        </BaseAddress>
+      )
 
-  if (!isExtension) return <BenzinDomainsAddress address={checksummedAddress} {...rest} />
+    if (!isExtension) return <BenzinDomainsAddress address={checksummedAddress} {...rest} />
 
-  return <DomainsAddress address={checksummedAddress} {...rest} />
+    return <DomainsAddress address={checksummedAddress} {...rest} />
+  }, [
+    highestPriorityAlias,
+    checksummedAddress,
+    rest,
+    accountPortfolio?.tokens,
+    accountsState.accounts,
+    address,
+    contacts,
+    t
+  ])
+
+  return (
+    <View style={flexbox.directionRow}>
+      {contractLogoUrl && <Image source={{ uri: contractLogoUrl }} style={styles.logo} />}
+      {addressComponentPart}
+    </View>
+  )
 }
 
 export default React.memo(Address)
