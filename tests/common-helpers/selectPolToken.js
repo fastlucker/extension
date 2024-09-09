06579a3718290f0ca81a4a926f703507d84e398a
Borislav Itskov <borislav@devlabs.bg>
2024-09-09 14:16:13 +0300

    fix e2e tests by replacing polygon.matic with polygon.pol

--- tests/common-helpers/selectPolToken.js
+++ tests/common-helpers/selectPolToken.js
@@ -4,6 +4,6 @@ export async function selectPolToken(page) {
   await clickOnElement(page, '[data-testid="tokens-select"]')
   await clickOnElement(
     page,
-    '[data-testid="option-0x0000000000000000000000000000000000000000.polygon.matic.false."]'
+    '[data-testid="option-0x0000000000000000000000000000000000000000.polygon.pol.false."]'
   )
 }
