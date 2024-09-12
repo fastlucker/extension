28040aa3623ae64ac5256e5f1f6f5c589a40953e
Kaloyan Kosev <superkalo@devlabs.bg>
2024-09-12 16:38:39 +0300

    Add: screen skeleton

--- src/web/modules/swap/screens/SwapScreen.tsx
+++ src/web/modules/swap/screens/SwapScreen.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
-import { View } from 'react-native'
+
+import useTheme from '@common/hooks/useTheme'
+import spacings from '@common/styles/spacings'
+import HeaderAccountAndNetworkInfo from '@web/components/HeaderAccountAndNetworkInfo'
+import { TabLayoutContainer, TabLayoutWrapperMainContent } from '@web/components/TabLayoutWrapper'
 
 const SwapScreen = () => {
-  return <View>This is the swap screen</View>
+  const { theme } = useTheme()
+
+  return (
+    <TabLayoutContainer
+      backgroundColor={theme.secondaryBackground}
+      width="xl"
+      header={<HeaderAccountAndNetworkInfo />}
+    >
+      <TabLayoutWrapperMainContent contentContainerStyle={spacings.pt2Xl}>
+        {/* TODO: Swap UI */}
+      </TabLayoutWrapperMainContent>
+    </TabLayoutContainer>
+  )
 }
 
 export default React.memo(SwapScreen)
