adf9e6e0ab890e742f80ffa743e45a51c37ce1b7
Petromir Petrov <petromirdev@gmail.com>
2024-09-20 16:06:42 +0300

    revert: delete portals

--- src/legends/LegendsInit.tsx
+++ src/legends/LegendsInit.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
-// import { PortalHost, PortalProvider } from '@gorhom/portal'
+import { PortalHost, PortalProvider } from '@gorhom/portal'
+
 import Router from './modules/router/Router'
 
 const BenzinInit = () => {
   return (
     <BrowserRouter>
-      {/* <PortalProvider> */}
-      <Router />
-      {/* <PortalHost name="global" /> */}
-      {/* </PortalProvider> */}
+      <PortalProvider>
+        <Router />
+        <PortalHost name="global" />
+      </PortalProvider>
     </BrowserRouter>
   )
 }
