782691284b775e892199329ceff56d12d9c991fc
sonytooo <simeonpetkov11999977@gmail.com>
2024-09-24 10:02:46 +0300

    fix paths of the safari dev build

--- scripts/safari-dev-build.sh
+++ scripts/safari-dev-build.sh
@@ -3,7 +3,7 @@
 SCRIPTS_DIR="$(dirname "$(realpath "$0")")"
 PROJECT_ROOT="$(cd "$SCRIPTS_DIR"; while [ ! -f "README.md" ] && [ "$PWD" != "/" ]; do cd ..; done; pwd)"
 SAFARI_DEV_DIR="$PROJECT_ROOT/build/safari-dev"
-SAFARI_XCODE_PROJECT="$PROJECT_ROOT/build/safari-xcode-project-dev"
+SAFARI_XCODE_PROJECT="$PROJECT_ROOT/safari-extension/wallet-dev"
 INDEX="$PROJECT_ROOT/build/safari-dev/index.html"
 BACKGROUND="$PROJECT_ROOT/build/safari-dev/background.js"
 EXPO_DEV_PORT=19000
@@ -17,9 +17,9 @@ cleanup() {
 trap cleanup SIGINT
 
 rm -rf ./build/safari-dev
-rm -rf ./build/safari-xcode-project-dev
-mkdir ./build/safari-xcode-project-dev
-chmod 777 ./build/safari-xcode-project-dev
+rm -rf ./safari-extension/wallet-dev
+mkdir ./safari-extension/wallet-dev
+chmod 777 ./safari-extension/wallet-dev
 
 WEB_ENGINE=webkit-safari WEBPACK_BUILD_OUTPUT_PATH=safari-dev APP_ENV=development expo start --web &
 
@@ -31,17 +31,17 @@ done
 if [ -d "$SAFARI_DEV_DIR" ] && [ -f "$INDEX" ] && [ -f "$BACKGROUND" ]; then
   sleep 5
 
-  yes | xcrun -v "/Applications/Xcode.app/Contents/Developer/usr/bin/safari-web-extension-converter" "$PROJECT_ROOT/build/safari-dev" --app-name safari-xcode-project-dev --project-location $PROJECT_ROOT/build --swift --force --no-prompt --no-open --macos-only --bundle-identifier com.ambire.app.wallet.extension
+  yes | xcrun -v "/Applications/Xcode.app/Contents/Developer/usr/bin/safari-web-extension-converter" "$PROJECT_ROOT/build/safari-dev" --app-name wallet-dev --project-location $PROJECT_ROOT/safari-extension --swift --force --no-prompt --no-open --macos-only --bundle-identifier com.ambire.app.wallet.extension
 
-  while [ ! -d "$SAFARI_XCODE_PROJECT/safari-xcode-project-dev.xcodeproj" ]; do
+  while [ ! -d "$SAFARI_XCODE_PROJECT/wallet-dev.xcodeproj" ]; do
     echo "Waiting for Xcode project creation to complete..."
     sleep 5
   done
 
-  xcodebuild -project "$PROJECT_ROOT/build/safari-xcode-project-dev/safari-xcode-project-dev.xcodeproj" -configuration Debug build
+  xcodebuild -project "$PROJECT_ROOT/safari-extension/wallet-dev/wallet-dev.xcodeproj" -configuration Debug build
   fswatch -o $PROJECT_ROOT/build/safari-dev | while read; do
     echo "Changes detected. Rebuilding Xcode project..."
-    xcodebuild -project "$PROJECT_ROOT/build/safari-xcode-project-dev/safari-xcode-project-dev.xcodeproj" -configuration Debug build
+    xcodebuild -project "$PROJECT_ROOT/safari-extension/wallet-dev/wallet-dev.xcodeproj" -configuration Debug build
   done
 fi
 
