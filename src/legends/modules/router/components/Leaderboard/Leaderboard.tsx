4bfb334d7b20e0df1afa36ea01d0a7ecd79d98ac
Gergana Mihaylova <gergana@devlabs.bg>
2024-09-27 15:24:44 +0300

    Add: pageRef in page scrollable div

--- src/legends/modules/router/components/Leaderboard/Leaderboard.tsx
+++ src/legends/modules/router/components/Leaderboard/Leaderboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react'
 
 import shortenAddress from '@ambire-common/utils/shortenAddress'
 import { fetchCaught } from '@common/services/fetch'
@@ -9,63 +9,82 @@ import Page from '@legends/components/Page'
 import Podium from './components/Podium'
 import styles from './Leaderboard.module.scss'
 
-const getLeaderboard = async (currentUser?: string) => {
+interface LeaderboardResponse {
+  leaderboard: Array<{ rank: number; account: string; level: number; xp: number; avatar: string }>
+  currentUser: { rank: number; account: string; xp: number; level: number }
+}
+
+const getLeaderboard = async (currentUser?: string): Promise<LeaderboardResponse> => {
   try {
     const res = await fetchCaught(
       `https://staging-relayer.ambire.com/legends/leaderboard${
-        currentUser && `?identity=${currentUser}`
+        currentUser ? `?identity=${currentUser}` : ''
       }`
     )
     return res.body
   } catch {
     console.error('Error fetching leaderboard')
-    return { leaderboard: [], currentUser: { rank: 0, avatar: '', account: '', xp: 0 } }
+    return { leaderboard: [], currentUser: { rank: 0, account: '', xp: 0, level: 0 } }
   }
 }
 
 const getBadge = (rank: number) => {
   if (rank === 1)
     return (
-      <FontAwesomeIcon className={(styles.throphy, styles.firstPlaceThrophy)} icon={faTrophy} />
+      <FontAwesomeIcon
+        className={`${styles.throphy} ${styles.firstPlaceThrophy}`}
+        icon={faTrophy}
+      />
     )
   if (rank === 2)
     return (
-      <FontAwesomeIcon className={(styles.throphy, styles.secondPlaceThrophy)} icon={faTrophy} />
+      <FontAwesomeIcon
+        className={`${styles.throphy} ${styles.secondPlaceThrophy}`}
+        icon={faTrophy}
+      />
     )
   if (rank === 3)
     return (
-      <FontAwesomeIcon className={(styles.throphy, styles.thirdPlaceThrophy)} icon={faTrophy} />
+      <FontAwesomeIcon
+        className={`${styles.throphy} ${styles.thirdPlaceThrophy}`}
+        icon={faTrophy}
+      />
     )
   return null
 }
 
 interface LeaderboardProps {
-  data: Array<{ rank: number; account: string; xp: number; avatar: string }>
-  currentUser: { rank: number; account: string; xp: number; avatar: string }
+  data: Array<{ rank: number; account: string; xp: number }>
+  currentUser: { rank: number; account: string; xp: number }
 }
-const USER_ADDR = '0x676E327EC5f53aA22906363Ef741a9f8894E0ad1'
+const USER_ADDR = '0x4a770E97F7fA83318C8Eda9F9E72874EBB6322ca'
 
 const LeaderboardContainer: React.FC<LeaderboardProps> = () => {
-  const [currentUserRef, setCurrentUserRef] = useState<React.RefObject<HTMLDivElement> | null>(null)
   const [loading, setLoading] = useState(true)
-  const [leaderboardData, setLeaderboardData] = useState([])
-  const [userLeaderboardData, setCurrentUser] = useState({
-    rank: 0,
-    avatar: '',
-    account: '',
-    xp: 0
-  })
+  const [leaderboardData, setLeaderboardData] = useState<
+    Array<{ rank: number; account: string; level: number; xp: number; avatar: string }>
+  >([])
+  const [userLeaderboardData, setCurrentUser] = useState<{
+    rank: number
+    account: string
+    xp: number
+    level: number
+  } | null>(null)
+
+  const tableRef = useRef(null)
+
+  const pageRef = useRef(null)
+  const currentUserRef = useRef(null)
+
+  const [stickyPosition, setStickyPosition] = useState(null) // 'top' | 'bottom' | null
 
   useEffect(() => {
     const fetchLeaderboard = async () => {
       try {
-        const { leaderboard, currentUser } = await getLeaderboard(USER_ADDR)
-        const duplicatedData = leaderboard.slice(0, 100).map((item, index) => ({
-          ...item,
-          account: `${item.account}_${index}`
-        }))
-        setLeaderboardData([...leaderboard, ...duplicatedData])
-        setCurrentUser(currentUser)
+        const { leaderboard, currentUser } = await getLeaderboard()
+
+        setLeaderboardData(leaderboard)
+        currentUser && setCurrentUser(currentUser)
       } catch (error) {
         console.error('Failed to fetch leaderboard:', error)
       } finally {
@@ -76,17 +95,54 @@ const LeaderboardContainer: React.FC<LeaderboardProps> = () => {
     fetchLeaderboard()
   }, [])
 
-  const sortedData = [
-    ...leaderboardData,
-    (!leaderboardData.find((user) => user.account === userLeaderboardData.account) &&
-      userLeaderboardData) ||
-      []
-  ]
-    .flat()
-    .sort((a, b) => b.xp - a.xp)
+  const sortedData = useMemo(
+    () =>
+      [
+        ...leaderboardData,
+        (userLeaderboardData &&
+          !leaderboardData.find((user) => user.account === userLeaderboardData.account) &&
+          userLeaderboardData) ||
+          []
+      ]
+        .flat()
+        .sort((a, b) => b.xp - a.xp),
+    [leaderboardData, userLeaderboardData]
+  )
+
+  useLayoutEffect(() => {
+    const handleScroll = () => {
+      if (!userLeaderboardData && !currentUserRef.current) return
+
+      const userRect = currentUserRef.current.getBoundingClientRect()
+      const windowHeight = window.innerHeight
+      console.log('userRect.bottom', userRect.bottom, 'windowHeight', windowHeight)
+      // Check if the current user's row is above the viewport (scrolling down)
+      if (userRect.top < 0) {
+        // If the user is above the viewport, pin to the top
+        setStickyPosition('top')
+      } else if (userRect.bottom > windowHeight) {
+        // If the user is below the viewport, pin to the bottom
+        setStickyPosition('bottom')
+      } else {
+        // Reset sticky behavior when the current user's row is in the viewport
+        // setStickyPosition(null)
+      }
+    }
+
+    // Attach the scroll event listener
+    pageRef.current.addEventListener('scroll', handleScroll)
+
+    // Trigger the handleScroll function immediately after component mount
+    handleScroll()
+
+    return () => {
+      // Clean up the event listener on component unmount
+      pageRef.current.removeEventListener('scroll', handleScroll)
+    }
+  }, [currentUserRef, sortedData])
 
   return (
-    <Page>
+    <Page pageRef={pageRef}>
       <div className={styles.wrapper}>
         <div className={styles.heading}>
           <h1 className={styles.title}>Leaderboard</h1>
@@ -96,35 +152,57 @@ const LeaderboardContainer: React.FC<LeaderboardProps> = () => {
             magna luctus et.
           </p>
         </div>
-        <Podium />
-        <div className={styles.table}>
+        <Podium data={[...sortedData].splice(0, 3)} />
+        <div ref={tableRef} className={styles.table}>
           <div className={styles.header}>
             <h5 className={styles.cell}>player</h5>
             <h5 className={styles.cell}>Level</h5>
             <h5 className={styles.cell}>XP</h5>
           </div>
-          {sortedData.map((item, index) => (
+          {sortedData.map((item) => (
             <div
-              key={index}
+              key={item.account}
               className={`${styles.row} ${
-                item.account === userLeaderboardData.account ? styles.currentUserRow : ''
+                userLeaderboardData && item.account === userLeaderboardData.account
+                  ? styles.currentUserRow
+                  : ''
               } ${item.rank <= 3 ? styles[`rankedRow${item.rank}`] : ''}`}
-              ref={(el) => {
-                if (item.account === userLeaderboardData.account) {
-                  setCurrentUserRef(el)
-                }
-              }}
-              style={
-                item.account === userLeaderboardData.account && currentUserRef
-                  ? { position: 'sticky', bottom: 0 }
-                  : {}
+              ref={
+                userLeaderboardData && item.account === userLeaderboardData.account
+                  ? currentUserRef
+                  : null
               }
+              style={{
+                position:
+                  userLeaderboardData &&
+                  item.account === userLeaderboardData.account &&
+                  stickyPosition
+                    ? 'sticky'
+                    : 'relative',
+                top:
+                  stickyPosition === 'top' &&
+                  userLeaderboardData &&
+                  item.account === userLeaderboardData.account
+                    ? 0
+                    : 'auto',
+                bottom:
+                  stickyPosition === 'bottom' &&
+                  userLeaderboardData &&
+                  item.account === userLeaderboardData.account
+                    ? 0
+                    : 'auto',
+                zIndex:
+                  userLeaderboardData && item.account === userLeaderboardData.account ? 1000 : 0
+              }}
             >
               <div className={styles.cell}>
-                {item.rank > 3 ? item.rank : getBadge(item.rank)}
-
+                {item.rank > 3 ? (
+                  <div className={styles.rank}>{item.rank}</div>
+                ) : (
+                  getBadge(item.rank)
+                )}
                 <img src="/images/leaderboard/avatar1.png" alt="avatar" className={styles.avatar} />
-                {shortenAddress(item.account, 20)}
+                {shortenAddress(item.account, 23)}
               </div>
               <h5 className={styles.cell}>{item.level}</h5>
               <h5 className={styles.cell}>{item.xp}</h5>
--- src/legends/modules/router/components/Leaderboard/components/Podium/Podium.tsx
+++ src/legends/modules/router/components/Leaderboard/components/Podium/Podium.tsx
@@ -1,41 +1,31 @@
 import React from 'react'
 
+import shortenAddress from '@ambire-common/utils/shortenAddress'
 import { faTrophy } from '@fortawesome/free-solid-svg-icons/faTrophy'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import styles from './Podium.module.scss'
 
 interface PodiumProps {
-  first: { account: string; xp: number; avatar: string }
-  second: { account: string; xp: number; avatar: string }
-  third: { account: string; xp: number; avatar: string }
+  data: Array<{ account: string; xp: number }>
 }
 
-const Podium: React.FC<PodiumProps> = ({ first, second, third }) => {
+const Podium: React.FC<PodiumProps> = ({ data }) => {
   return (
     <div className={styles.podium}>
-      <div className={`${styles.step} ${styles.second}`}>
-        <div className={styles.contentWrapper}>
-          <img src="/images/leaderboard/avatar3.png" alt="avatar" className={styles.avatar} />
-          <h5 className={styles.name}>goshokazana.x</h5>
-          <h4 className={styles.xp}>18 349</h4>
+      {data.map((item, index) => (
+        <div
+          key={`${item.account}-podium`}
+          className={`${styles.step} ${styles[`position${index + 1}`]}`}
+        >
+          <div className={styles.contentWrapper}>
+            {index === 0 && <FontAwesomeIcon className={styles.throphy} icon={faTrophy} />}
+            <img src="/images/leaderboard/avatar2.png" alt="avatar" className={styles.avatar} />
+            <h5 className={styles.name}>{shortenAddress(item.account, 11)}</h5>
+            <h4 className={styles.xp}>{item.xp}</h4>
+          </div>
         </div>
-      </div>
-      <div className={`${styles.step} ${styles.first}`}>
-        <div className={styles.contentWrapper}>
-          <FontAwesomeIcon className={styles.throphy} icon={faTrophy} />
-          <img src="/images/leaderboard/avatar2.png" alt="avatar" className={styles.avatar} />
-          <h5 className={styles.name}>elmoto.eth</h5>
-          <h4 className={styles.xp}>19 349</h4>
-        </div>
-      </div>
-      <div className={`${styles.step} ${styles.third}`}>
-        <div className={styles.contentWrapper}>
-          <img src="/images/leaderboard/avatar4.png" alt="avatar" className={styles.avatar} />
-          <h5 className={styles.name}>0x66fE...04d08</h5>
-          <h4 className={styles.xp}>18 349</h4>
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
