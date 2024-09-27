22cf45157ec57da565e00c2bbace0d247568ed2a
sonytooo <simeonpetkov11999977@gmail.com>
2024-09-27 17:21:44 +0300

    renaming

--- src/web/modules/swap-and-bridge/components/RouteStepsPreview/RouteStepsPreview.tsx
+++ src/web/modules/swap-and-bridge/components/RouteStepsPreview/RouteStepsPreview.tsx
@@ -6,11 +6,11 @@ import useTheme from '@common/hooks/useTheme'
 
 import getStyles from './styles'
 
-const RouteSteps = () => {
+const RouteStepsPreview = () => {
   const { t } = useTranslation()
   const { styles } = useTheme(getStyles)
 
   return null
 }
 
-export default React.memo(RouteSteps)
+export default React.memo(RouteStepsPreview)
