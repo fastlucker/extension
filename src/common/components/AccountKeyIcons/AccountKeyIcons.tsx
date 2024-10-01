60e7e28f059c18ac0d0ba7c3435422103eeaf1fa
Borislav Itskov <borislav@devlabs.bg>
2024-10-01 14:17:43 +0300

    add: styling for when there are more than 1 key for an account

--- src/common/components/AccountKeyIcons/AccountKeyIcons.tsx
+++ src/common/components/AccountKeyIcons/AccountKeyIcons.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { View } from 'react-native'
 
 import { Account as AccountInterface } from '@ambire-common/interfaces/account'
 import NoKeysIcon from '@common/assets/svg/NoKeysIcon'
 import useTheme from '@common/hooks/useTheme'
+import flexbox from '@common/styles/utils/flexbox'
 import useKeystoreControllerState from '@web/hooks/useKeystoreControllerState'
 
 import AccountKeyIcon from '../AccountKeyIcon/AccountKeyIcon'
@@ -16,12 +18,17 @@ const AccountKeyIcons = ({ account }: { account: AccountInterface }) => {
   const { theme } = useTheme()
 
   return (
-    <>
+    <View style={[flexbox.directionRow]}>
       {importedKeyTypes.length === 0 && <NoKeysIcon color={theme.secondaryText} />}
-      {importedKeyTypes.map((type) => {
-        return <AccountKeyIcon key={type || 'internal'} type={type || 'internal'} />
+      {importedKeyTypes.map((type, index) => {
+        return (
+          // @ts-ignore
+          <View key={type || 'internal'} style={index !== 0 ? { 'margin-left': '-12px' } : {}}>
+            <AccountKeyIcon type={type || 'internal'} />
+          </View>
+        )
       })}
-    </>
+    </View>
   )
 }
 
