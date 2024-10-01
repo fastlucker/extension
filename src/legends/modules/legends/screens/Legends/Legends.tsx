e7f90bc98ee91d3e98ab9f0cbde2331271b8177d
Petromir Petrov <petromirdev@gmail.com>
2024-10-01 10:41:13 +0300

    legends add(wip): legends route, topbar and card

--- src/legends/modules/legends/screens/Legends/Legends.tsx
+++ src/legends/modules/legends/screens/Legends/Legends.tsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Page from '@legends/components/Page'
-import Balance from '@legends/modules/legends/components/Balance'
+import Topbar from '@legends/modules/legends/components/Topbar'
+
+export type Filter = {
+  label: string
+  value: string
+}
+const MOCK_FILTERS: Filter[] = [
+  {
+    label: 'All Legends',
+    value: 'all'
+  },
+  {
+    label: 'DeFi',
+    value: 'defi'
+  },
+  {
+    label: 'Social',
+    value: 'social'
+  },
+  {
+    label: 'NFT',
+    value: 'nft'
+  }
+]
 
 const Legends = () => {
+  const [selectedFilter, setSelectedFilter] = useState<Filter['value']>(MOCK_FILTERS[0].value)
+
+  const selectFilter = (filter: Filter) => {
+    setSelectedFilter(filter.value)
+  }
+
   return (
     <Page>
-      <Balance />
+      <Topbar filters={MOCK_FILTERS} selectedFilter={selectedFilter} selectFilter={selectFilter} />
     </Page>
   )
 }
