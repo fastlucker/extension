f216c3da15314bbeaf29aa70a30e8ba75a93f810
sonytooo <simeonpetkov11999977@gmail.com>
2024-10-07 15:53:08 +0300

    fixes

--- src/web/extension-services/background/background.ts
+++ src/web/extension-services/background/background.ts
@@ -888,11 +888,11 @@ handleKeepAlive()
                 return mainCtrl.destroySignAccOp()
 
               case 'SWAP_AND_BRIDGE_CONTROLLER_INIT_FORM':
-                return mainCtrl.swapAndBridge.initForm(params.sessionId)
+                return await mainCtrl.swapAndBridge.initForm(params.sessionId)
               case 'SWAP_AND_BRIDGE_CONTROLLER_UPDATE_FORM':
                 return mainCtrl.swapAndBridge.updateForm(params)
               case 'SWAP_AND_BRIDGE_CONTROLLER_SWITCH_FROM_AND_TO_TOKENS':
-                return mainCtrl.swapAndBridge.switchFromAndToTokens()
+                return await mainCtrl.swapAndBridge.switchFromAndToTokens()
               case 'SWAP_AND_BRIDGE_CONTROLLER_SUBMIT_FORM':
                 return await mainCtrl.buildSwapAndBridgeUserRequest()
               case 'SWAP_AND_BRIDGE_CONTROLLER_ACTIVE_ROUTE_BUILD_NEXT_USER_REQUEST':
