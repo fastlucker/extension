3db006637499f24cae043e88c2d661aa1f69e7c2
Petromir Petrov <petromirdev@gmail.com>
2024-10-07 15:43:29 +0300

    update: toast background to $tertiaryBackground

--- src/legends/modules/legends/screens/Legends/Legends.tsx
+++ src/legends/modules/legends/screens/Legends/Legends.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 import Page from '@legends/components/Page'
-import useToast from '@legends/hooks/useToast'
 import Card from '@legends/modules/legends/components/Card'
 import Topbar from '@legends/modules/legends/components/Topbar'
 import { Filter } from '@legends/modules/legends/types/filter'
@@ -10,14 +9,9 @@ import styles from './Legends.module.scss'
 import { MOCK_CARDS, MOCK_FILTERS } from './mockData'
 
 const Legends = () => {
-  const { addToast } = useToast()
   const [selectedFilter, setSelectedFilter] = useState<Filter['value']>(MOCK_FILTERS[0].value)
 
   const selectFilter = (filter: Filter) => {
-    addToast('info', `Selected filter: ${filter.label}`)
-    setTimeout(() => addToast('error', `Selected filter: ${filter.label}`), 100)
-    setTimeout(() => addToast('warning', `Selected filter: ${filter.label}`), 200)
-    setTimeout(() => addToast('success', `Selected filter: ${filter.label}`), 300)
     setSelectedFilter(filter.value)
   }
 
