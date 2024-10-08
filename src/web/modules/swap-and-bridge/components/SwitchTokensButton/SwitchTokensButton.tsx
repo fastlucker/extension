9f172654fb4f112ebe114e5fbafde372f46a1c4f
Kaloyan Kosev <superkalo@devlabs.bg>
2024-10-08 13:46:46 +0300

    Add: tooltip for switch tokens disabled condition

--- src/web/modules/swap-and-bridge/components/SwitchTokensButton/SwitchTokensButton.tsx
+++ src/web/modules/swap-and-bridge/components/SwitchTokensButton/SwitchTokensButton.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Animated, Pressable, PressableProps, View } from 'react-native'
 
 import SwapBridgeToggleIcon from '@common/assets/svg/SwapBridgeToggleIcon'
+import Tooltip from '@common/components/Tooltip'
 import useTheme from '@common/hooks/useTheme'
 import { useCustomHover } from '@web/hooks/useHover'
 
 import getStyles from './styles'
 
-const SwitchTokensButton = (props?: PressableProps) => {
+const SWITCH_TOKENS_CONDITION_TOOLTIP_ID = 'switch-tokens-condition-tooltip'
+
+const SwitchTokensButton = ({ disabled, ...rest }: PressableProps) => {
+  const { t } = useTranslation()
   const { styles } = useTheme(getStyles)
   const [bindAnim, , , , animatedValues] = useCustomHover({
     property: 'rotateZ' as any,
@@ -23,12 +28,24 @@ const SwitchTokensButton = (props?: PressableProps) => {
     : null
 
   return (
-    <View style={styles.switchTokensButtonWrapper}>
-      <Pressable style={styles.switchTokensButton} {...bindAnim} {...props}>
+    <View
+      style={styles.switchTokensButtonWrapper}
+      // @ts-ignore `dataSet` exists, but lacks a type
+      dataSet={disabled && { tooltipId: SWITCH_TOKENS_CONDITION_TOOLTIP_ID }}
+    >
+      <Pressable style={styles.switchTokensButton} {...bindAnim} disabled={disabled} {...rest}>
         <Animated.View style={{ transform: [{ rotateZ: rotateInterpolate || '0deg' }] }}>
           <SwapBridgeToggleIcon />
         </Animated.View>
       </Pressable>
+      {disabled && (
+        <Tooltip
+          content={t(
+            'Switching tokens is only possible if the account already holds some of the receive token.'
+          )}
+          id={SWITCH_TOKENS_CONDITION_TOOLTIP_ID}
+        />
+      )}
     </View>
   )
 }
