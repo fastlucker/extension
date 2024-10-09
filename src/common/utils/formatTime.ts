98a682b987fea3343845e1babe7933e9085f1c89
Kaloyan Kosev <superkalo@devlabs.bg>
2024-10-09 17:20:13 +0300

    Refactor: workaround for the eslint warn + a few notes

--- src/common/utils/formatTime.ts
+++ src/common/utils/formatTime.ts
@@ -1,14 +1,24 @@
-/* eslint-disable no-param-reassign */
-
+/**
+ * Converts a given number of seconds into a human-readable string format.
+ * The format will be in weeks, days, hours, minutes, and seconds, depending on the input.
+ *
+ * Examples:
+ * - 90061 seconds will be formatted as "1d 1h"
+ * - 3600 seconds will be formatted as "1h"
+ * - 61 seconds will be formatted as "1min, 1sec"
+ * - 59 seconds will be formatted as "59sec"
+ */
 function formatTime(seconds: number) {
-  const weeks = Math.floor(seconds / (7 * 24 * 3600))
-  seconds %= 7 * 24 * 3600
-  const days = Math.floor(seconds / (24 * 3600))
-  seconds %= 24 * 3600
-  const hours = Math.floor(seconds / 3600)
-  seconds %= 3600
-  const minutes = Math.floor(seconds / 60)
-  seconds %= 60
+  let remainingSeconds = seconds
+
+  const weeks = Math.floor(remainingSeconds / (7 * 24 * 3600))
+  remainingSeconds %= 7 * 24 * 3600
+  const days = Math.floor(remainingSeconds / (24 * 3600))
+  remainingSeconds %= 24 * 3600
+  const hours = Math.floor(remainingSeconds / 3600)
+  remainingSeconds %= 3600
+  const minutes = Math.floor(remainingSeconds / 60)
+  remainingSeconds %= 60
 
   if (weeks > 0) {
     return days !== 0 ? `${weeks}w ${days}d` : `${weeks}w`
@@ -20,10 +30,10 @@ function formatTime(seconds: number) {
     return minutes !== 0 ? `${hours}h, ${minutes}min` : `${hours}h`
   }
   if (minutes > 0) {
-    return seconds !== 0 ? `${minutes}min, ${seconds}sec` : `${minutes}min`
+    return remainingSeconds !== 0 ? `${minutes}min, ${remainingSeconds}sec` : `${minutes}min`
   }
 
-  return `${seconds}sec`
+  return `${remainingSeconds}sec`
 }
 
 export default formatTime
