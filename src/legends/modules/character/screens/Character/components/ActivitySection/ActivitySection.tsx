ef44f68016cdc808559a9dec1b8d347628ae68e8
Jordan Enev <jordantoen@gmail.com>
2024-10-15 13:11:03 +0300

    Add: Legends - Activity basic data visualisation.

--- src/legends/modules/character/screens/Character/components/ActivitySection/ActivitySection.tsx
+++ src/legends/modules/character/screens/Character/components/ActivitySection/ActivitySection.tsx
@@ -1,13 +1,65 @@
 import React from 'react'
 
+import useActivityContext from '@legends/hooks/useActivityContext'
+import shortenAddress from '@ambire-common/utils/shortenAddress'
+import { networks } from '@ambire-common/consts/networks'
 import SectionHeading from '../SectionHeading'
 import styles from './ActivitySection.module.scss'
 
 const ActivitySection = () => {
+  const { activity, isLoading } = useActivityContext()
   return (
     <div className={styles.wrapper}>
       <SectionHeading>Legends Activity</SectionHeading>
-      <p>TBD</p>
+      <p>{isLoading ? 'Loading ...' : ''}</p>
+      {activity && (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Transaction</th>
+              <th>Network</th>
+              <th>Submitted at</th>
+              <th>Total XP</th>
+              <th>Legends actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {activity.map((act) => {
+              const network = networks.find(({ id }) => id === act.network)
+              const txnId = shortenAddress(act.txId, 12)
+
+              return (
+                <tr key={act.txId}>
+                  <td>
+                    {network ? (
+                      <a
+                        href={`${network.explorerUrl}/tx/${act.txId}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {txnId}
+                      </a>
+                    ) : (
+                      txnId
+                    )}
+                  </td>
+                  <td>{act.network}</td>
+                  <td>{new Date(act.submittedAt).toLocaleString()}</td>
+                  <td>{act.legends.totalXp}</td>
+                  <td>
+                    {act.legends.activities.map((legendActivity, index) => (
+                      // eslint-disable-next-line react/no-array-index-key
+                      <div key={index}>
+                        {legendActivity.action} (+{legendActivity.xp} xp)
+                      </div>
+                    ))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
