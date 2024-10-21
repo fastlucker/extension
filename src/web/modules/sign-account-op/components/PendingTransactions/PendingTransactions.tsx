0877739c99f0663f8ae0a3a30f18df3fa1615a09
Yosif Hamed <josif.hameed@gmail.com>
2024-10-21 16:51:09 +0300

    Change is accountOp updated checks

--- src/web/modules/sign-account-op/components/PendingTransactions/PendingTransactions.tsx
+++ src/web/modules/sign-account-op/components/PendingTransactions/PendingTransactions.tsx
@@ -1,11 +1,12 @@
+import { keccak256, toUtf8Bytes } from 'ethers'
 import React, { FC, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { View } from 'react-native'
 
 import { Network } from '@ambire-common/interfaces/network'
-import { Call } from '@ambire-common/libs/accountOp/types'
 import { humanizeAccountOp } from '@ambire-common/libs/humanizer'
 import { IrCall } from '@ambire-common/libs/humanizer/interfaces'
+import { stringify } from '@ambire-common/libs/richJson/richJson'
 import ScrollableWrapper from '@common/components/ScrollableWrapper'
 import useTheme from '@common/hooks/useTheme'
 import spacings from '@common/styles/spacings'
@@ -24,22 +25,26 @@ const PendingTransactions: FC<Props> = ({ network }) => {
   const { t } = useTranslation()
   const { styles } = useTheme(getStyles)
   const { accountOp } = useSignAccountOpControllerState() || {}
-  const lastHumanizedCalls = React.useRef<Call[]>([])
+  const oldAccountOpRelevantInfoHash = React.useRef<string>('')
   const [callsToVisualize, setCallsToVisualize] = React.useState<IrCall[]>([])
 
   useEffect(() => {
     if (!accountOp) return
-    const areAccountOpCallsUpdated = accountOp.calls.some(
-      (call, i) => call.data !== lastHumanizedCalls.current[i]?.data
-    )
-    const hasAccountOpChanged = accountOp.calls.length !== lastHumanizedCalls.current.length
+    const actualDependencyArrayAsString = stringify([
+      accountOp.calls,
+      accountOp.networkId,
+      accountOp.accountAddr
+    ])
+    const newAccountOpRelevantInfoHash = keccak256(toUtf8Bytes(actualDependencyArrayAsString))
 
-    if (!hasAccountOpChanged && !areAccountOpCallsUpdated) return
+    const hasAccountOpChangedSincePrevRender =
+      oldAccountOpRelevantInfoHash.current !== newAccountOpRelevantInfoHash
 
-    const newHumanizedCalls = humanizeAccountOp(accountOp, {})
+    if (!hasAccountOpChangedSincePrevRender) return
 
-    setCallsToVisualize(newHumanizedCalls)
-    lastHumanizedCalls.current = accountOp.calls
+    setCallsToVisualize(humanizeAccountOp(accountOp, {}))
+
+    oldAccountOpRelevantInfoHash.current = newAccountOpRelevantInfoHash
   }, [accountOp])
 
   return (
