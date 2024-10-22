3805f4765c38279e3ef68aa5eb85dccf63fdbdbc
Kaloyan Kosev <superkalo@devlabs.bg>
2024-10-22 11:29:10 +0300

    Refactor: set networks logic for components used across Benzin and Extension

--- src/common/components/TokenIcon/TokenIcon.tsx
+++ src/common/components/TokenIcon/TokenIcon.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { Image, ImageProps, View, ViewStyle } from 'react-native'
 
-import { networks as predefinedNetworks } from '@ambire-common/consts/networks'
+import useBenzinNetworksContext from '@benzin/hooks/useBenzinNetworksContext'
 import MissingTokenIcon from '@common/assets/svg/MissingTokenIcon'
 import NetworkIcon from '@common/components/NetworkIcon'
 import useTheme from '@common/hooks/useTheme'
@@ -53,11 +53,12 @@ const TokenIcon: React.FC<Props> = ({
   ...props
 }) => {
   const { styles } = useTheme(getStyles)
-  const { networks: controllerNetworks } = useNetworksControllerState()
   const [uriStatus, setUriStatus] = useState<UriStatus>(UriStatus.UNKNOWN)
   const [imageUrl, setImageUrl] = useState<string | undefined>()
-  // In the Benzin context, the networks controller is not defined, fallback to predefined networks
-  const networks = controllerNetworks || predefinedNetworks
+  const { networks: controllerNetworks } = useNetworksControllerState()
+  const { benzinNetworks } = useBenzinNetworksContext()
+  // Component used across Benzin and Extension, make sure to always set networks
+  const networks = controllerNetworks ?? benzinNetworks
 
   const network = useMemo(
     () =>
