d3768a3f3bb5e30fdd2b9746ba0092defcd1b3be
sonytooo <simeonpetkov11999977@gmail.com>
2024-10-28 11:17:20 +0200

    move action handlers to a separate function for better readability

--- src/web/extension-services/background/handlers/handleCleanDappSessions.ts
+++ src/web/extension-services/background/handlers/handleCleanDappSessions.ts
@@ -5,7 +5,6 @@ export const handleCleanDappSessions = async () => {
   const openedTabs = await getAllOpenedTabs()
   const dappSessions = await storage.get('dappSessions', {})
 
-  console.log(openedTabs, dappSessions)
   Object.keys(dappSessions).forEach((key) => {
     if (!openedTabs.find((t) => t.id === dappSessions[key].tabId)) {
       delete dappSessions[key]
