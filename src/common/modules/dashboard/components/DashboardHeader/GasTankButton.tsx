e4ecb7a176509cd05eb394e9c1dddfaf3add3f11
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2024-11-05 13:18:34 +0200

    Fix: styles on hover GasTankButton

--- src/common/modules/dashboard/components/DashboardHeader/GasTankButton.tsx
+++ src/common/modules/dashboard/components/DashboardHeader/GasTankButton.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from '@common/config/localization'
 import spacings from '@common/styles/spacings'
 import common from '@common/styles/utils/common'
 import flexbox from '@common/styles/utils/flexbox'
-import { AnimatedPressable } from '@web/hooks/useHover'
+import { AnimatedPressable, useCustomHover } from '@web/hooks/useHover'
 
 import { NEUTRAL_BACKGROUND_HOVERED } from '../../screens/styles'
 
@@ -17,27 +17,32 @@ type Props = {
 const GasTankButton = ({ onPress }: Props) => {
   const { t } = useTranslation()
 
+  const [bindGasTankBtnAim, removeTankBtnStyle] = useCustomHover({
+    property: 'backgroundColor',
+    values: { from: NEUTRAL_BACKGROUND_HOVERED, to: '#14183380' } // TODO: Remove hardcoded hex
+  })
+
   return (
     <View>
-      <AnimatedPressable onPress={onPress}>
-        <View
-          style={{
-            ...flexbox.directionRow,
-            ...flexbox.center,
-            ...spacings.phTy,
-            ...common.borderRadiusPrimary,
-            ...common.shadowPrimary,
-            backgroundColor: NEUTRAL_BACKGROUND_HOVERED
-          }}
-        >
-          <GasTankIcon width={16} color="white" />
-          <Text style={[spacings.mlTy]} color="white" weight="number_bold" fontSize={12}>
-            $25.23
-          </Text>
-          <Text style={[spacings.mlTy, { opacity: 0.57 }]} fontSize={12} color="white">
-            {t('on Gas Tank')}
-          </Text>
-        </View>
+      <AnimatedPressable
+        onPress={onPress}
+        style={{
+          ...flexbox.directionRow,
+          ...flexbox.center,
+          ...spacings.phTy,
+          ...common.borderRadiusPrimary,
+          ...common.shadowPrimary,
+          ...removeTankBtnStyle
+        }}
+        {...bindGasTankBtnAim}
+      >
+        <GasTankIcon width={20} color="white" />
+        <Text style={[spacings.mlTy]} color="white" weight="number_bold" fontSize={12}>
+          $25.23
+        </Text>
+        <Text style={[spacings.mlTy, { opacity: 0.57 }]} fontSize={12} color="white">
+          {t('on Gas Tank')}
+        </Text>
       </AnimatedPressable>
     </View>
   )
