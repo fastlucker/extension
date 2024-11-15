8ada75d4618c630f308d703446c16a1744b96c90
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2024-11-15 07:51:26 +0200

    Refactor styles

--- src/common/modules/dashboard/components/TabsAndSearch/styles.ts
+++ src/common/modules/dashboard/components/TabsAndSearch/styles.ts
@@ -1,17 +1,53 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, ViewStyle } from 'react-native'
 
 import spacings from '@common/styles/spacings'
+import { ThemeProps } from '@common/styles/themeConfig'
+import common, { BORDER_RADIUS_PRIMARY } from '@common/styles/utils/common'
 import flexbox from '@common/styles/utils/flexbox'
 import commonWebStyles from '@web/styles/utils/common'
 
-const styles = StyleSheet.create({
-  container: {
-    ...commonWebStyles.contentContainer,
-    ...flexbox.directionRow,
-    ...flexbox.justifySpaceBetween,
-    ...flexbox.alignCenter,
-    ...spacings.mb
-  }
-})
+type Style = {
+  container: ViewStyle
+  searchContainer: ViewStyle
+  searchIconWrapper: ViewStyle
+  searchInputWrapper: ViewStyle
+}
 
-export default styles
+const getStyles = (theme: ThemeProps) =>
+  StyleSheet.create<Style>({
+    container: {
+      ...commonWebStyles.contentContainer,
+      ...flexbox.directionRow,
+      ...flexbox.justifySpaceBetween,
+      ...flexbox.alignCenter,
+      ...spacings.mb
+    },
+    searchIconWrapper: {
+      ...flexbox.center,
+      height: 32,
+      width: 32,
+      borderRadius: BORDER_RADIUS_PRIMARY,
+      borderWidth: 1,
+      borderColor: theme.secondaryBorder,
+      backgroundColor: theme.secondaryBackground
+    },
+    searchContainer: {
+      position: 'absolute',
+      top: '100%',
+      left: 0,
+      right: 0,
+      marginTop: 5,
+      zIndex: 10,
+      borderWidth: 1,
+      borderColor: theme.primaryBorder,
+      ...common.borderRadiusPrimary,
+      ...common.shadowPrimary
+    },
+    searchInputWrapper: {
+      backgroundColor: theme.primaryBackground,
+      borderWidth: 0,
+      borderRadius: 0
+    }
+  })
+
+export default getStyles
