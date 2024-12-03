4b6d8253b51053d39e8c5490b2b2e4ef129c333b
sonytooo <simeonpetkov11999977@gmail.com>
2024-12-03 11:13:16 +0200

    fix: cleaup of the ctrl on force action-window close (there was an issue on some OS)

--- src/web/extension-services/background/handlers/handleCleanUpOnPortDisconnect.ts
+++ src/web/extension-services/background/handlers/handleCleanUpOnPortDisconnect.ts
@@ -1,17 +1,18 @@
 import { MainController } from '@ambire-common/controllers/main/main'
 import { Port } from '@web/extension-services/messengers'
 
-export const handleControllersSessionCleanup = ({
+export const handleCleanUpOnPortDisconnect = ({
   port,
   mainCtrl
 }: {
   port: Port
   mainCtrl: MainController
 }) => {
-  if (!port.sender) return
+  if (!port.sender || !port.sender?.url) return
+
+  const url = new URL(port.sender.url)
 
   if (port.sender.url?.includes('sessionId')) {
-    const url = new URL(port.sender.url)
     const sessionId = url.searchParams.get('sessionId')!
 
     if (url.pathname.includes('swap-and-bridge')) {
@@ -25,5 +26,13 @@ export const handleControllersSessionCleanup = ({
     if (url.pathname.includes('signed-messages')) {
       mainCtrl.activity.resetSignedMessagesFilters(sessionId)
     }
+  }
+
+  if (url.pathname.includes('sign-account-op')) {
+    mainCtrl.destroySignAccOp()
+  }
+
+  if (url.pathname.includes('sign-message')) {
+    mainCtrl.signMessage.reset()
   }
 }
