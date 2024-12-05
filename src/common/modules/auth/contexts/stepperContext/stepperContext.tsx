ac5057ec069f69eaa9d7356978decba94bc9950e
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2024-12-05 18:24:29 +0200

    Fix: texts in SeedPhraseImportScreen and AccountPersonalizeScreen

--- src/common/modules/auth/contexts/stepperContext/stepperContext.tsx
+++ src/common/modules/auth/contexts/stepperContext/stepperContext.tsx
@@ -5,7 +5,7 @@ import { WEB_ROUTES } from '@common/modules/router/constants/common'
 
 const ACCOUNT_ADDER_STEP = 'Select accounts\nto import'
 const DEVICE_PASSWORD_STEP = 'Set up a\ndevice password'
-const PERSONALIZE_STEP = 'Personalize\nyour accounts'
+const PERSONALIZE_STEP = 'Name\nyour accounts'
 
 export type StepperFlow =
   | 'email'
