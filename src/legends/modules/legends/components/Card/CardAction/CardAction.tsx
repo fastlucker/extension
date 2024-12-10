28fea127853fc29bc92c908cee1de55ec3144a74
Gergana Mihaylova <gergana@devlabs.bg>
2024-12-10 14:46:08 +0200

    Add: display modal on Summoner complete

--- src/legends/modules/legends/components/Card/CardAction/CardAction.tsx
+++ src/legends/modules/legends/components/Card/CardAction/CardAction.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react'
 
+import CardActionButton from '@legends/modules/legends/components/Card/CardAction/actions/CardActionButton'
 import { CARD_PREDEFINED_ID } from '@legends/modules/legends/constants'
 import { CardAction, CardActionType, CardFromResponse } from '@legends/modules/legends/types'
 
-import CardActionButton from '@legends/modules/legends/components/Card/CardAction/actions/CardActionButton'
 import LinkAcc from './actions/LinkAcc'
 import SendAccOp from './actions/SendAccOp'
 import SummonAcc from './actions/SummonAcc'
@@ -18,7 +18,7 @@ type Props = {
 const CardActionComponent: FC<Props> = ({ action, buttonText, meta, onComplete }) => {
   if (action.type === CardActionType.predefined) {
     if (action.predefinedId === CARD_PREDEFINED_ID.addEOA) {
-      return <SummonAcc onComplete={onComplete} buttonText={buttonText} meta={meta} />
+      return <SummonAcc onComplete={onComplete} buttonText={buttonText} />
     }
     if (action.predefinedId === CARD_PREDEFINED_ID.LinkAccount) {
       return <LinkAcc onComplete={onComplete} />
--- src/legends/modules/legends/components/Card/CardAction/actions/SummonAcc.tsx
+++ src/legends/modules/legends/components/Card/CardAction/actions/SummonAcc.tsx
@@ -13,10 +13,9 @@ import CardActionWrapper from './CardActionWrapper'
 type Props = {
   buttonText: string
   onComplete: () => void
-  meta: CardFromResponse['meta']
 }
 
-const SummonAcc: FC<Props> = ({ buttonText, onComplete, meta }) => {
+const SummonAcc: FC<Props> = ({ buttonText, onComplete }) => {
   const { addToast } = useToast()
   const {
     inviteEOA,
@@ -25,10 +24,8 @@ const SummonAcc: FC<Props> = ({ buttonText, onComplete, meta }) => {
     setEoaAddress,
     isEOAAddressValid: isValid
   } = useInviteEOA()
-  const { onLegendComplete } = useLegendsContext()
 
   const [isInProgress, setIsInProgress] = useState(false)
-  const [step, setStep] = useState(0)
 
   const inputValidation = useMemo(() => {
     if (!eoaAddress) return null
@@ -39,33 +36,13 @@ const SummonAcc: FC<Props> = ({ buttonText, onComplete, meta }) => {
     }
   }, [eoaAddress, isValid])
 
-  const copyToClipboard = async () => {
-    try {
-      await navigator.clipboard.writeText(
-        `⚠️ TRADE OFFER ⚠️ \nYou download Ambire, we both win 🎉 \n1. Download the Ambire extension: https://www.ambire.com/get-extension \n2. Use my referral code so we both get XP: ${
-          meta?.invitationKey || ''
-        }\n3. Join Ambire Legends - on-chain quests by Ambire with XP and rewards: https://legends.ambire.com/`
-      )
-      addToast('Text with referral code copied to clipboard', 'success')
-    } catch (e: any) {
-      addToast('Failed to copy referral code', 'error')
-      console.error(e)
-    }
-  }
-
   const onButtonClick = async () => {
-    if (step === 1) {
-      copyToClipboard()
-      onComplete()
-      return
-    }
     try {
       await switchNetwork()
       setIsInProgress(true)
       await inviteEOA()
       addToast('Successfully invited EOA address', 'success')
-      setStep(1)
-      onLegendComplete()
+      onComplete()
     } catch (e: any) {
       addToast('Failed to invite EOA address', 'error')
       console.error(e)
@@ -78,54 +55,17 @@ const SummonAcc: FC<Props> = ({ buttonText, onComplete, meta }) => {
     <CardActionWrapper
       isLoading={isInProgress}
       loadingText="Signing..."
-      buttonText={step ? 'Copy and Close' : buttonText}
-      disabled={!(step === 1) && !isValid}
+      buttonText={buttonText}
+      disabled={!isValid}
       onButtonClick={onButtonClick}
     >
-      {step === 0 && (
-        <Input
-          label="EOA Address"
-          placeholder="Enter EOA Address"
-          value={eoaAddress}
-          validation={inputValidation}
-          onChange={(e) => setEoaAddress(e.target.value)}
-        />
-      )}
-      {step === 1 && (
-        <>
-          <div> 🎉 Congratulations! 🎉</div>
-          The EOA address has been successfully added to your referred friends. <br /> Now is your
-          turn to invite them in a way they couldn&apos;t refuse.Here is an example:
-          <br />
-          <div className={styles.copySectionWrapper}>
-            <div className={styles.copyField}>
-              <div>
-                ⚠️ TRADE OFFER ⚠️ <br />
-                You download Ambire, we both win 🎉 <br /> 1. Download the Ambire extension:{' '}
-                <a
-                  target="_blank"
-                  href="https://www.ambire.com/get-extension"
-                  rel="noreferrer"
-                  className={styles.link}
-                >
-                  https://www.ambire.com/get-extension
-                </a>
-                <br /> 2. Use my referral code so we both get XP: {meta?.invitationKey} <br /> 3.
-                Join Ambire Legends - on-chain quests by Ambire with XP and rewards:{' '}
-                <a
-                  target="_blank"
-                  href="https://legends.ambire.com/"
-                  rel="noreferrer"
-                  className={styles.link}
-                >
-                  https://legends.ambire.com/{' '}
-                </a>
-              </div>
-              <CopyIcon className={styles.copyIcon} onClick={copyToClipboard} />
-            </div>
-          </div>
-        </>
-      )}
+      <Input
+        label="EOA Address"
+        placeholder="Enter EOA Address"
+        value={eoaAddress}
+        validation={inputValidation}
+        onChange={(e) => setEoaAddress(e.target.value)}
+      />
     </CardActionWrapper>
   )
 }
