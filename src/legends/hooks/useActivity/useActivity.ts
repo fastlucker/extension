59072bdd7073a501bb8030d3212f2c26324fabb1
sonytooo <simeonpetkov11999977@gmail.com>
2024-12-12 18:25:53 +0200

    impl activity polling mechanism

--- src/legends/hooks/useActivity/useActivity.ts
+++ src/legends/hooks/useActivity/useActivity.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import { RELAYER_URL } from '@env'
 import { ActivityResponse } from '@legends/contexts/recentActivityContext/types'
@@ -12,9 +12,14 @@ const useActivity = ({ page, accountAddress }: Props) => {
   const [activity, setActivity] = useState<ActivityResponse | null>(null)
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState<string | null>(null)
-
+  const getActivityIntervalRef: any = useRef(null)
+  const [shouldGetOnFocus, setShouldGetOnFocus] = useState(false)
+  const activityUpdateInterval = useMemo(() => (page === 0 ? 90000 : 300000), [page])
   const getActivity = useCallback(async () => {
     if (!accountAddress) return null
+
+    // Ensure there isn't already a scheduled timeout before setting a new one.
+    if (getActivityIntervalRef.current) clearTimeout(getActivityIntervalRef.current)
 
     try {
       const activityResponse = await fetch(
@@ -37,14 +42,55 @@ const useActivity = ({ page, accountAddress }: Props) => {
       throw e
     } finally {
       setIsLoading(false)
-    }
-  }, [accountAddress, page])
+      const runActivityContinuousUpdate = async () => {
+        if (!document.hidden) {
+          await getActivity()
+        } else {
+          setShouldGetOnFocus(true)
+        }
 
+        getActivityIntervalRef.current = setTimeout(
+          runActivityContinuousUpdate,
+          activityUpdateInterval
+        )
+      }
+
+      getActivityIntervalRef.current = setTimeout(
+        runActivityContinuousUpdate,
+        activityUpdateInterval
+      )
+    }
+  }, [accountAddress, page, activityUpdateInterval])
+
+  // call get on init
   useEffect(() => {
     getActivity().catch(() =>
       setError("Couldn't fetch Character's activity! Please try again later!")
     )
+
+    return () => {
+      clearTimeout(getActivityIntervalRef.current)
+    }
   }, [getActivity])
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === 'visible' && shouldGetOnFocus) {
+        setShouldGetOnFocus(false)
+        getActivity().catch(() => {
+          if (!activity) {
+            setError("Couldn't fetch Character's activity! Please try again later!")
+          }
+        })
+      }
+    }
+
+    document.addEventListener('visibilitychange', handleVisibilityChange)
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange)
+    }
+  }, [activity, getActivity, shouldGetOnFocus])
 
   return {
     activity,
