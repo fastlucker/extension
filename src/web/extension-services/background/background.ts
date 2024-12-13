267ae07d0845aceb644186c1f0d64d9c927c5113
Borislav Itskov <borislav@devlabs.bg>
2024-12-13 17:17:08 +0200

    reduce re-estimation time to 30s

--- src/web/extension-services/background/background.ts
+++ src/web/extension-services/background/background.ts
@@ -438,7 +438,7 @@ handleKeepAlive()
   }
 
   function createEstimateRecurringTimeout() {
-    return createRecurringTimeout(() => mainCtrl.estimateSignAccountOp(), 60000)
+    return createRecurringTimeout(() => mainCtrl.estimateSignAccountOp(), 30000)
   }
 
   function debounceFrontEndEventUpdatesOnSameTick(
