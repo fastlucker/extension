c8066ca8c2aae8ea4daab627a492a6b0b449b0fc
Petromir Petrov <petromirdev@gmail.com>
2024-12-16 16:48:17 +0200

    fix: devserver breaks websites that use websocket

--- webpack.config.js
+++ webpack.config.js
@@ -353,6 +353,10 @@ module.exports = async function (env, argv) {
           return chunk.name !== 'ambire-inpage' && chunk.name !== 'ethereum-inpage'
         }
       }
+    } else {
+      const host = 'localhost'
+      config.devServer.client.webSocketURL.hostname = host
+      config.devServer.webSocketServer.options.host = host
     }
 
     config.experiments = {
