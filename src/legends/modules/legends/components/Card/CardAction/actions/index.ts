e0c6232cd4f7dd31da2c58c45bd526928191ebd2
Petromir Petrov <petromirdev@gmail.com>
2024-12-20 10:43:33 +0200

    legends rename: SummonAcc -> InviteAcc

--- src/legends/modules/legends/components/Card/CardAction/actions/index.ts
+++ src/legends/modules/legends/components/Card/CardAction/actions/index.ts
@@ -1,6 +1,6 @@
+import InviteAcc from './InviteAcc'
 import LinkAcc from './LinkAcc'
 import SendAccOp from './SendAccOp'
 import StakeWallet from './StakeWallet'
-import SummonAcc from './SummonAcc'
 
-export { LinkAcc, SendAccOp, StakeWallet, SummonAcc }
+export { LinkAcc, SendAccOp, StakeWallet, InviteAcc }
