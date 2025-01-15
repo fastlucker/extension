2c9d0d63b87e4cdb7f029bb0b674cf14d0c8acf7
Kaloyan Kosev <superkalo@devlabs.bg>
2025-01-15 15:27:06 +0200

    Added: a tooltip "Coming sometime in 2025", year is dynamic :D

--- src/common/modules/dashboard/components/Tokens/TokenDetails/Button/Button.tsx
+++ src/common/modules/dashboard/components/Tokens/TokenDetails/Button/Button.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import { View } from 'react-native'
 
 import { TokenResult } from '@ambire-common/libs/portfolio'
 import Spinner from '@common/components/Spinner'
 import Text from '@common/components/Text'
+import Tooltip from '@common/components/Tooltip'
 import useTheme from '@common/hooks/useTheme'
 import spacings from '@common/styles/spacings'
 import text from '@common/styles/utils/text'
@@ -38,6 +40,7 @@ const TokenDetailsButton: FC<Props> = ({
   isTokenInfoLoading,
   testID
 }) => {
+  const { t } = useTranslation()
   const { styles, theme } = useTheme(getStyles)
   const [bindAnim, animStyle] = useCustomHover({
     property: 'backgroundColor',
@@ -47,31 +50,45 @@ const TokenDetailsButton: FC<Props> = ({
     }
   })
   const isTokenInfo = id === 'info'
+  const tooltipId = `tooltip-${id}`
 
   return (
-    <AnimatedPressable
-      testID={testID}
-      key={id}
-      style={[styles.action, animStyle, isDisabled && { opacity: 0.4 }]}
-      disabled={isDisabled}
-      onPress={() => {
-        onPress(token)
+    <>
+      <AnimatedPressable
+        testID={testID}
+        key={id}
+        // @ts-ignore missing type, but the prop is valid
+        dataSet={isDisabled && { tooltipId }}
+        style={[styles.action, animStyle, isDisabled && { opacity: 0.4 }]}
+        // Purposely don't disable - in order to show the tooltip, but block `onPress` when disabled
+        // disabled={isDisabled}
+        onPress={() => {
+          if (isDisabled) return
 
-        handleClose()
-      }}
-      {...bindAnim}
-    >
-      <View style={spacings.mbMi}>
-        {isTokenInfo && isTokenInfoLoading ? (
-          <Spinner style={{ width: 32, height: 32 }} />
-        ) : (
-          <Icon color={theme.primary} width={iconWidth} height={32} strokeWidth={strokeWidth} />
-        )}
-      </View>
-      <Text fontSize={14} weight="medium" style={text.center} numberOfLines={1}>
-        {btnText}
-      </Text>
-    </AnimatedPressable>
+          onPress(token)
+          handleClose()
+        }}
+        {...bindAnim}
+      >
+        <View style={spacings.mbMi}>
+          {isTokenInfo && isTokenInfoLoading ? (
+            <Spinner style={{ width: 32, height: 32 }} />
+          ) : (
+            <Icon color={theme.primary} width={iconWidth} height={32} strokeWidth={strokeWidth} />
+          )}
+        </View>
+        <Text fontSize={14} weight="medium" style={text.center} numberOfLines={1}>
+          {btnText}
+        </Text>
+      </AnimatedPressable>
+      {isDisabled && (
+        <Tooltip id={tooltipId}>
+          <Text fontSize={14} appearance="secondaryText">
+            {t('Coming sometime in {{year}}.', { year: new Date().getFullYear() })}
+          </Text>
+        </Tooltip>
+      )}
+    </>
   )
 }
 
