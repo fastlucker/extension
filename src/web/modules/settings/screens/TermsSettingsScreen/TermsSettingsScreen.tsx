75192bd2ccf3388fdfad9dc17757c98ecbd49094
Petromir Petrov <petromirdev@gmail.com>
2025-01-20 17:28:45 +0200

    update: terms back button positions

--- src/web/modules/settings/screens/TermsSettingsScreen/TermsSettingsScreen.tsx
+++ src/web/modules/settings/screens/TermsSettingsScreen/TermsSettingsScreen.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { View } from 'react-native'
 
 import useNavigation from '@common/hooks/useNavigation'
 import HeaderBackButton from '@common/modules/header/components/HeaderBackButton'
 import { ROUTES } from '@common/modules/router/constants/common'
+import spacings from '@common/styles/spacings'
 import SettingsPageHeader from '@web/modules/settings/components/SettingsPageHeader'
 import TermsComponent from '@web/modules/terms/components'
 
@@ -15,9 +17,10 @@ const TermsSettingsScreen = () => {
 
   return (
     <>
-      <SettingsPageHeader title="Terms of Service">
-        <HeaderBackButton hideInPopup forceBack onGoBackPress={goBack} />
-      </SettingsPageHeader>
+      <View style={spacings.mb}>
+        <HeaderBackButton forceBack onGoBackPress={goBack} />
+      </View>
+      <SettingsPageHeader title="Terms of Service" />
       <TermsComponent />
     </>
   )
