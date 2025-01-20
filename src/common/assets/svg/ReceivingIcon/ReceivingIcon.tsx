395ddb9a7d20370ee06f73619b87db030ba0bc81
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2025-01-20 10:13:46 +0200

    Fix: ReceivingIcon.tsx

--- src/common/assets/svg/ReceivingIcon/ReceivingIcon.tsx
+++ src/common/assets/svg/ReceivingIcon/ReceivingIcon.tsx
@@ -10,20 +10,15 @@ interface Props extends SvgProps {
 
 const ReceivingIcon: React.FC<Props> = ({ width = 24, height = 24, fillColor = '#6000ff' }) => (
   <Svg width={width} height={height} viewBox="0 0 32 32">
-    <G data-name="receive icon">
-      <Circle cx="16" cy="16" fill={fillColor} r="16" data-name="Ellipse 410" />
-      <G fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="1.5" data-name="Group 4409">
-        <Path
-          d="M12.805 15.527a1.1 1.1 0 001.065 1.121h1.2a.956.956 0 00.929-.977.847.847 0 00-.633-.929l-1.92-.672a.845.845 0 01-.641-.929.956.956 0 01.929-.977h1.2a1.1 1.1 0 011.071 1.124"
-          data-name="Path 10011"
-        />
-        <Path d="M14.406 16.688v.593" data-name="Path 10012" />
-        <Path d="M14.406 11.531v.625" data-name="Path 10013" />
-        <Path d="M14.4 20.797A6.4 6.4 0 108 14.398a6.4 6.4 0 006.4 6.399z" data-name="Path 10014" />
-        <Path d="M16.793 22.318a3.966 3.966 0 105.549-5.509" data-name="Path 10015" />
-      </G>
+    <Circle cx="16" cy="16" fill={fillColor} r="16" />
+    <G fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="1.5">
+      <Path d="M12.805 15.527a1.1 1.1 0 001.065 1.121h1.2a.956.956 0 00.929-.977.847.847 0 00-.633-.929l-1.92-.672a.845.845 0 01-.641-.929.956.956 0 01.929-.977h1.2a1.1 1.1 0 011.071 1.124" />
+      <Path d="M14.406 16.688v.593" />
+      <Path d="M14.406 11.531v.625" />
+      <Path d="M14.4 20.797A6.4 6.4 0 108 14.398a6.4 6.4 0 006.4 6.399z" />
+      <Path d="M16.793 22.318a3.966 3.966 0 105.549-5.509" />
     </G>
   </Svg>
 )
 
-export default ReceivingIcon
+export default React.memo(ReceivingIcon)
