0a44b137035df8b5dfd00b694dcf1d010a5e338d
Filipv95 <filipv65@gmail.com>
2025-02-14 18:09:32 +0100

    WIP: Swap And Bridge - switch from token amount to USD value and vise-versa

--- tests/features/swap_and_bridge/constants.js
+++ tests/features/swap_and_bridge/constants.js
@@ -1,4 +1,8 @@
 export const FETCHING_BEST_ROUTE = 'text=Fetching best route...'
 export const NETWORK_SELECTOR = '[data-tooltip-id^="network-"].css-1rynq56'
-export const SWITCH_BUTTON_SELECTOR =
+export const ENTER_AMOUNT_SELECTOR = 'input[placeholder="0"]'
+export const SWITCH_TOKENS_TOOLTIP_SELECTOR = '[data-tooltip-id="switch-tokens-condition-tooltip"]'
+export const SWITCH_TOKEN_SELECTOR =
   "//div[contains(@class, 'r-1awozwy') and contains(@class, 'r-1kihuf0')]//div[@tabindex='0']"
+export const SWITCH_USD_SELECTOR =
+  "//div[@tabindex='0' and contains(@class, 'r-1loqt21')]//div[contains(@class, 'css-1rynq56') and string-length(translate(text(), '0123456789', '')) != string-length(text())]"
--- tests/features/swap_and_bridge/functions.js
+++ tests/features/swap_and_bridge/functions.js
@@ -2,7 +2,18 @@ import { clickOnElement } from '../../common-helpers/clickOnElement'
 import { typeText } from '../../common-helpers/typeText'
 import { SELECTORS } from '../../common/selectors/selectors'
 // ToDo: Import and reuse '../../common/transactions'
-import { FETCHING_BEST_ROUTE, NETWORK_SELECTOR, SWITCH_BUTTON_SELECTOR } from './constants'
+import {
+  FETCHING_BEST_ROUTE,
+  NETWORK_SELECTOR,
+  ENTER_AMOUNT_SELECTOR,
+  SWITCH_TOKENS_TOOLTIP_SELECTOR,
+  SWITCH_TOKEN_SELECTOR,
+  SWITCH_USD_SELECTOR
+} from './constants'
+
+export async function selectButton(page, text) {
+  await clickOnElement(page, `text=${text}`)
+}
 
 export async function checkIfOnDashboardPage(page) {
   // Assert if the page is Dashboard
@@ -24,14 +35,25 @@ export async function openSwapAndBridge(page) {
 
 export async function checkIfSwitchIsActive(page, reference = true) {
   await page.waitForTimeout(500)
-  const isActive = (await page.$('[data-tooltip-id="switch-tokens-condition-tooltip"]')) === null
+  const isActive = (await page.$(SWITCH_TOKENS_TOOLTIP_SELECTOR)) === null
   await expect(isActive).toBe(reference)
 }
 
-export async function clickSwitcOnSwapAndBridge(page) {
-  const [element] = await page.$x(SWITCH_BUTTON_SELECTOR)
+export async function clickSwitchTokensOnSwapAndBridge(page) {
+  const [element] = await page.$x(SWITCH_TOKEN_SELECTOR)
   expect(element).not.toBeNull()
   await element.click()
+}
+
+export async function clickSwitchUSDOnSwapAndBridge(page) {
+  const [element] = await page.$x(SWITCH_USD_SELECTOR)
+  expect(element).not.toBeNull()
+  // Find its parent
+  const parentElement = await element.evaluateHandle((el) => el.parentElement)
+  expect(parentElement).not.toBeNull()
+
+  // Click the parent
+  await parentElement.click()
 }
 
 async function getElement(page, selector) {
@@ -40,7 +62,13 @@ async function getElement(page, selector) {
   return element
 }
 
-async function getElementContentWord(page, selector, index = 1) {
+async function getElementValue(page, selector) {
+  const element = await getElement(page, selector)
+  const content = await element.evaluate((el) => el.value.trim())
+  return content
+}
+
+async function getElementContentWords(page, selector, index = 1) {
   const element = await getElement(page, selector)
   const content = (await element.evaluate((el) => el.textContent.trim())).split(' ')[index - 1]
   return content
@@ -50,29 +78,99 @@ export async function switchTokensOnSwapAndBridge(page, delay = 500) {
   await page.waitForTimeout(delay)
 
   // Extract text content from the elements
-  const sendToken = await getElementContentWord(page, SELECTORS.sendTokenSaB)
-  const receiveToken = await getElementContentWord(page, SELECTORS.receiveTokenSaB)
-  const network = await getElementContentWord(page, NETWORK_SELECTOR)
+  const sendToken = await getElementContentWords(page, SELECTORS.sendTokenSaB)
+  const receiveToken = await getElementContentWords(page, SELECTORS.receiveTokenSaB)
+  const network = await getElementContentWords(page, NETWORK_SELECTOR)
 
-  // Click the switch button
-  await clickSwitcOnSwapAndBridge(page)
+  // Click the switch Tokens button
+  await clickSwitchTokensOnSwapAndBridge(page)
 
   // Ensure the tokens are switched
-  expect(await getElementContentWord(page, SELECTORS.sendTokenSaB)).toBe(receiveToken)
-  expect(await getElementContentWord(page, SELECTORS.receiveTokenSaB)).toBe(sendToken)
+  expect(await getElementContentWords(page, SELECTORS.sendTokenSaB)).toBe(receiveToken)
+  expect(await getElementContentWords(page, SELECTORS.receiveTokenSaB)).toBe(sendToken)
 
   // Network name is 3rd word in the sendToken content
-  expect(await getElementContentWord(page, SELECTORS.sendTokenSaB, 3)).toBe(network)
+  expect(await getElementContentWords(page, SELECTORS.sendTokenSaB, 3)).toBe(network)
 }
 
-export async function selectButton(page, text) {
-  await clickOnElement(page, `text=${text}`)
+async function getUSDTextContent(page) {
+  const selector = SWITCH_USD_SELECTOR
+  const [element] = await page.$x(selector)
+  expect(element).not.toBeNull()
+  const content = await element.evaluate((el) => el.textContent.trim())
+
+  // Regex to match amount and currency (last word)
+  const match = content.match(/([\d,.]+)\s*([\w.]+)$/)
+
+  // Extract amount and currency
+  const amount = match ? match[1] : null
+  const currency = match ? match[2] : null
+
+  return [Number(amount), currency]
+}
+
+async function getSendAmount(page) {
+  const amount = await getElementValue(page, ENTER_AMOUNT_SELECTOR)
+  return Number(amount)
+}
+
+async function roundAmount(amount) {
+  // ToDo: Check if USD swithed value should be int-ed or rounded
+  const changedAmount = Math.trunc(amount * 100) / 100
+  return changedAmount
+}
+
+export async function switchUSDValueOnSwapAndBridge(page, send_token, send_network, send_amount, delay = 500) {
+  await page.waitForTimeout(delay)
+
+  // Navigate to Swap and Bridge
+  await openSwapAndBridge(page)
+
+  // Select Send Token on Network
+  await clickOnElement(page, SELECTORS.sendTokenSaB)
+  await clickOnElement(page, `[data-testid*="${send_network}.${send_token.toLowerCase()}"]`)
+
+  // Enter the amount
+  await typeText(page, ENTER_AMOUNT_SELECTOR, send_amount.toString())
+
+  // Get current values in the USD text contenet
+  const [usdOldAmount, ccy] = await getUSDTextContent(page)
+  expect(ccy).toBe('USD')
+  const oldAmount = await getSendAmount(page)
+
+  // Click the switch USD button
+  await clickSwitchUSDOnSwapAndBridge(page)
+
+  // Get new amounts
+  const [usdNewAmount, newCcy] = await getUSDTextContent(page)
+  const newAmount = await roundAmount(await getSendAmount(page))
+
+  // Assert the amount and USD value are switched
+  expect(oldAmount).toBe(usdNewAmount)
+  expect(usdOldAmount).toBe(newAmount)
+  expect(newCcy).toBe(send_token)
+
+  // Wait 500ms and click again to the switch USD button
+  await page.waitForTimeout(500)
+  await clickSwitchUSDOnSwapAndBridge(page)
+
+  // Get second amounts
+  const [usdSecondAmount, secondCcy] = await getUSDTextContent(page)
+  const secondAmount = await getSendAmount(page)
+
+  // Assert the amount and USD value are switched again
+  expect(newAmount).toBe(usdSecondAmount)
+  expect(usdNewAmount).toBe(secondAmount)
+  expect(secondCcy).toBe('USD')
+
+  // Return back to Dashboard
+  await selectButton(page, 'Back')
 }
 
 export async function enterNumber(page, new_amount, is_valid = true) {
   const message = 'Something went wrong! Please contact support'
   // Enter the amount
-  await typeText(page, 'input[placeholder="0"]', new_amount.toString())
+  await typeText(page, ENTER_AMOUNT_SELECTOR, new_amount.toString())
   // Assert if the message should be displayed
   if (is_valid) {
     await expect(page).not.toMatchElement('span', { text: `${message}` })
@@ -94,12 +192,15 @@ export async function prepareSwapAndBridge(
 
     // Select Send Token on Network
     await clickOnElement(page, SELECTORS.sendTokenSaB)
-    await clickOnElement(page, `[data-testid*="${send_network}.${send_token}"]`)
+    await clickOnElement(
+      page,
+      `[data-testid*="${send_network.toLowerCase()}.${send_token.toLowerCase()}"]`
+    )
 
     // Select Receive Token on the same Network, which is automatically selected
     await page.waitForTimeout(1000) // Wait 1000ms before click for the Receive Token list to be populated
     await clickOnElement(page, SELECTORS.receiveTokenSaB)
-    await clickOnElement(page, `[data-testid*="${receive_token}"]`)
+    await clickOnElement(page, `[data-testid*="${receive_token.toLowerCase()}"]`)
 
     // If checking prepareSwapAndBridge functionality without providing send amount
     if (send_amount === null) {
@@ -112,7 +213,7 @@ export async function prepareSwapAndBridge(
     }
 
     // Enter the amount
-    await typeText(page, 'input[placeholder="0"]', send_amount.toString())
+    await typeText(page, ENTER_AMOUNT_SELECTOR, send_amount.toString())
 
     // Wait for Proceed to be enabled, i.e., wait for "Fetching best route..." text to appear and disappear
     await page.waitForSelector(FETCHING_BEST_ROUTE, { visible: true })
