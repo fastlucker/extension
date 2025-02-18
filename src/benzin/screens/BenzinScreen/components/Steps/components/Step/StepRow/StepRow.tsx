62d940cb2d4f8996a6eacf066638847e7338fc6d
Petromir Petrov <petromirdev@gmail.com>
2025-02-18 15:15:02 +0200

    update: StepRow erc20 ui

--- src/benzin/screens/BenzinScreen/components/Steps/components/Step/StepRow/StepRow.tsx
+++ src/benzin/screens/BenzinScreen/components/Steps/components/Step/StepRow/StepRow.tsx
@@ -2,9 +2,9 @@ import { FC } from 'react'
 import { View } from 'react-native'
 
 import { IS_MOBILE_UP_BENZIN_BREAKPOINT } from '@benzin/screens/BenzinScreen/styles'
+import StarsIcon from '@common/assets/svg/StarsIcon'
 import Spinner from '@common/components/Spinner'
 import Text from '@common/components/Text'
-import useTheme from '@common/hooks/useTheme'
 import spacings from '@common/styles/spacings'
 import flexbox from '@common/styles/utils/flexbox'
 
@@ -16,56 +16,54 @@ export interface StepRowProps {
   error?: boolean
 }
 
-const StepRow: FC<StepRowProps> = ({ label, value, isValueSmall, isErc20Highlight, error }) => {
-  const { theme } = useTheme()
-
-  return (
-    <View
-      style={[
-        ...(IS_MOBILE_UP_BENZIN_BREAKPOINT
-          ? [flexbox.directionRow, flexbox.alignCenter, flexbox.justifySpaceBetween, spacings.mbTy]
-          : [spacings.mbTy]),
-        isErc20Highlight
-          ? {
-              shadowColor: theme.primary,
-              shadowRadius: 4,
-              shadowOpacity: 0.5,
-              borderRadius: 4,
-              ...spacings.pvMi,
-              ...spacings.phTy,
-              borderWidth: 1,
-              borderColor: theme.primary,
-              backgroundColor: theme.secondaryBackground
-            }
-          : {}
-      ]}
-      key={label}
-    >
-      <View style={[flexbox.directionRow, flexbox.alignCenter]}>
-        <Text
-          appearance={error ? 'errorText' : 'secondaryText'}
-          fontSize={IS_MOBILE_UP_BENZIN_BREAKPOINT ? 14 : 12}
-        >
-          {label}
-          {!IS_MOBILE_UP_BENZIN_BREAKPOINT && ':'}
-        </Text>
-        {isErc20Highlight && (
-          <Text appearance="primary" weight="medium" fontSize={12} style={spacings.mlTy}>
-            ✨ Paid with ERC-20
-          </Text>
-        )}
-      </View>
+const StepRow: FC<StepRowProps> = ({ label, value, isValueSmall, isErc20Highlight, error }) => (
+  <View
+    style={
+      IS_MOBILE_UP_BENZIN_BREAKPOINT
+        ? [flexbox.directionRow, flexbox.alignCenter, flexbox.justifySpaceBetween, spacings.mbTy]
+        : [spacings.mbTy]
+    }
+    key={label}
+  >
+    <View style={[flexbox.directionRow, flexbox.alignCenter]}>
       <Text
-        appearance={
-          error ? 'errorText' : IS_MOBILE_UP_BENZIN_BREAKPOINT ? 'secondaryText' : 'primaryText'
-        }
-        fontSize={!isValueSmall || !IS_MOBILE_UP_BENZIN_BREAKPOINT ? 14 : 12}
-        selectable
+        appearance={error ? 'errorText' : 'secondaryText'}
+        fontSize={IS_MOBILE_UP_BENZIN_BREAKPOINT ? 14 : 12}
       >
-        {value === 'loading' ? <Spinner style={{ width: 18, height: 18 }} /> : value}
+        {label}
+        {!IS_MOBILE_UP_BENZIN_BREAKPOINT && ':'}
       </Text>
+      {isErc20Highlight && (
+        <View
+          style={[
+            flexbox.directionRow,
+            flexbox.alignCenter,
+            spacings.mlTy,
+            spacings.pvMi,
+            spacings.phSm,
+            {
+              backgroundColor: '#6000FF14',
+              borderRadius: 20
+            }
+          ]}
+        >
+          <StarsIcon width={12} height={12} />
+          <Text style={spacings.mlMi} appearance="primary" weight="medium" fontSize={10}>
+            Paid with ERC-20
+          </Text>
+        </View>
+      )}
     </View>
-  )
-}
+    <Text
+      appearance={
+        error ? 'errorText' : IS_MOBILE_UP_BENZIN_BREAKPOINT ? 'secondaryText' : 'primaryText'
+      }
+      fontSize={!isValueSmall || !IS_MOBILE_UP_BENZIN_BREAKPOINT ? 14 : 12}
+      selectable
+    >
+      {value === 'loading' ? <Spinner style={{ width: 18, height: 18 }} /> : value}
+    </Text>
+  </View>
+)
 
 export default StepRow
