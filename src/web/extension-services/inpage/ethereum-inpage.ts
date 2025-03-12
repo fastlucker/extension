2ebd3d3977c508750add1076af05ed150df4f19d
sonytooo <simeonpetkov11999977@gmail.com>
2025-03-12 09:53:57 +0200

    reduce window ethereum inpage size

--- src/web/extension-services/inpage/ethereum-inpage.ts
+++ src/web/extension-services/inpage/ethereum-inpage.ts
@@ -1,35 +1,15 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable no-param-reassign */
-
-// if is the Opera browser
 if (/Opera|OPR\//i.test(navigator.userAgent)) {
-  const ambireProxy = new Proxy(window.ambire, {
-    get(target, property, receiver) {
-      return Reflect.get(target, property, receiver)
-    }
-  })
-
-  window.ethereum = ambireProxy
+  window.ethereum = new Proxy(window.ambire, { get: (t, p, r) => Reflect.get(t, p, r) })
 } else {
-  const descriptor = Object.getOwnPropertyDescriptor(window, 'ethereum')
-  const canDefine = !descriptor || descriptor.configurable
-
-  if (canDefine) {
-    try {
-      Object.defineProperty(window, 'ethereum', {
-        configurable: false,
-        enumerable: true,
-        get() {
-          return window.ambire
-        },
-        set() {} // Prevent other wallet providers from throwing errors when attempting to override `window.ethereum`
-      })
-    } catch (e) {
-      window.ethereum = window.ambire
-    }
+  const d = Object.getOwnPropertyDescriptor(window, 'ethereum')
+  if (!d || d.configurable) {
+    Object.defineProperty(window, 'ethereum', {
+      configurable: false,
+      enumerable: true,
+      get: () => window.ambire,
+      set: () => {}
+    })
   }
-  if (!window.web3) window.web3 = { currentProvider: window.ambire }
+  window.web3 ||= { currentProvider: window.ambire }
 }
-
 window.dispatchEvent(new Event('ethereum#initialized'))
