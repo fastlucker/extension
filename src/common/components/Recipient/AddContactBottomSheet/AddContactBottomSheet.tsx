7dc283aa231f7bb0dac748177830f1286d2177c4
Petromir Petrov <petromirdev@gmail.com>
2025-03-21 13:12:07 +0200

    fix: add contact bottom sheet input cursor jumping

--- src/common/components/Recipient/AddContactBottomSheet/AddContactBottomSheet.tsx
+++ src/common/components/Recipient/AddContactBottomSheet/AddContactBottomSheet.tsx
@@ -88,7 +88,7 @@ const AddContactBottomSheet: FC<Props> = ({ sheetRef, closeBottomSheet, address 
           label={t('Name')}
           placeholder={t('Contact name')}
           onChangeText={setName}
-          value={name}
+          defaultValue={name}
           maxLength={32}
           onSubmitEditing={handleAddContact}
         />
