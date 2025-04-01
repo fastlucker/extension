822ed453893585534b2a940c7407a076e099ee81
Gergana Mihaylova <gergana@devlabs.bg>
2025-04-01 19:40:47 +0300

    Update: logos

--- src/legends/components/NetworkIcons/EthereumLogo/EthereumLogo.tsx
+++ src/legends/components/NetworkIcons/EthereumLogo/EthereumLogo.tsx
@@ -7,43 +7,52 @@ interface Props extends SvgProps {
 }
 
 const EthereumLogo: React.FC<Props> = ({ width = 32, height = 32, ...rest }) => (
-  <Svg width={width} height={height} viewBox="0 0 32 32" fill="none" {...rest}>
-    <Rect x="1.25" y="1.25" width="29.5" height="29.5" rx="14.75" fill="#191A1F" />
-    <Rect x="1.25" y="1.25" width="29.5" height="29.5" rx="14.75" stroke="#F5EDE2" />
+  <Svg width={width} height={height} viewBox="0 0 28 28" fill="none" {...rest}>
+    <Rect x="2" y="2" width="24" height="24" rx="12" fill="#191A1F" />
+    <Rect
+      x="1.25"
+      y="1.25"
+      width="25.5"
+      height="25.5"
+      rx="12.75"
+      stroke="#6A6F86"
+      strokeOpacity="0.3"
+      strokeWidth="1.5"
+    />
     <Path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M16.0146 4L8.60779 16.2225L16.0151 20.5892L23.3723 16.2393L16.0146 4Z"
+      d="M14.0122 4L7.83984 14.1855L14.0127 17.8244L20.1437 14.1994L14.0122 4Z"
       fill="#62688F"
     />
     <Path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M16.0146 4V20.5887L23.3717 16.2387L16.0146 4Z"
+      d="M14.0127 4V17.824L20.1436 14.199L14.0127 4Z"
       fill="#454A75"
     />
     <Path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M16.0146 4L8.60779 16.2225L16.0151 12.8634L23.3723 16.2393L16.0146 4Z"
+      d="M14.0122 4L7.83984 14.1855L14.0127 11.3863L20.1437 14.1994L14.0122 4Z"
       fill="#8A92B2"
     />
     <Path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M16.0146 4V12.8634L23.3717 16.2393L16.0146 4Z"
+      d="M14.0127 4V11.3863L20.1436 14.1994L14.0127 4Z"
       fill="#62688F"
     />
     <Path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M8.64124 17.6226L16.0146 28L23.3922 17.6216L16.0151 21.9763L8.64124 17.6226Z"
+      d="M7.86719 15.3521L14.0117 24L20.1598 15.3513L14.0121 18.9802L7.86719 15.3521Z"
       fill="#8A92B2"
     />
     <Path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M16.014 28L23.3917 17.6216L16.0146 21.9763L16.014 28Z"
+      d="M14.0127 24L20.1608 15.3513L14.0131 18.9802L14.0127 24Z"
       fill="#62688F"
     />
   </Svg>
--- src/legends/components/NetworkIcons/OptimismLogo/OptimismLogo.tsx
+++ src/legends/components/NetworkIcons/OptimismLogo/OptimismLogo.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import Svg, { Path, SvgProps } from 'react-native-svg'
+import Svg, { Path, Rect, SvgProps } from 'react-native-svg'
 
 const OptimismLogo: React.FC<SvgProps> = ({ width = 32, height = 32, ...rest }) => (
   <Svg width={width} height={height} fill="none" viewBox="0 0 32 32" {...rest}>
     <Path fill="#fff" d="M1 15.75a14.75 14.75 0 1 1 29.5 0 14.75 14.75 0 0 1-29.5 0Z" />
+    <Rect
+      x="1.25"
+      y="1.25"
+      width="30"
+      height="30"
+      rx="12.75"
+      stroke="#6A6F86"
+      strokeOpacity="0.3"
+      strokeWidth="1.5"
+    />
     <Path
       fill="#FF0420"
       d="M15.75 1a14.75 14.75 0 1 0 0 29.5 14.75 14.75 0 0 0 0-29.5Zm0 22.26v6.16a10.59 10.59 0 0 1 0-21.18V2.08a10.59 10.59 0 0 1 0 21.18Zm5.078-7.557v.094a10.925 10.925 0 0 0-5.03 5.031h-.095a10.927 10.927 0 0 0-5.031-5.03v-.095a10.925 10.925 0 0 0 5.03-5.031h.095a10.925 10.925 0 0 0 5.031 5.03Z"
     />
     <Path
-      stroke="#F5EDE2"
+      stroke="#191A1F"
       d="M30.5 15.75C30.5 7.604 23.896 1 15.75 1S1 7.604 1 15.75 7.604 30.5 15.75 30.5 30.5 23.896 30.5 15.75Z"
     />
   </Svg>
