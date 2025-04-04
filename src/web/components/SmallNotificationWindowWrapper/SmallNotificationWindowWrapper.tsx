c1aedd9180708e3ffc49b1edd8b8ee78c5b9cb08
Petromir Petrov <petromirdev@gmail.com>
2025-04-04 11:58:56 +0300

    update: SmallNotificationWindowWrapper maxWidth logic

--- src/web/components/SmallNotificationWindowWrapper/SmallNotificationWindowWrapper.tsx
+++ src/web/components/SmallNotificationWindowWrapper/SmallNotificationWindowWrapper.tsx
@@ -1,15 +1,11 @@
 import React, { FC } from 'react'
 import { View } from 'react-native'
 
-import useWindowSize from '@common/hooks/useWindowSize'
-
 type Props = {
   children: React.ReactNode
 }
 
 const SmallNotificationWindowWrapper: FC<Props> = ({ children }) => {
-  const { maxWidthSize } = useWindowSize()
-
   return (
     <View
       style={{
@@ -17,10 +13,10 @@ const SmallNotificationWindowWrapper: FC<Props> = ({ children }) => {
         // Even the the action window is created with a width of 720px
         // it is not guaranteed that the page will be 720px wide
         // as the user may have zoomed in or out.
-        // That's why we increase the threshold to 1280px
-        // < 1280px the window will be full width
-        // > 1280px the window will be 720px wide
-        width: maxWidthSize(1280) ? 720 : '100%',
+        // That's why we set a higher maxWidth
+        // to ensure that most users won't see white space on the sides
+        maxWidth: 1280,
+        width: '100%',
         marginHorizontal: 'auto'
       }}
     >
