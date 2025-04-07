8fc37712543c901c7079a5d5512864851831b3ae
Gergana Mihaylova <gergana@devlabs.bg>
2025-04-07 15:48:15 +0300

    Update: card content

--- src/legends/modules/legends/components/Card/CardContent/CardContent.tsx
+++ src/legends/modules/legends/components/Card/CardContent/CardContent.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react'
 
 import LockIcon from '@legends/common/assets/svg/LockIcon'
 import MidnightTimer from '@legends/components/MidnightTimer'
+import smokeAndLights from '@legends/modules/leaderboard/screens/Leaderboard/Smoke-and-lights.png'
 import { CARD_PREDEFINED_ID } from '@legends/modules/legends/constants'
 import { CardFromResponse, CardStatus, CardType } from '@legends/modules/legends/types'
 import { isMatchingPredefinedId } from '@legends/modules/legends/utils'
 
 import styles from './CardContent.module.scss'
+import cardImage from './cardImage.png'
 import CompletedRibbon from './CompletedRibbon'
 
 type Props = Pick<
@@ -37,42 +39,66 @@ const CardContent: FC<Props> = ({
   buttonText
 }) => {
   const isCompleted = card.status === CardStatus.completed
-  console.log(xp)
+  console.log(
+    'CARD_FREQUENCY[card.type]}',
+    CARD_FREQUENCY[card.type],
+    card.type,
+    `rewardFrequency${CARD_FREQUENCY[card.type]}`
+  )
   return (
-    <div className={`${styles.wrapper} ${disabled && styles.disabled}`}>
+    <div
+      className={`${styles.wrapper} ${(disabled || isCompleted) && styles.disabled}`}
+      role="button"
+      onClick={!!disabled || (!isCompleted && openActionModal)}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          openActionModal()
+        }
+      }}
+      tabIndex={0}
+    >
       {isCompleted ? (
         <div className={styles.overlay}>
           <CompletedRibbon className={styles.overlayIcon} />
-          <div className={styles.overlayTitle}>
+          {/* <div className={styles.overlayTitle}>
             Completed
             {isMatchingPredefinedId(action, CARD_PREDEFINED_ID.wheelOfFortune) ||
             isMatchingPredefinedId(action, CARD_PREDEFINED_ID.chest) ? (
               <MidnightTimer className={styles.overlayText} />
             ) : null}
-          </div>
+          </div> */}
         </div>
       ) : null}
       {disabled && (
         <div className={styles.overlay}>
           <LockIcon className={styles.overlayIcon} />
-          <div className={styles.overlayTitle}>Coming soon</div>
+          {/* <div className={styles.overlayTitle}>Coming soon</div> */}
         </div>
       )}
       <div className={styles.contentAndAction}>
         <div className={styles.content}>
-          <span className={styles.rewardFrequency}>{CARD_FREQUENCY[card.type]}</span>
           <h2 className={styles.heading}>{title}</h2>
+          <img src={cardImage} alt="Card" className={styles.image} />
+          <div
+            className={styles.backgroundEffect}
+            style={{
+              backgroundImage: `url(${smokeAndLights})`,
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover'
+            }}
+          />
         </div>
         <div className={styles.actionAndRewards}>
-          <button
-            disabled={disabled}
-            className={styles.button}
-            type="button"
-            onClick={openActionModal}
-          >
-            {/* {action.type ? buttonText : 'Read more'} */}
-            Open
-          </button>
+          <div className={styles.rewardFrequencyWrapper}>
+            <span
+              className={`${styles.rewardFrequency} ${
+                styles[`rewardFrequency${CARD_FREQUENCY[card.type]}`]
+              }`}
+            >
+              {CARD_FREQUENCY[card.type]}
+            </span>
+          </div>
           <div>
             <div className={styles.rewardTitle}>
               {Array.isArray(xp) && xp.length > 1 ? (
