7c852006cdec9d3de3329c636cabd3eb4b2ee94d
Filipv95 <filipv65@gmail.com>
2025-04-08 15:18:48 +0200

    Changed function typeText with .type for input token amount

--- tests/features/swap_and_bridge/functions.js
+++ tests/features/swap_and_bridge/functions.js
@@ -141,8 +141,9 @@ export async function switchUSDValueOnSwapAndBridge(
 
   await openSwapAndBridge(page)
   await selectSendTokenOnNetwork(page, send_token, send_network)
-  // Enter the amount
-  await typeText(page, SELECTORS.fromAmountInputSab, send_amount.toString())
+
+  // await typeText(page, SELECTORS.fromAmountInputSab, send_amount.toString()) // it has given error with value 0.0004, so changed to page.type
+  await page.type(SELECTORS.fromAmountInputSab, send_amount.toString())
 
   // Get current values in the USD text contenet
   const [usdOldAmount, ccy] = await getUSDTextContent(page)
