03684bc98c595e37c7990710bdae495708b5274a
Gergana Mihaylova <gergana@devlabs.bg>
2025-04-09 11:04:03 +0300

    Update: wheel image and wheel data

--- src/legends/modules/legends/components/WheelComponentModal/wheelData.ts
+++ src/legends/modules/legends/components/WheelComponentModal/wheelData.ts
@@ -1,56 +1,23 @@
 const WHEEL_PRIZE_DATA: {
   [key: number]: { from: number; to: number }[]
 } = {
-  300: [
-    {
-      from: 11,
-      to: -10
-    }
-  ],
-  150: [
-    {
-      from: -215,
-      to: -245
-    }
-  ],
+  300: [{ from: 5, to: -5 }], // covers top center
+  150: [{ from: -205, to: -215 }],
   80: [
-    {
-      from: -46,
-      to: -76
-    },
-    {
-      from: -181,
-      to: -211
-    },
-    {
-      from: -249,
-      to: -279
-    }
-  ],
-  20: [
-    { from: -114, to: -144 },
-    {
-      from: -317,
-      to: -347
-    }
+    { from: -55, to: -65 },
+    { from: -175, to: -185 },
+    { from: -235, to: -245 },
+    { from: -325, to: -335 }
   ],
   50: [
-    {
-      from: -11,
-      to: -43
-    },
-    {
-      from: -79,
-      to: -111
-    },
-    {
-      from: -147,
-      to: -178
-    },
-    {
-      from: -282,
-      to: -313
-    }
+    { from: -25, to: -35 },
+    { from: -85, to: -95 },
+    { from: -145, to: -155 },
+    { from: -265, to: -275 }
+  ],
+  20: [
+    { from: -115, to: -125 },
+    { from: -295, to: -305 }
   ]
 }
 
