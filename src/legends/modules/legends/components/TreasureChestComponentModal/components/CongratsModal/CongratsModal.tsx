22ae4b99f08458bd32f23ceb47e792288497c3ac
Gergana Mihaylova <gergana@devlabs.bg>
2025-04-15 18:46:14 +0300

    Update: congrats modal text

--- src/legends/modules/legends/components/TreasureChestComponentModal/components/CongratsModal/CongratsModal.tsx
+++ src/legends/modules/legends/components/TreasureChestComponentModal/components/CongratsModal/CongratsModal.tsx
@@ -38,7 +38,7 @@ const CongratsModal: React.FC<CongratsModalProps> = ({
           <img src={chestImageOpened} alt="chest-opened" className={styles.chestOpenedImage} />
         </div>
         <button type="button" className={styles.button} onClick={onButtonClick}>
-          Go back
+          Cha-ching
         </button>
       </div>
     </Modal>
