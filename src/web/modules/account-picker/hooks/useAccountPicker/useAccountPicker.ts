9854fccdaa2a1922122faace233d64680e4a5014
sonytooo <simeonpetkov11999977@gmail.com>
2025-04-15 13:51:33 +0300

    fixes

--- src/web/modules/account-picker/hooks/useAccountPicker/useAccountPicker.ts
+++ src/web/modules/account-picker/hooks/useAccountPicker/useAccountPicker.ts
@@ -1,24 +1,29 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import useOnboardingNavigation from '@common/modules/auth/hooks/useOnboardingNavigation'
 import { WEB_ROUTES } from '@common/modules/router/constants/common'
 import useAccountPickerControllerState from '@web/hooks/useAccountPickerControllerState'
 import useBackgroundService from '@web/hooks/useBackgroundService'
 
-const ACCOUNT_PICKER_PAGE_SIZE = 5
-
 const useAccountPicker = () => {
   const { goToNextRoute } = useOnboardingNavigation()
-  const { pageSize } = useAccountPickerControllerState()
+  const { pageSize, subType } = useAccountPickerControllerState()
   const shouldResetAccountsSelectionOnUnmount = useRef(true)
   const { dispatch } = useBackgroundService()
   const [isReady, setIsReady] = useState(false)
 
+  const ACCOUNT_PICKER_PAGE_SIZE = useMemo(() => {
+    return subType === 'private-key' ? 1 : 5
+  }, [subType])
+
   const setPage = React.useCallback(
     (page = 1) => {
-      dispatch({ type: 'MAIN_CONTROLLER_ACCOUNT_PICKER_SET_PAGE', params: { page, pageSize: 5 } })
+      dispatch({
+        type: 'MAIN_CONTROLLER_ACCOUNT_PICKER_SET_PAGE',
+        params: { page, pageSize: ACCOUNT_PICKER_PAGE_SIZE }
+      })
     },
-    [dispatch]
+    [dispatch, ACCOUNT_PICKER_PAGE_SIZE]
   )
 
   useEffect(() => {
@@ -29,7 +34,7 @@ const useAccountPicker = () => {
     if (pageSize === ACCOUNT_PICKER_PAGE_SIZE && !isReady) {
       setIsReady(true)
     }
-  }, [pageSize, isReady])
+  }, [pageSize, isReady, ACCOUNT_PICKER_PAGE_SIZE])
 
   const onImportReady = useCallback(() => {
     shouldResetAccountsSelectionOnUnmount.current = false
