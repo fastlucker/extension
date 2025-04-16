a2567a54ae3c7be7d488fb8d89de32d49f194b17
Borislav Itskov <borislav.ickov@gmail.com>
2025-04-16 20:47:06 +0300

    create a BatchAdded component and move the logic from the BatchModal there

---
+++ src/web/modules/swap-and-bridge/components/BatchModal/BatchAdded.tsx
@@ -0,0 +1,58 @@
+import React, { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { View } from 'react-native'
+
+import CartIcon from '@common/assets/svg/CartIcon'
+import Button from '@common/components/Button'
+import Text from '@common/components/Text'
+import useTheme from '@common/hooks/useTheme'
+import spacings from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
+import text from '@common/styles/utils/text'
+
+type Props = {
+  onPrimaryButtonPress: () => void
+  onSecondaryButtonPress: () => void
+}
+
+const BatchAdded: FC<Props> = ({ onPrimaryButtonPress, onSecondaryButtonPress }) => {
+  const { t } = useTranslation()
+  const { theme } = useTheme()
+
+  return (
+    <>
+      <View style={[flexbox.alignCenter, flexbox.justifyCenter, spacings.pt2Xl, spacings.pbXl]}>
+        <CartIcon width={64} height={64} color={theme.secondaryText} />
+        <Text fontSize={20} weight="medium" style={[spacings.mbTy, spacings.mtLg, text.center]}>
+          {t('Added to batch')}
+        </Text>
+        <Text weight="medium" appearance="secondaryText" style={text.center}>
+          {t('You can manage your batch in the dashboard.')}
+        </Text>
+      </View>
+      <View
+        style={{
+          height: 1,
+          backgroundColor: theme.secondaryBorder,
+          ...spacings.mvLg
+        }}
+      />
+      <View style={[flexbox.directionRow, flexbox.alignCenter, flexbox.justifySpaceBetween]}>
+        <Button
+          onPress={onSecondaryButtonPress}
+          hasBottomSpacing={false}
+          type="secondary"
+          text={t('Add more swaps?')}
+        />
+        <Button
+          onPress={onPrimaryButtonPress}
+          hasBottomSpacing={false}
+          style={{ width: 160 }}
+          text={t('Close')}
+        />
+      </View>
+    </>
+  )
+}
+
+export default BatchAdded
--- src/web/modules/swap-and-bridge/components/BatchModal/BatchModal.tsx
+++ src/web/modules/swap-and-bridge/components/BatchModal/BatchModal.tsx
@@ -1,17 +1,9 @@
 import React, { FC, useCallback } from 'react'
-import { useTranslation } from 'react-i18next'
-import { View } from 'react-native'
 
-import CartIcon from '@common/assets/svg/CartIcon'
 import BottomSheet from '@common/components/BottomSheet'
-import Button from '@common/components/Button'
-import Text from '@common/components/Text'
 import useNavigation from '@common/hooks/useNavigation'
-import useTheme from '@common/hooks/useTheme'
 import { WEB_ROUTES } from '@common/modules/router/constants/common'
-import spacings from '@common/styles/spacings'
-import flexbox from '@common/styles/utils/flexbox'
-import text from '@common/styles/utils/text'
+import BatchAdded from './BatchAdded'
 
 type Props = {
   sheetRef: React.RefObject<any>
@@ -19,8 +11,6 @@ type Props = {
 }
 
 const BatchModal: FC<Props> = ({ sheetRef, closeBottomSheet }) => {
-  const { t } = useTranslation()
-  const { theme } = useTheme()
   const { navigate } = useNavigation()
 
   const onSecondaryButtonPress = useCallback(() => {
@@ -37,36 +27,10 @@ const BatchModal: FC<Props> = ({ sheetRef, closeBottomSheet }) => {
       closeBottomSheet={closeBottomSheet}
       id="batch-modal"
     >
-      <View style={[flexbox.alignCenter, flexbox.justifyCenter, spacings.pt2Xl, spacings.pbXl]}>
-        <CartIcon width={64} height={64} color={theme.secondaryText} />
-        <Text fontSize={20} weight="medium" style={[spacings.mbTy, spacings.mtLg, text.center]}>
-          {t('Added to batch')}
-        </Text>
-        <Text weight="medium" appearance="secondaryText" style={text.center}>
-          {t('You can manage your batch in the dashboard.')}
-        </Text>
-      </View>
-      <View
-        style={{
-          height: 1,
-          backgroundColor: theme.secondaryBorder,
-          ...spacings.mvLg
-        }}
+      <BatchAdded
+        onPrimaryButtonPress={onPrimaryButtonPress}
+        onSecondaryButtonPress={onSecondaryButtonPress}
       />
-      <View style={[flexbox.directionRow, flexbox.alignCenter, flexbox.justifySpaceBetween]}>
-        <Button
-          onPress={onSecondaryButtonPress}
-          hasBottomSpacing={false}
-          type="secondary"
-          text={t('Add more swaps?')}
-        />
-        <Button
-          onPress={onPrimaryButtonPress}
-          hasBottomSpacing={false}
-          style={{ width: 160 }}
-          text={t('Close')}
-        />
-      </View>
     </BottomSheet>
   )
 }
