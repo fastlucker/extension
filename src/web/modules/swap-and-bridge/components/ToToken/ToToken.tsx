1a0e48e870e50ed4e97cbd48caefbd3f19624306
Petromir Petrov <petromirdev@gmail.com>
2025-04-28 17:25:48 +0300

    update: to token network select to open as bottom sheet (swap and bridge)

--- src/web/modules/swap-and-bridge/components/ToToken/ToToken.tsx
+++ src/web/modules/swap-and-bridge/components/ToToken/ToToken.tsx
@@ -3,6 +3,7 @@ import React, { FC, memo, useCallback, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { View } from 'react-native'
 
+import { EstimationStatus } from '@ambire-common/controllers/estimation/types'
 import { SwapAndBridgeFormStatus } from '@ambire-common/controllers/swapAndBridge/swapAndBridge'
 import { SwapAndBridgeToToken } from '@ambire-common/interfaces/swapAndBridge'
 import { getIsNetworkSupported } from '@ambire-common/libs/swapAndBridge/swapAndBridge'
@@ -27,7 +28,6 @@ import ToTokenSelect from '@web/modules/swap-and-bridge/components/ToToken/ToTok
 import useSwapAndBridgeForm from '@web/modules/swap-and-bridge/hooks/useSwapAndBridgeForm'
 import { getTokenId } from '@web/utils/token'
 
-import { EstimationStatus } from '@ambire-common/controllers/estimation/types'
 import NotSupportedNetworkTooltip from '../NotSupportedNetworkTooltip'
 
 type Props = Pick<ReturnType<typeof useSwapAndBridgeForm>, 'setIsAutoSelectRouteDisabled'> & {
@@ -235,6 +235,8 @@ const ToToken: FC<Props> = ({ isAutoSelectRouteDisabled, setIsAutoSelectRouteDis
           options={toNetworksOptions}
           size="sm"
           value={getToNetworkSelectValue}
+          mode="bottomSheet"
+          bottomSheetTitle={t('Receive token network')}
           selectStyle={{
             backgroundColor: '#54597A14',
             borderWidth: 0
