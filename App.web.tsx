dc17e99189caad25a6abce78aa28e20f640c773a
Kaloyan Kosev <superkalo@devlabs.bg>
2025-05-02 18:24:47 +0300

    Change: App.web.tsx that excludes expo-for-mobile-only modules

--- App.web.tsx
+++ App.web.tsx
@@ -1,8 +1,6 @@
 // So that the localization gets initialized at the beginning.
 import '@common/config/localization'
 
-import * as SplashScreen from 'expo-splash-screen'
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
@@ -11,8 +9,6 @@ import AppInit from '@common/modules/app-init/screens/AppInit'
 import colors from '@common/styles/colors'
 import flexboxStyles from '@common/styles/utils/flexbox'
 import { isExtension } from '@web/constants/browserapi'
-
-SplashScreen.preventAutoHideAsync().catch(console.warn) // TODO: log a sentry error
 
 const consoleWarn = console.warn
 const SUPPRESSED_WARNINGS = [
@@ -35,8 +31,6 @@ const App = () => {
 
   return (
     <GestureHandlerRootView style={[flexboxStyles.flex1, { backgroundColor: colors.white }]}>
-      <StatusBar style="light" backgroundColor={colors.zircon} />
-
       <AppInit />
     </GestureHandlerRootView>
   )
