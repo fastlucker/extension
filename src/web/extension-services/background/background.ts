596ad6152950f36d4926d92146b7bd90f6da6291
Petromir Petrov <petromirdev@gmail.com>
2025-05-13 17:19:41 +0300

    fix: error emitted from destroyed controller

--- src/web/extension-services/background/background.ts
+++ src/web/extension-services/background/background.ts
@@ -828,9 +828,14 @@ function getIntervalRefreshTime(constUpdateInterval: number, newestOpTimestamp: 
         if (!hasOnErrorInitialized) {
           ;(mainCtrl as any)[ctrlName]?.onError(() => {
             stateDebug(`onError (${ctrlName} ctrl)`, mainCtrl, ctrlName)
+            const controller = (mainCtrl as any)[ctrlName]
+
+            // In case the controller was destroyed and an error was emitted
+            if (!controller) return
+
             pm.send('> ui-error', {
               method: ctrlName,
-              params: { errors: (mainCtrl as any)[ctrlName].emittedErrors, controller: ctrlName }
+              params: { errors: controller.emittedErrors, controller: ctrlName }
             })
           }, 'background')
         }
