7acfea09c9dba2b8e0ec134b68b7b927c85697d7
sonytooo <simeonpetkov11999977@gmail.com>
2025-05-28 18:14:59 +0300

    icons colors fixes

--- src/common/assets/svg/AmbireDevice/AmbireDevice.tsx
+++ src/common/assets/svg/AmbireDevice/AmbireDevice.tsx
@@ -1,186 +1,195 @@
 import React from 'react'
 import Svg, { Defs, G, LinearGradient, Path, Rect, Stop } from 'react-native-svg'
 
-import colors from '@common/styles/colors'
+import useTheme from '@common/hooks/useTheme'
 
-const AmbireDevice: React.FC<any> = (props) => (
-  <Svg width="144" height="94" viewBox="0 0 144 94" {...props}>
-    <Defs>
-      <LinearGradient
-        id="linear-gradient"
-        x1="0.554"
-        y1="0.58"
-        x2="0.052"
-        y2="0.409"
-        gradientUnits="objectBoundingBox"
-      >
-        <Stop offset="0" stopColor="#6000ff" />
-        <Stop offset="0.651" stopColor="#4900c3" />
-        <Stop offset="1" stopColor="#320086" />
-      </LinearGradient>
-      <LinearGradient
-        id="linear-gradient-2"
-        x1="0.06"
-        y1="-0.087"
-        x2="0.486"
-        y2="0.653"
-        gradientUnits="objectBoundingBox"
-      >
-        <Stop offset="0" stopColor="#6a0aff" />
-        <Stop offset="0.047" stopColor="#8c2dff" />
-        <Stop offset="0.102" stopColor="#6a0aff" />
-        <Stop offset="0.902" stopColor="#af50ff" />
-        <Stop offset="1" stopColor="#af50ff" />
-      </LinearGradient>
-      <LinearGradient
-        id="linear-gradient-3"
-        x1="1.071"
-        y1="0.061"
-        x2="0.095"
-        y2="1.049"
-        gradientUnits="objectBoundingBox"
-      >
-        <Stop offset="0" stopColor="#6a0aff" />
-        <Stop offset="0.51" stopColor="#8c2dff" />
-        <Stop offset="0.969" stopColor="#af50ff" />
-        <Stop offset="1" stopColor="#af50ff" />
-      </LinearGradient>
-      <LinearGradient
-        id="linear-gradient-4"
-        x1="0.448"
-        y1="0.297"
-        x2="0.538"
-        y2="0.8"
-        gradientUnits="objectBoundingBox"
-      >
-        <Stop offset="0" stopColor="#6000ff" />
-        <Stop offset="1" stopColor="#3e00a5" />
-      </LinearGradient>
-      <LinearGradient
-        id="linear-gradient-5"
-        x1="-0.529"
-        y1="1.069"
-        x2="1.092"
-        y2="0.86"
-        gradientUnits="objectBoundingBox"
-      >
-        <Stop offset="0" stopColor="#ae60ff" />
-        <Stop offset="0.322" stopColor="#af50ff" />
-        <Stop offset="1" stopColor="#6000ff" />
-      </LinearGradient>
-      <LinearGradient
-        id="linear-gradient-6"
-        x1="-0.111"
-        y1="0.274"
-        x2="0.872"
-        y2="1.224"
-        gradientUnits="objectBoundingBox"
-      >
-        <Stop offset="0" stopColor="#6f0fff" />
-        <Stop offset="0.702" stopColor="#af50ff" />
-        <Stop offset="1" stopColor="#af50ff" />
-      </LinearGradient>
-      <LinearGradient
-        id="linear-gradient-7"
-        x1="0.015"
-        y1="0.007"
-        x2="0.986"
-        y2="0.95"
-        gradientUnits="objectBoundingBox"
-      >
-        <Stop offset="0" stopColor="#ae60ff" />
-        <Stop offset="0.031" stopColor="#b670fa" />
-        <Stop offset="1" stopColor="#be80f5" />
-      </LinearGradient>
-    </Defs>
-    <G transform="translate(-499 15308)">
-      <Rect
-        width="130"
-        height="83"
-        rx="5"
-        transform="translate(506 -15308)"
-        fill="rgba(103,112,179,0.2)"
-      />
-      <Rect
-        width="144"
-        height="8"
-        rx="4"
-        transform="translate(499 -15222)"
-        fill="rgba(103,112,179,0.2)"
-      />
-      <Rect width="120" height="72" rx="2" transform="translate(511 -15303)" fill={colors.titan} />
-      <G transform="translate(525.344 -15281.437)" opacity="0.3">
-        <G id="_1967955583232" transform="translate(36.039 -0.01)">
-          <G>
-            <Path
-              d="M727.2,420.5l3.791,8.071a.229.229,0,0,1-.047.252l-9.974,9.637a.11.11,0,0,1-.187-.084v-8.854l6.285-6.067a.2.2,0,0,0,.065-.158l.019-2.8c0-.019.037-.019.047,0Z"
-              transform="translate(-711.771 -409.601)"
-              fill="#6000ff"
-            />
-            <Path
-              d="M720.76,540.047V548.9a.111.111,0,0,0,.187.084h0l9.974-9.637a.229.229,0,0,0,.047-.252h0l-3.894-5.147-6.314,6.1Z"
-              transform="translate(-711.752 -520.122)"
-              fillRule="evenodd"
-              fill="url(#linear-gradient)"
-            />
-            <Path
-              d="M964.646,420.11h0a.021.021,0,0,0-.022.022h0l-.016,2.8a.23.23,0,0,1-.036.13l3.909,5.276a.245.245,0,0,0-.019-.14h0l-3.792-8.071a.026.026,0,0,0-.023-.013Z"
-              transform="translate(-949.248 -409.23)"
-              fillRule="evenodd"
-              fill="url(#linear-gradient-2)"
-            />
-            <Path
-              id="Path_661"
-              d="M974.684,547.594a.245.245,0,0,0-.019-.14h0l-.2-.419-3.529-4.494Z"
-              transform="translate(-955.453 -528.49)"
-              fill="#be80f5"
-              fillRule="evenodd"
-            />
-            <Path
-              d="M401.887,158.056l-1.784,5.051a.234.234,0,0,0,.009.177l1.662,3.271-4.576,2.591a.12.12,0,0,1-.159-.047l-.99-2.06a.2.2,0,0,1,.019-.214l5.771-8.8a.028.028,0,0,1,.047.028Z"
-              transform="translate(-395.429 -153.928)"
-              fill="#6000ff"
-            />
-            <Path
-              d="M412.49,158.03h0a.026.026,0,0,0-.018.008h0l-5.525,8.422-.006.011,3.794-3.354.012-.034h0v0l1.771-5.015a.028.028,0,0,0-.027-.035Z"
-              transform="translate(-406.059 -153.937)"
-              fillRule="evenodd"
-              fill="url(#linear-gradient-3)"
-            />
-            <Path
-              d="M396.083,358.169a.2.2,0,0,0-.019.214h0l.99,2.06a.12.12,0,0,0,.159.046h0l4.576-2.591-1.662-3.271a.234.234,0,0,1-.009-.177Z"
-              transform="translate(-395.442 -345.271)"
-              fillRule="evenodd"
-              fill="url(#linear-gradient-4)"
-            />
-            <Path
-              d="M381.92.046V7.9a.223.223,0,0,1-.047.131h0l-8.919,12.088a.216.216,0,0,0,.028.289h0l6.257,6.039a.107.107,0,0,0,.177-.037h0l6.8-15.909a.258.258,0,0,0,0-.168h0L382,.027A.04.04,0,0,0,381.966,0h0a.046.046,0,0,0-.046.046Z"
-              transform="translate(-372.912 0)"
-              fillRule="evenodd"
-              fill="url(#linear-gradient-5)"
-            />
-            <Path
-              d="M720.375-.01h0a.046.046,0,0,0-.046.046l0,7.857a.221.221,0,0,1-.012.065l4.252,2.238L720.413.017a.04.04,0,0,0-.037-.027Zm-.055,7.968.812.466Z"
-              transform="translate(-711.323 0.01)"
-              fillRule="evenodd"
-              fill="url(#linear-gradient-6)"
-            />
-            <Path
-              d="M724.642,310.124h0a.257.257,0,0,0-.013-.082h0l-.054-.133-4.255-2.238.815.466,3.507,1.989Z"
-              transform="translate(-711.323 -299.702)"
-              fillRule="evenodd"
-              fill="url(#linear-gradient-7)"
-            />
-            <Path
-              d="M398,357.816l-.235.358,4.031-3.714Z"
-              transform="translate(-397.126 -345.28)"
-              fill="#be80f5"
-            />
+const AmbireDevice: React.FC<any> = (props) => {
+  const { theme } = useTheme()
+  return (
+    <Svg width="144" height="94" viewBox="0 0 144 94" {...props}>
+      <Defs>
+        <LinearGradient
+          id="linear-gradient"
+          x1="0.554"
+          y1="0.58"
+          x2="0.052"
+          y2="0.409"
+          gradientUnits="objectBoundingBox"
+        >
+          <Stop offset="0" stopColor="#6000ff" />
+          <Stop offset="0.651" stopColor="#4900c3" />
+          <Stop offset="1" stopColor="#320086" />
+        </LinearGradient>
+        <LinearGradient
+          id="linear-gradient-2"
+          x1="0.06"
+          y1="-0.087"
+          x2="0.486"
+          y2="0.653"
+          gradientUnits="objectBoundingBox"
+        >
+          <Stop offset="0" stopColor="#6a0aff" />
+          <Stop offset="0.047" stopColor="#8c2dff" />
+          <Stop offset="0.102" stopColor="#6a0aff" />
+          <Stop offset="0.902" stopColor="#af50ff" />
+          <Stop offset="1" stopColor="#af50ff" />
+        </LinearGradient>
+        <LinearGradient
+          id="linear-gradient-3"
+          x1="1.071"
+          y1="0.061"
+          x2="0.095"
+          y2="1.049"
+          gradientUnits="objectBoundingBox"
+        >
+          <Stop offset="0" stopColor="#6a0aff" />
+          <Stop offset="0.51" stopColor="#8c2dff" />
+          <Stop offset="0.969" stopColor="#af50ff" />
+          <Stop offset="1" stopColor="#af50ff" />
+        </LinearGradient>
+        <LinearGradient
+          id="linear-gradient-4"
+          x1="0.448"
+          y1="0.297"
+          x2="0.538"
+          y2="0.8"
+          gradientUnits="objectBoundingBox"
+        >
+          <Stop offset="0" stopColor="#6000ff" />
+          <Stop offset="1" stopColor="#3e00a5" />
+        </LinearGradient>
+        <LinearGradient
+          id="linear-gradient-5"
+          x1="-0.529"
+          y1="1.069"
+          x2="1.092"
+          y2="0.86"
+          gradientUnits="objectBoundingBox"
+        >
+          <Stop offset="0" stopColor="#ae60ff" />
+          <Stop offset="0.322" stopColor="#af50ff" />
+          <Stop offset="1" stopColor="#6000ff" />
+        </LinearGradient>
+        <LinearGradient
+          id="linear-gradient-6"
+          x1="-0.111"
+          y1="0.274"
+          x2="0.872"
+          y2="1.224"
+          gradientUnits="objectBoundingBox"
+        >
+          <Stop offset="0" stopColor="#6f0fff" />
+          <Stop offset="0.702" stopColor="#af50ff" />
+          <Stop offset="1" stopColor="#af50ff" />
+        </LinearGradient>
+        <LinearGradient
+          id="linear-gradient-7"
+          x1="0.015"
+          y1="0.007"
+          x2="0.986"
+          y2="0.95"
+          gradientUnits="objectBoundingBox"
+        >
+          <Stop offset="0" stopColor="#ae60ff" />
+          <Stop offset="0.031" stopColor="#b670fa" />
+          <Stop offset="1" stopColor="#be80f5" />
+        </LinearGradient>
+      </Defs>
+      <G transform="translate(-499 15308)">
+        <Rect
+          width="130"
+          height="83"
+          rx="5"
+          transform="translate(506 -15308)"
+          fill={theme.tertiaryBackground}
+        />
+        <Rect
+          width="144"
+          height="8"
+          rx="4"
+          transform="translate(499 -15222)"
+          fill={theme.tertiaryBackground}
+        />
+        <Rect
+          width="120"
+          height="72"
+          rx="2"
+          transform="translate(511 -15303)"
+          fill={theme.iconPrimary}
+        />
+        <G transform="translate(525.344 -15281.437)" opacity="0.7">
+          <G id="_1967955583232" transform="translate(36.039 -0.01)">
+            <G>
+              <Path
+                d="M727.2,420.5l3.791,8.071a.229.229,0,0,1-.047.252l-9.974,9.637a.11.11,0,0,1-.187-.084v-8.854l6.285-6.067a.2.2,0,0,0,.065-.158l.019-2.8c0-.019.037-.019.047,0Z"
+                transform="translate(-711.771 -409.601)"
+                fill="#6000ff"
+              />
+              <Path
+                d="M720.76,540.047V548.9a.111.111,0,0,0,.187.084h0l9.974-9.637a.229.229,0,0,0,.047-.252h0l-3.894-5.147-6.314,6.1Z"
+                transform="translate(-711.752 -520.122)"
+                fillRule="evenodd"
+                fill="url(#linear-gradient)"
+              />
+              <Path
+                d="M964.646,420.11h0a.021.021,0,0,0-.022.022h0l-.016,2.8a.23.23,0,0,1-.036.13l3.909,5.276a.245.245,0,0,0-.019-.14h0l-3.792-8.071a.026.026,0,0,0-.023-.013Z"
+                transform="translate(-949.248 -409.23)"
+                fillRule="evenodd"
+                fill="url(#linear-gradient-2)"
+              />
+              <Path
+                id="Path_661"
+                d="M974.684,547.594a.245.245,0,0,0-.019-.14h0l-.2-.419-3.529-4.494Z"
+                transform="translate(-955.453 -528.49)"
+                fill="#be80f5"
+                fillRule="evenodd"
+              />
+              <Path
+                d="M401.887,158.056l-1.784,5.051a.234.234,0,0,0,.009.177l1.662,3.271-4.576,2.591a.12.12,0,0,1-.159-.047l-.99-2.06a.2.2,0,0,1,.019-.214l5.771-8.8a.028.028,0,0,1,.047.028Z"
+                transform="translate(-395.429 -153.928)"
+                fill="#6000ff"
+              />
+              <Path
+                d="M412.49,158.03h0a.026.026,0,0,0-.018.008h0l-5.525,8.422-.006.011,3.794-3.354.012-.034h0v0l1.771-5.015a.028.028,0,0,0-.027-.035Z"
+                transform="translate(-406.059 -153.937)"
+                fillRule="evenodd"
+                fill="url(#linear-gradient-3)"
+              />
+              <Path
+                d="M396.083,358.169a.2.2,0,0,0-.019.214h0l.99,2.06a.12.12,0,0,0,.159.046h0l4.576-2.591-1.662-3.271a.234.234,0,0,1-.009-.177Z"
+                transform="translate(-395.442 -345.271)"
+                fillRule="evenodd"
+                fill="url(#linear-gradient-4)"
+              />
+              <Path
+                d="M381.92.046V7.9a.223.223,0,0,1-.047.131h0l-8.919,12.088a.216.216,0,0,0,.028.289h0l6.257,6.039a.107.107,0,0,0,.177-.037h0l6.8-15.909a.258.258,0,0,0,0-.168h0L382,.027A.04.04,0,0,0,381.966,0h0a.046.046,0,0,0-.046.046Z"
+                transform="translate(-372.912 0)"
+                fillRule="evenodd"
+                fill="url(#linear-gradient-5)"
+              />
+              <Path
+                d="M720.375-.01h0a.046.046,0,0,0-.046.046l0,7.857a.221.221,0,0,1-.012.065l4.252,2.238L720.413.017a.04.04,0,0,0-.037-.027Zm-.055,7.968.812.466Z"
+                transform="translate(-711.323 0.01)"
+                fillRule="evenodd"
+                fill="url(#linear-gradient-6)"
+              />
+              <Path
+                d="M724.642,310.124h0a.257.257,0,0,0-.013-.082h0l-.054-.133-4.255-2.238.815.466,3.507,1.989Z"
+                transform="translate(-711.323 -299.702)"
+                fillRule="evenodd"
+                fill="url(#linear-gradient-7)"
+              />
+              <Path
+                d="M398,357.816l-.235.358,4.031-3.714Z"
+                transform="translate(-397.126 -345.28)"
+                fill="#be80f5"
+              />
+            </G>
           </G>
         </G>
       </G>
-    </G>
-  </Svg>
-)
+    </Svg>
+  )
+}
 
-export default AmbireDevice
+export default React.memo(AmbireDevice)
