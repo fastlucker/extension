8682f362babce5c2b0230053f7a3ed03ec2008fc
Yosif Hamed <josif.hameed@gmail.com>
2025-05-30 16:59:09 +0300

    fix(rewards): quests sorting

--- src/legends/modules/legends/utils/cards.ts
+++ src/legends/modules/legends/utils/cards.ts
@@ -22,11 +22,18 @@ export const isMatchingPredefinedId = (legendAction: CardAction, predefinedIdToM
 const sortCards = (cards: CardFromResponse[]) => {
   return cards.sort((a, b) => {
     // Display Wheel of Fortune first
-    if (isMatchingPredefinedId(a.action, CARD_PREDEFINED_ID.wheelOfFortune)) {
-      return -1
+    if (
+      isMatchingPredefinedId(a.action, CARD_PREDEFINED_ID.wheelOfFortune) ||
+      isMatchingPredefinedId(a.action, CARD_PREDEFINED_ID.chest)
+    ) {
+      return -Infinity
     }
-    if (isMatchingPredefinedId(b.action, CARD_PREDEFINED_ID.wheelOfFortune)) {
-      return 1
+
+    if (
+      isMatchingPredefinedId(b.action, CARD_PREDEFINED_ID.wheelOfFortune) ||
+      isMatchingPredefinedId(b.action, CARD_PREDEFINED_ID.chest)
+    ) {
+      return Infinity
     }
 
     const order = {
