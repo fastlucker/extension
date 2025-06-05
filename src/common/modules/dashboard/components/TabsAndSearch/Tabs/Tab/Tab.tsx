a9815c78e24e16410ac36de8fae9a82582b2bdb9
sonytooo <simeonpetkov11999977@gmail.com>
2025-06-05 14:39:20 +0300

    add: tabs hover effect

--- src/common/modules/dashboard/components/TabsAndSearch/Tabs/Tab/Tab.tsx
+++ src/common/modules/dashboard/components/TabsAndSearch/Tabs/Tab/Tab.tsx
@@ -50,47 +50,51 @@ const Tab = ({
         setOpenTab(tab)
       }}
     >
-      <LinearGradient
-        colors={
-          isActive
-            ? themeType === THEME_TYPES.DARK
-              ? [
-                  `${DASHBOARD_OVERVIEW_BACKGROUND}80`,
-                  mixHexColors(`${DASHBOARD_OVERVIEW_BACKGROUND}80`, avatarColors[1], 0.7)
-                ]
-              : [
-                  DASHBOARD_OVERVIEW_BACKGROUND,
-                  mixHexColors(DASHBOARD_OVERVIEW_BACKGROUND, avatarColors[1], 0.8)
-                ]
-            : ['transparent', 'transparent']
-        }
-        start={{ x: 0.0, y: 1 }}
-        end={{ x: 0.2, y: 0 }}
-        locations={[0.4, 1]}
-        style={[
-          styles.toggleItem,
-          spacings.phLg,
-          {
-            opacity: disabled ? 0.4 : 1,
-            // @ts-ignore cursor is web only
-            cursor: disabled ? 'not-allowed' : 'pointer'
-          }
-        ]}
-      >
-        <Text
-          weight={isActive ? 'medium' : 'regular'}
-          color={
+      {({ hovered }: any) => (
+        <LinearGradient
+          colors={
             isActive
               ? themeType === THEME_TYPES.DARK
-                ? theme.primary
-                : theme.primaryBackground
-              : theme.secondaryText
+                ? [
+                    `${DASHBOARD_OVERVIEW_BACKGROUND}80`,
+                    mixHexColors(`${DASHBOARD_OVERVIEW_BACKGROUND}80`, avatarColors[1], 0.7)
+                  ]
+                : [
+                    DASHBOARD_OVERVIEW_BACKGROUND,
+                    mixHexColors(DASHBOARD_OVERVIEW_BACKGROUND, avatarColors[1], 0.8)
+                  ]
+              : ['transparent', 'transparent']
           }
-          fontSize={16}
+          start={{ x: 0.0, y: 1 }}
+          end={{ x: 0.2, y: 0 }}
+          locations={[0.4, 1]}
+          style={[
+            styles.toggleItem,
+            spacings.phLg,
+            {
+              opacity: disabled ? 0.4 : 1,
+              // @ts-ignore cursor is web only
+              cursor: disabled ? 'not-allowed' : 'pointer'
+            }
+          ]}
         >
-          {t(tabLabel)}
-        </Text>
-      </LinearGradient>
+          <Text
+            weight={isActive ? 'medium' : 'regular'}
+            color={
+              isActive
+                ? themeType === THEME_TYPES.DARK
+                  ? theme.primary
+                  : theme.primaryBackground
+                : hovered
+                ? theme.primaryText
+                : theme.secondaryText
+            }
+            fontSize={16}
+          >
+            {t(tabLabel)}
+          </Text>
+        </LinearGradient>
+      )}
     </Pressable>
   )
 }
