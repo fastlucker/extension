0e2de2bec810ac3d25cb81de88068cd5b891cf8d
Jordan Enev <jordantoen@gmail.com>
2025-06-06 15:30:28 +0300

    Change: Make Playwright tests passing successfully with the new token test-id (option-{address}.{chain})

--- e2e-playwright-tests/tests/swapAndBridge/swapAndBridge.spec.ts
+++ e2e-playwright-tests/tests/swapAndBridge/swapAndBridge.spec.ts
@@ -1,4 +1,5 @@
 import { saParams } from '../../config/constants'
+import tokens from '../../constants/tokens'
 import { test } from '../../fixtures/pageObjects' // your extended test with authPage
 
 test.describe.parallel('swapAndBridgePage Smart Account', () => {
@@ -9,36 +10,51 @@ test.describe.parallel('swapAndBridgePage Smart Account', () => {
   test('should accept amount starting with zeros like "00.01" with during Swap & Bridge with a Smart Account', async ({
     swapAndBridgePage
   }) => {
-    await swapAndBridgePage.prepareSwapAndBridge(0.1, 'DAI', '10', 'USDC.E')
+    const fromToken = tokens.dai.optimism
+    const toToken = tokens.usdce.optimism
+
+    await swapAndBridgePage.prepareSwapAndBridge(0.1, fromToken, toToken)
     await swapAndBridgePage.enterNumber('00.01', true)
   })
 
   test('should accept amount starting with point like ".01" during Swap & Bridge with a Smart Account', async ({
     swapAndBridgePage
   }) => {
-    await swapAndBridgePage.prepareSwapAndBridge(0.1, 'DAI', '10', 'USDC.E')
+    const fromToken = tokens.dai.optimism
+    const toToken = tokens.usdce.optimism
+
+    await swapAndBridgePage.prepareSwapAndBridge(0.1, fromToken, toToken)
     await swapAndBridgePage.enterNumber('.01', true)
   })
 
   test('should not accept chars as amount during Swap & Bridge with a Smart Account', async ({
     swapAndBridgePage
   }) => {
-    await swapAndBridgePage.prepareSwapAndBridge(0.1, 'DAI', '10', 'USDC.E')
+    const fromToken = tokens.dai.optimism
+    const toToken = tokens.usdce.optimism
+
+    await swapAndBridgePage.prepareSwapAndBridge(0.1, fromToken, toToken)
     await swapAndBridgePage.enterNumber('abc', true)
   })
 
   test('should switch tokens during Swap & Bridge with a Smart Account', async ({
     swapAndBridgePage
   }) => {
+    const fromToken = tokens.usdc.base
+    const toToken = tokens.wallet.base
+
     await swapAndBridgePage.openSwapAndBridge()
-    await swapAndBridgePage.prepareSwapAndBridge(null, 'USDC', '8453', 'WALLET')
+    await swapAndBridgePage.prepareSwapAndBridge(null, fromToken, toToken)
     await swapAndBridgePage.switchTokensOnSwapAndBridge()
   })
 
   test('should do MAX token "From" amount during Swap & Bridge with a Smart Account', async ({
     swapAndBridgePage
   }) => {
-    await swapAndBridgePage.verifySendMaxTokenAmount('DAI', '10')
-    await swapAndBridgePage.verifySendMaxTokenAmount('USDC', '8453')
+    const dai = tokens.dai.optimism
+    const usdc = tokens.usdc.base
+
+    await swapAndBridgePage.verifySendMaxTokenAmount(dai)
+    await swapAndBridgePage.verifySendMaxTokenAmount(usdc)
   })
 })
