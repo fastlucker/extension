2fa38c1cb20d808dc3be16a813ca1667f83d72a2
Yosif Hamed <josif.hameed@gmail.com>
2025-06-10 20:33:13 +0300

    sentry: use web version for legends

---
+++ src/legends/instrument.ts
@@ -0,0 +1,13 @@
+import { SENTRY_DSN_LEGENDS } from '@env'
+import * as Sentry from '@sentry/react'
+
+if (SENTRY_DSN_LEGENDS)
+  Sentry.init({
+    dsn: SENTRY_DSN_LEGENDS,
+
+    // Adds request headers and IP for users, for more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#sendDefaultPii
+    sendDefaultPii: true,
+
+    integrations: []
+  })
