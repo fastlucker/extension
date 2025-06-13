558851ae3f3e80135e606f15e3581b2edddef413
Yosif Hamed <josif.hameed@gmail.com>
2025-06-13 22:05:08 +0300

    add(sentry): version of the build to the instrument

--- src/legends/instrument.ts
+++ src/legends/instrument.ts
@@ -1,6 +1,8 @@
 import { SENTRY_DSN_LEGENDS } from '@env'
 import * as Sentry from '@sentry/react'
 
+const { version } = require('../ambire-common/package.json')
+
 if (SENTRY_DSN_LEGENDS)
   Sentry.init({
     dsn: SENTRY_DSN_LEGENDS,
@@ -8,6 +10,6 @@ if (SENTRY_DSN_LEGENDS)
     // Adds request headers and IP for users, for more info visit:
     // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#sendDefaultPii
     sendDefaultPii: true,
-
+    release: `legends@${version}`,
     integrations: []
   })
