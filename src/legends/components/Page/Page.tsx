db9c44194e59018c5415065024e0f87669ff6521
Gergana Mihaylova <gergana@devlabs.bg>
2025-06-17 15:48:01 +0300

    Update: remove banner season 1

--- src/legends/components/Page/Page.tsx
+++ src/legends/components/Page/Page.tsx
@@ -4,7 +4,6 @@ import { useLocation } from 'react-router-dom'
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import AccountInfo from '@legends/components/AccountInfo'
-import Banner from '@legends/components/Banner'
 import Sidebar from '@legends/components/Sidebar'
 import useAccountContext from '@legends/hooks/useAccountContext'
 import { LEGENDS_ROUTES } from '@legends/modules/router/constants'
@@ -37,7 +36,7 @@ const Page = ({
         <Sidebar handleClose={closeSidebar} isOpen={isSidebarOpen} />
 
         <div ref={pageRef} className={`${styles.scroll} ${styles.containerfull}`} style={style}>
-          <Banner />
+          {/* <Banner /> */}
           <div className={`${styles.container} ${customContainerSizeClass}`}>
             <div className={styles.header}>
               <button className={styles.sidebarButton} type="button" onClick={openSidebar}>
