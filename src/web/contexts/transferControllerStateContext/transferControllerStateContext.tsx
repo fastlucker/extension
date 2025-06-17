36c2d81c987d1144c8f911f8870e762ae972abd5
Petromir Petrov <petromirdev@gmail.com>
2025-06-17 11:40:25 +0300

    fix: isTopUp flag is always false in TransferControllerStateProvider

--- src/web/contexts/transferControllerStateContext/transferControllerStateContext.tsx
+++ src/web/contexts/transferControllerStateContext/transferControllerStateContext.tsx
@@ -1,17 +1,18 @@
 import React, { createContext, useEffect, useMemo } from 'react'
 import { View } from 'react-native'
+
 import { TransferController } from '@ambire-common/controllers/transfer/transfer'
 import { TokenResult } from '@ambire-common/libs/portfolio'
 import { getTokenAmount } from '@ambire-common/libs/portfolio/helpers'
 import { sortPortfolioTokenList } from '@ambire-common/libs/swapAndBridge/swapAndBridge'
 import Spinner from '@common/components/Spinner'
+import useDeepMemo from '@common/hooks/useDeepMemo'
 import flexbox from '@common/styles/utils/flexbox'
+import useBackgroundService from '@web/hooks/useBackgroundService'
+import useControllerState from '@web/hooks/useControllerState'
+import useMainControllerState from '@web/hooks/useMainControllerState'
 import useNetworksControllerState from '@web/hooks/useNetworksControllerState'
 import useSelectedAccountControllerState from '@web/hooks/useSelectedAccountControllerState'
-import useControllerState from '@web/hooks/useControllerState'
-import useBackgroundService from '@web/hooks/useBackgroundService'
-import useMainControllerState from '@web/hooks/useMainControllerState'
-import useDeepMemo from '@common/hooks/useDeepMemo'
 
 type ContextReturn = {
   state: TransferController
@@ -31,17 +32,12 @@ export const getInfoFromSearch = (search: string | undefined) => {
   }
 }
 
-const TransferControllerStateProvider = ({
-  children,
-  isTopUp
-}: {
-  children: any
-  isTopUp?: boolean
-}) => {
+const TransferControllerStateProvider = ({ children }: { children: any }) => {
   const controller = 'transfer'
   const state = useControllerState(controller)
   const { dispatch } = useBackgroundService()
   const mainState = useMainControllerState()
+  const isTopUp = state.isTopUp
 
   useEffect(() => {
     if (!Object.keys(state).length) {
