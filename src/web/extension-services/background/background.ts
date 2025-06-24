b38739f1f14c28dc5dc1e1b6c6139f6eb682990f
sonytooo <simeonpetkov11999977@gmail.com>
2025-06-24 12:31:13 +0300

    add more advanced debouncing for the defi positions updates

--- src/web/extension-services/background/background.ts
+++ src/web/extension-services/background/background.ts
@@ -15,7 +15,6 @@ import {
   ACTIVE_EXTENSION_DEFI_POSITIONS_UPDATE_INTERVAL,
   ACTIVE_EXTENSION_PORTFOLIO_UPDATE_INTERVAL,
   ACTIVITY_REFRESH_INTERVAL,
-  INACTIVE_EXTENSION_DEFI_POSITION_UPDATE_INTERVAL,
   INACTIVE_EXTENSION_PORTFOLIO_UPDATE_INTERVAL,
   UPDATE_SWAP_AND_BRIDGE_QUOTE_INTERVAL
 } from '@ambire-common/consts/intervals'
@@ -410,18 +409,21 @@ function getIntervalRefreshTime(constUpdateInterval: number, newestOpTimestamp: 
     if (backgroundState.updateDefiPositionsInterval)
       clearTimeout(backgroundState.updateDefiPositionsInterval)
 
-    const isExtensionActive = pm.ports.length > 0 // (opened tab, popup, action-window)
-    const updateInterval = isExtensionActive
-      ? ACTIVE_EXTENSION_DEFI_POSITIONS_UPDATE_INTERVAL
-      : INACTIVE_EXTENSION_DEFI_POSITION_UPDATE_INTERVAL
-
     async function updateDefiPositions() {
-      await mainCtrl.defiPositions.updatePositions()
+      const isExtensionActive = pm.ports.length > 0 // (opened tab, popup, action-window)
+
+      if (!isExtensionActive) return
+
+      await mainCtrl.defiPositions.updatePositions({ maxDataAgeMs: 300000 }) // 5 min in ms
+
       // Schedule the next update only when the previous one completes
-      backgroundState.updateDefiPositionsInterval = setTimeout(updateDefiPositions, updateInterval)
+      backgroundState.updateDefiPositionsInterval = setTimeout(
+        updateDefiPositions,
+        ACTIVE_EXTENSION_DEFI_POSITIONS_UPDATE_INTERVAL
+      )
     }
 
-    backgroundState.updateDefiPositionsInterval = setTimeout(updateDefiPositions, updateInterval)
+    await updateDefiPositions()
   }
 
   function initAccountsOpsStatusesContinuousUpdate(updateInterval: number) {
@@ -909,6 +911,7 @@ function getIntervalRefreshTime(constUpdateInterval: number, newestOpTimestamp: 
 
       initPortfolioContinuousUpdate()
       initDefiPositionsContinuousUpdate()
+
       mainCtrl.phishing.updateIfNeeded()
 
       // @ts-ignore
@@ -978,7 +981,6 @@ function getIntervalRefreshTime(constUpdateInterval: number, newestOpTimestamp: 
   })
 
   initPortfolioContinuousUpdate()
-  initDefiPositionsContinuousUpdate()
   await initLatestAccountStateContinuousUpdate(backgroundState.accountStateIntervals.standBy)
 })()
 
