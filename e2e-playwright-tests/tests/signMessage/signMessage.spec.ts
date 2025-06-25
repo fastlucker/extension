b724995a0bf77f4abeefca4b9f2b71662c6c74ad
Jordan Enev <jordantoen@gmail.com>
2025-06-25 16:55:09 +0300

    Change: Linting.

--- e2e-playwright-tests/tests/signMessage/signMessage.spec.ts
+++ e2e-playwright-tests/tests/signMessage/signMessage.spec.ts
@@ -7,48 +7,49 @@ test.describe.parallel('signMessage', () => {
   })
 
   test('should sign plain message', async ({ signMessagePage }) => {
-    const message = "Hello, Ambire!"
+    const message = 'Hello, Ambire!'
     await signMessagePage.signMessage(message, 'plain')
   })
 
   test('should sign hex message', async ({ signMessagePage }) => {
     // "Hello, Ambire!" as hex
-    const message = "0x48656c6c6f2c20416d6269726521"
+    const message = '0x48656c6c6f2c20416d6269726521'
     await signMessagePage.signMessage(message, 'hex')
   })
 
   test('should sign typed message', async ({ signMessagePage }) => {
     const message = {
-      "domain": {
-        "name": "Ambire Wallet News",
-        "chainId": 1
+      domain: {
+        name: 'Ambire Wallet News',
+        chainId: 1
       },
-      "types": {
-        "article": [
+      types: {
+        article: [
           {
-            "name": "title",
-            "type": "string"
+            name: 'title',
+            type: 'string'
           },
           {
-            "name": "description",
-            "type": "string"
+            name: 'description',
+            type: 'string'
           },
           {
-            "name": "publisher",
-            "type": "address"
+            name: 'publisher',
+            type: 'address'
           },
           {
-            "name": "rating",
-            "type": "uint256"
+            name: 'rating',
+            type: 'uint256'
           }
         ]
       },
-      "primaryType": "article",
-      "message": {
-        "title": "Example of eip-712 typed data message",
-        "description": "Tip: When chainId is specified in the domain, you should select the appropriate network in your wallet",
-        "publisher": "0x942f9CE5D9a33a82F88D233AEb3292E680230348",
-        "rating": "5"
+      primaryType: 'article',
+      message: {
+        title: 'Example of eip-712 typed data message',
+        description:
+          'Tip: When chainId is specified in the domain, you should select the appropriate network in your wallet',
+        publisher: '0x942f9CE5D9a33a82F88D233AEb3292E680230348',
+        rating: '5'
       }
     }
 
