d9cf66e32b5659e7d9ecaf9a8d419b1206f10be7
Petromir Petrov <petromirdev@gmail.com>
2025-07-10 16:27:29 +0300

    fix: sentry debug ids not injected properly

--- scripts/build-extensions.sh
+++ scripts/build-extensions.sh
@@ -16,34 +16,35 @@ fi
 # Read the build target
 TARGET="$1"
 
-# Define filenames
-WEBKIT_FILENAME="ambire-extension-v${VERSION}-webkit.zip"
-GECKO_FILENAME="ambire-extension-v${VERSION}-gecko.zip"
-WEBKIT_SOURCEMAPS_FILENAME="ambire-extension-v${VERSION}-webkit-sourcemaps.zip"
-GECKO_SOURCEMAPS_FILENAME="ambire-extension-v${VERSION}-gecko-sourcemaps.zip"
-
-# Define build directories
-WEBKIT_BUILD_DIR="./build/webkit-prod"
-GECKO_BUILD_DIR="./build/gecko-prod"
-WEBKIT_SOURCEMAPS_BUILD_DIR="./build/webkit-prod-source-maps"
-GECKO_SOURCEMAPS_BUILD_DIR="./build/gecko-prod-source-maps"
+# Injects debug ids and uploads source maps to Sentry
+# Must be done before separating the source maps from the build directories
+inject_debug_ids() {
+  echo "Step 2: Injecting debug ids and uploading source maps to Sentry"
+  # Check if sentry-cli is installed, if not install it
+  if ! command -v sentry-cli &> /dev/null; then
+    echo "sentry-cli not found, installing..."
+    curl -sL https://sentry.io/get-cli/ | bash
+  fi
+  
+  SENTRY_PROJECT=extension
+  sentry-cli --version
+  # sentry-cli releases new extension-$ENGINE@$VERSION --project=$SENTRY_PROJECT
+  sentry-cli sourcemaps inject build/
+  # sentry-cli sourcemaps upload --release=extension-$ENGINE@$VERSION --project=$SENTRY_PROJECT build/$ENGINE-prod-source-maps/
+  # sentry-cli releases finalize extension-$ENGINE@$VERSION --project=$SENTRY_PROJECT
+}
 
 # Function to build and zip Webkit
 build_webkit() {
   echo "Step 1: Building the webkit extension"
   yarn build:web:webkit
-
-  echo "Step 2: Exporting webkit extension sourcemaps"
-  yarn export:web:webkit:sourcemaps
+  
 }
 
 # Function to build and zip Gecko
 build_gecko() {
   echo "Step 1: Building the gecko extension"
-  yarn build:web:gecko
-
-  echo "Step 2: Exporting gecko extension sourcemaps"
-  yarn export:web:gecko:sourcemaps
+  yarn build:web:gecko  
 }
 
 # Decide what to build
@@ -59,5 +60,35 @@ case "$TARGET" in
     build_gecko
     ;;
 esac
+
+## Step 2: Inject debug ids and upload source maps to Sentry
+inject_debug_ids
+
+## Step 3: Separate source maps from the build directories
+echo "Step 3: Separating source maps from the build directories"
+case "$TARGET" in
+  --webkit)
+    yarn export:web:webkit:sourcemaps
+    ;;
+  --gecko)
+    yarn export:web:gecko:sourcemaps
+    ;;
+  *)
+    yarn export:web:webkit:sourcemaps
+    yarn export:web:gecko:sourcemaps
+    ;;
+esac
+
+
+# Create .zip files
+cd build
+for dir in */; do
+  if [ -d "$dir" ]; then
+    # Remove -prod suffix and add prefix with version
+    clean_name=$(echo "${dir%/}" | sed 's/-prod//g')
+    # Create zip with contents of directory, not the directory itself
+    (cd "$dir" && zip -r "../ambire-extension-v${VERSION}-${clean_name}.zip" .)
+  fi
+done
 
 echo -e "\nAll ready! Good luck with the build reviews 🤞"
