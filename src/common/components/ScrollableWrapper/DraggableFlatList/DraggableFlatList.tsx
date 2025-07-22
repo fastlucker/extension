67695e310c4570298d770231daad9d591e78f815
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2025-07-22 13:58:32 +0300

    Fix: slow dragging

--- src/common/components/ScrollableWrapper/DraggableFlatList/DraggableFlatList.tsx
+++ src/common/components/ScrollableWrapper/DraggableFlatList/DraggableFlatList.tsx
@@ -1,7 +1,6 @@
-import React, { forwardRef } from 'react'
-import { ScrollView, ScrollViewProps, StyleProp, ViewStyle } from 'react-native'
+import React, { forwardRef, useCallback } from 'react'
+import { FlatList, FlatListProps, ScrollViewProps, StyleProp, ViewStyle } from 'react-native'
 
-import flexbox from '@common/styles/utils/flexbox'
 import { closestCenter, DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
 import { restrictToVerticalAxis } from '@dnd-kit/modifiers'
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
@@ -19,40 +18,43 @@ type DraggableFlatListProps<T> = {
     attributes: any
   ) => React.ReactNode
   onDragEnd: (fromIndex: number, toIndex: number) => void
-
+  getItemLayout?: FlatListProps<T>['getItemLayout']
   scrollableWrapperStyles?: ViewStyle
   contentContainerStyle?: StyleProp<ViewStyle>
 
   keyboardShouldPersistTaps?: ScrollViewProps['keyboardShouldPersistTaps']
   keyboardDismissMode?: ScrollViewProps['keyboardDismissMode']
-} & Omit<ScrollViewProps, 'children'>
+} & Omit<FlatListProps<T>, 'renderItem' | 'data'>
 
-const DraggableFlatList = forwardRef<ScrollView, DraggableFlatListProps<any>>(
-  (
+const DraggableFlatList = forwardRef(
+  <T,>(
     {
       data,
       keyExtractor,
       renderItem,
       onDragEnd,
+      getItemLayout,
       scrollableWrapperStyles,
-      contentContainerStyle,
-      keyboardShouldPersistTaps = 'handled',
-      keyboardDismissMode = 'none',
+      keyboardShouldPersistTaps,
+      keyboardDismissMode,
       ...rest
-    },
-    ref
+    }: DraggableFlatListProps<T>,
+    ref: any
   ) => {
     const sensors = useSensors(useSensor(PointerSensor))
 
-    const handleDragEnd = (event: any) => {
-      const { active, over } = event
-      if (!over || active.id === over.id) return
+    const handleDragEnd = useCallback(
+      (event: any) => {
+        const { active, over } = event
+        if (!over || active.id === over.id) return
 
-      const oldIndex = data.findIndex((item) => keyExtractor(item) === active.id)
-      const newIndex = data.findIndex((item) => keyExtractor(item) === over.id)
+        const oldIndex = data.findIndex((item) => keyExtractor(item) === active.id)
+        const newIndex = data.findIndex((item) => keyExtractor(item) === over.id)
 
-      onDragEnd(oldIndex, newIndex)
-    }
+        onDragEnd(oldIndex, newIndex)
+      },
+      [data, keyExtractor, onDragEnd]
+    )
 
     return (
       <DndContext
@@ -62,23 +64,24 @@ const DraggableFlatList = forwardRef<ScrollView, DraggableFlatListProps<any>>(
         modifiers={[restrictToVerticalAxis]}
       >
         <SortableContext items={data.map(keyExtractor)} strategy={verticalListSortingStrategy}>
-          <ScrollView
+          <FlatList
             ref={ref}
-            style={[{ flex: 1 }, scrollableWrapperStyles]}
-            contentContainerStyle={[flexbox.flex1, contentContainerStyle]}
-            keyboardShouldPersistTaps={keyboardShouldPersistTaps}
-            keyboardDismissMode={keyboardDismissMode}
-            scrollEnabled
-            {...rest}
-          >
-            {data.map((item, index) => (
+            data={data}
+            keyExtractor={keyExtractor}
+            getItemLayout={getItemLayout}
+            style={scrollableWrapperStyles}
+            keyboardShouldPersistTaps={keyboardShouldPersistTaps || 'handled'}
+            keyboardDismissMode={keyboardDismissMode || 'none'}
+            alwaysBounceVertical={false}
+            renderItem={({ item, index }) => (
               <DraggableItem key={keyExtractor(item)} id={keyExtractor(item)}>
                 {(isDragging, listeners, attributes) =>
                   renderItem(item, index, isDragging, listeners, attributes)
                 }
               </DraggableItem>
-            ))}
-          </ScrollView>
+            )}
+            {...rest}
+          />
         </SortableContext>
       </DndContext>
     )
