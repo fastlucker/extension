a2bf203da1f01a377eeffa7574939fda3e98e0b7
Kaloyan Kosev <superkalo@devlabs.bg>
2025-07-29 18:51:13 +0300

    Change: avoid respecting outdated user preferences from v5.11.x–v5.14.x (where users may have manually disabled it)

--- src/web/utils/instrument.ts
+++ src/web/utils/instrument.ts
@@ -38,7 +38,13 @@ const initializeSentry = async () => {
     return
   }
 
-  const isEnabled = await storage.get('crashAnalyticsEnabled', CRASH_ANALYTICS_ENABLED_DEFAULT)
+  /**
+   * Since v5.15.0, we enable anonymous crash reporting by default. To avoid respecting
+   * outdated user preferences from v5.11.x–v5.14.x (where users may have manually disabled it),
+   * we ignore stored values and always use the new default.
+   */
+  // const isEnabled = await storage.get('crashAnalyticsEnabled', CRASH_ANALYTICS_ENABLED_DEFAULT)
+  const isEnabled = CRASH_ANALYTICS_ENABLED_DEFAULT
 
   if (!isEnabled) return
 
