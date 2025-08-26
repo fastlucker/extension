fe08fb9afee603f5929d51e5152817401ec52566
Jordan Enev <jordantoen@gmail.com>
2025-08-26 17:06:28 +0300

    Fix: Linting.

--- e2e-playwright-tests/tests/stability/stability.spec.ts
+++ e2e-playwright-tests/tests/stability/stability.spec.ts
@@ -20,15 +20,18 @@ test.describe('stability', () => {
       await pages.stability.blockRouteAndUnlock('**/invictus.ambire.com/polygon')
     })
 
-    await test.step('click on the error indicator and appropriate message is expected to be shown', async () => {
-      const page = pages.stability.page
-      await page.getByTestId(selectors.dashboard.balanceErrorIcon).click()
+    await test.step(
+      'click on the error indicator and appropriate message is expected to be shown',
+      async () => {
+        const page = pages.stability.page
+        await page.getByTestId(selectors.dashboard.balanceErrorIcon).click()
 
-      const rpcErrorBanner = page.getByTestId(selectors.dashboard.portfolioErrorAlert).first()
+        const rpcErrorBanner = page.getByTestId(selectors.dashboard.portfolioErrorAlert).first()
 
-      await expect(rpcErrorBanner).toBeVisible()
-      await expect(rpcErrorBanner).toContainText('Failed to retrieve network data for Polygon')
-    })
+        await expect(rpcErrorBanner).toBeVisible()
+        await expect(rpcErrorBanner).toContainText('Failed to retrieve network data for Polygon')
+      }
+    )
   })
 
   test('Velcro fail: Should find tokens using previous hints; Should not display an error banner as there are cached hints', async ({
@@ -51,15 +54,18 @@ test.describe('stability', () => {
       await expect(walletToken).toBeVisible()
     })
 
-    await test.step('click on the error indicator and appropriate message is expected to be shown', async () => {
-      await page.getByTestId(selectors.dashboard.balanceErrorIcon).click()
-      const velcroErrorBanner = page.getByTestId(selectors.dashboard.portfolioErrorAlert)
+    await test.step(
+      'click on the error indicator and appropriate message is expected to be shown',
+      async () => {
+        await page.getByTestId(selectors.dashboard.balanceErrorIcon).click()
+        const velcroErrorBanner = page.getByTestId(selectors.dashboard.portfolioErrorAlert)
 
-      await expect(velcroErrorBanner).toBeVisible()
-      await expect(velcroErrorBanner).toContainText(
-        'Automatic asset discovery is temporarily unavailable'
-      )
-    })
+        await expect(velcroErrorBanner).toBeVisible()
+        await expect(velcroErrorBanner).toContainText(
+          'Automatic asset discovery is temporarily unavailable'
+        )
+      }
+    )
   })
 
   test('Monitor fetch requests on Dashboard', async ({ pages }) => {
@@ -70,30 +76,33 @@ test.describe('stability', () => {
       await pages.stability.unlock()
     })
 
-    await test.step('wait for the dashboard to fully load and validate the requests being made', async () => {
-      // Wait for all requests to be triggered
-      await page.waitForTimeout(10000)
+    await test.step(
+      'wait for the dashboard to fully load and validate the requests being made',
+      async () => {
+        // Wait for all requests to be triggered
+        await page.waitForTimeout(10000)
 
-      const categorized = pages.stability.getCategorizedRequests()
+        const categorized = pages.stability.getCategorizedRequests()
 
-      // ⚠️ Note: It's difficult to accurately track the exact number of requests being made,
-      // as even minor changes (e.g. adding a new default network, cache misses, retries) can shift totals.
-      // The expectations below are based on rough estimates and include some tolerance.
-      // If any of them are exceeded significantly, it may signal a regression.
-      // ---
-      // Note on RPC requests:
-      // Each network typically triggers 2 RPC calls.
-      // So the total number of RPC requests scales linearly with the number of networks configured.
-      // This is why the threshold here is intentionally higher to account for all supported networks.
-      expect(categorized.rpc.length).toBeLessThanOrEqual(30)
-      expect(categorized.hints.length).toBeLessThanOrEqual(1)
-      expect(categorized.nativePrices.length).toBeLessThanOrEqual(10)
-      expect(categorized.thirdParty.length).toBeLessThanOrEqual(10)
-      expect(categorized.allowedUncategorized.length).toBeLessThanOrEqual(10)
+        // ⚠️ Note: It's difficult to accurately track the exact number of requests being made,
+        // as even minor changes (e.g. adding a new default network, cache misses, retries) can shift totals.
+        // The expectations below are based on rough estimates and include some tolerance.
+        // If any of them are exceeded significantly, it may signal a regression.
+        // ---
+        // Note on RPC requests:
+        // Each network typically triggers 2 RPC calls.
+        // So the total number of RPC requests scales linearly with the number of networks configured.
+        // This is why the threshold here is intentionally higher to account for all supported networks.
+        expect(categorized.rpc.length).toBeLessThanOrEqual(30)
+        expect(categorized.hints.length).toBeLessThanOrEqual(1)
+        expect(categorized.nativePrices.length).toBeLessThanOrEqual(10)
+        expect(categorized.thirdParty.length).toBeLessThanOrEqual(10)
+        expect(categorized.allowedUncategorized.length).toBeLessThanOrEqual(10)
 
-      // ☢️ Critical: there should be no truly uncategorized requests.
-      // Anything uncategorized is likely unexpected or suspicious.
-      expect(categorized.uncategorized.length).toBeLessThanOrEqual(0)
-    })
+        // ☢️ Critical: there should be no truly uncategorized requests.
+        // Anything uncategorized is likely unexpected or suspicious.
+        expect(categorized.uncategorized.length).toBeLessThanOrEqual(0)
+      }
+    )
   })
 })
