72154fc996ac4880f2b36b1a53ff885cd757a72d
sonytooo <simeonpetkov11999977@gmail.com>
2025-10-09 17:40:27 +0300

    add comments

--- src/web/extension-services/utils/frames.ts
+++ src/web/extension-services/utils/frames.ts
@@ -1,3 +1,5 @@
+// Used to skip running scripts inside cross-origin iframes like ads, trackers,
+// social media embeds, and  third-party widgets
 export function isCrossOriginFrame(): boolean {
   try {
     const topOrigin = window?.top?.location?.origin
@@ -7,7 +9,8 @@ export function isCrossOriginFrame(): boolean {
     return true
   }
 }
-
+// Used to avoid running scripts in deeply nested iframes, since these frames are very unlikely
+// to be used for wallet communication and would only add unnecessary load to already heavy pages.
 export function isTooDeepFrameInTheFrameHierarchy(maxDepth = 10): boolean {
   let depth = 0
   let win: Window | null = window
