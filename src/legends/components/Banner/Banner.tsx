02eb4cd307c033f01710a0adbf91c0bcf10af664
Gergana Mihaylova <gergana@devlabs.bg>
2025-10-13 17:03:56 +0300

    Update: rewards current banner

--- src/legends/components/Banner/Banner.tsx
+++ src/legends/components/Banner/Banner.tsx
@@ -8,23 +8,23 @@ const Banner: React.FC = () => {
     <div className={styles.container}>
       <img className={styles.iconPlaceholder} src={governance} alt="Governance banner icon" />
       <div className={styles.textContent}>
-        <div className={styles.title}>🗳️ Cast your vote for the proposals until October 12!</div>
+        <div className={styles.title}>🗳️ Cast your vote for the proposals until October 18!</div>
         <a
-          href="https://snapshot.box/#/s:ambire.eth/proposal/0x4063d925ac63f29a35cdd9cc24c098946c0cbcd65ae11f538c76634ca110de65"
+          href="https://snapshot.box/#/s:ambire.eth/proposal/0x2f6992e51015e6781b529ddc6cff1a8e752ad4b16b750c1b27e6a9b3168c87e0"
           className={styles.readMoreLink}
           target="_blank"
           rel="noreferrer"
         >
-          🚀 Update XP for some quests
+          🚀 Double XP for activity on Ethereum
         </a>
         <br />
         <a
-          href="https://snapshot.box/#/s:ambire.eth/proposal/0x80e1135e5ea98924e2200ee2ce6690cb1716d2d3f4a64ceff4f6cda9d1379853"
+          href="https://snapshot.box/#/s:ambire.eth/proposal/0x006a5dd1db40fc48a5f78cfe9f9aa2920df17d83a141d2d32dc65828f9358dd9"
           className={styles.readMoreLink}
           target="_blank"
           rel="noreferrer"
         >
-          🔥 Remove NFT in Rewards
+          🔥 Increase XP for governance participation
         </a>
       </div>
     </div>
