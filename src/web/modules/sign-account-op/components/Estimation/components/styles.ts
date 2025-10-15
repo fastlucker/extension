4e03007cf212da9eb036c2e6153bc6e0de3fa5af
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2025-10-15 11:07:59 +0300

    Applied the new design for the Gas Tank fee token

---
+++ src/web/modules/sign-account-op/components/Estimation/components/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet, ViewStyle } from 'react-native'
+
+import spacings from '@common/styles/spacings'
+import { THEME_TYPES, ThemeProps, ThemeType } from '@common/styles/themeConfig'
+import flexbox from '@common/styles/utils/flexbox'
+
+interface Style {
+  gasTankIconWrapper: ViewStyle
+  gasTankBadge: ViewStyle
+}
+
+const getStyles = (theme: ThemeProps, themeType: ThemeType) =>
+  StyleSheet.create<Style>({
+    gasTankIconWrapper: {
+      backgroundColor: theme.primaryBackground,
+      borderRadius: 50,
+      ...flexbox.center,
+      width: 40,
+      height: 40
+    },
+    gasTankBadge: {
+      borderRadius: 50,
+      backgroundColor: themeType === THEME_TYPES.DARK ? '#8B3DFF' : theme.primaryLight,
+      borderColor: themeType === THEME_TYPES.DARK ? '#8B3DFF' : theme.primaryLight,
+      borderWidth: 1,
+      ...spacings.phMi,
+      ...spacings.mlMi
+    }
+  })
+
+export default getStyles
