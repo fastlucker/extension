2ec9638d92810e49d4a3b221725da3adc81e4259
Filipv95 <filipv65@gmail.com>
2025-10-15 11:47:19 +0200

    additional assertions for transaction

--- e2e-playwright-tests/pages/swapAndBridgePage.ts
+++ e2e-playwright-tests/pages/swapAndBridgePage.ts
@@ -410,28 +410,52 @@ export class SwapAndBridgePage extends BasePage {
   }
 
   async verifyBatchTransactionDetails(page): Promise<void> {
-    const entireRow = await page.getByTestId('recipient-address-1').innerText() // grab entire row on transaction page
-    const routeSelector = entireRow.trim().split(/\s+/).pop() || '' // grab last item from row e.g. LI.FI
+    await this.pause()
 
-    // possible routes: socket (Socket gateway), LIFI, BungeeInbox (rare case)
-    switch (routeSelector) {
-      case 'WALLET':
-        await expect(page.getByTestId('recipient-address-1')).toHaveText(/WALLET/)
-        await expect(page.getByTestId('recipient-address-3')).toHaveText(/WALLET/)
-        break
-      case 'LI.FI':
-        await expect(page.getByTestId('recipient-address-1')).toHaveText(/LI.FI/)
-        await expect(page.getByTestId('recipient-address-3')).toHaveText(/LI.FI/)
-        break
-      case 'BungeeInbox':
-        // TODO: define assertion; atm could not rep on FE
-        break
-      default:
-        throw new Error(`Unexpected route: ${routeSelector}`)
-    }
+    // check first row
+    const firstRow = await page.getByTestId('recipient-address-0').innerText() // grab entire row on transaction page
+    const firstRouteSelector = firstRow.trim().split(/\s+/).pop() || '' // grab last item from row e.g. LI.FI
 
+    await expect(page.getByTestId('recipient-address-0')).toHaveText(/Grant approval/) // for either LI.FI or Socket transaction name is GrantApproval with amount and token name
     await expect(page.getByTestId('recipient-address-0')).toHaveText(/0\.01/)
+    await expect(page.getByTestId('recipient-address-0')).toHaveText(/USDC/)
+    expect(['LI.FI', 'SocketGateway']).toContain(firstRouteSelector)
+
+    // check second row
+    const secondRow = await page.getByTestId('recipient-address-1').innerText()
+    const secondRouteSelector = secondRow.trim().split(/\s+/).pop() || ''
+
+    if (secondRouteSelector === 'WALLET') {
+      await expect(page.getByTestId('recipient-address-1')).toHaveText(/Swap/) // in case its socket route transaction name is Swap with amount
+      await expect(page.getByTestId('recipient-address-1')).toHaveText(/0\.01/)
+    } else if (secondRouteSelector === 'LI.FI') {
+      await expect(page.getByTestId('recipient-address-1')).toHaveText(/Swap\/Bridge/) // in case its LIFI route transaction name is Swap/Bridge
+    }
+    expect(['LI.FI', 'SocketGateway']).toContain(secondRouteSelector)
+
+    // check third row
+    const thirdRow = await page.getByTestId('recipient-address-2').innerText()
+    const thirdRouteSelector = thirdRow.trim().split(/\s+/).pop() || ''
+
+    await expect(page.getByTestId('recipient-address-2')).toHaveText(/Grant approval/) // for either LI.FI or Socket transaction name is GrantApproval with amount and token name
     await expect(page.getByTestId('recipient-address-2')).toHaveText(/0\.01/)
+    await expect(page.getByTestId('recipient-address-2')).toHaveText(/USDC/)
+    expect(['LI.FI', 'SocketGateway']).toContain(thirdRouteSelector)
+
+    // check fourth row
+    const fourthRow = await page.getByTestId('recipient-address-3').innerText()
+    const fourthRouteSelector = fourthRow.trim().split(/\s+/).pop() || ''
+
+    if (secondRouteSelector === 'WALLET') {
+      await expect(page.getByTestId('recipient-address-3')).toHaveText(/Swap/) // in case of Socket route transaction name is Swap with amount and token name
+      await expect(page.getByTestId('recipient-address-3')).toHaveText(/0\.01/)
+      await expect(page.getByTestId('recipient-address-3')).toHaveText(/USDC/)
+    } else if (secondRouteSelector === 'LI.FI') {
+      await expect(page.getByTestId('recipient-address-3')).toHaveText(/Swap\/Bridge/) // in case of LIFI route transaction name is Swap/Bridge
+    }
+    expect(['LI.FI', 'SocketGateway']).toContain(fourthRouteSelector)
+
+    // sign transaction
     await page.getByTestId(selectors.signTransactionButton).click()
   }
 
